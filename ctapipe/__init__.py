073a26aa427338beae5bdcf18ef0e7a0bbbf9098
Christoph Deil <Deil.Christoph@gmail.com>
2015-06-24 12:01:40 +0200

    Only test Python 3.4 on travis-ci

--- ctapipe/__init__.py
+++ ctapipe/__init__.py
@@ -1,7 +1,6 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-
 """
-This is an Astropy affiliated package.
+ctapipe - CTA Python pipeline experimental version
 """
 
 # Affiliated packages may add whatever they like to this file, but
@@ -12,4 +11,4 @@ from ._astropy_init import *
 
 # For egg_info test builds to pass, put package imports here.
 if not _ASTROPY_SETUP_:
-    from example_mod import *
+    pass
