4588397b6247964436df7d714e786d862d02bebd
Christoph Deil <Deil.Christoph@gmail.com>
2015-06-24 11:07:49 +0200

    Add Hillas reco function (copy from Gammapy)

---
+++ ctapipe/reco/tests/test_hillas.py
@@ -0,0 +1,7 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+from __future__ import print_function, division
+from ..hillas import hillas_parameters
+
+
+def test_hillas_parameters():
+    pass
