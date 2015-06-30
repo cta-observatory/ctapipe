6bf6ca453b67734862646fd04d615430c1fad3f9
Karl Kosack <karl.kosack@cea.fr>
2015-06-30 14:48:23 +0200

    added tests

---
+++ ctapipe/io/tests/test_camera.py
@@ -0,0 +1,12 @@
+from ..camera import *
+
+
+def test_make_rectangular_camera_geometry():
+    geom = make_rectangular_camera_geometry()
+    assert(geom.pix_x.shape == geom.pix_y.shape)
+
+
+def test_load_hess_camera():
+    geom = get_camera_geometry("hess", 1)
+
+
