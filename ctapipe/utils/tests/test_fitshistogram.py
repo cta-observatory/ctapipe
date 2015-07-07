b5ec56e3f1b5a507bfe080d219eab1b588445855
Karl Kosack <karl.kosack@cea.fr>
2015-07-07 14:42:32 +0200

    added test for outliers

--- ctapipe/utils/tests/test_fitshistogram.py
+++ ctapipe/utils/tests/test_fitshistogram.py
@@ -4,8 +4,10 @@ from ..fitshistogram import Histogram
 
 
 def test_histogram_str():
-    hist = Histogram(nbins=[5, 10], ranges=[[-2.5, 2.5], [-1, 1]], name="testhisto")
-    expected = "Histogram(name='testhisto', axes=['axis0', 'axis1'], nbins=[ 5 10], ranges=[[-2.5  2.5]\n [-1.   1. ]])"
+    hist = Histogram(nbins=[5, 10],
+                     ranges=[[-2.5, 2.5], [-1, 1]], name="testhisto")
+    expected = ("Histogram(name='testhisto', axes=['axis0', 'axis1'], "
+                "nbins=[ 5 10], ranges=[[-2.5  2.5]\n [-1.   1. ]])")
     assert str(hist) == expected
 
 
@@ -48,16 +50,18 @@ def test_histogram_range_fill_and_read():
             assert val == N
             del hist
 
-# def testOutliers(self):
-#     """
-#     Check that out-of-range values work as expected
-#     """
-#     H = fitshistogram.Histogram( nbins=[5,10], range=[[-2.5,2.5],[-1,1]] )
-#     H.fill( np.array( [[1,1],]) )
-#     val1= H.getValue( (100,100), outlierValue = -10000)[0]
-#     val2= H.getValue( (-100,0), outlierValue = None)[0]
-#     self.assertEqual(val1,-10000)
-#     self.assertEqual(val2,0)
+
+def test_outliers():
+    """
+    Check that out-of-range values work as expected
+    """
+    H = Histogram(nbins=[5, 10], ranges=[[-2.5, 2.5], [-1, 1]])
+    H.fill(np.array([[1, 1], ]))
+    val1 = H.getValue((100, 100), outlierValue=-10000)
+    val2 = H.getValue((-100, 0), outlierValue=None)
+    assert val1 == -10000
+    assert val2 == 0
+
 
 def test_histogram_write_fits():
     """
