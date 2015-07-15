07437a1454b1254d30e18d5e67f0c848b3393593
Karl Kosack <karl.kosack@cea.fr>
2015-07-15 17:53:01 +0200

    updated example

--- examples/camera_animation.py
+++ examples/camera_animation.py
@@ -14,11 +14,13 @@ from matplotlib import pylab as plt
 
 if __name__ == '__main__':
 
+    plt.style.use("ggplot")
     fig, ax = plt.subplots()
 
     # load the camera
     geom = io.get_camera_geometry("hess", 1)
     disp = visualization.CameraDisplay(geom, axes=ax)
+    disp.set_cmap(plt.cm.terrain)
 
     def update(frame):
         centroid = np.random.uniform(-0.5, 0.5, size=2)
@@ -26,21 +28,15 @@ if __name__ == '__main__':
         length = np.random.uniform(0, 0.03) + width
         angle = np.random.uniform(0, 360)
         intens = np.random.uniform(20, 100)
-
-        # create a fake camera image to display:
         model = mock.generate_2d_shower_model(centroid=centroid,
                                               width=width,
                                               length=length,
                                               psi=np.radians(angle))
-
         image, sig, bg = mock.make_mock_shower_image(geom, model.pdf,
                                                      intensity=intens,
                                                      nsb_level_pe=1000)
-
-        #image -= image.mean()
         image /= image.max()
-        
         disp.set_image(image)
 
-    anim = FuncAnimation(fig, update, range(1, 200), interval=10)
+    anim = FuncAnimation(fig, update, interval=50)
     plt.show()
