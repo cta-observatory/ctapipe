5d2af7e156a9f4d4d000b6c7ef34518bf0499edb
Karl Kosack <karl.kosack@cea.fr>
2015-07-24 14:53:32 +0200

    cleaned up and fixed small bug

--- examples/read_hessio.py
+++ examples/read_hessio.py
@@ -1,6 +1,9 @@
 # run this example with:
 #
-# ipython --pylab read_hessio.py <filename>
+# python read_hessio.py <filename>
+#
+# if no filename is given, a default example file will be used
+# containing ~10 events
 #
 import sys
 import logging
@@ -21,24 +24,24 @@ cmaps = [plt.cm.jet, plt.cm.winter,
          plt.cm.cool, plt.cm.coolwarm]
 
 
-def display_event(data):
+def display_event(event):
     """an extremely inefficient display. It creates new instances of
     CameraDisplay for every event and every camera, and also new axes
     for each event. It's hacked, but it works
     """
     print("Displaying... please wait (this is an inefficient implementation)")
     global fig
-    ntels = len(data.dl0.tels_with_data)
+    ntels = len(event.dl0.tels_with_data)
     fig.clear()
 
-    plt.suptitle("EVENT {}".format(data.dl0.event_id))
+    plt.suptitle("EVENT {}".format(event.dl0.event_id))
 
-    for ii, tel_id in enumerate(data.dl0.tels_with_data):
+    for ii, tel_id in enumerate(event.dl0.tels_with_data):
         print("\t draw cam {}...".format(tel_id))
         nn = int(ceil(sqrt(ntels)))
         ax = plt.subplot(nn, nn, ii + 1)
 
-        x, y = data.meta.pixel_pos[tel_id]
+        x, y = event.meta.pixel_pos[tel_id]
         geom = io.guess_camera_geometry(x * u.m, y * u.m)
         disp = visualization.CameraDisplay(geom, axes=ax,
                                            title="CT{0}".format(tel_id))
@@ -46,7 +49,7 @@ def display_event(data):
         disp.autoupdate = False
         disp.set_cmap(random.choice(cmaps))
         chan = 0
-        signals = data.dl0.tel[tel_id].adc_sums[chan]
+        signals = event.dl0.tel[tel_id].adc_sums[chan]
         signals -= signals.mean()
         disp.set_image(signals)
         disp.add_colorbar()
@@ -63,9 +66,6 @@ def get_input():
 
 if __name__ == '__main__':
 
-    print("If you don't see a plot, run this with "
-          "'ipython -i --matplotlib read_hessio.py <filename>")
-
     if len(sys.argv) > 1:
         filename = sys.argv.pop(1)
     else:
@@ -77,29 +77,33 @@ if __name__ == '__main__':
     # loop over events and display menu at each event:
     source = hessio_event_source(filename, max_events=1000000)
 
-    for data in source:
+    for event in source:
 
-        print("EVENT_ID: ", data.dl0.event_id, "TELS: ",
-              data.dl0.tels_with_data)
+        print("EVENT_ID: ", event.dl0.event_id, "TELS: ",
+              event.dl0.tels_with_data)
 
         while True:
             response = get_input()
             if response.startswith("d"):
-                display_event(data)
+                display_event(event)
                 plt.pause(0.1)
             elif response.startswith("p"):
-                print(data)
-                for teldata in data.dl0.tel.values():
+                print("--event-------------------")
+                print(event)
+                print("--event.dl0---------------")
+                print(event.dl0)
+                print("--event.dl0.tel-----------")
+                for teldata in event.dl0.tel.values():
                     print(teldata)
             elif response == "" or response.startswith("n"):
                 break
             elif response.startswith('i'):
-                for tel_id in sorted(data.dl0.tel):
-                    for chan in data.dl0.tel[tel_id].adc_samples:
-                        npix = len(data.meta.pixel_pos[tel_id][0])
+                for tel_id in sorted(event.dl0.tel):
+                    for chan in event.dl0.tel[tel_id].adc_samples:
+                        npix = len(event.meta.pixel_pos[tel_id][0])
                         print("CT{:4d} ch{} pixels:{} samples:{}"
-                              .format(tel, chan, npix,
-                                      event.tel[tel_id].
+                              .format(tel_id, chan, npix,
+                                      event.dl0.tel[tel_id].
                                       adc_samples[chan].shape[1]))
 
             elif response.startswith('q'):
