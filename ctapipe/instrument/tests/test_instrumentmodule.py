ab45a2a24dca7e45c28414c42a5a7318e9ab1bc3
justuszorn <justus.zorn@desy.de>
2015-12-16 13:18:15 +0100

    test for instrument module 'test_instrumentmodule.py' as well as example 'read_instrument_description.py' added

--- ctapipe/instrument/tests/test_instrumentmodule.py
+++ ctapipe/instrument/tests/test_instrumentmodule.py
@@ -5,48 +5,32 @@ Created on Wed Nov 18 17:37:19 2015
 @author: zornju
 """
 
-from ctapipe.instrument.InstrumentDescription import Telescope, Camera, Optics
-from ctapipe.instrument.util_functions import load, close
+import ctapipe.instrument.InstrumentDescription as ID
 from ctapipe.utils.datasets import get_path
 
 filename1 = get_path('PROD2_telconfig.fits.gz')
 filename2 = get_path('gamma_test.simtel.gz')
+filename3 = get_path('CTA-ULTRA6-SCT.cfg')
 
-item1 = load(filename1)
-item2 = load(filename2)
-
-def test_load_file():
-    assert(item1)
-    assert(item2==0)
-    
 def test_read_telescope_data():
-    instr1 = Telescope.initialize(filename1,item1)
-    instr2 = Telescope.initialize(filename2,item2)
+    instr1 = ID.Telescope.from_file(filename1)
+    instr2 = ID.Telescope.from_file(filename2)
+    instr3 = ID.Telescope.from_file(filename3)
     tel1 = instr1[0]
-    tel2 = instr2[0]    
+    tel2 = instr2[0]
+    tel3 = instr3[0]    
     assert(len(tel1.tel_id)>0)
     assert(len(tel2.tel_id)>0)
+    assert(len(tel3.tel_id)>0)
     opt1 = instr1[1]
     opt2 = instr2[1]
-    assert(opt1[0].mir_area != 0)    
-    assert(opt2[0].mir_area != 0) 
+    opt3 = instr3[1]
+    assert(opt1[0].mir_area.value != 0)    
+    assert(opt2[0].mir_area.value != 0)
+    assert(opt3[0].mir_area.value != 0)
     cam1 = instr1[2]
     cam2 = instr2[2]
-    assert(cam1[0].cam_fov != 0)
-    assert(cam2[0].cam_fov != 0)
-    
-def test_read_camera_data():
-    cam1 = Camera.initialize(filename1,1,item1)
-    cam2 = Camera.initialize(filename2,18,item2)
-    assert(cam1.cam_fov != 0)
-    assert(cam2.cam_fov != 0)
-
-def test_read_optics_data():
-    opt1 = Optics.initialize(filename1,1,item1)
-    opt2 = Optics.initialize(filename2,18,item2)
-    assert(opt1.mir_area != 0)    
-    assert(opt2.mir_area != 0)
-
-def test_close_file():
-    assert(close(filename1,item1)==None)
-    assert(close(filename2,item2)==None)
+    cam3 = instr3[2]
+    assert(cam1[0].cam_fov.value != 0)
+    assert(cam2[0].cam_fov.value != 0)
+    assert(cam3[0].cam_fov.value != 0)
--- examples/read_instrument_description.py
+++ examples/read_instrument_description.py
@@ -12,13 +12,13 @@ from ctapipe.instrument import InstrumentDescription as ID, CameraDescription as
 from ctapipe.utils.datasets import get_path
 from astropy import units as u
 import matplotlib.pyplot as plt
-import time
 
 if __name__ == '__main__':
     
     filename1 = get_path('PROD2_telconfig.fits.gz')
     filename2 = get_path('gamma_test.simtel.gz')
-    filename3 ='/afs/ifh.de/group/cta/MC_Production/d20150828_GM/mrg/sim_telarray/cfg/CTA/CTA-ULTRA6-SCT.cfg'
+    #filename3 = get_path('CTA-ULTRA6-SCT.cfg')
+    filename3 = '/afs/ifh.de/group/cta/MC_Production/d20150828_GM/mrg/sim_telarray/cfg/CTA/CTA-ULTRA6-SCT.cfg'    
     
     instr1 = ID.Telescope.from_file(filename1)
     instr2 = ID.Telescope.from_file(filename2)
@@ -72,40 +72,36 @@ if __name__ == '__main__':
     #a specific telescope with ID `tel_id` (of course the configuration of this
     #specific telescope must be stored in the file) without loading all the
     #telescope configurations, e.g. load camera and optics configurations of
-    #camera with ID 1 and ID 18:
-    cam1 = ID.Camera.from_file(filename1,1)
-    cam2 = ID.Camera.from_file(filename2,18)
-    cam3 = ID.Camera.from_file(filename3,1)
-    print('Camera FOV of the first (18th) telescope of fits (hessio) file.',
+    #camera with ID 1 from FITS and ASCII file:
+    cam10 = ID.Camera.from_file(filename1,1)
+    cam30 = ID.Camera.from_file(filename3,1)
+    print('Camera FOV of the first telescope of fits file.',
           'Returned, when only the camera data is read from the file:')
-    print('{0:.2f}'.format(cam1.cam_fov))
-    print('{0:.2f}'.format(cam2.cam_fov))
+    print('{0:.2f}'.format(cam10.cam_fov))
     print('Rotate the Camera by 180 degree, i.e. the x- & y-positions will be',
           'rotated by 190 degree:')
     print('x-position of the camera before using the rotate method:')
-    print(cam1.pix_posX)
+    print(cam10.pix_posX)
     print('x-position of the camera after using the rotate method:')
-    cam1.rotate(cam1,180*u.degree)
-    print(cam1.pix_posX)
+    cam10.rotate(cam10,180*u.degree)
+    print(cam10.pix_posX)
     print('FADC pulse shape over time of ASCII file')
-    plt.plot(cam3.fadc_pulsshape[0],cam3.fadc_pulsshape[1],'+')
+    plt.plot(cam30.fadc_pulsshape[0],cam30.fadc_pulsshape[1],'+')
     plt.show()    
     print('----------------------------')
     
     print('Print imported camera data as a table:')
-    table = CD.write_table(1,cam1.cam_class,cam1.pix_id[:1],cam1.pix_posX,
-                           cam1.pix_posY,cam1.pix_area,cam1.pix_type)
+    table = CD.write_table(1,cam10.cam_class,cam10.pix_id[:1],cam10.pix_posX,
+                           cam10.pix_posY,cam10.pix_area,cam10.pix_type)
     print(table)
     print('---------------------------')
     
-    opt1 = ID.Optics.from_file(filename1,1)
-    opt2 = ID.Optics.from_file(filename2,18)
-    opt3 = ID.Optics.from_file(filename3,1)
-    print('Mirror Area of the first (18th) telescope of fits (hessio) file.',
+    opt10 = ID.Optics.from_file(filename1,1)
+    opt30 = ID.Optics.from_file(filename3,1)
+    print('Mirror Area of the first telescope of fits file.',
           'Returned, when only the optics data is read from the file:')
-    print('{0:.2f}'.format(opt1.mir_area))
-    print('{0:.2f}'.format(opt2.mir_area))
+    print('{0:.2f}'.format(opt10.mir_area))
     print('Mirror reflection over wavelength [nm]')
-    plt.plot(opt3.mir_reflection[0].value,opt3.mir_reflection[1],'+')
+    plt.plot(opt30.mir_reflection[0].value,opt30.mir_reflection[1],'+')
     plt.show()
     print('----------------------------')
