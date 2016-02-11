adc0f2a2a6781a15be7bfa023a3c419a4b497237
jacquem <jacquem@lapp.in2p3.fr>
2016-02-11 10:56:55 +0100

    create base class for stage_process

--- examples/brainstorm/pipeline/pipeline_cta_analysis/stager_process.py
+++ examples/brainstorm/pipeline/pipeline_cta_analysis/stager_process.py
@@ -2,51 +2,18 @@ from time import sleep
 import threading
 import subprocess
 import os
+from base_process import BaseProcess
 
 
-class StagerProcess():
+class StagerProcess(BaseProcess):
 	def __init__(self,configuration=None):
-		self.section_name = None
-		self.configuration = configuration
-		self.executable = None
-		self.out_extension = None
-		self.output_dir = None
-		self.options = None
-		
-	def init(self):
-		self.executable =  self.configuration.get('executable', section=self.section_name)
-		self.options = self.configuration.get('options', section=self.section_name)
-		self.output_dir = self.configuration.get('output_dir', section=self.section_name)
-		self.out_extension = self.configuration.get('out_extension', section=self.section_name)
-		if self.output_dir == None or self.out_extension == None: 
-			print("StagerProcess :configuration error ")
-			print('output_dir:', self.output_dir, 'out_extension:',self.out_extension)
-			return False
-		
-		
-		if not os.path.exists(self.output_dir):
-			try:
-				os.mkdir(self.output_dir)
-			except OSError:
-				print("StagerProcess: could not create output_dir", self.output_dir)
-				return False
-		return True
-
+		BaseProcess.__init__(self,configuration)
+	
 	def run(self,input_file):
-		print('StagerProcess', input_file)
-		if  self.output_dir != None:
-			output_file =  self.output_dir + '/'+input_file.split('/')[-1] 
-			if self.out_extension != None:
-				output_file = output_file.rsplit('.',2)[0]
-				output_file+= "." + self.out_extension
-			cmd = self.executable + " -i " + input_file  + " -o " + output_file + " " +  self.options 
-		else:
-			cmd = self.executable + " -i " + input_file + " " +  self.options 
-		ps = subprocess.Popen(cmd,shell=True,stderr=subprocess.STDOUT)
-		ps.communicate()[0]
+		cmd,output_file = super().build_command(input_file)
+		proc = subprocess.Popen(cmd)
+		proc.wait()
 		return  output_file
 
-
-
 	def finish(self):
-		print("--- StagerProcess finish ---")
+		print('--- ', self.section_name, ' finish ---')
