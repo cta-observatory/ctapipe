b6d8b2fb183c80b4645a2d2da3f2e1fca9e97a97
justuszorn <justus.zorn@desy.de>
2016-03-14 11:20:43 +0100

    small changes to the InstrumentModule

---
+++ ctapipe/instrument/tests/test_instrumentmodule.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 18 17:37:19 2015
+
+@author: zornju
+"""
+
+import ctapipe.instrument.InstrumentDescription as ID
+from ctapipe.utils.datasets import get_path
+import os
+
+filename1 = get_path('PROD2_telconfig.fits.gz')
+filename2 = get_path('gamma_test.simtel.gz')
+filename3 = get_path('CTA-ULTRA6-SCT.cfg')
+
+def test_load_and_write_telescope_data():
+    tel1,cam1,opt1 = ID.load(filename1)
+    tel2,cam2,opt2 = ID.load(filename2)
+    tel3,cam3,opt3 = ID.load(filename3)
+    tel4,cam4,opt4 = ID.load()
+    assert(len(tel1)>0)
+    assert(len(tel2)>0)
+    assert(len(opt3)>0)
+    assert(len(tel4)>0)
+    
+    ID.write_fits(instr_dict=tel4,filename='test.fits',overwrite=True)
+    tel41,cam41,opt41 = ID.load('test.fits')
+    assert(len(tel41)>0)
+    os.remove('test.fits')
