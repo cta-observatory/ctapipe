b4c3598f4e9240b874e3d6563c4b31bb1281c3b3
Abelardo Moralejo Olaizola <moralejo@ifae.es>
2016-03-18 10:10:51 +0100

    Changed the way camera geometries are guessed from a simtelarray file. Now focal length is used to distinguish cameras with the same number of pixels (instead of pixel separation)
    Got rid of assumption that pixels 0 and 1 are contiguous in the calculation
    of pixel spacing
    
    Introduced an overall camera rotation to bring all cameras to a common
    system of coordinates

--- examples/read_hessio_single_tel.py
+++ examples/read_hessio_single_tel.py
@@ -74,7 +74,7 @@ if __name__ == '__main__':
 
         if disp is None:
             x, y = event.meta.pixel_pos[args.tel]
-            geom = io.CameraGeometry.guess(x, y)
+            geom = io.CameraGeometry.guess(x, y, event.meta.optical_foclen[args.tel])
             disp = visualization.CameraDisplay(geom, title='CT%d' % args.tel)
             disp.enable_pixel_picker()
             disp.add_colorbar()
