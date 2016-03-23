1f45708c6a5b9bc3d3d677a0cfd5ba97a29f9dd0
Dan Parsons <parsonsrd@gmail.com>
2016-03-23 14:28:20 +0100

    Added examples for all systems

--- examples/coordinate_transformations.py
+++ examples/coordinate_transformations.py
@@ -2,77 +2,68 @@
 Some simple examples of using the frames and transformations
 defined in `ctapipe.coordinates`
 
-Basically this is me learning how `astropy.coordinates` works,
-and how the coordinate frames and transformations work at the same time
-... it could be all wrong and comments / contributions are welcome!
-
-TODO: Goal: Define CameraFrame (attributes: pointing Alt, Az + focal length) and connect it to AltAzFrame
 """
 import astropy.units as u
 from astropy.time import Time
 from astropy.coordinates import SkyCoord, Angle, AltAz
-#from astropy.coordinates.builtin_frames.altaz import AltAz
 import timeit
 
 from ctapipe.coordinates import CameraFrame, TelescopeFrame, GroundFrame, TiltedGroundFrame, NominalFrame
+from ctapipe.coordinates.frames import altaz_to_offset
 from astropy import coordinates as c
 
-import math
 import numpy as np
 
+# Convert camera frame to telescope frame
 def cam_to_tel():
 
+    # Coordinates in any fram can be given as a numpy array of the xyz positions
+    # e.g. in this case the position on pixels in the camera
     pix = [np.ones(2048),np.ones(2048),np.zeros(2048)] * u.m
+    # first define the camera frame
     camera_coord = CameraFrame(pix)
-    telescope_coord = camera_coord.transform_to(TelescopeFrame(focal_length = 10*u.m,rotation=0*u.deg))
 
-def tel_to_cam():
-    pix = [np.ones(2048),np.ones(2048),np.zeros(2048)] * u.deg
-    telescope_coord = TelescopeFrame(pix,focal_length = 10*u.m)
-    camera_coord = telescope_coord.transform_to(CameraFrame())
+    # then use transform to function to convert to a new system
+    # making sure to give the required values for the conversion (these are not checked yet)
+    telescope_coord = camera_coord.transform_to(TelescopeFrame(focal_length = 15*u.m,rotation=0*u.deg))
 
+    # Print cordinates in the new frame
+    print("Telescope Coordinate",telescope_coord)
+
+    # Transforming back is then easy
+    camera_coord2 = telescope_coord.transform_to(CameraFrame())
+
+    # We can easily check the distance between 2 coordinates in the same frame
+    # In this case they should be the same
+    print("Separation",np.sum(camera_coord.separation_3d(camera_coord2)))
+
+# The astropy system is clever enough to transform through several intermediate
+# steps to get to the sytem you want (provided it has sufficient information)
 def cam_to_nom():
     pix = [np.ones(2048),np.ones(2048),np.zeros(2048)] * u.m
     camera_coord = CameraFrame(pix)
+    # In this case we bypass the telescope system
+    nom_coord = camera_coord.transform_to(NominalFrame(array_direction = [75*u.deg,180*u.deg],pointing_direction = [70*u.deg,180*u.deg],focal_length = 15*u.m))
+    print("Nominal Coordinate",nom_coord)
 
+# Once we are at the nominal system where most reconstruction will be done we
+# can then convert to AltAz (currently we cannot transform directly from camera)
+def nominal_to_altaz():
+    t = np.zeros(10)
+    t[5] = 1
+    nom = NominalFrame(x=t*u.deg,y=t*u.deg,z=t*u.deg,array_direction = [75*u.deg,180*u.deg])
+    alt_az = nom.transform_to(AltAz)
+    print("AltAz Coordinate",alt_az)
+    # Provided we know when and where the AltAz was measured we can them convert this to any astronomical
 
-def test_camera_telescope_transform():
-    camera_coord = CameraFrame(x=1*u.m, y=2*u.m, z=0*u.m)
-    pix = [[1,1,1],[2,2,2],[3,3,3]] * u.m
-    #print (pix)
-    camera_coord = CameraFrame(pix)
-    #print(camera_coord,pix)
-
-    telescope_coord = camera_coord.transform_to(TelescopeFrame(focal_length = 10*u.m))
-    #print(telescope_coord)
-
-    camera_coord2 = telescope_coord.transform_to(CameraFrame)
-    #print(camera_coord2)
-
+# We also have the ground and tilted ground systems needed for core reconstruction
+def grd_to_tilt():
     grd_coord = GroundFrame(x=1*u.m, y=2*u.m, z=0*u.m)
-    #print (grd_coord)
-    alt = 50 * u.deg
-    #print(alt.to(u.rad)/u.rad)
     tilt_coord = grd_coord.transform_to(TiltedGroundFrame(pointing_direction = [90*u.deg,180*u.deg]))
-    grd_coord2 = tilt_coord.transform_to(GroundFrame())
-
-    #print (tilt_coord)
-    #print (grd_coord2)
-    tel_coord = TelescopeFrame(0.1*u.deg,0.1*u.deg,0.0*u.deg,pointing_direction = [70*u.deg,180*u.deg])
-    nominal_coord = tel_coord.transform_to(NominalFrame(pointing_direction = [75*u.deg,180*u.deg]))
-    #print(tel_coord,nominal_coord)
-    #print(nominal_coord.transform_to(TelescopeFrame(pointing_direction=[70*u.deg,180*u.deg])))
-    obs = Time( "2015-10-11 15:17:45.3", format="iso", scale="utc")
-    paris = c.EarthLocation( lat=48.8567*u.deg, lon=2.3508*u.deg )
-    a = AltAz(1*u.deg, 2*u.deg)
-    a2 = AltAz()
-
-    print(a,a2)
-    #altaz = SkyCoord(0,0,frame='altaz')
-    #print(nominal_coord.transform_to(a))
-    print("here")
+    print("Tilted Coordinate",tilt_coord)
 
 if __name__ == '__main__':
-    #test_camera_telescope_transform()
-    print (timeit.timeit(cam_to_tel, number=1000)/1000.)
-    print (timeit.timeit(tel_to_cam, number=1000)/1000.)
+    cam_to_tel()
+    cam_to_nom()
+    nominal_to_altaz()
+    grd_to_tilt()
