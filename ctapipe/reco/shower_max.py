1aeb94826ebaa2bddeb462ded72b3eed0aacbc04
Tino Michael <tino.michael@cea.fr>
2016-06-06 11:42:54 +0200

    added documentation to ShowerMaxEstimator

--- ctapipe/reco/shower_max.py
+++ ctapipe/reco/shower_max.py
@@ -4,9 +4,22 @@ from astropy import units as u
 
 from ctapipe.utils.histogram import nDHistogram
 
-
 class ShowerMaxEstimator:
-    def __init__(self, filename):
+    def __init__(self, filename, col_altitude=0, col_thickness=2):
+        """ small class that calculates the height of the shower maximum
+            given a parametrisation of the atmosphere 
+            and certain parameters of the shower itself
+            
+            Parameters:
+            -----------
+            filename : string
+                path to text file that contains a table of the atmosphere parameters
+            col_altitude : int
+                column in the text file that contains the altitude/height
+            col_thickness : int
+                column in the text file that contains the thickness
+        """
+        
         altitude  = []
         thickness = []
         atm_file = open(filename, "r")
@@ -20,6 +33,24 @@ class ShowerMaxEstimator:
 
         
     def find_shower_max_height(self,energy,h_first_int,gamma_alt):
+        """ estimates the height of the shower maximum in the atmosphere
+            according to equation (3) in [arXiv:0907.2610v3]
+        
+        Parameters:
+        -----------
+        energy : astropy.Quantity
+            energy of the parent gamma photon
+        h_first_int : astropy.Quantity
+            hight of the first interaction
+        gamma_alt : astropy.Quantity or float
+            altitude / pi-minus-zenith (in radians in case of float) of the parent gamma photon
+        
+        Returns:
+        --------
+        shower_max_height : astropy.Quantity
+            height of the shower maximum
+        """
+        
         # offset of the shower-maximum in radiation lengths
         c = 0.97 * log(energy / (83 * u.MeV)) - 1.32
         # radiation length in dry air at 1 atm = 36,62 g / cm**2 [PDG]
@@ -29,7 +60,7 @@ class ShowerMaxEstimator:
         c *= np.sin(gamma_alt)
         
         # find the thickness at the height of the first interaction
-        t_first_int = self.atmosphere.interpolate_linear([h_first_int])
+        t_first_int = self.atmosphere.interpolate([h_first_int])
 
         # total thickness at shower maximum = thickness at first interaction + thickness traversed to shower maximum
         t_shower_max = t_first_int + c
