b3dad77544ff2304cddb65e02cb1cc63355286e6
Dan Parsons <parsonsrd@gmail.com>
2016-06-13 11:18:40 +0200

    Added function for weighted distance calcuations

--- ctapipe/reco/weighted_axis_minimisation.py
+++ ctapipe/reco/weighted_axis_minimisation.py
@@ -13,7 +13,7 @@ __all__ = [
 ]
 
 
-def reconstruct_event(hillas_parameters,telescope_positions,pixel_list,shower_seed=None):
+def reconstruct_event(hillas_parameters,telescope_positions,pixel_pos_x,pixel_pos_y,pixel_weight=1,shower_seed=None):
     """
     Perform event reconstruction
 
@@ -22,7 +22,7 @@ def reconstruct_event(hillas_parameters,telescope_positions,pixel_list,shower_se
     hillas_parameters: list
         Hillas parameter objects
     telescope_positions: list
-        XY positions of telescopes (ground system)
+        XY positions of telescopes (tilted system)
     shower_seed: shower object?
         Seed position to begin shower minimisation
 
@@ -37,6 +37,43 @@ def reconstruct_event(hillas_parameters,telescope_positions,pixel_list,shower_se
     return None
 
 
+def weighted_dist(x_src,y_src,x_grd,y_grd,pixel_pos_x,pixel_pos_y,tel_pos_x,tel_pos_y,pixel_weight):
+
+    sum = 0
+    for pos_x,pos_y,tel_x,tel_y,weight in pixel_pos_x,pixel_pos_y,tel_pos_x,tel_pos_y,pixel_weight:
+        phi = np.atan((tel_y-y_grd)/(tel_x-x_grd))
+        sum += get_dist_from_axis(pos_x,pos_y,x_src,y_src,phi,weight)
+    return sum
+
+
+def get_dist_from_axis(pixel_pos_x,pixel_pos_y,x_trans,y_trans,phi,pixel_weight):
+    """
+    Function to perform weighting of pixel positions from the shower axis for an individual image
+
+    Parameters
+    ----------
+    pixel_pos_x: ndarray
+        Array of pixel x positions
+    pixel_pos_y: ndarray
+        Array of pixel x positions
+    x_trans: float
+        Translation of position in x coordinates
+    y_trans: float
+        Translation of position in y coordinates
+    phi: float
+        Rotation angle of pixels
+    pixel_weight: ndarray
+        Weighting of individual pixels (usually amplitude)
+
+    Returns
+    -------
+        Weighted sum of distances of pixels from shower axis
+    """
+    pixel_pos_x_trans,pixel_pos_y_trans = rotate_translate(pixel_pos_x,pixel_pos_y,x_trans,y_trans,phi)
+    pixel_pos_y_trans *= pixel_weight
+
+    return np.sum(pixel_pos_y_trans)
+
 def rotate_translate(pixel_pos_x,pixel_pos_y,x_trans,y_trans,phi):
     """
     Function to perform rotation and translation of pixel lists
@@ -49,7 +86,7 @@ def rotate_translate(pixel_pos_x,pixel_pos_y,x_trans,y_trans,phi):
         Array of pixel x positions
     x_trans: float
         Translation of position in x coordinates
-    y: float
+    y_trans: float
         Translation of position in y coordinates
     phi: float
         Rotation angle of pixels
