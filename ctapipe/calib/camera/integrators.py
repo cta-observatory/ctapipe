Removed all conversions to int16 as it was causing incorrect values. Improved creation and storage of geometries

ctapipe/calib/camera/integrators.py
@@ -260,7 +260,7 @@ def global_peak_integration(data, params):
         peakpos[0] = np.round(max_time_sample_sum[0] / max_sample_sum[0])
         peakpos[1] = peakpos[0]
         peakpos = peakpos
-    start = (peakpos - params['shift']).astype(np.int16, copy=False)
+    start = (peakpos - params['shift'])
     window = params['window']
 
     # Check window is within readout
@@ -344,7 +344,7 @@ def local_peak_integration(data, params):
     if nchan > 1:  # If the LG is not significant, takes the HG peakpos
         peakpos[1] = np.where(sig_pixels[1] < sig_pixels[0], peakpos[0],
                               peakpos[1])
-    start = (peakpos - params['shift']).astype(np.int16, copy=False)
+    start = (peakpos - params['shift'])
     window = params['window']
 
     # Check window is within readout
@@ -443,7 +443,7 @@ def nb_peak_integration(data, geom, params):
         all_data = np.concatenate((nb_data, pixel_expanded), axis=1)
         sum_data = all_data.sum(1)
         peakpos[:, ipix] = sum_data.argmax(1)
-    start = (peakpos - params['shift']).astype(np.int16, copy=False)
+    start = (peakpos - params['shift'])
     window = params['window']
 
     # Check window is within readout

ctapipe/calib/camera/mc.py
@@ -80,6 +80,7 @@ def set_integration_correction(event, telid, params):
 
     correction = round((sum(y) * refstep) / (sum(y1[start:start + window]) *
                                              time_slice), 7)
+
     return correction
 
 
@@ -135,6 +136,8 @@ def calibrate_amplitude_mc(event, charge, telid, params):
     """
     scaled_pe = pe * calib_scale
     # TODO: create dict of CALIB_SCALE for every instrument
+    print(pe[0,804], calib[0,804], scaled_pe[0, 804])
+
 
     return scaled_pe
 
@@ -200,7 +203,9 @@ def integration_mc(event, telid, params, geom=None):
         int_corr = 1
 
     # Convert integration into charge
-    charge = np.round(integration * int_corr).astype(np.int16, copy=False)
+    charge = np.round(integration * int_corr)
+
+    print(integration[0][804], int_corr, charge[0][804])
 
     return charge, integration_window, data_ped
 