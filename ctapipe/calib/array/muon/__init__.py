bcc8f61093bdad8dcba0c478da774aa1f89af985
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2016-09-28 16:14:01 +0200

    Add import for new fit in muon __init__ and fix return value

--- ctapipe/calib/array/muon/__init__.py
+++ ctapipe/calib/array/muon/__init__.py
@@ -1,5 +1,6 @@
 from .fitting import kundu_chaudhuri_circle_fit
 from .fitting import psf_likelihood_fit
+from .fitting import impact_parameter_fit
 
 from .features import mean_squared_error
 from .features import photon_ratio_inside_ring
