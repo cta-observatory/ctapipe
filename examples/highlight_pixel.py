f7cce00083c8385ccde044174e86e204c8c46c76
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2016-09-30 19:03:58 +0200

    Add pixel highlight example

---
+++ examples/highlight_pixel.py
@@ -0,0 +1,33 @@
+from matplotlib import pyplot as plt
+
+from ctapipe.io import CameraGeometry
+from ctapipe.visualization import CameraDisplay
+
+from ctapipe.reco import mock
+
+
+if __name__ == '__main__':
+
+    plt.style.use('ggplot')
+
+    fig = plt.figure(figsize=(12, 8))
+    ax = fig.add_subplot(1, 1, 1)
+
+    geom = CameraGeometry.from_name('hess', 1)
+    disp = CameraDisplay(geom, ax=ax)
+    disp.add_colorbar()
+
+    model = mock.generate_2d_shower_model(
+        centroid=(0.05, 0.0), width=0.005, length=0.025, psi='35d'
+    )
+
+    image, sig, bg = mock.make_mock_shower_image(
+        geom, model.pdf, intensity=50, nsb_level_pe=20
+    )
+
+    disp.image = image
+
+    mask = disp.image > 15
+    disp.highlight_pixels(mask, linewidth=3)
+
+    plt.show()
