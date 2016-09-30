5ee109d47aca26c79caa78c5a09b9a93d7393ffe
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2016-09-30 13:40:02 +0200

    Simplify efficiency estimation by using the other fit methods

--- ctapipe/calib/array/muon/__init__.py
+++ ctapipe/calib/array/muon/__init__.py
@@ -1,7 +1,7 @@
 from .fitting import kundu_chaudhuri_circle_fit
 from .fitting import psf_likelihood_fit
 from .fitting import impact_parameter_chisq_fit
-from .fitting import efficiency_likelihood_fit
+from .fitting import efficiency_fit
 
 from .features import mean_squared_error
 from .features import photon_ratio_inside_ring
