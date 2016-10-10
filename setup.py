1158d1873e44bbbcea49892ad92796e168eaecb2
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2016-10-10 21:45:26 +0200

    Cleanup setup.py, remove ah_bootstrap and astropy helpers

--- setup.py
+++ setup.py
@@ -1,83 +1,7 @@
 #!/usr/bin/env python
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
-
-import glob
+from setuptools import setup, find_packages
 import os
-import sys
 
-import ah_bootstrap
-from setuptools import setup
-
-import builtins
-builtins._ASTROPY_SETUP_ = True
-
-from astropy_helpers.setup_helpers import (
-    register_commands, adjust_compiler, get_debug_option, get_package_info
-)
-from astropy_helpers.git_helpers import get_git_devstr
-from astropy_helpers.version_helpers import generate_version_py
-
-# Get some values from the setup.cfg
-from configparser import RawConfigParser
-conf = RawConfigParser()
-conf.read(['setup.cfg'])
-metadata = dict(conf.items('metadata'))
-
-PACKAGENAME = metadata.get('package_name', 'packagename')
-DESCRIPTION = metadata.get('description', 'Astropy affiliated package')
-AUTHOR = metadata.get('author', '')
-AUTHOR_EMAIL = metadata.get('author_email', '')
-LICENSE = metadata.get('license', 'unknown')
-URL = metadata.get('url', 'http://astropy.org')
-
-# Get the long description from the package's docstring
-__import__(PACKAGENAME)
-package = sys.modules[PACKAGENAME]
-LONG_DESCRIPTION = package.__doc__
-
-# Store the package name in a built-in variable so it's easy
-# to get from other parts of the setup infrastructure
-builtins._ASTROPY_PACKAGE_NAME_ = PACKAGENAME
-
-# VERSION should be PEP386 compatible (http://www.python.org/dev/peps/pep-0386)
-VERSION = metadata.get('version', '0.0.dev')
-
-# Indicates if this version is a release version
-RELEASE = 'dev' not in VERSION
-
-if not RELEASE:
-    VERSION += get_git_devstr(False)
-
-# Populate the dict of setup command overrides; this should be done before
-# invoking any other functionality from distutils since it can potentially
-# modify distutils' behavior.
-cmdclassd = register_commands(PACKAGENAME, VERSION, RELEASE)
-
-# Adjust the compiler in case the default on this platform is to use a
-# broken one.
-adjust_compiler(PACKAGENAME)
-
-# Freeze build information in version.py
-generate_version_py(PACKAGENAME, VERSION, RELEASE,
-                    get_debug_option(PACKAGENAME))
-
-# Treat everything in scripts except README.rst as a script to be installed
-scripts = [fname for fname in glob.glob(os.path.join('scripts', '*'))
-           if os.path.basename(fname) != 'README.rst']
-
-
-# Get configuration information from all of the various subpackages.
-# See the docstring for setup_helpers.update_package_files for more
-# details.
-package_info = get_package_info()
-
-# Add the project-global data
-package_info['package_data'].setdefault(PACKAGENAME, [])
-package_info['package_data'][PACKAGENAME].append('data/*')
-
-# Define entry points for command-line scripts
-# TODO: this shuold be automated (e.g. look for main functions and
-# rename _ to -, and prepend 'ctapipe'
 entry_points = {}
 entry_points['console_scripts'] = [
     'ctapipe-info = ctapipe.tools.info:main',
@@ -90,44 +14,53 @@ entry_points['console_scripts'] = [
 # Cython, since we can not do this in MANIFEST.in with a "dynamic"
 # directory name.
 c_files = []
-for root, dirs, files in os.walk(PACKAGENAME):
+for root, dirs, files in os.walk('ctapipe'):
     for filename in files:
         if filename.endswith('.c'):
-            c_files.append(
+            package_data['ctapipe'].append(
                 os.path.join(
-                    os.path.relpath(root, PACKAGENAME), filename))
-package_info['package_data'][PACKAGENAME].extend(c_files)
+                    os.path.relpath(root, PACKAGENAME),
+                    filename
+                )
+            )
 
 # Note that requires and provides should not be included in the call to
 # ``setup``, since these are now deprecated. See this link for more details:
 # https://groups.google.com/forum/#!topic/astropy-dev/urYO8ckB2uM
 
-setup(name=PACKAGENAME,
-      version=VERSION,
-      description=DESCRIPTION,
-      scripts=scripts,
-      install_requires=['astropy','scipy','matplotlib',
-                        'scikit-learn','numpy','traitlets'],
-      author=AUTHOR,
-      author_email=AUTHOR_EMAIL,
-      license=LICENSE,
-      url=URL,
-      long_description=LONG_DESCRIPTION,
-      classifiers=[
-          'Intended Audience :: Science/Research',
-          'License :: OSI Approved :: BSD License',
-          'Operating System :: OS Independent',
-          'Programming Language :: C',
-          'Programming Language :: Cython',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: Implementation :: CPython',
-          'Topic :: Scientific/Engineering :: Astronomy',
-          'Development Status :: 3 - Alpha',
-      ],
-      cmdclass=cmdclassd,
-      zip_safe=False,
-      use_2to3=False,
-      entry_points=entry_points,
-      **package_info
+setup(
+    name='ctapipe',
+    version='0.1.dev',
+    description='CTA Python pipeline experimental version',
+    author='ctapipe developers',
+    author_email='karl.kosack@cea.fr',
+    license='BSD3',
+    url='https://github.com/cta-observatory/ctapipe',
+    edit_on_github=False,
+    github_project='cta-observatory/ctapipe',
+    packages=find_packages(),
+    tests_require=['pytest>=3.0.0'],
+    setup_requires=['pytest-runner>=2.9'],
+    install_requires=[
+        'astropy',
+        'scipy',
+        'matplotlib',
+        'scikit-learn',
+        'numpy',
+        'traitlets',
+    ],
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: C',
+        'Programming Language :: Cython',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'Development Status :: 3 - Alpha',
+    ],
+    zip_safe=False,
+    entry_points=entry_points,
 )
