43f5cf95e04308052b25ea0566ae1710b5e73963
Jean Jacquemier <jacquem@lapp.in2p3.fr>
2016-10-10 11:11:58 +0200

    calibration flow works

--- ctapipe/flow/algorithms/simtelarray_reader.py
+++ ctapipe/flow/algorithms/simtelarray_reader.py
@@ -9,7 +9,7 @@ class SimTelArrayReader(Component):
     """`SimTelArrayReader` class represents a Producer for pipeline.
         It opens simtelarray file and yields even to the next stage
     """
-    filename = Unicode('gamma', help='simtel MC input file').tag(
+    filename = Unicode('gamma_test.simtel.gz', help='simtel MC input file').tag(
         config=True, allow_none=True)
     source = None
 
@@ -17,7 +17,7 @@ class SimTelArrayReader(Component):
         self.log.debug("--- SimTelArrayReader init {}---".format(self.filename))
         try:
             in_file = get_path(self.filename)
-            self.source = hessio_event_source(in_file,max_events=100)
+            self.source = hessio_event_source(in_file,max_events=3)
             self.log.debug('{} successfully opened {}'.format(self.filename,self.source))
         except:
             self.log.error('could not open ' + in_file)
@@ -26,6 +26,7 @@ class SimTelArrayReader(Component):
 
     def run(self):
         for event in self.source:
+            self.log.debug('\n--- SimTelArrayReader send event {}'.format(event.dl0.event_id))
             yield (event)
         self.log.debug("\n--- SimTelArrayReader Done ---")
 
