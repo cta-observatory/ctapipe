35b32df7553b3590061b64da45d9dae039e2c6cd
Markus Gaug <markusgaug@gmail.com>
2016-10-12 17:08:06 +0200

    Make the test meaningful by creating a real circle

--- ctapipe/image/muon/tests/test_muon_ring_finder.py
+++ ctapipe/image/muon/tests/test_muon_ring_finder.py
@@ -7,8 +7,14 @@ def test_ChaudhuriKunduRingFitter():
 
     x = np.linspace(-100,100,200)
     y = np.linspace(-100,100,200)
-    w = np.random.normal(1,1,200)
 
-    c_x,c_y,r = fitter.fit(x,y,w)
+    XX,YY = np.meshgrid(x,y)
+    ZZ = np.zeros_like(XX)
+    r =  np.sqrt((XX-50)**2+(YY-20)**2)
+    ZZ[(r>10) & (r<20)]=1
+
+    c_x,c_y,r = fitter.fit(XX,YY,ZZ)
     
-    assert(c_x!=0 and c_y!=0 and r!=0)
+    print(c_x,c_y,r)
+
+    assert(abs(c_x-50)<0.01 and abs(c_y-20)<0.01 and abs(r-15)<1)
