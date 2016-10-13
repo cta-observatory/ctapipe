a53c4183bc88ebc4276abb493d2a6e064305dd60
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2016-10-13 18:08:36 +0200

    Remove return status_codes, pep8

--- ctapipe/utils/json2fits.py
+++ ctapipe/utils/json2fits.py
@@ -7,8 +7,9 @@ logger = logging.getLogger(__name__)
 
 __all__ = ['traitlets_config_to_fits', 'json_to_fits']
 
+
 def traitlets_config_to_fits(config, fits_filename, clobber=True):
-    """Write a FITS file that represents configuration.
+    '''Write a FITS file that represents configuration.
 
     Parameters
     ----------
@@ -22,16 +23,16 @@ def traitlets_config_to_fits(config, fits_filename, clobber=True):
     Raises
     ------
     OSError : If FITS file containing the traitlets config is not written
-    """
+    '''
 
-    if not isinstance(config,Config):
-        logger.error("traitlets_config_to_fits: config must be an instance of traitlets.config.loader.Config")
-        return False
+    if not isinstance(config, Config):
+        raise TypeError('Config must be an instance of traitlets.config.loader.Config')
+
     # hduList will contain one TableHDU per section
     hduList = fits.HDUList()
     # get all Configuration entries
     # loop over section
-    for section,entry in config.items():
+    for section, entry in config.items():
         header = fits.Header()
         for key,value in entry.items():
             header[key] = value
@@ -44,11 +45,8 @@ def traitlets_config_to_fits(config, fits_filename, clobber=True):
         raise
 
 
-
-
-
 def json_to_fits(json_filename, fits_filename, clobber=True):
-    """Write a FITS file that represents json file for traitlets configuration.
+    '''Write a FITS file that represents json file for traitlets configuration.
 
     Parameters
     ----------
@@ -64,7 +62,7 @@ def json_to_fits(json_filename, fits_filename, clobber=True):
     ------
     OSError : if FITS file containing a copy of json content is not written
     FileNotFoundError : if fits_filename could not be open
-    """
+    '''
     try:
         f = open(json_filename, 'r')
         # hduList will contain one TableHDU per section
@@ -72,9 +70,9 @@ def json_to_fits(json_filename, fits_filename, clobber=True):
         json_object = load(f)
         # Create a global header for key/value that not depend of a section
         global_header = fits.Header()
-        #loop over json entries (corresponding to Python class or general purpose)
-        for section,entry in json_object.items():
-            #create a new header for this section
+
+        # loop over json entries (corresponding to Python class or general purpose)
+        for section, entry in json_object.items():
             header = fits.Header()
             if  isinstance(entry, dict):
                 #loop over key/value entries
@@ -87,13 +85,14 @@ def json_to_fits(json_filename, fits_filename, clobber=True):
                 # create a new TableHDU with current header and append it to hduList
                 table_0 = fits.TableHDU(data=None, header=header, name=section)
                 hduList.append(table_0)
+
             else:
                 # entry is not a dictionary of key value but a simple value.
                 # That means last section is not a section but a general purpose entry
                 global_header[section] = entry
 
         # create a new TableHDU for global_header and append it to hduList
-        table_0 = fits.TableHDU(data=None, header=global_header, name="GLOBAL")
+        table_0 = fits.TableHDU(data=None, header=global_header, name='GLOBAL')
         hduList.append(table_0)
         # write hduList to FITS file
         try:
@@ -101,7 +100,7 @@ def json_to_fits(json_filename, fits_filename, clobber=True):
         except OSError as e:
             logging.exception('Could not do save {}'.format(fits_filename))
             raise
-        return True
+
     except FileNotFoundError:
         logging.exception('Could not open  {}'.format(fits_filename))
         raise
