ebee47879e21256a7ea0176ed68ffba91951f1ab
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2016-10-17 15:44:16 +0200

    Do not override __init__ of component (This would break everything)

--- ctapipe/image/muon/intensity_fitter.py
+++ ctapipe/image/muon/intensity_fitter.py
@@ -1,12 +1,13 @@
 from ctapipe.core import Component
 
+
 class IntensityFitter(Component):
-    """This is the base class from which all muon intensity, impact parameter and ring width fitters should inherit from"""
+    """
+    This is the base class from which all muon intensity,
+    impact parameter and ring width fitters should inherit from
+    """
 
-    def __init__(self):
-        pass
-
-    def fit(self,x,y,charge,center_x, center_y, radius, times=None):
+    def fit(self, x, y, charge, center_x, center_y, radius, times=None):
         """overwrite this method with your favourite muon intensity fitting algorithm
 
         Parameters:
@@ -15,22 +16,21 @@ class IntensityFitter(Component):
            vector of pixel x-coordinates as astropy quantities
         y: array
            vector of pixel y-coordinates as astropy quantities
-        charge: 
+        charge:
            array of pixel charges as astropy quantities
-        center_x: 
+        center_x:
            previously fitted ring center position x as astropy quantity
-        center_y: 
+        center_y:
            previously fitted ring center position y as astropy quantity
-        radius: 
+        radius:
            previously fitted ring radius as astropy quantity
         times: array
            optional vector of pixel DAQ times as astropy quantities
 
         Returns:
         --------
-        tuple of impact_x, impact_y, muon size and efficiency (as astropy quantities) 
-
+        impact_x
+        impact_y
+        size
+        efficiency
         """
-
-        return None
-
