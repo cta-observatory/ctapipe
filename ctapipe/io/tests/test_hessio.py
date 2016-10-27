2c8a04b38599a97c687af41e375e2a383b7cf13a
Michele Mastropietro <michele.mastropietro@gmail.com>
2016-10-27 15:29:16 +0200

    Reorder imports and fix tabs in hessio files

--- ctapipe/io/tests/test_hessio.py
+++ ctapipe/io/tests/test_hessio.py
@@ -1,11 +1,12 @@
 from ctapipe.io.hessio import hessio_event_source
 from ctapipe.utils.datasets import get_datasets_path
 
+
 def test_get_run_id():
-	filename = get_datasets_path("gamma_test.simtel.gz")
-	print(filename)
-	gen = hessio_event_source(filename)
-	event = next(gen)
-	tels = event.dl0.tels_with_data
-	print(tels)
-	assert tels == {38, 47}
+    filename = get_datasets_path("gamma_test.simtel.gz")
+    print(filename)
+    gen = hessio_event_source(filename)
+    event = next(gen)
+    tels = event.dl0.tels_with_data
+    print(tels)
+    assert tels == {38, 47}
