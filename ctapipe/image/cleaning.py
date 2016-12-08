5c1508c53c8358f56071afaf7d16408079969740
Dan Parsons <parsonsrd@gmail.com>
2016-12-08 13:39:23 +0100

    Removed function included my mistake

--- ctapipe/image/cleaning.py
+++ ctapipe/image/cleaning.py
@@ -2,7 +2,7 @@
 Image Cleaning Algorithms (identification of noisy pixels)
 """
 
-__all__ = ['tailcuts_clean', 'clean_single_pixels', 'dilate']
+__all__ = ['tailcuts_clean', 'dilate']
 
 
 def tailcuts_clean(geom, image, pedvars, picture_thresh=4.25,
@@ -54,11 +54,6 @@ def tailcuts_clean(geom, image, pedvars, picture_thresh=4.25,
 
     clean_mask[boundary_ids] = True
     return clean_mask
-
-
-def clean_single_pixels(geom, mask):
-
-    mask = [pix_id for pix_id in geom.pix_id[mask] if mask[geom.neighbors[pix_id]].any()]
 
 
 def dilate(geom, mask):
