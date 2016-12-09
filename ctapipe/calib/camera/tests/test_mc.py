deb62dbb5e4c5610a857d876c3ac862327706171
Jason Watson <jason.jw@live.co.uk>
2016-12-09 13:41:46 +0100

    New mc.py
    The new mc.py is extremely simple, as the only operation unique to MC data is the conversion from r0 to dl0, as the data is obtained as r0 from hessio files. In the future this functionality will be called at IO, and dl0 will correctly contain the photoelectron samples that we will obtain from real data files.
    mc_r0_to_dl0_calibration simply subtracts the MC pedestal, and applies the MC dc_to_pe, resulting in a numpy array of photoelectron samples.

--- ctapipe/calib/camera/tests/test_mc.py
+++ ctapipe/calib/camera/tests/test_mc.py
@@ -1,17 +1,8 @@
-
+from numpy.testing import assert_almost_equal
 from ctapipe.io.hessio import hessio_event_source
 from ctapipe.utils.datasets import get_path
-
-from ..mc import set_integration_correction, calibrate_amplitude_mc, \
-    integration_mc, calibrate_mc
-
-
-def get_test_parameters():
-    parameters = {"integrator": "nb_peak_integration",
-                  "integration_window": [7, 3],
-                  "integration_sigamp": [2, 4],
-                  "integration_lwt": 0}
-    return parameters
+from ctapipe.calib.camera.mc import mc_r0_to_dl0_calibration
+from IPython import embed
 
 
 def get_test_event():
@@ -21,58 +12,8 @@ def get_test_event():
             return event
 
 
-def test_set_integration_correction():
+def test_mc_r0_to_dl0_calibration():
     telid = 11
     event = get_test_event()
-    int_corr = set_integration_correction(event, telid, get_test_parameters())
-    assert int_corr == float(round(1.0497408130033212, 7))
-
-
-def test_calibrate_amplitude_mc():
-    telid = 11
-    event = get_test_event()
-    params = get_test_parameters()
-    charge, window, dped, peakpos = integration_mc(event, telid, params)
-    pe = calibrate_amplitude_mc(event, charge, telid, get_test_parameters())
-    assert pe[0][0] == -1.891745344400406
-
-
-def test_integration_mc():
-    telid = 11
-    event = get_test_event()
-    params = get_test_parameters()
-    nsamples = event.inst.num_samples[telid]
-
-    params['integrator'] = 'full_integration'
-    charge, window, data_ped, peakpos = integration_mc(event, telid, params)
-    assert charge[0][0] == 149
-    assert sum(window[0][0]) == nsamples
-    assert data_ped[0][0][0] == -2.8340006510416629
-
-    params['integrator'] = 'simple_integration'
-    charge, window, data_ped, peakpos = integration_mc(event, telid, params)
-    assert charge[0][0] == 70
-    assert sum(window[0][0]) == params['integration_window'][0]
-
-    params['integrator'] = 'global_peak_integration'
-    charge, window, data_ped, peakpos = integration_mc(event, telid, params)
-    assert charge[0][0] == 61
-    assert sum(window[0][0]) == params['integration_window'][0]
-
-    params['integrator'] = 'local_peak_integration'
-    charge, window, data_ped, peakpos = integration_mc(event, telid, params)
-    assert charge[0][0] == 80
-    assert sum(window[0][0]) == params['integration_window'][0]
-
-    params['integrator'] = 'nb_peak_integration'
-    charge, window, data_ped, peakpos = integration_mc(event, telid, params)
-    assert charge[0][0] == -67
-    assert sum(window[0][0]) == params['integration_window'][0]
-
-
-def test_calibrate_mc():
-    telid = 11
-    event = get_test_event()
-    pe, window, data_ped, peakpos = calibrate_mc(event, telid,
-                                                 get_test_parameters())
-    assert round(pe[0], 5) == -1.89175
+    dl0 = mc_r0_to_dl0_calibration(event, telid)
+    assert_almost_equal(dl0[0, 0, 0].value, -0.087, 3)
