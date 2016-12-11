669f0ac95fc0ec30fdda4b2340d292f208b98213
Jason Watson <jason.jw@live.co.uk>
2016-12-11 19:20:35 +0100

    Moved factory.py from ctapipe/calib/camera to ctapipe/core

--- ctapipe/calib/camera/charge_extractors.py
+++ ctapipe/calib/camera/charge_extractors.py
@@ -1,9 +1,10 @@
 from abc import abstractmethod
+
 import numpy as np
 from traitlets import Int, CaselessStrEnum
 
+from core.factory import Factory
 from ctapipe.core import Component
-from ctapipe.calib.camera.factory import Factory
 
 
 class ChargeExtractor(Component):
