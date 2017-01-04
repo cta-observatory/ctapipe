f19a1474b3457151ac54ea8ffb5d72c660eaa35c
Michael Punch <michael.punch@lnu.se>
2017-01-04 10:52:23 +0100

    Added 2 hillas_parameters routines, based on old Whipple ForTran code, other corrections
    Removed returning of asymmetry, just left skewness...
        either MP/Whipple or MAGIC asymmetry can be calculated then in function
    
    Added two Boolean variables to the call: higherMoments (default True) and ReCalcPix (default True)
        If higherMoments is False, then it doesn't calculate them (if True, calculated for those routines have them)
        If reCalcPix is False, then the multiples of pixel positions are calculated always (if True, only if not existing)
    
    Hillas_parameter_1 doesn't work for the case of horizontal image
        It raises an exception wrongly; no reason to raise exception for S_xy = 0
    
    In hillas_parameter_1, it says delta/psi is angle with x-axis, but adds pi/2 (as does hillas_parameter_2).
        cos_delta and sin_delta are correct, though.
         => changed this in hillas_parameter_1, and in modified the display visualization (in visualization/mpl.py)... and "mock.py" in reco.
    
    Added higher moments to hillas_parameters_2, and added a sub-function to hold static variables
        of pixel multiplications, if pixel positions don't change
    
    Added Pythonified version of hillas_parameters_3, called hillal_parameters_4, also using
        a sub-function's attributes to hold "static variables" of pixel multiplications, if pixel positions don't change
    
    Added the historic 1993 Whipple calculation (translated Fortran code via c), as hillas_parameters_3
        Note, MAGIC's "skewness" is the same as the Whipple/MP "asymmetry^3", why ???
        ... and also, Whipple/MP "asymmetry" * "length" = MAGIC "asymmetry"
        ... so, MAGIC "asymmetry" = MAGIC "skewness"^(1/3) * "length"
        I don't know what MAGIC's "asymmetry" is supposed to be.
    
    MAGIC (hillas_parameters_1) seems to be using the Whipple 1989 Weekes et al. formula calculation!
      The Whipple 1993 Reynolds et al formula calculation is better (using non central or raw moments, not central moments).

--- ctapipe/reco/tests/test_hillas.py
+++ ctapipe/reco/tests/test_hillas.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
   disp.add_colorbar()
 
   # make a mock shower model
-  model = mock.generate_2d_shower_model(centroid=(0.2, 0.2), width=0.01, length=0.1, psi='45d')
+  model = mock.generate_2d_shower_model(centroid=(0.2, 0.2), width=0.01, length=0.1, psi='30d')
 
   # generate mock image in camera for this shower model.
   image, signal, noise = mock.make_mock_shower_image(geom, model.pdf, intensity=50, nsb_level_pe=100)
