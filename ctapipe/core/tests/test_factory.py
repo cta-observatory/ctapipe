31014d6d1e15199e68a0010747799d0a51f3e9c7
Karl Kosack <karl.kosack@cea.fr>
2017-01-12 17:59:33 +0100

    fix tests for factory and tool
    - Factory tests had classes with names staring with "Test" which get
    collected as pytest tests (even though they are not tests!). Renamed to
    "example*"
    - add test for version string in tool

--- ctapipe/core/tests/test_factory.py
+++ ctapipe/core/tests/test_factory.py
@@ -3,29 +3,29 @@ from ctapipe.core.component import Component
 from traitlets import Unicode, Int
 
 
-class TestComponentParent(Component):
-    name = 'TestComponentParent'
+class ExampleComponentParent(Component):
+    name = 'ExampleComponentParent'
     value = Int(123, help="").tag(config=True)
 
 
-class TestComponent1(TestComponentParent):
-    name = 'TestComponent1'
+class ExampleComponent1(ExampleComponentParent):
+    name = 'ExampleComponent1'
     value = Int(123111, help="").tag(config=True)
 
 
-class TestComponent2(TestComponentParent):
-    name = 'TestComponent2'
+class ExampleComponent2(ExampleComponentParent):
+    name = 'ExampleComponent2'
     value = Int(123222, help="").tag(config=True)
 
 
-class TestFactory(Factory):
-    name = 'TestFactory'
+class ExampleFactory(Factory):
+    name = 'ExampleFactory'
     description = "Test Factory class"
 
-    subclasses = Factory.child_subclasses(TestComponentParent)
+    subclasses = Factory.child_subclasses(ExampleComponentParent)
     subclass_names = [c.__name__ for c in subclasses]
 
-    discriminator = Unicode('TestComponent1',
+    discriminator = Unicode('ExampleComponent1',
                             help='Product to obtain: {}'
                             .format(subclass_names)).tag(config=True)
 
@@ -40,10 +40,10 @@ class TestFactory(Factory):
 
 
 def test_factory():
-    factory = TestFactory(config=None, tool=None)
-    factory.discriminator = 'TestComponent2'
+    factory = ExampleFactory(config=None, tool=None)
+    factory.discriminator = 'ExampleComponent2'
     factory.value = 111
     cls = factory.get_class()
     obj = cls(config=factory.config, parent=None)
-    assert(obj.name == 'TestComponent2')
+    assert(obj.name == 'ExampleComponent2')
     assert(obj.value == 111)
