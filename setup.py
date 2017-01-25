ba36db77a13073dee7fcb2276d6d42a3eadf38ad
Karl Kosack <karl.kosack@cea.fr>
2017-01-25 17:35:16 +0100

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ctapipe_resources',
-    version='0.1.0',
+    version='0.1.1',
     packages=['ctapipe_resources', ],
     package_data={'ctapipe_resources': '*'},
 )
