355266a1953a67ace16c71ddcb3f740fa24c1e4a
Karl Kosack <karl.kosack@cea.fr>
2017-03-07 10:23:01 +0100

    cleanups to proveance class and core init

--- ctapipe/core/provenance.py
+++ ctapipe/core/provenance.py
@@ -5,39 +5,73 @@ TODO: have this register whenever ctapipe is loaded
 
 """
 
-import sys
-from astropy.time import Time
-import json
 import platform
-import psutil
-import numpy as np
-import ctapipe
+import sys
 
+import ctapipe
+import numpy as np
+import psutil
+from astropy.time import Time
+
+__all__ = ['Provenance']
 
 class Provenance:
     """
-    Collect provenance information for later storage.
+    Collect local provenance information for later storage.
+
+
+    This class is used accumulate information on system execution
+    environment, inputs, and outputs for a given Tool or script.
     """
 
     def __init__(self):
-        self._prov = {}
+        self._prov = {'STARTUP': {},
+                      'SHUTDOWN': {},
+                      'SYSTEM': {},
+                      'INPUT': [],
+                      'OUTPUT': []}
 
     def start(self):
         """ begin recording provenance. Set's up the system and startup
         provenance data. Generally should be called at start of a program."""
-        self._prov['START'] = self._sample_provenance()
-        self._prov['SYSTEM'] = self._get_system_provenance()
+        self._prov['STARTUP'].update(self._sample_cpu_and_memory())
+        self._prov['SYSTEM'].update(self._get_system_provenance())
+
+    def register_input(self, url):
+        """
+        Add a URL of a file to the list of inputs (can be a filename or full
+        url, if no URL specifier is given, assume 'file://')
+
+        Parameters
+        ----------
+        url: str
+            filename or url of input file
+        """
+        self._prov['INPUT'].append(url)
+
+    def register_output(self, url):
+        """
+        Add a URL of a file to the list of outputs (can be a filename or full
+        url, if no URL specifier is given, assume 'file://')
+
+        Parameters
+        ----------
+        url: str
+            filename or url of output file
+        """
+        self._prov['OUTPUT'].append(url)
 
     def finish(self):
         """ record final provenance information, normally called at shutdown."""
-        self._prov['END'] = self._sample_provenance()
+        self._prov['SHUTDOWN'] = self._sample_cpu_and_memory()
 
     def sample(self):
-        """take a sample of provenance information, including current cpu
-        time and other stats."""
+        """
+        Record a snapshot of current CPU and memory information.
+        """
         if 'SAMPLES' not in self._prov:
             self._prov['SAMPLES'] = []
-        self._prov['SAMPLES'].append(self._sample_provenance())
+        self._prov['SAMPLES'].append(self._sample_cpu_and_memory())
 
     @property
     def provenance(self):
@@ -69,7 +103,7 @@ class Provenance:
             ),
             python=dict(
                 version_string=sys.version,
-                version = [
+                version=[
                     pyver.major,
                     pyver.minor,
                     pyver.micro,
@@ -79,27 +113,20 @@ class Provenance:
             start_time_utc=Time.now().isot,
         )
 
-
-
-    def _sample_provenance(self):
+    def _sample_cpu_and_memory(self):
         times = np.asarray(psutil.cpu_times(percpu=True))
         mem = psutil.virtual_memory()
 
         return dict(
-            time_utc = Time.now().utc,
-            memory = dict(total=mem.total,
-                          inactive=mem.inactive,
-                          available=mem.available,
-                          free=mem.free,
-                          wired=mem.wired),
-            cpu = dict(ncpu=psutil.cpu_count(),
-                           user=list(times[:, 0]),
-                           nice=list(times[:, 1]),
-                           system=list(times[:, 2]),
-                           idle=list(times[:, 3])),
+            time_utc=Time.now().utc.isot,
+            memory=dict(total=mem.total,
+                        inactive=mem.inactive,
+                        available=mem.available,
+                        free=mem.free,
+                        wired=mem.wired),
+            cpu=dict(ncpu=psutil.cpu_count(),
+                     user=list(times[:, 0]),
+                     nice=list(times[:, 1]),
+                     system=list(times[:, 2]),
+                     idle=list(times[:, 3])),
         )
-
-
-
-
-
--- ctapipe/core/tests/test_provenance.py
+++ ctapipe/core/tests/test_provenance.py
@@ -1,26 +1,31 @@
-from ctapipe.core import provenance
-from pprint import pprint
+import json
+
+from ctapipe.core import Provenance
+
 
 def test_prov():
-
-    prov = provenance.Provenance()
+    prov = Provenance()
     prov.start()
+    prov.register_input("input.txt")
+    prov.register_output("output.txt")
     prov.finish()
-
 
 
 if __name__ == '__main__':
 
     import time
 
-    prov = provenance.Provenance()
+    prov = Provenance()
     prov.start()
+    prov.register_input('test.txt')
+    prov.register_input('test2.txt')
+    prov.register_output('out.txt')
 
     print("please wait...")
-    for ii in range(10):
+    for ii in range(3):
+        print("sample", ii)
         time.sleep(1)
         prov.sample()
 
-
     prov.finish()
-    pprint(prov.provenance)
+    print(json.dumps(prov.provenance, indent=4))
