98cf0a1c7fd47c29637e0b193bb85de58d05dbdb
Karl Kosack <karl.kosack@cea.fr>
2017-04-07 16:20:40 +0200

    clean up example

--- docs/instrument/camerageometry_example.py
+++ docs/instrument/camerageometry_example.py
@@ -1,9 +1,10 @@
 from ctapipe.instrument import CameraGeometry
 from matplotlib import pyplot as plt
 
+geom = CameraGeometry.from_name("HESS", 1)
+
 plt.figure(figsize=(8, 3))
 plt.subplot(1, 2, 1)
-geom = CameraGeometry.from_name("HESS", 1)
 plt.imshow(geom.neighbor_matrix)
 plt.title('Pixel Neighbor Matrix')
 
