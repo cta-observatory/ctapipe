02d957e344a8e65b9da3b304c56202200c006a52
Dan Parsons <parsonsrd@gmail.com>
2017-04-10 15:38:43 +0200

    modified for use with arbitrary numpy axis

--- ctapipe/plotting/array.py
+++ ctapipe/plotting/array.py
@@ -9,7 +9,7 @@ class ArrayPlotter:
     Simple plotter for drawing array level items
     """
 
-    def __init__(self, instrument, telescopes=None, system=None):
+    def __init__(self, instrument, telescopes=None, system=None, ax=None):
         """
 
         Parameters
@@ -37,6 +37,8 @@ class ArrayPlotter:
         tel_y = [self.instrument.tel_pos[i][1].to(u.m).value for i in self.telescopes]
         tel_z = [self.instrument.tel_pos[i][2].to(u.m).value for i in self.telescopes]
 
+        self.axes = ax if ax is not None else plt.gca()
+
         tel_type = np.asarray([type_dict[self.instrument.optical_foclen[i].to(u.m).value] for i in self.telescopes])
         self.tel_type = tel_type
 
@@ -50,7 +52,8 @@ class ArrayPlotter:
             self.tel_y = tel_y*u.m
 
         self.centre = (0,0)
-        self.array = ArrayDisplay(telx=np.asarray(self.tel_x), tely=np.asarray(self.tel_y), tel_type=tel_type)
+        self.array = ArrayDisplay(telx=np.asarray(self.tel_x), tely=np.asarray(self.tel_y), tel_type=tel_type,
+                                  axes=self.axes)
 
         self.hillas = None
 
@@ -106,7 +109,7 @@ class ArrayPlotter:
         None
         """
 
-        plt.contour(x, y, background, **kwargs)
+        self.axes.contour(x, y, background, **kwargs)
 
         # Annoyingly we need to redraw everything
         self.array = ArrayDisplay(telx=np.asarray(self.tel_x), tely=np.asarray(self.tel_y), tel_type=self.tel_type)
@@ -131,8 +134,8 @@ class ArrayPlotter:
         self.array.axes.set_xlim((self.centre[0]+range[0][0], range[0][1]+self.centre[0]))
         self.array.axes.set_ylim((self.centre[1]+range[1][0], range[1][1]+self.centre[1]))
 
-        plt.tight_layout()
-        plt.show()
+        #self.axes.tight_layout()
+        #self.axes.show()
 
     def draw_position(self, core_x, core_y, use_centre=False, **kwargs):
         """
@@ -168,7 +171,7 @@ class NominalPlotter:
     Simple plotter for drawing camera level items in the nominal system
     """
 
-    def __init__(self, hillas_parameters, draw_axes=False, **kwargs):
+    def __init__(self, hillas_parameters, draw_axes=False, ax=None, **kwargs):
         """
 
         Parameters
@@ -180,16 +183,18 @@ class NominalPlotter:
         system: Coordinate system
             Coordinate system to transform coordinates into
         """
+        self.axes = ax if ax is not None else plt.gca()
 
         self.cen_x = [i.cen_x.to(u.deg).value for i in hillas_parameters.values()]
         self.cen_y = [i.cen_y.to(u.deg).value for i in hillas_parameters.values()]
 
         self.centre = (0,0)
         self.array = ArrayDisplay(telx=np.asarray(self.cen_x), tely=np.asarray(self.cen_y),
-                                  tel_type=np.ones(len(self.cen_y)))
+                                  tel_type=np.ones(len(self.cen_y)), axes=self.axes)
 
         self.hillas = hillas_parameters
-        scale_fac = 10
+        scale_fac = 57.3 * 5
+        print(hillas_parameters)
         self.array.overlay_moments(hillas_parameters, (self.cen_x, self.cen_y), scale_fac,
                                    cmap="viridis", alpha=0.5, **kwargs)
 
@@ -216,7 +221,7 @@ class NominalPlotter:
         None
         """
 
-        plt.contour(x, y, background, **kwargs)
+        self.axes.contour(x, y, background, **kwargs)
 
         # Annoyingly we need to redraw everything
         self.array = ArrayDisplay(telx=np.asarray(self.tel_x), tely=np.asarray(self.tel_y))
@@ -238,9 +243,8 @@ class NominalPlotter:
         self.array.axes.set_xlim((self.centre[0]+range[0][0], range[0][1]+self.centre[0]))
         self.array.axes.set_ylim((self.centre[1]+range[1][0], range[1][1]+self.centre[1]))
 
-        plt.tight_layout()
-        plt.show()
-
+        #self.axes.tight_layout()
+        #self.axes.show()
 
     def draw_position(self, source_x, source_y, use_centre=False, **kwargs):
         """
@@ -262,6 +266,6 @@ class NominalPlotter:
         -------
         None
         """
-        self.array.add_polygon(centroid=(source_x.to(u.deg).value,source_y.to(u.deg).value), radius=10, nsides=3, **kwargs)
+        self.array.add_polygon(centroid=(source_x.to(u.deg).value,source_y.to(u.deg).value), radius=0.1, nsides=3, **kwargs)
         if use_centre:
             self.centre = (source_x.value,source_y.value)
