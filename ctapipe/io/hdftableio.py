33f8b494daafa214a778a676627a161e58dded74
Karl Kosack <karl.kosack@cea.fr>
2017-04-14 13:58:16 +0200

    refactor and add column exclusions
    - separate out common functionality into a base class, anticipating
    more implementations
    - add ability to exclude columns from being written using a regexp

--- ctapipe/io/hdftableio.py
+++ ctapipe/io/hdftableio.py
@@ -1,4 +1,6 @@
 import logging
+import re
+from abc import abstractmethod, ABCMeta
 from collections import defaultdict
 from functools import partial
 
@@ -6,6 +8,10 @@ import ctapipe
 import numpy as np
 import tables
 from astropy.units import Quantity
+
+__all__ = ['SimpleHDF5TableWriter',
+           'tr_convert_and_strip_unit',
+           'tr_list_to_mask']
 
 log = logging.getLogger(__name__)
 
@@ -18,7 +24,68 @@ PYTABLES_TYPE_MAP = {
 }
 
 
-class SimpleHDF5TableWriter:
+class TableWriter(metaclass=ABCMeta):
+    def __init__(self):
+        self._transforms = defaultdict(dict)
+        self._exclusions = defaultdict(list)
+
+    def exclude(self, table_name, pattern):
+        """
+        Exclude any columns matching the pattern from being written
+        
+        Parameters
+        ----------
+        table_name: str 
+            name of table on which to apply the exclusion
+        pattern: str
+            regular expression string to match column name
+        """
+        self._exclusions[table_name].append(re.compile(pattern))
+
+    def _is_column_excluded(self, table_name, col_name):
+        for pattern in self._exclusions[table_name]:
+            if pattern.match(col_name):
+                return True
+        return False
+
+    def add_column_transform(self, table_name, col_name, transform):
+        """
+        Add a transformation function for a column. This function will be 
+        called on the value in the container before it is written to the 
+        output file. 
+        
+        Parameters
+        ----------
+        table_name: str
+            identifier of table being written
+        col_name: str
+            name of column in the table (or item in the Container)
+        transform: callable
+            function that take a value and returns a new one 
+        """
+        self._transforms[table_name][col_name] = transform
+        log.debug("Added transform: {}/{} -> {}".format(table_name, col_name,
+                                                        transform))
+
+    @abstractmethod
+    def write(self, table_name, container):
+        """
+        Write the contents of the given container to a table.  The first call 
+        to write  will create a schema and initialize the table within the 
+        file. The shape of data within the container must not change between 
+        calls, since variable-length arrays are not supported. 
+    
+        Parameters
+        ----------
+        table_name: str 
+            name of table to write to 
+        container: `ctapipe.core.Container` 
+            container to write
+        """
+        pass
+
+
+class SimpleHDF5TableWriter(TableWriter):
     """
     A very basic table writer that can take a container (or more than one) 
     and write it to an HDF5 file. It does _not_ recursively write the 
@@ -53,16 +120,16 @@ class SimpleHDF5TableWriter:
     """
 
     def __init__(self, filename, group_name):
+        super().__init__()
         self._schemas = {}
         self._tables = {}
-        self._transforms = defaultdict(dict)
         self._h5file = tables.open_file(filename, mode="w")
         self._group = self._h5file.create_group("/", group_name)
 
     def __del__(self):
         self._h5file.close()
 
-    def _create_schema(self, table_name, container):
+    def _create_hdf5_table_schema(self, table_name, container):
         """
         Creates a pytables description class for a container 
         and registers it in the Writer
@@ -90,6 +157,10 @@ class SimpleHDF5TableWriter:
             typename = ""
             shape = 1
 
+            if self._is_column_excluded(table_name, col_name):
+                log.debug("excluded column: {}/{}".format(table_name,col_name))
+                continue
+
             if isinstance(value, Quantity):
                 req_unit = container.attributes[col_name].unit
                 if req_unit is not None:
@@ -100,7 +171,7 @@ class SimpleHDF5TableWriter:
                     meta['{}_UNIT'.format(col_name)] = str(value.unit)
 
                 value = tr(value)
-                self.add_transform(table_name, col_name, tr)
+                self.add_column_transform(table_name, col_name, tr)
 
             if isinstance(value, np.ndarray):
                 typename = value.dtype.name
@@ -120,34 +191,44 @@ class SimpleHDF5TableWriter:
         meta['CTAPIPE_VERSION'] = ctapipe.__version__
         return meta
 
-    def add_transform(self, table_name, col_name, transform):
-        """
-        Add a transformation function for a column. This function will be 
-        called on the value in the container before it is written to the 
-        output file. 
-        
-        Parameters
-        ----------
-        table_name: str
-            identifier of table being written
-        col_name: str
-            name of column in the table (or item in the Container)
-        transform: function(value)
-            function that converts value 
-        """
-        self._transforms[table_name][col_name] = transform
-        log.debug("Added transform: {}/{} -> {}".format(table_name, col_name,
-                                                        transform))
 
-    def _create_table(self, table_name, title, metadata):
+    def _setup_new_table(self, table_name, container):
+        """ set up the table. This is called the first time `write()` 
+        is called on a new table """
+        log.debug("Initializing table '{}'".format(table_name))
+        meta = self._create_hdf5_table_schema(table_name, container)
+        meta.update(container.meta) # copy metadata from container
+
         table = self._h5file.create_table(where=self._group,
                                           name=table_name,
-                                          title=title,
+                                          title=container.__class__.__name__,
                                           description=self._schemas[table_name])
-        for key, val in metadata.items():
+        for key, val in meta.items():
             table.attrs[key] = val
 
         self._tables[table_name] = table
+
+
+    def _append_row(self, table_name, container):
+        """
+        append a row to an already initialized table. This is called 
+        automatically by `write()`
+        """
+        table = self._tables[table_name]
+        row = table.row
+
+        for colname in table.colnames:
+
+            value = container[colname]
+
+            # apply value transform function if it exists for this column
+            if colname in self._transforms[table_name]:
+                tr = self._transforms[table_name][colname]
+                value = tr(value)
+
+            row[colname] = value
+
+        row.append()
 
     def write(self, table_name, container):
         """
@@ -165,30 +246,9 @@ class SimpleHDF5TableWriter:
         """
 
         if table_name not in self._schemas:
-            log.debug("Initializing table '{}'".format(table_name))
-            meta = self._create_schema(table_name, container)
-            meta.update(container.meta)
-            self._create_table(table_name,
-                               container.__class__.__name__,
-                               meta)
+            self._setup_new_table(table_name, container)
 
-        # append a row to the table
-        table = self._tables[table_name]
-        row = table.row
-
-        for colname in table.colnames:
-
-            value = container[colname]
-
-            # apply value transform function if it exists for this column
-            if colname in self._transforms[table_name]:
-                tr = self._transforms[table_name][colname]
-                value = tr(value)
-
-            row[colname] = value
-
-        row.append()
-
+        self._append_row(table_name, container)
 
 def tr_convert_and_strip_unit(quantity, unit):
     return quantity.to(unit).value
--- ctapipe/io/tests/test_hdf5.py
+++ ctapipe/io/tests/test_hdf5.py
@@ -8,16 +8,17 @@ def test_write_container():
     r0tel = R0CameraContainer()
     mc = MCEventContainer()
     mc.reset()
-    r0tel.adc_samples = np.random.uniform(size=(50,10))
+    r0tel.adc_samples = np.random.uniform(size=(50, 10))
     r0tel.adc_sums = np.random.uniform(size=50)
     r0tel.num_samples = 10
     r0tel.meta['test_attribute'] = 3.14159
     r0tel.meta['date'] = "2020-10-10"
 
     writer = SimpleHDF5TableWriter('test.h5', group_name='R0')
+    writer.exclude("tel_002",".*samples")  # test exclusion of columns
 
     for ii in range(100):
-        r0tel.adc_samples[:] = np.random.uniform(size=(50,10))
+        r0tel.adc_samples[:] = np.random.uniform(size=(50, 10))
         r0tel.adc_sums[:] = np.random.uniform(size=50)
         r0tel.num_samples = 10
         mc.energy = 10**np.random.uniform(1,2) * u.TeV
