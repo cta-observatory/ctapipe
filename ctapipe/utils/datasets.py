0f2e50ee856d5e5d4af55f8af90ffbaf64344d0c
Karl Kosack <karl.kosack@cea.fr>
2017-05-02 14:21:12 +0200

    minor improvement to documentation for datasets module

--- ctapipe/utils/datasets.py
+++ ctapipe/utils/datasets.py
@@ -6,14 +6,27 @@ from astropy.utils.decorators import deprecated
 try:
     import ctapipe_resources
 except:
-    raise RuntimeError("Please install the ctapipe-extra package, "
-                       "which contains the ctapipe_resources module needed by ctapipe")
+    raise RuntimeError("Please install the 'ctapipe-extra' package, "
+                       "which contains the ctapipe_resources module "
+                       "needed by ctapipe. (conda install ctapipe-extra)")
 
 
 __all__ = ['get_dataset'  ]
 
 
 def get_dataset(filename):
+    """
+    Returns the full file path to an auxiliary dataset needed by ctapipe 
+    
+    Parameters
+    ----------
+    filename: str
+        name of dataset to fetch
+
+    Returns
+    -------
+    string with full path to the given dataset
+    """
     return ctapipe_resources.get(filename)
 
 @deprecated("ctapipe-0.4.1",alternative='get_dataset()')
