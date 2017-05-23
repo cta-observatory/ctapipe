0c939c63e09f4cc1c4a10e56b2a48f58b3c5a37d
Karl Kosack <karl.kosack@cea.fr>
2017-05-23 15:20:54 +0200

    update example

--- examples/plot_all_cameras.py
+++ examples/plot_all_cameras.py
@@ -1,25 +1,26 @@
 from ctapipe.instrument import CameraGeometry
 from ctapipe.visualization import CameraDisplay
 from matplotlib import pyplot as plt
-
-
+import numpy as np
 
 
 if __name__ == '__main__':
 
     plt.style.use("ggplot")
-    
-    N=2
-    M=4
-    plt.figure(figsize=(10, 6))
+
+    N = 4
+    M = 5
+    plt.figure(figsize=(10, 10))
 
     camera_names = CameraGeometry.get_known_camera_names()
 
-    for ii, name in enumerate(camera_names):
+    for ii, name in enumerate(sorted(camera_names)):
+        print("plotting", name)
         geom = CameraGeometry.from_name(name)
-        plt.subplot(N,M, ii+1)
+        ax = plt.subplot(N, M, ii + 1)
         disp = CameraDisplay(geom)
-        
+        disp.image = np.random.uniform(size=geom.pix_id.shape)
+
 
     plt.tight_layout()
     plt.show()
