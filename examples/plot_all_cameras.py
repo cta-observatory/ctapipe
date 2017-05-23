99c3b9c4a4d5eaa53f1009d6a6b5cabfc881301b
Karl Kosack <karl.kosack@cea.fr>
2017-05-23 16:16:32 +0200

    update example plot

--- examples/plot_all_cameras.py
+++ examples/plot_all_cameras.py
@@ -6,11 +6,11 @@ import numpy as np
 
 if __name__ == '__main__':
 
-    plt.style.use("ggplot")
+    plt.style.use("bmh")
 
-    N = 4
-    M = 5
-    plt.figure(figsize=(10, 10))
+    N = 2
+    M = 7
+    plt.figure(figsize=(15, 6))
 
     camera_names = CameraGeometry.get_known_camera_names()
 
@@ -20,7 +20,9 @@ if __name__ == '__main__':
         ax = plt.subplot(N, M, ii + 1)
         disp = CameraDisplay(geom)
         disp.image = np.random.uniform(size=geom.pix_id.shape)
-
+        disp.cmap = 'viridis'
+        plt.xlabel("")
+        plt.ylabel("")
 
     plt.tight_layout()
     plt.show()
