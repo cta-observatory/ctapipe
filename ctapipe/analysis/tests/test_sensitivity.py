8babd55dfa3da09bb66cc731d1876ed8e9f00345
Karl Kosack <karl.kosack@cea.fr>
2017-06-27 17:14:59 +0200

    autopep8 to remove style warnings

--- ctapipe/analysis/tests/test_sensitivity.py
+++ ctapipe/analysis/tests/test_sensitivity.py
@@ -32,8 +32,8 @@ def test_check_background_contamination(n_1=2, n_2=2):
 def test_SensitivityPointSource_effective_area():
 
     # areas used in the event generator
-    gen_area_g = np.pi*(1000*u.m)**2
-    gen_area_p = np.pi*(2000*u.m)**2
+    gen_area_g = np.pi * (1000 * u.m)**2
+    gen_area_p = np.pi * (2000 * u.m)**2
 
     # energy list of "selected" events
     energy_sel_gammas = np.logspace(2, 6, 200, False) * u.TeV
@@ -58,21 +58,21 @@ def test_SensitivityPointSource_effective_area():
 
     # constructer gets fed with energy lists and desired energy binning
     sens = SensitivityPointSource(
-                    mc_energies={'g': energy_sel_gammas,
-                                 'p': energy_sel_proton,
-                                 'e': energy_sel_electr},
-                    energy_bin_edges={'g': energy_edges,
-                                      'p': energy_edges,
-                                      'e': energy_edges})
+        mc_energies={'g': energy_sel_gammas,
+                     'p': energy_sel_proton,
+                     'e': energy_sel_electr},
+        energy_bin_edges={'g': energy_edges,
+                          'p': energy_edges,
+                          'e': energy_edges})
 
     # effective areas
     sens.get_effective_areas(
-                    generator_energy_hists={'g': energy_sim_hist_gamma,
-                                            'p': energy_sim_hist_proton,
-                                            'e': energy_sim_hist_elect},
-                    generator_areas={'g': gen_area_g,
-                                     'p': gen_area_p,
-                                     'e': gen_area_g})
+        generator_energy_hists={'g': energy_sim_hist_gamma,
+                                'p': energy_sim_hist_proton,
+                                'e': energy_sim_hist_elect},
+        generator_areas={'g': gen_area_g,
+                         'p': gen_area_p,
+                         'e': gen_area_g})
 
     # midway result are the effective areas
     eff_a = sens.effective_areas
@@ -80,8 +80,8 @@ def test_SensitivityPointSource_effective_area():
 
     # "selected" events are half of the "generated" events
     # so effective areas should be half of generator areas, too
-    np.testing.assert_allclose(eff_area_g.value, gen_area_g.value/2)
-    np.testing.assert_allclose(eff_area_p.value, gen_area_p.value/2)
+    np.testing.assert_allclose(eff_area_g.value, gen_area_g.value / 2)
+    np.testing.assert_allclose(eff_area_p.value, gen_area_p.value / 2)
 
 
 def test_SensitivityPointSource_sensitivity_MC():
@@ -89,12 +89,13 @@ def test_SensitivityPointSource_sensitivity_MC():
     alpha = 1.
     n_sig = 20
     n_bgr = 10
-    energy_bin_edges = np.array([.1, 10])*u.TeV  # one bin with the bin-centre at 1 TeV
+    # one bin with the bin-centre at 1 TeV
+    energy_bin_edges = np.array([.1, 10]) * u.TeV
     energies_sig = np.ones(n_sig)
     energies_bgr = np.ones(n_bgr)
 
-    sens = SensitivityPointSource(reco_energies={'s': energies_sig*u.TeV,
-                                                 'b': energies_bgr*u.TeV})
+    sens = SensitivityPointSource(reco_energies={'s': energies_sig * u.TeV,
+                                                 'b': energies_bgr * u.TeV})
 
     # sens.event_weights = {'s': energies_sig, 'b': energies_bgr}
     sensitivity = sens.get_sensitivity(alpha=alpha, signal_list=("s"), mode="MC",
@@ -105,8 +106,9 @@ def test_SensitivityPointSource_sensitivity_MC():
     # the ratio between sensitivity and reference flux (i.e. from the crab nebula) should
     # be the scaling factor that needs to be applied to n_sig to produce a 5 sigma result
     # in the lima formula
-    ratio = sensitivity["Sensitivity"][0]/(crab_source_rate(1*u.TeV)).value
-    np.testing.assert_allclose([sigma_lima(ratio*n_sig+alpha*n_bgr, n_bgr, alpha)], [5])
+    ratio = sensitivity["Sensitivity"][0] / (crab_source_rate(1 * u.TeV)).value
+    np.testing.assert_allclose(
+        [sigma_lima(ratio * n_sig + alpha * n_bgr, n_bgr, alpha)], [5])
 
 
 def test_SensitivityPointSource_sensitivity_data():
@@ -114,12 +116,13 @@ def test_SensitivityPointSource_sensitivity_data():
     alpha = 1.
     n_on = 30
     n_off = 10
-    energy_bin_edges = np.array([.1, 10])*u.TeV  # one bin with the bin-centre at 1 TeV
+    # one bin with the bin-centre at 1 TeV
+    energy_bin_edges = np.array([.1, 10]) * u.TeV
     energies_sig = np.ones(n_on)
     energies_bgr = np.ones(n_off)
 
-    sens = SensitivityPointSource(reco_energies={'s': energies_sig*u.TeV,
-                                                 'b': energies_bgr*u.TeV})
+    sens = SensitivityPointSource(reco_energies={'s': energies_sig * u.TeV,
+                                                 'b': energies_bgr * u.TeV})
 
     sensitivity = sens.get_sensitivity(alpha=alpha, signal_list=("s"), mode="data",
                                        sensitivity_source_flux=crab_source_rate,
@@ -129,8 +132,8 @@ def test_SensitivityPointSource_sensitivity_data():
     # the ratio between sensitivity and reference flux (i.e. from the crab nebula) should
     # be the scaling factor that needs to be applied to n_sig=n_on-n_off*alpha to produce
     # a 5 sigma result  in the lima formula
-    ratio = sensitivity["Sensitivity"][0]/(crab_source_rate(1*u.TeV)).value
-    np.testing.assert_allclose([sigma_lima(ratio*(n_on-n_off*alpha)+n_off*alpha,
+    ratio = sensitivity["Sensitivity"][0] / (crab_source_rate(1 * u.TeV)).value
+    np.testing.assert_allclose([sigma_lima(ratio * (n_on - n_off * alpha) + n_off * alpha,
                                            n_off, alpha)], [5])
 
 
@@ -190,9 +193,9 @@ def test_draw_events_with_flux_weight():
     energy_weights /= np.sum(energy_weights)
 
     indices = SensitivityPointSource.draw_events_from_flux_weight(
-                        {'g': energy},
-                        {'g': energy_weights},
-                        {'g': n_draws})
+        {'g': energy},
+        {'g': energy_weights},
+        {'g': n_draws})
 
     assert len(energy[indices['g']]) == n_draws
     # TODO come up with more tests...
@@ -207,21 +210,21 @@ def test_draw_events_from_flux_histogram():
     n_bins = 30
     n_draws = 1000
 
-    energy_edges = np.linspace(e_min, e_max, n_bins+1, True) * u.TeV
+    energy_edges = np.linspace(e_min, e_max, n_bins + 1, True) * u.TeV
     energy = np.random.uniform(e_min, e_max, 50000) * u.TeV
 
-    target_distribution = make_mock_event_rate(lambda e: (e/u.TeV)**-3,
+    target_distribution = make_mock_event_rate(lambda e: (e / u.TeV)**-3,
                                                energy_edges,
                                                norm=n_draws, log_e=False)
 
     indices = SensitivityPointSource.draw_events_from_flux_histogram(
-                        {'g': energy},
-                        {'g': target_distribution},
-                        {'g': energy_edges})
+        {'g': energy},
+        {'g': target_distribution},
+        {'g': energy_edges})
 
     hist, _ = np.histogram(energy[indices['g']], bins=energy_edges[::])
 
     # checking the χ² between `target_distribution` and the drawn one
     chisquare = scipy.stats.chisquare(target_distribution, hist)[0]
     # the test that the reduced χ² is close to 1 (tollorance of 1)
-    np.testing.assert_allclose([chisquare/n_bins], [1], atol=0.1)
+    np.testing.assert_allclose([chisquare / n_bins], [1], atol=0.1)
