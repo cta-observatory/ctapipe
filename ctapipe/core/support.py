4530dbbfddaaef1091a780e8e85a2fe5e014ba5c
Karl Kosack <karl.kosack@cea.fr>
2017-06-27 17:32:58 +0200

    fix a bunch of flake8 warnings

--- ctapipe/core/support.py
+++ ctapipe/core/support.py
@@ -1,8 +1,8 @@
 class Singleton(type):
     """" metaclass for singleton pattern """
     instance = None
+
     def __call__(cls, *args, **kw):
         if not cls.instance:
-             cls.instance = super(Singleton, cls).__call__(*args, **kw)
+            cls.instance = super(Singleton, cls).__call__(*args, **kw)
         return cls.instance
-
