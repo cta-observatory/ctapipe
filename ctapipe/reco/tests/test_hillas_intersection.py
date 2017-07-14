b0584eef99f61f971863b601e714d600cba4d295
Dan Parsons <parsonsrd@gmail.com>
2017-07-15 08:38:13 +0900

    Fixed import problem in hillas intersection test

--- ctapipe/reco/tests/test_hillas_intersection.py
+++ ctapipe/reco/tests/test_hillas_intersection.py
@@ -1,4 +1,4 @@
-from ctapipe.reco.hillas_intersection import intersect_lines
+from ctapipe.reco.hillas_intersection import HillasIntersection
 import astropy.units as u
 from numpy.testing import assert_allclose
 import numpy as np
@@ -8,6 +8,7 @@ def test_intersect():
     Simple test to check the intersection of lines. Try to intersect positions at (0,1) and (1,0)
     with angles perpendicular and test they cross at (0,0)
     """
+    hill = HillasIntersection()
     x1 = 0
     y1 = 1
     theta1 = 90*u.deg
@@ -16,7 +17,7 @@ def test_intersect():
     y2 = 0
     theta2 = 0*u.deg
 
-    sx,sy = intersect_lines(x1,y1,theta1,x2,y2,theta2)
+    sx,sy = hill.intersect_lines(x1,y1,theta1,x2,y2,theta2)
 
     assert_allclose(sx, 0, atol=1e-6)
     assert_allclose(sy, 0, atol=1e-6)
@@ -26,6 +27,7 @@ def test_parallel():
     Simple test to check the intersection of lines. Try to intersect positions at (0,0) and (0,1)
     with angles parallel and check the behaviour
     """
+    hill = HillasIntersection()
     x1 = 0
     y1 = 0
     theta1 = 0*u.deg
@@ -34,7 +36,7 @@ def test_parallel():
     y2 = 0
     theta2 = 0*u.deg
 
-    sx,sy = intersect_lines(x1,y1,theta1,x2,y2,theta2)
+    sx,sy = hill.intersect_lines(x1,y1,theta1,x2,y2,theta2)
     assert_allclose(sx, np.nan, atol=1e-6)
     assert_allclose(sy, np.nan, atol=1e-6)
 
