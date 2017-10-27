3d216acc2c09775055a27a91b71985876741242f
Johan Bregeon <johan.bregeon@gmail.com>
2017-10-27 11:53:56 +0200

    pylint and pep8

--- ctapipe/atmosphere/atmo_prof.py
+++ ctapipe/atmosphere/atmo_prof.py
@@ -6,27 +6,43 @@ Read an atmospheric profile (Density, P, T...)
 @author: Johan Bregeon
 """
 
-ATMOPROF_DICT={'Armazones':26, 'La Palma':36}
+import logging
+logger = logging.getLogger(__name__)
 
-def read_atmo_prof(fpath):
-    ''' Read atmospheric profile table
-    '''
-    content=open(fpath).readlines()
-    
-    altitude=[]
-    rho=[]
-    thick=[]
-    index=[]
-    temperature=[]
-    pressure=[]
-    pw_p=[]
+
+ATMOPROF_DICT = {'Armazones': 26, 'La Palma': 36}
+
+
+def read_atmo_prof(filepath):
+    """ Read atmospheric profile table
+
+    Parameters
+    ----------
+    fpath : filepath
+
+    Returns
+    ----------
+    7 lists corresponding to:
+    altitude, rho, thick, index, temperature, pressure, pw_p
+
+    """
+    content = open(filepath).readlines()
+
+    altitude = []
+    rho = []
+    thick = []
+    index = []
+    temperature = []
+    pressure = []
+    pw_p = []
     for line in content:
         if line[0] is '#':
-            print(line)
+            logger.info(line)
         else:
-            ## Alt [km]    rho [g/cm^3] thick [g/cm^2]    n-1        T [K]    p [mbar]      pw / p
+            # Alt [km]    rho [g/cm^3] thick [g/cm^2]    n-1        T [K]\
+            # p [mbar]      pw / p
             all_el = [float(el) for el in line.split()]
-            altitude.append(all_el[0]*1000.)
+            altitude.append(all_el[0] * 1000.)
             rho.append(all_el[1])
             thick.append(all_el[2])
             index.append(all_el[3])
@@ -34,20 +50,21 @@ def read_atmo_prof(fpath):
             pressure.append(all_el[5])
             pw_p.append(all_el[6])
     return altitude, rho, thick, index, temperature, pressure, pw_p
-    
-    
-    
-if __name__=="__main__":
-    ''' Write up an example of reading a file
-    '''
+
+
+if __name__ == "__main__":
+    """ Open and read atmoprof26.dat to get its content in forms of lists
+        @TODO:
+            * print a few values
+    """
     from traitlets import Unicode
     from ctapipe.utils import get_dataset
     # set CTAPIPE_SVC_PATH to a path containing the file below
-    input_path=get_dataset('atmprof26.dat')
-    
-    # can't make this work...
-    #input_path = Unicode(get_dataset('atmprof26.dat'), allow_none=True,
+    input_path = get_dataset('atmprof26.dat')
+
+    # JB can't make this work...
+    # input_path = Unicode(get_dataset('atmprof26.dat'), allow_none=True,
     #                     help='Path to the atmospheric profile file, e.g. '
-    #                          'atmprof26.dat').tag(config=True)    
-    altitudes, rho, thick, index, temperature, pressure, pw_p = \
-       read_atmo_prof(input_path)
+    #                          'atmprof26.dat').tag(config=True)
+    altitudes, rhos, thicks, indexs, temperatures, pressures, pw_ps = \
+        read_atmo_prof(input_path)
