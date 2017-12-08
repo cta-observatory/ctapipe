15f4294c9d23874843cb2cd36e791eeb5febad63
Dominik Neise <neised@phys.ethz.ch>
2017-12-08 07:29:41 +0100

    move import into test, so skipping it has an effect, when the reader is not installed

--- ctapipe/io/tests/test_zfits.py
+++ ctapipe/io/tests/test_zfits.py
@@ -1,10 +1,11 @@
 import pytest
 from ctapipe.utils.datasets import get_dataset
-from ctapipe.io.zfits import zfits_event_source
 import numpy as np
 
 @pytest.mark.skip
 def test_loop_over_events():
+    from ctapipe.io.zfits import zfits_event_source
+
     _url = get_dataset("sst-1m_5evts.fits.fz")
     inputfile_reader = zfits_event_source(url=_url, max_events= 5 )
     i=0
