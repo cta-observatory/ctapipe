3cd72857da5a9e58c2d41490e1230862d99ccc48
Dominik Neise <neised@phys.ethz.ch>
2017-12-12 12:43:08 +0100

    remove unused import

--- ctapipe/instrument/__init__.py
+++ ctapipe/instrument/__init__.py
@@ -1,8 +1,5 @@
 from .camera import CameraGeometry
-from .atmosphere import (
-    get_atmosphere_profile_table,
-    get_atmosphere_profile_functions,
-)
+from .atmosphere import get_atmosphere_profile_functions
 from .telescope import TelescopeDescription
 from .optics import OpticsDescription
 from .subarray import SubarrayDescription
