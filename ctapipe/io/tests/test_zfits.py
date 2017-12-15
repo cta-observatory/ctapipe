c43ce49ae64ea75061b443ce7b980185c3c94859
Dominik Neise <neised@phys.ethz.ch>
2017-12-15 10:12:41 +0100

    use an existing file for testing
    This test was not written by me, and I assumed the file to be
    used for testing would at least exist ... I was wrong.
    I have no idea what file the original author intended to use,
    so I fall back to use the file from the protozfitsreader package

--- ctapipe/io/tests/test_zfits.py
+++ ctapipe/io/tests/test_zfits.py
@@ -1,12 +1,22 @@
 from ctapipe.utils.datasets import get_dataset
 import numpy as np
+from pkg_resources import resource_filename
+import os
+
+example_file_path = resource_filename(
+    'protozfitsreader',
+    os.path.join(
+        'tests',
+        'resources',
+        'example_10evts.fits.fz'
+    )
+)
 
 
 def test_loop_over_events():
     from ctapipe.io.zfits import zfits_event_source
 
-    _url = get_dataset("sst-1m_5evts.fits.fz")
-    inputfile_reader = zfits_event_source(url=_url, max_events=5)
+    inputfile_reader = zfits_event_source(url=example_file_path, max_events=5)
 
     for i, event in enumerate(inputfile_reader):
         tels = event.r0.tels_with_data
