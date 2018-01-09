cab25b1d02ddaf35b756a93ed765d42df88336c8
Jason Watson <jason.jw@live.co.uk>
2018-01-09 12:07:46 +0000

    Added test for event_id __getitem__

--- ctapipe/io/tests/test_hessiofilereader.py
+++ ctapipe/io/tests/test_hessiofilereader.py
@@ -19,3 +19,5 @@ def test_hessio_file_reader():
     assert event.r0.tels_with_data == {38, 47}
     event = reader[0]
     assert event.r0.tels_with_data == {38, 47}
+    event = reader['409']
+    assert event.r0.tels_with_data == {11, 21, 24, 26, 61, 63, 118, 119}
