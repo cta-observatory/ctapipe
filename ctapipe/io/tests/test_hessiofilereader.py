e6c5a82b59e7abd79eb98fa0226eb3f7cf1e9e40
Jason Watson <jason.jw@live.co.uk>
2018-01-09 04:26:59 +0000

    Created tests

---
+++ ctapipe/io/tests/test_hessiofilereader.py
@@ -0,0 +1,21 @@
+from os.path import join, dirname
+from ctapipe.utils import get_dataset
+from ctapipe.io.hessiofilereader import HessioFileReader
+
+
+def test_hessio_file_reader():
+    dataset = get_dataset("gamma_test.simtel.gz")
+    reader = HessioFileReader(None, None, input_path=dataset)
+    assert reader.is_compatible(dataset)
+    assert reader.camera == 'hessio'
+    assert len(reader) == 9
+    event = next(reader)
+    assert event.r0.tels_with_data == {38, 47}
+    event = next(reader)
+    assert event.r0.tels_with_data == {11, 21, 24, 26, 61, 63, 118, 119}
+    event = reader[0]
+    assert event.r0.tels_with_data == {38, 47}
+    for event in reader:
+        pass
+    event = next(reader)
+    assert event.r0.tels_with_data == {38, 47}
