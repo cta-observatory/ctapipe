e2a8d16d4f1ab298019612e5e7eb22b8b4ce2e91
Jason Watson <jason.jw@live.co.uk>
2018-01-12 16:00:21 +0000

    Added test for ability to implement an EventFileReader

--- ctapipe/io/tests/test_eventfilereader.py
+++ ctapipe/io/tests/test_eventfilereader.py
@@ -13,6 +13,29 @@ def test_event_file_reader():
                     "instantiated due to its abstract methods")
 
 
+class TestReader(EventFileReader):
+    """
+    Simple working EventFileReader
+    """
+    def _generator(self):
+        return range(len(self.input_path))
+
+    def is_compatible(self, path):
+        return False
+
+
+def test_can_be_implemented():
+    dataset = get_dataset("gamma_test.simtel.gz")
+    test_reader = TestReader(None, None, input_path=dataset)
+
+
+def test_is_iterable():
+    dataset = get_dataset("gamma_test.simtel.gz")
+    test_reader = TestReader(None, None, input_path=dataset)
+    for _ in test_reader:
+        pass
+
+
 def test_event_file_reader_factory():
     dataset = get_dataset("gamma_test.simtel.gz")
     factory = EventFileReaderFactory(None, None)
