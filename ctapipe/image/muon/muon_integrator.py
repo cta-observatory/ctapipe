Fixed issues relating to inconsistant astropy units

ctapipe/image/muon/muon_integrator.py
@@ -68,7 +68,7 @@ class MuonLineIntegrate:
         self.prediction = 0
         self.minlambda = 300.e-9
         self.maxlambda = 600.e-9
-        self.photemit = alpha * (self.minlambda**-1 - self.maxlambda**-1)  # 12165.45
+        self.photemit = 1.0/137.0 * (self.minlambda**-1 - self.maxlambda**-1)  # 12165.45
         self.unit = u.deg
 
     @staticmethod
@@ -90,10 +90,10 @@ class MuonLineIntegrate:
         ndarray: chord length
         """
         chord = 1 - (rho * rho * np.sin(phi) * np.sin(phi))
-
-        if rho <= 1:
+        
+        if rho <= 1.0:
             chord = radius * (np.sqrt(chord) + rho * np.cos(phi))
-        elif rho > 1:
+        elif rho > 1.0:
             chord = 2. * radius * np.sqrt(chord)
 
         chord[np.isnan(chord)] = 0
@@ -240,17 +240,17 @@ class MuonLineIntegrate:
         gauss = norm.pdf(radial_dist, radius, ring_width)
 
         # interpolate profile to find prediction for each pixel
-        pred = np.interp(ang, ang_prof, profile)
+        pred = u.Quantity(np.interp(ang, ang_prof, profile))
 
         # Multiply by integrated emissivity between 300 and 600 nm
-        pred *= 0.5 * self.photemit
+        photval=self.photemit
+        pred *= 0.5 * photval
 
         # weight by pixel width
         pred *= (self.pixel_width / radius)
         pred *= np.sin(2 * radius)
         # weight by gaussian width
         pred *= self.pixel_width * gauss
-
         return pred
 
     def likelihood(self, impact_parameter, phi, centre_x, centre_y,
@@ -302,9 +302,10 @@ class MuonLineIntegrate:
 
         # scale prediction by optical efficiency of array
         self.prediction *= optical_efficiency_muon
-
+        
         # Multiply sum of likelihoods by -2 to make them behave like chi-squared
-        return -2 * np.sum(self.calc_likelihood(self.image, self.prediction, 0.5, 1.1))
+        like_value=np.sum(self.calc_likelihood(self.image, self.prediction, 0.5, 1.1))
+        return like_value
 
     @staticmethod
     def calc_likelihood(image, pred, spe_width, ped):
@@ -327,13 +328,15 @@ class MuonLineIntegrate:
         ndarray: likelihood for each pixel
 
         """
+        pred=pred/u.deg**2
         sq = 1 / np.sqrt(2 * np.pi * (ped**2 + pred * (1 + spe_width**2)))
         diff = (image - pred)**2
         denom = 2 * (ped**2 + pred * (1 + spe_width**2))
         expo = np.exp(-diff / denom)
         sm = expo < 1e-300
         expo[sm] = 1e-300
-        return np.log(sq * expo)
+        likelihood_value=-2*np.log(sq*expo)
+        return likelihood_value
 
     def fit_muon(self, centre_x, centre_y, radius, pixel_x, pixel_y, image):
         """
@@ -395,18 +398,19 @@ class MuonLineIntegrate:
 
         print("radius =", radius, " pre migrad")
 
-
         # Create Minuit object with first guesses at parameters
         # strip away the units as Minuit doesnt like them
+        #
+        #raise RuntimeWarning(self.likelihood,init_params,init_errs,init_constrain)
         minuit = Minuit(
             self.likelihood,
             # forced_parameters=parameter_names,
             **init_params,
             **init_errs,
             **init_constrain,
-            errordef=1.,
-            print_level=1
-            # pedantic=False
+            errordef=0.,
+            print_level=0,
+            pedantic=False
         )
 
         # Perform minimisation