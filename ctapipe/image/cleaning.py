198e0c7c7f14f1b3f291a24917dbcf08c1db8b6b
Dominik Neise <neised@phys.ethz.ch>
2018-03-07 12:09:01 +0100

    ctapipe.image.cleaning.tailcuts_clean speed up factor ~10 (#666)
    * and new lazyproperty: neighbor_matrix_sparse
    
    * use sparse neighbor matrix in "dot-products"
    
    * implement sum instead of any
    
    * use sparse neighbor matrix also in dilate()

--- ctapipe/image/cleaning.py
+++ ctapipe/image/cleaning.py
@@ -30,11 +30,11 @@ def tailcuts_clean(geom, image, picture_thresh=7, boundary_thresh=5,
     picture_thresh: float or array
         threshold above which all pixels are retained
     boundary_thresh: float or array
-        threshold above which pixels are retained if they have a neighbor 
+        threshold above which pixels are retained if they have a neighbor
         already above the picture_thresh
     keep_isolated_pixels: bool
-        If True, pixels above the picture threshold will be included always, 
-        if not they are only included if a neighbor is in the picture or 
+        If True, pixels above the picture threshold will be included always,
+        if not they are only included if a neighbor is in the picture or
         boundary
     min_number_picture_neighbors: int
         A picture pixel survives cleaning only if it has at least this number
@@ -56,8 +56,8 @@ def tailcuts_clean(geom, image, picture_thresh=7, boundary_thresh=5,
     else:
         # Require at least min_number_picture_neighbors. Otherwise, the pixel
         #  is not selected
-        number_of_neighbors_above_picture = np.sum(pixels_above_picture &
-                                                   geom.neighbor_matrix, axis=1)
+        number_of_neighbors_above_picture = geom.neighbor_matrix_sparse.dot(
+            pixels_above_picture.view(np.byte))
         pixels_in_picture = pixels_above_picture & (
             number_of_neighbors_above_picture >= min_number_picture_neighbors
         )
@@ -66,23 +66,21 @@ def tailcuts_clean(geom, image, picture_thresh=7, boundary_thresh=5,
     # matrix (2d), we find all pixels that are above the boundary threshold
     # AND have any neighbor that is in the picture
     pixels_above_boundary = image >= boundary_thresh
-    pixels_with_picture_neighbors = (pixels_in_picture &
-                                     geom.neighbor_matrix).any(axis=1)
-
+    pixels_with_picture_neighbors = geom.neighbor_matrix_sparse.dot(
+        pixels_in_picture)
     if keep_isolated_pixels:
         return (pixels_above_boundary
                 & pixels_with_picture_neighbors) | pixels_in_picture
     else:
-        pixels_with_boundary_neighbors = (pixels_above_boundary &
-                                          geom.neighbor_matrix).any(axis=1)
-
+        pixels_with_boundary_neighbors = geom.neighbor_matrix_sparse.dot(
+            pixels_above_boundary)
         return ((pixels_above_boundary & pixels_with_picture_neighbors) |
                 (pixels_in_picture & pixels_with_boundary_neighbors))
 
 
 def dilate(geom, mask):
     """
-    Add one row of neighbors to the True values of a pixel mask and return 
+    Add one row of neighbors to the True values of a pixel mask and return
     the new mask.
     This can be used to include extra rows of pixels in a mask that was
     pre-computed, e.g. via `tailcuts_clean`.
@@ -91,7 +89,7 @@ def dilate(geom, mask):
     ----------
     geom: `~ctapipe.instrument.CameraGeometry`
         Camera geometry information
-    mask: ndarray 
+    mask: ndarray
         input mask (array of booleans) to be dilated
     """
-    return mask | (mask & geom.neighbor_matrix).any(axis=1)
+    return mask | geom.neighbor_matrix_sparse.dot(mask)
