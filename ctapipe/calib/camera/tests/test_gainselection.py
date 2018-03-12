726861f7459159e33e74be88b649d12c30e144cd
Karl Kosack <karl.kosack@cea.fr>
2018-03-12 23:24:14 +0200

    add SimpleGainSelector
    SimpleGainSelector just selects the given channel and discards the other

--- ctapipe/calib/camera/tests/test_gainselection.py
+++ ctapipe/calib/camera/tests/test_gainselection.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-from ctapipe.calib.camera.gainselection import ThresholdGainSelector
+from ctapipe.calib.camera.gainselection import ThresholdGainSelector, SimpleGainSelector
 from ctapipe.calib.camera.gainselection import pick_gain_channel
 
 
@@ -55,8 +55,7 @@ def test_pick_gain_channel_bad_input():
     assert gain_mask is not None
     assert (waveforms == input_waveforms).all()
 
-
-def test_gain_selector():
+def test_threshold_gain_selector():
     selector = ThresholdGainSelector()
     print(selector)
 
@@ -101,3 +100,17 @@ def test_gain_selector():
     wf1, gm = selector.select_gains("ASTRICam", wf0)
     assert wf1.shape == (1000,)
     assert gm.shape == (1000,)
+
+def test_simple_gain_selector():
+
+    gs = SimpleGainSelector()
+
+    for chan in [0,1]:
+        gs.channel = chan
+
+        waveforms_2g = np.random.normal(size=(2,1000,30))
+        waveforms_1g, mask = gs.select_gains("NectarCam", waveforms_2g)
+
+        assert waveforms_1g.shape == (1000,30)
+        assert (waveforms_1g == waveforms_2g[chan]).all()
+
