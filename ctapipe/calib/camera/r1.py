e5150118b756872b4535b54ce316ef87d0992dda
Dominik Neise <neised@phys.ethz.ch>
2018-03-21 17:19:41 +0100

    make test pass

--- ctapipe/calib/camera/r1.py
+++ ctapipe/calib/camera/r1.py
@@ -357,7 +357,8 @@ class SST1MR1Calibrator(CameraR1Calibrator):
     gain_drop_from_baseline_shift = interp1d(
         x=baseline_shift,
         y=gain_drop,
-        kind='cubic'
+        kind='cubic',
+        fill_value="extrapolate",
     )
 
     n_bins_for_baseline_estimation = Integer(1000).tag(config=True)
@@ -386,11 +387,12 @@ class SST1MR1Calibrator(CameraR1Calibrator):
             r1 = event.r1.tel[telescope_id]
 
             baseline_subtracted = r0.waveform - sst1m.digicam_baseline[:, np.newaxis]
-
+            print(sst1m.digicam_baseline, sst1m.digicam_baseline.min(), sst1m.digicam_baseline.mean(), sst1m.digicam_baseline.max())
             baseline_shift = sst1m.digicam_baseline - self.dark_baseline
+            print(baseline_shift)
             gain_drop = self.gain_drop_from_baseline_shift(baseline_shift)
 
-            uniform_over_camera = baseline_subtracted * gain_drop
+            uniform_over_camera = baseline_subtracted * gain_drop[:, np.newaxis]
             r1.waveform = uniform_over_camera.round().astype(np.int16)
         return event
 
--- ctapipe/calib/camera/tests/test_r1.py
+++ ctapipe/calib/camera/tests/test_r1.py
@@ -151,7 +151,7 @@ def test_sst1m_r1_calibration():
     )
     with NamedTemporaryFile(suffix='.npz') as named_tmp_file:
 
-        np.savez(named_tmp_file.name, baseline=np.ones(1296) * 500)
+        np.savez(named_tmp_file.name, baseline=np.ones(1296) * 200)
 
         calibrator = SST1MR1Calibrator(
             dark_baseline_path=named_tmp_file.name
