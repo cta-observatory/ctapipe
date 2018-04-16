88ce1c61dd958d50f8aeacf4afebecac79108ca9
Dan Parsons <parsonsrd@gmail.com>
2018-04-16 13:05:34 +0200

    Created the first simple implementation and tests of a class to perform linear interpolation on unstructured datapoints

---
+++ ctapipe/utils/tests/test_unstructured_interpolator.py
@@ -0,0 +1,58 @@
+from ctapipe.utils.unstructured_interpolator import UnstructuredInterpolator
+import numpy as np
+from scipy.interpolate import LinearNDInterpolator
+
+
+def test_simple_interpolation():
+    """
+    Simple test function to asses the basic funtionality of the unstructured
+    interpolator, check if we just spread points on a grid
+    """
+
+    interpolation_points = {(0, 0): 0,
+                            (0, 1): 0,
+                            (1, 0): 1,
+                            (1, 1): 1}
+
+    interpolator = UnstructuredInterpolator(interpolation_points)
+
+    # OK lets first just check we get the values at the grid points back out again...
+    interpolated_point = interpolator([[0, 0], [0, 1],
+                                       [1, 0], [1, 1]])
+    assert np.all(interpolated_point == [0., 0., 1., 1.])
+
+    interpolated_point = interpolator([[0., 0.5], [0.5, 0.5], [1, 0.5] ])
+    assert np.all(interpolated_point == [0, 0.5, 1])
+
+
+def test_linear_nd():
+    """
+    In its simplest configuration this code should behave exactly the same as the scipy
+    LinearNDInterpolator, so lets test that
+    """
+
+    # First set up 4 grid points and fill them randomly
+    interpolation_points = {(0, 0): np.random.rand(),
+                            (0, 1): np.random.rand(),
+                            (1, 0): np.random.rand(),
+                            (1, 1): np.random.rand()}
+
+    # Create UnstructuredInterpolator and LinearNDInterpolator with these points
+    interpolator = UnstructuredInterpolator(interpolation_points)
+    linear_nd = LinearNDInterpolator(list(interpolation_points.keys()),
+                                     list(interpolation_points.values()))
+
+    # Create some random coordinates in this space
+    points = np.random.rand(10,2)
+    # And interpolate...
+    interpolated_points = interpolator(points)
+    linear_nd_points = linear_nd(points)
+
+    # Check everything agrees to a reasonable precision
+    assert np.all(np.abs(interpolated_points - linear_nd_points) < 1e-10)
+
+
+if __name__ == '__main__':
+
+    test_simple_interpolation()
+    test_linear_nd()
---
+++ ctapipe/utils/unstructured_interpolator.py
@@ -0,0 +1,81 @@
+"""
+TODO:
+- Implement class interpolation
+- Figure out what to do when out of bounds
+"""
+
+import numpy as np
+from scipy.spatial import Delaunay
+
+
+class UnstructuredInterpolator:
+    """
+    This class performs linear interpolation between an unstructured set of data
+    points. However the class is expanded such that it can interpolate between the
+    values returned from class member functions. The primary use case of this being to
+    interpolate between the predictions of a set of machine learning algorithms or
+    regular grid interpolators.
+
+    In the case that a numpy array is passed as the interpolation valus this class will
+    behave exactly the same as the scipy LinearNDInterpolator
+    """
+    def __init__(self, interpolation_points, function_name=""):
+        """
+        Parameters
+        ----------
+        interpolation_points: dict
+            Dictionary of interpolation points (stored as key) and values
+        function_name: str
+            Name of class member function to call in the case we are interpolating
+            between class predictions, for numpy arrays leave blank
+        """
+
+        self.keys = np.array(list(interpolation_points.keys()))
+        self.values = np.array(list(interpolation_points.values()))
+
+        self.num_dimensions = len(self.keys[0])
+
+        # create an object with triangulation
+        self.tri = Delaunay(self.keys)
+        self.function_name = function_name
+
+        return None
+
+    def __call__(self, points):
+
+        # Convert to a numpy array here incase we get a list
+        points = np.array(points)
+
+        if len(points.shape) == 1:
+            points = np.array([points])
+
+        # find simplexes that contain interpolated points
+        s = self.tri.find_simplex(points)
+        # get the vertices for each simplex
+        v = self.tri.vertices[s]
+        # get transform matrices for each simplex (see explanation bellow)
+        m = self.tri.transform[s]
+
+        # Here comes some serious numpy magic, it could be done with a loop but would
+        # be pretty inefficient I had to rip this from stack overflow somewhere - RDP
+        # For each interpolated point, take the the transform matrix and multiply it by
+        # the vector p-r, where r=m[:,n,:] is one of the simplex vertices to which
+        # the matrix m is related to
+        b = np.einsum('ijk,ik->ij', m[:, :self.num_dimensions, :self.num_dimensions],
+                      points - m[:,self.num_dimensions, :])
+
+        # Use the above array to get the weights for the vertices; `b` contains an
+        # n-dimensional vector with weights for all but the last vertices of the simplex
+        # (note that for n-D grid, each simplex consists of n+1 vertices);
+        # the remaining weight for the last vertex can be copmuted from
+        # the condition that sum of weights must be equal to 1
+        w = np.c_[b, 1 - b.sum(axis=1)]
+
+        selected_points = self.values[v]
+
+        # Multiply point values by weight
+        p_values = np.einsum('ij,ij->i', selected_points, w)
+
+        return p_values
+
+
