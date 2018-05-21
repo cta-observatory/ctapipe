d4954fd3b4343a27b68c49a4778889d0362ec8fe
Thomas Armstrong <thomas.armstrong@physics.ox.ac.uk>
2018-05-21 10:26:24 +0200

    switched trigger pixels to R0 container

--- ctapipe/io/hessioeventsource.py
+++ ctapipe/io/hessioeventsource.py
@@ -133,8 +133,6 @@ class HESSIOEventSource(EventSource):
 
                     data.mc.tel[tel_id].dc_to_pe = file.get_calibration(tel_id)
                     data.mc.tel[tel_id].pedestal = file.get_pedestal(tel_id)
-                    data.mc.tel[tel_id].num_trig_pix = file.get_num_trig_pixels(tel_id)
-                    data.mc.tel[tel_id].trig_pix_id = file.get_trig_pixels(tel_id)
                     data.r0.tel[tel_id].waveform = (file.
                                                     get_adc_sample(tel_id))
                     if data.r0.tel[tel_id].waveform.size == 0:
@@ -142,6 +140,8 @@ class HESSIOEventSource(EventSource):
                         data.r0.tel[tel_id].waveform = (file.
                                                         get_adc_sum(tel_id)[..., None])
                     data.r0.tel[tel_id].image = file.get_adc_sum(tel_id)
+                    data.r0.tel[tel_id].num_trig_pix = file.get_num_trig_pixels(tel_id)
+                    data.r0.tel[tel_id].trig_pix_id = file.get_trig_pixels(tel_id)
                     data.mc.tel[tel_id].reference_pulse_shape = (file.
                                                                  get_ref_shapes(tel_id))
 
