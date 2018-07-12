cf4e3a32767b94ac03b0938c5174865b421bdbfb
Vuillaume <thomas.vuillaume@lapp.in2p3.fr>
2018-07-12 15:32:09 +0300

    simplified a bit this example and corrected the line length < 90 columns

--- ctapipe/image/geometry_converter_hex.py
+++ ctapipe/image/geometry_converter_hex.py
@@ -325,21 +325,10 @@ def convert_geometry_hex1d_to_rect2d(geom, signal, key=None, add_rot=0):
 
     Example
     -------
-    from ctapipe.utils import get_dataset
-    from ctapipe.image.geometry_converter import convert_geometry_hex1d_to_rect2d
-    from ctapipe.io import event_source
-    import copy
-    import matplotlib.pyplot as plt
-
-    filename = get_dataset('gamma_test.simtel.gz')
-    source = event_source(filename)
-    source.allowed_tels = {1}
-    for event in source:
-        camera = event.inst.subarray.tel[1].camera
-        image = event.r0.tel[1].image[0]
-        key = camera.cam_id
-        square_geom, square_image = convert_geometry_hex1d_to_rect2d(camera, image, key=key)
-
+    camera = event.inst.subarray.tel[tel_id].camera
+    image = event.r0.tel[tel_id].image[0]
+    key = camera.cam_id
+    square_geom, square_image = convert_geometry_hex1d_to_rect2d(camera, image, key=key)
     """
 
     if key in rot_buffer:
@@ -497,21 +486,12 @@ def convert_geometry_rect2d_back_to_hexe1d(geom, signal, key=None,
 
     Example
     -------
-    from ctapipe.utils import get_dataset
-    from ctapipe.image.geometry_converter import convert_geometry_hex1d_to_rect2d
-    from ctapipe.io import event_source
-    import copy
-    import matplotlib.pyplot as plt
-
-    filename = get_dataset('gamma_test.simtel.gz')
-    source = event_source(filename)
-    source.allowed_tels = {1}
-    for event in source:
-        camera = event.inst.subarray.tel[1].camera
-        image = event.r0.tel[1].image[0]
-        key = camera.cam_id
-        square_geom, square_image = convert_geometry_hex1d_to_rect2d(camera, image, key=key)
-        hex_geom, hex_image = convert_geometry_rect2d_back_to_hexe1d(square_geom, square_image, key=key)
+    camera = event.inst.subarray.tel[tel_id].camera
+    image = event.r0.tel[tel_id].image[0]
+    key = camera.cam_id
+    square_geom, square_image = convert_geometry_hex1d_to_rect2d(camera, image, key=key)
+    hex_geom, hex_image = convert_geometry_rect2d_back_to_hexe1d(square_geom,
+    square_image, key = key)
     """
 
     if key not in rot_buffer:
