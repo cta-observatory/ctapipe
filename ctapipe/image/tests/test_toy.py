796263b746628fcd7036e211539f8e9a5e71d2c0
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2018-09-27 18:06:06 +0200

    Use larger tolerance

--- ctapipe/image/tests/test_toy.py
+++ ctapipe/image/tests/test_toy.py
@@ -3,16 +3,16 @@ from ctapipe.instrument import CameraGeometry
 from pytest import approx
 from scipy.stats import poisson
 
-np.random.seed(0)
-
 
 def test_intensity():
     from .. import toymodel
 
+    np.random.seed(0)
+
     geom = CameraGeometry.from_name('LSTCam')
 
-    width = 0.02
-    length = 0.1
+    width = 0.05
+    length = 0.15
     intensity = 50
 
     # make a toymodel shower model
@@ -27,15 +27,15 @@ def test_intensity():
     )
 
     # test if signal reproduces given cog values
-    assert np.average(geom.pix_x.value, weights=signal) == approx(0.2, rel=0.05)
-    assert np.average(geom.pix_y.value, weights=signal) == approx(0.3, rel=0.05)
+    assert np.average(geom.pix_x.value, weights=signal) == approx(0.2, rel=0.15)
+    assert np.average(geom.pix_y.value, weights=signal) == approx(0.3, rel=0.15)
 
     # test if signal reproduces given width/length values
     cov = np.cov(geom.pix_x.value, geom.pix_y.value, aweights=signal)
     eigvals, eigvecs = np.linalg.eigh(cov)
 
-    assert eigvals[0] == approx(width**2, rel=0.15)
-    assert eigvals[1] == approx(length**2, rel=0.15)
+    assert np.sqrt(eigvals[0]) == approx(width, rel=0.15)
+    assert np.sqrt(eigvals[1]) == approx(length, rel=0.15)
 
     # test if total intensity is inside in 99 percent confidence interval
     assert poisson(intensity).ppf(0.05) <= signal.sum() <= poisson(intensity).ppf(0.95)
