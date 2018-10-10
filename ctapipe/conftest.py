eecdc369d19968cec5c910685679839fec9dae80
Karl Kosack <karl.kosack@cea.fr>
2018-10-10 14:56:51 +0200

    change test_event scope to session
    This speeds up the tests in that the test_event is only loaded once
    per pytest run.  The only problem then is that when used, the event
    should be deepcopied first so that it doesn't get modified between
    tests... Not sure if there is a better way to do that.

--- ctapipe/conftest.py
+++ ctapipe/conftest.py
@@ -6,17 +6,21 @@ import pytest
 from ctapipe.utils import get_dataset_path
 from ctapipe.io.eventseeker import EventSeeker
 from ctapipe.io.hessioeventsource import HESSIOEventSource
+from copy import deepcopy
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope='session')
 def test_event():
     """ an example event for algorithm testing"""
     filename = get_dataset_path('gamma_test.simtel.gz')
+
+    print("******************** LOAD TEST EVENT ***********************")
 
     with HESSIOEventSource(input_url=filename) as reader:
         seeker = EventSeeker(reader)
         event = seeker['409']
 
-    yield event
+    return event
+
 
 
 
