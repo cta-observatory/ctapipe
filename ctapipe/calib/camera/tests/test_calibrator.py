2b4af6fa8b18c5265dcc8aef46f3792ceb0eacd7
Karl Kosack <kosack@users.noreply.github.com>
2018-10-11 11:17:10 +0200

    speed up unit tests that use test_event fixture (#798)
    * change test_event scope to session
    
    This speeds up the tests in that the test_event is only loaded once
    per pytest run. 
    
    * better fixture for the test event, no longer needs deepcopy in individual tests
    
    * renamed test_event fixture -> example_event (to avoid pytest name conflict)
    
    * cleaned up tests with example_event

--- ctapipe/calib/camera/tests/test_calibrator.py
+++ ctapipe/calib/camera/tests/test_calibrator.py
@@ -1,5 +1,5 @@
-from copy import deepcopy
 from numpy.testing import assert_allclose
+
 from ctapipe.calib.camera import (
     CameraCalibrator,
     HESSIOR1Calibrator,
@@ -10,14 +10,13 @@ from ctapipe.io import HESSIOEventSource
 from ctapipe.utils import get_dataset_path
 
 
-def test_camera_calibrator(test_event):
-    event = deepcopy(test_event) # so we don't modify the test event
+def test_camera_calibrator(example_event):
     telid = 11
 
     calibrator = CameraCalibrator(r1_product="HESSIOR1Calibrator")
 
-    calibrator.calibrate(event)
-    image = event.dl1.tel[telid].image
+    calibrator.calibrate(example_event)
+    image = example_event.dl1.tel[telid].image
     assert_allclose(image[0, 0], -2.216, 1e-3)
 
 
