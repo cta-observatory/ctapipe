8bf7232110002972b8126bf9c334b00a6d35c455
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2018-12-13 10:43:13 +0100

    Set argv for tool tests, fixes error when using pytest options

--- ctapipe/tools/tests/test_tools.py
+++ ctapipe/tools/tests/test_tools.py
@@ -1,3 +1,4 @@
+import sys
 from ctapipe.tools.camdemo import CameraDemo
 from ctapipe.tools.dump_triggers import DumpTriggersTool
 from ctapipe.tools.dump_instrument import DumpInstrumentTool
@@ -11,6 +12,7 @@ def test_info():
 
 
 def test_dump_triggers(tmpdir):
+    sys.argv = ['dump_triggers']
     outfile = tmpdir.join("triggers.fits")
 
     tool = DumpTriggersTool(
@@ -24,6 +26,7 @@ def test_dump_triggers(tmpdir):
 
 
 def test_dump_instrument(tmpdir):
+    sys.argv = ['dump_instrument']
     tmpdir.chdir()
 
     tool = DumpInstrumentTool(
@@ -37,6 +40,7 @@ def test_dump_instrument(tmpdir):
 
 
 def test_camdemo():
+    sys.argv = ['camera_demo']
     tool = CameraDemo()
     tool.num_events = 10
     tool.cleanframes = 2
@@ -45,6 +49,7 @@ def test_camdemo():
 
 
 def test_bokeh_file_viewer():
+    sys.argv = ['bokeh_file_viewer']
     tool = BokehFileViewer(disable_server=True)
     tool.run()
 
