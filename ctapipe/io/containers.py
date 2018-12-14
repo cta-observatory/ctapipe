a1fc0eb62612e0be4f121a37b5791049eb7adc2a
Franca Cassol <32127708+FrancaCassol@users.noreply.github.com>
2018-12-14 10:49:01 +0100

    Updates of NectarCam and LSTCam real data eventsource class (#812)
    * Improve LSTCam and NectarCam EventSources

--- ctapipe/io/containers.py
+++ ctapipe/io/containers.py
@@ -47,6 +47,7 @@ __all__ = [
 ]
 
 
+
 class SST1MCameraContainer(Container):
     pixel_flags = Field(None, 'numpy array containing pixel flags')
     digicam_baseline = Field(None, 'Baseline computed by DigiCam')
@@ -419,26 +420,58 @@ class SST1MDataContainer(DataContainer):
     sst1m = Field(SST1MContainer(), "optional SST1M Specific Information")
 
 
+class NectarCAMServiceContainer(Container):
+    """
+    Container for Fields that are specific to each NectarCAM camera configuration
+    """
+
+    # Data from the CameraConfig table
+    telescope_id = Field(-1, "telescope id")
+    cs_serial = Field(None, "serial number of the camera server")
+    configuration_id = Field(None, "id of the CameraConfiguration")
+    date = Field(None, "NTP start of run date")
+    num_pixels = Field(-1, "number of pixels")
+    num_samples = Field(-1, "num samples")
+    pixel_ids = Field([], "id of the pixels in the waveform array")
+    data_model_version = Field(None, "data model version")
+
+    idaq_version = Field(0o0, "idaq version")
+    cdhs_version = Field(0o0, "cdhs version")
+    acquisition_mode = Field(-1, "acquisition mode")
+    algorithms = Field(None, "algorithms")
+    # pre_proc_algorithms = Field(None, "pre processing algorithms")
+    module_ids = Field([], "module ids")
+    num_modules = Field(-1, "number of modules")
+
+
+class NectarCAMEventContainer(Container):
+    """
+    Container for Fields that are specific to each NectarCAM event
+    """
+
+    # Data from the CameraEvent table
+    configuration_id = Field(None, "id of the CameraConfiguration")
+    event_id = Field(None, "local id of the event")
+    tel_event_id = Field(None, "global id of the event")
+    pixel_status = Field([], "status of the pixels")
+    ped_id = Field(None, "tel_event_id of the event used for pedestal substraction")
+    module_status = Field([], "status of the modules")
+    extdevices_presence = Field(None, "presence of data for external devices")
+    tib_data = Field([], "TIB data array")
+    cdts_data = Field([], "CDTS data array")
+    swat_data = Field([], "SWAT data array")
+    counters = Field([], "counters")
+
+
+
 class NectarCAMCameraContainer(Container):
     """
-    Container for Fields that are specific to camera that use zfit
+    Container for Fields that are specific to each NectarCAM camera
     """
-    camera_event_type = Field(int, "camera event type")
+    evt = Field(NectarCAMEventContainer(), "NectarCAM specific event Information")
+    svc = Field(NectarCAMServiceContainer(), "NectarCAM specific camera_config "
+                                             "Information")
 
-
-    integrals = Field(None, (
-        "numpy array containing waveform integrals"
-        "(n_channels x n_pixels)"
-    ))
-
-
-    def fill_from_zfile_event(self, event, numTraces):
-        self.camera_event_type = event.eventType
-
-        self.integrals = np.array([
-            event.hiGain.integrals.gains,
-            event.loGain.integrals.gains,
-        ])
 
 
 
@@ -447,24 +480,20 @@ class NectarCAMContainer(Container):
     Storage for the NectarCAMCameraContainer for each telescope
     """
     tels_with_data = Field([], "list of telescopes with data")
+
+    # create the camera container
     tel = Field(
         Map(NectarCAMCameraContainer),
-        "map of tel_id to NectarCameraContainer")
+        "map of tel_id to NectarCAMCameraContainer")
 
-    def fill_from_zfile_event(self, event, numTraces):
-        self.tels_with_data = [event.telescopeID, ]
-        nectar_cam_container = self.tel[event.telescopeID]
-        nectar_cam_container.fill_from_zfile_event(
-            event,
-            numTraces,
-        )
 
 
 class NectarCAMDataContainer(DataContainer):
     """
     Data container including NectarCAM information
     """
-    nectarcam = Field(NectarCAMContainer(), "NectarCAM Specific Information")
+    nectarcam = Field(NectarCAMContainer(), "NectarCAM specific Information")
+
 
 
 class LSTServiceContainer(Container):
--- ctapipe/io/tests/test_nectarcameventsource.py
+++ ctapipe/io/tests/test_nectarcameventsource.py
@@ -2,38 +2,36 @@ from pkg_resources import resource_filename
 import os
 
 import pytest
-pytest.importorskip("protozfits", minversion="0.44.5")
-
-example_file_path = resource_filename(
-    'protozfits',
-    os.path.join(
-        'tests',
-        'resources',
-        'example_9evts_NectarCAM.fits.fz'
-    )
-)
+from ctapipe.utils import get_dataset_path
+pytest.importorskip("protozfits", minversion="1.4.2")
 
 FIRST_EVENT_NUMBER_IN_FILE = 1
-ADC_SAMPLES_SHAPE = (2, 84, 60)
 
+# example_file_path="NectarCam.Run0890.10events.fits.fz"
+example_file_path = get_dataset_path("NectarCAM.Run0890.10events.fits.fz")
 
 def test_loop_over_events():
     from ctapipe.io.nectarcameventsource import NectarCAMEventSource
 
-    N_EVENTS = 3
+    n_events = 10
     inputfile_reader = NectarCAMEventSource(
         input_url=example_file_path,
-        max_events=N_EVENTS
+        max_events=n_events
     )
 
     for i, event in enumerate(inputfile_reader):
         assert event.r0.tels_with_data == [0]
         for telid in event.r0.tels_with_data:
             assert event.r0.event_id == FIRST_EVENT_NUMBER_IN_FILE + i
-            assert event.r0.tel[telid].waveform.shape == ADC_SAMPLES_SHAPE
+            n_gain = 2
+            n_camera_pixels = event.inst.subarray.tels[telid].camera.n_pixels
+
+            num_samples = event.nectarcam.tel[telid].svc.num_samples
+            waveform_shape = (n_gain, n_camera_pixels, num_samples)
+            assert event.r0.tel[telid].waveform.shape == waveform_shape
 
     # make sure max_events works
-    assert i == N_EVENTS - 1
+    assert i == n_events - 1
 
 
 def test_is_compatible():
