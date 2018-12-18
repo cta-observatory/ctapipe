c9d57a848b19e625a10923dd8063ed9decaf2703
Dominik Neise <neised@phys.ethz.ch>
2018-12-18 17:02:13 +0100

    some renaming; white spaces; comments

--- ctapipe/calib/camera/flatfield.py
+++ ctapipe/calib/camera/flatfield.py
@@ -115,6 +115,13 @@ class FlasherFlatFieldCalculator(FlatFieldCalculator):
         self.log.info("Used events statistics : %d", self.max_events)
         self.num_events_seen = 0
 
+        # members to keep state in calculate_relative_gain()
+        self.time_start = None  # trigger time of first event in sample
+        self.event_median = None  # med. charge in camera per event in sample
+        self.trace_integral = None  # charge per event in sample
+        self.trace_time = None  # arrival time per event in sample
+        self.bad_pixels_of_sample = None  # bad pixels per event in sample
+
     def _extract_charge(self, event, tel_id):
         """
         Extract the charge and the time from a calibration event
@@ -175,11 +182,13 @@ class FlasherFlatFieldCalculator(FlatFieldCalculator):
             self.event_median = np.zeros((self.max_events, self.n_channels))
 
             n_pix = waveform.shape[1]
-            self.trace_integral = np.zeros((self.max_events, self.n_channels, n_pix))
-            self.trace_time = np.zeros((self.max_events, self.n_channels, n_pix))
-            self.trace_mask = np.zeros((self.max_events, self.n_channels, n_pix))
+            shape = (self.max_events, self.n_channels, n_pix)
+            self.trace_integral = np.zeros(shape)
+            self.trace_time = np.zeros(shape)
+            self.bad_pixels_of_sample = np.zeros(shape)
 
-        # extract the charge of the event and the peak position (assumed as time for the moment)
+        # extract the charge of the event and
+        # the peak position (assumed as time for the moment)
         integral, peakpos = self._extract_charge(event, tel_id)
 
         # remember the charge
@@ -189,36 +198,51 @@ class FlasherFlatFieldCalculator(FlatFieldCalculator):
         self.trace_time[self.num_events_seen] = peakpos
 
         # keep the mask of not working pixels (to be improved)
-        self.trace_mask[self.num_events_seen] = [pixel_status == 0, pixel_status == 0]
+        bad_pixels = np.array([pixel_status == 0, pixel_status == 0])
+        self.bad_pixels_of_sample[self.num_events_seen, :] = bad_pixels
 
-        # extract the median on all the camera per event: <x>(i) (for not masked pixels)
-        masked_integral = np.ma.array(integral, mask=self.trace_mask[self.num_events_seen])
-        self.event_median[self.num_events_seen, :] = np.ma.median(masked_integral, axis=1)
+        # extract the median on all the camera per event: <x>(i)
+        # (for not masked pixels)
+        masked_integral = np.ma.array(integral, mask=bad_pixels)
+        self.event_median[self.num_events_seen, :] = np.ma.median(
+            masked_integral, axis=1)
 
         self.num_events_seen += 1
 
+        sample_age = trigger_time - self.time_start
         # check if to create a calibration event
         if (
-            (trigger_time - self.time_start) > self.max_time_range_s
+            sample_age > self.max_time_range_s
             or self.num_events_seen == self.max_events
         ):
 
             # consider only not masked data
-            masked_trace_integral = np.ma.array(self.trace_integral, mask=self.trace_mask)
-            masked_trace_time = np.ma.array(self.trace_time, mask=self.trace_mask)
+            masked_trace_integral = np.ma.array(
+                self.trace_integral,
+                mask=self.bad_pixels_of_sample
+            )
+            masked_trace_time = np.ma.array(
+                self.trace_time,
+                mask=self.bad_pixels_of_sample
+            )
 
             # extract for each pixel and each event : x(i,j)/<x>(i) = g(i,j)
-            masked_relative_gain_event = masked_trace_integral/self.event_median[:,:, np.newaxis]
+            masked_relative_gain_event = (
+                masked_trace_integral / self.event_median[:, :, np.newaxis])
             relative_gain_event = np.ma.getdata(masked_relative_gain_event)
 
             # extract the median, mean and std over all the events <g>j and
             # fill the container and return it
-            self.container.time_mean = (trigger_time - self.time_start)/2 * u.s
+            self.container.time_mean = (
+                (trigger_time - self.time_start) / 2 * u.s)
             self.container.time_range = [self.time_start, trigger_time] * u.s
             self.container.n_events = self.num_events_seen
-            self.container.relative_gain_median = np.median(relative_gain_event, axis=0)
-            self.container.relative_gain_mean = np.mean(relative_gain_event, axis=0)
-            self.container.relative_gain_rms = np.std(relative_gain_event, axis=0)
+            self.container.relative_gain_median = np.median(
+                relative_gain_event, axis=0)
+            self.container.relative_gain_mean = np.mean(
+                relative_gain_event, axis=0)
+            self.container.relative_gain_rms = np.std(
+                relative_gain_event, axis=0)
 
             # extract the average time over the camera and the events
             camera_time_median = np.ma.median(masked_trace_time)
@@ -227,8 +251,12 @@ class FlasherFlatFieldCalculator(FlatFieldCalculator):
             pixel_time_mean = np.ma.mean(masked_trace_time, axis=0)
 
             # fill the container
-            self.container.relative_time_median = np.ma.getdata(pixel_time_median-camera_time_median)
-            self.container.relative_time_mean = np.ma.getdata(pixel_time_mean-camera_time_mean)
+            self.container.relative_time_median = np.ma.getdata(
+                pixel_time_median - camera_time_median
+            )
+            self.container.relative_time_mean = np.ma.getdata(
+                pixel_time_mean - camera_time_mean
+            )
 
             # re-initialize the event count
             self.num_events_seen = 0
