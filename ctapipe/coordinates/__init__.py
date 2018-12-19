22f18f5b357edbecc26c7d1404ce603c87c4ec74
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2018-12-19 19:27:39 +0100

    Do not set astropy config at import

--- ctapipe/coordinates/__init__.py
+++ ctapipe/coordinates/__init__.py
@@ -7,9 +7,6 @@ from .nominal_frame import NominalFrame
 from .ground_frames import GroundFrame, TiltedGroundFrame, project_to_ground
 from .camera_frame import CameraFrame
 
-from astropy.utils import iers
-iers.conf.auto_download = True  # auto-fetch updates to IERS_A table
-
 
 # HorizonFrame is not in __all__ because the docs complain otherwise
 __all__ = [
