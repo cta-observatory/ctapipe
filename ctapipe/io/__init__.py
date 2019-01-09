377a3f09c93c82408fd726b912a4652e3d69aade
Dominik Neise <neised@phys.ethz.ch>
2019-01-09 10:05:10 +0100

    instead of plugin detection, we need to list them somewhere

--- ctapipe/io/__init__.py
+++ ctapipe/io/__init__.py
@@ -5,6 +5,16 @@ from .simteleventsource import SimTelEventSource
 from .hdf5tableio import HDF5TableReader, HDF5TableWriter
 from .tableio import TableWriter, TableReader
 
+# import all eventsources, otherwise they cannot be
+# detected in EventSource.from_name as sub-classes of EventSource :-(
+from . import (
+    hessioeventsource,
+    targetioeventsource,
+    lsteventsource,
+    nectarcameventsource,
+    sst1meventsource,
+)
+
 
 __all__ = [
     'get_array_layout',
