Corrected bug in BaselineWaveformCleaner (wrong shape in the baseline_sub caliculation) Patched setting of event trigger time for MC events FlatFieldCalculator.calculate_relative_gain

ctapipe/image/waveform_cleaning.py
@@ -78,8 +78,8 @@ class BaselineWaveformCleaner(WaveformCleaner):
     def apply(self, waveforms):
         # self.log.debug(f"calculate baseline on first {self.baseline_width} samples")
         # Subtract initial baseline
-        baseline_sub = waveforms - np.mean(waveforms[:, :self.baseline_width],
-                                           axis=1)[:, None]
+        baseline_sub = waveforms - np.mean(waveforms[:,:, :self.baseline_width],
+                                           axis=2)[:,:, None]
         
         return baseline_sub
 