907f84cd617a791e4acdea5429b8d95ead84cb7c
Dominik Neise <neised@phys.ethz.ch>
2019-01-22 10:07:44 +0100

    use the matrix way

--- create_conda_env.sh
+++ create_conda_env.sh
@@ -5,7 +5,7 @@ set -ev
 
 # I assume if this folder exists the environment is already installed
 if [ ! -d $HOME/miniconda/envs/cta-dev ]; then
-    conda create --name cta-dev python=$TRAVIS_PYTHON_VERSION
+    conda create --name cta-dev python=$PYTHON_VERSION
 else
     echo 'cta-dev env already created'
 fi
