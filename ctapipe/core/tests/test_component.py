3c90739cc10d3b8739a1dfcc29d0d62c063475d4
watsonjj <jason.jw@live.co.uk>
2019-01-25 21:40:22 +0100

    Add additional tests to Component

--- ctapipe/core/tests/test_component.py
+++ ctapipe/core/tests/test_component.py
@@ -5,6 +5,22 @@ from ctapipe.core import Component
 from abc import abstractmethod
 
 
+class ExampleComponent(Component):
+    description = "this is a test"
+    param = Float(default_value=1.0,
+                  help="float parameter").tag(config=True)
+
+
+class ExampleSubclass1(ExampleComponent):
+    description = "this is a test"
+
+
+class ExampleSubclass2(ExampleComponent):
+    description = "this is a test"
+    param = Float(default_value=3.0,
+                  help="float parameter").tag(config=True)
+
+
 def test_component_is_abstract():
 
     class AbstractComponent(Component):
@@ -21,12 +37,6 @@ def test_component_simple():
     very basic test to construct a component and check
     that it's traits work correctly
     """
-
-    class ExampleComponent(Component):
-        description = "this is a test"
-        param = Float(default_value=1.0,
-                      help="float parameter").tag(config=True)
-
     comp = ExampleComponent()
 
     assert comp.has_trait('param') is True
@@ -37,10 +47,6 @@ def test_component_simple():
 
 
 def test_component_kwarg_setting():
-    class ExampleComponent(Component):
-        description = "this is a test"
-        param = Float(default_value=1.0,
-                      help="float parameter").tag(config=True)
 
     comp = ExampleComponent(param=3)
     assert comp.param == 3
@@ -52,3 +58,55 @@ def test_component_kwarg_setting():
     # Invalid traitlet
     with pytest.raises(TraitError):
         comp = ExampleComponent(incorrect="wrong")
+
+
+def test_help():
+    help_msg = ExampleComponent.class_get_help()
+    assert "Default: 1.0" in help_msg
+
+
+def test_default():
+    comp = ExampleComponent()
+    assert comp.param == 1.
+
+
+def test_default_subclass():
+    comp = ExampleSubclass1()
+    assert comp.param == 1.
+
+
+def test_default_subclass_override():
+    comp = ExampleSubclass2()
+    assert comp.param == 3.
+
+
+def test_change_default():
+    old_default = ExampleComponent.param.default_value
+    ExampleComponent.param.default_value = 199.
+    comp = ExampleComponent()
+    assert comp.param == 199.
+    ExampleComponent.param.default_value = old_default
+
+
+def test_change_default_subclass():
+    old_default = ExampleComponent.param.default_value
+    ExampleComponent.param.default_value = 199.
+    comp = ExampleSubclass1()
+    assert comp.param == 199.
+    ExampleComponent.param.default_value = old_default
+
+
+def test_change_default_subclass_override():
+    old_default = ExampleComponent.param.default_value
+    ExampleComponent.param.default_value = 199.
+    comp = ExampleSubclass2()
+    assert comp.param == 199.
+    ExampleComponent.param.default_value = old_default
+
+
+def test_help_changed_default():
+    old_default = ExampleComponent.param.default_value
+    ExampleComponent.param.default_value = 199.
+    help_msg = ExampleComponent.class_get_help()
+    assert "Default: 199.0" in help_msg
+    ExampleComponent.param.default_value = old_default
