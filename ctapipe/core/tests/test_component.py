8fd43cd74f3c2e8d9176097b76d85652f06e9ec1
watsonjj <jason.jw@live.co.uk>
2019-01-26 13:15:25 +0100

    Add tests for setting config

--- ctapipe/core/tests/test_component.py
+++ ctapipe/core/tests/test_component.py
@@ -1,6 +1,6 @@
 import pytest
 from traitlets import Float, TraitError
-
+from traitlets.config.loader import Config
 from ctapipe.core import Component
 from abc import abstractmethod
 
@@ -18,6 +18,8 @@ class ExampleSubclass1(ExampleComponent):
 class ExampleSubclass2(ExampleComponent):
     description = "this is a test"
     param = Float(default_value=3.0,
+                  help="float parameter").tag(config=True)
+    extra = Float(default_value=5.0,
                   help="float parameter").tag(config=True)
 
 
@@ -63,6 +65,126 @@ def test_component_kwarg_setting():
 def test_help():
     help_msg = ExampleComponent.class_get_help()
     assert "Default: 1.0" in help_msg
+
+
+def test_config():
+    config = Config()
+    config['ExampleComponent'] = Config()
+    config['ExampleComponent']['param'] = 199.
+    comp = ExampleComponent(config=config)
+    assert comp.param == 199.
+
+
+def test_config_baseclass():
+    config = Config()
+    config['ExampleComponent'] = Config()
+    config['ExampleComponent']['param'] = 199.
+    comp1 = ExampleSubclass1(config=config)
+    assert comp1.param == 199.
+    comp2 = ExampleSubclass2(config=config)
+    assert comp2.param == 199.
+
+
+def test_config_subclass1():
+    config = Config()
+    config['ExampleSubclass1'] = Config()
+    config['ExampleSubclass1']['param'] = 199.
+    comp = ExampleComponent(config=config)
+    assert comp.param == 1.
+
+
+def test_config_subclass2():
+    config = Config()
+    config['ExampleSubclass2'] = Config()
+    config['ExampleSubclass2']['param'] = 199.
+    comp = ExampleComponent(config=config)
+    assert comp.param == 1.
+
+
+def test_config_sibling1():
+    config = Config()
+    config['ExampleSubclass1'] = Config()
+    config['ExampleSubclass1']['param'] = 199.
+    comp1 = ExampleSubclass1(config=config)
+    assert comp1.param == 199.
+    comp2 = ExampleSubclass2(config=config)
+    assert comp2.param == 3.
+
+
+def test_config_sibling2():
+    config = Config()
+    config['ExampleSubclass2'] = Config()
+    config['ExampleSubclass2']['param'] = 199.
+    comp1 = ExampleSubclass1(config=config)
+    assert comp1.param == 1.
+    comp2 = ExampleSubclass2(config=config)
+    assert comp2.param == 199.
+
+
+def test_config_baseclass_then_subclass():
+    config = Config()
+    config['ExampleComponent'] = Config()
+    config['ExampleComponent']['param'] = 199.
+    config['ExampleSubclass1'] = Config()
+    config['ExampleSubclass1']['param'] = 229.
+    comp = ExampleSubclass1(config=config)
+    assert comp.param == 229.
+
+
+def test_config_subclass_then_baseclass():
+    config = Config()
+    config['ExampleSubclass1'] = Config()
+    config['ExampleSubclass1']['param'] = 229.
+    config['ExampleComponent'] = Config()
+    config['ExampleComponent']['param'] = 199.
+    comp = ExampleSubclass1(config=config)
+    assert comp.param == 229.
+
+
+def test_config_override():
+    config = Config()
+    config['ExampleComponent'] = Config()
+    config['ExampleComponent']['param'] = 199.
+    comp = ExampleComponent(config=config, param=229.)
+    assert comp.param == 229.
+
+
+def test_config_override_subclass():
+    config = Config()
+    config['ExampleComponent'] = Config()
+    config['ExampleComponent']['param'] = 199.
+    comp = ExampleSubclass1(config=config, param=229.)
+    assert comp.param == 229.
+
+
+def test_extra():
+    comp = ExampleSubclass2(extra=229.)
+    assert comp.has_trait('extra') is True
+    assert comp.extra == 229.
+
+
+def test_extra_config():
+    config = Config()
+    config['ExampleSubclass2'] = Config()
+    config['ExampleSubclass2']['extra'] = 229.
+    comp = ExampleSubclass2(config=config)
+    assert comp.extra == 229.
+
+
+def test_extra_missing():
+    with pytest.raises(TraitError):
+        comp = ExampleSubclass1(extra=229.)
+
+
+def test_extra_config_missing():
+    config = Config()
+    config['ExampleSubclass1'] = Config()
+    config['ExampleSubclass1']['extra'] = 199.
+    with pytest.warns(UserWarning):
+        comp = ExampleSubclass1(config=config)
+    assert comp.has_trait('extra') is False
+    with pytest.raises(AttributeError):
+        assert comp.extra == 229.
 
 
 def test_default():
