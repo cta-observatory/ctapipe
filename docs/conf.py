4dfa596a2cdd35dc0339b64f061af495ca34f02c
Karl Kosack <karl.kosack@cea.fr>
2019-01-28 18:01:48 +0100

    exclude jupyter checkpoints

--- docs/conf.py
+++ docs/conf.py
@@ -55,6 +55,7 @@ extensions = ['sphinx.ext.autodoc',
 numpydoc_show_class_members = False
 nbsphinx_timeout = 120 # for long-running notebooks
 
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -95,7 +96,7 @@ language = 'en'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
