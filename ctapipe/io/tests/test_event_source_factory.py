b108351f47061c06e8cad22f50a49e4d34360f40
watsonjj <jason.jw@live.co.uk>
2019-01-29 19:54:18 +0100

    Add additional tests for event source factory

--- ctapipe/io/tests/test_event_source_factory.py
+++ ctapipe/io/tests/test_event_source_factory.py
@@ -1,6 +1,9 @@
 import pytest
 from ctapipe.io import event_source
+from ctapipe.io.eventsource import from_config, EventSource
 from ctapipe.utils import get_dataset_path
+from traitlets.config.loader import Config
+from traitlets import TraitError
 
 
 def test_factory():
@@ -27,3 +30,93 @@ def test_factory_nonexistant_file():
     with pytest.raises(FileNotFoundError):
         dataset = "/fake_path/fake_file.fake_extension"
         event_source(input_url=dataset)
+
+
+def test_from_config():
+    dataset = get_dataset_path("gamma_test_large.simtel.gz")
+    config = Config()
+    config['EventSource'] = Config()
+    config['EventSource']['input_url'] = dataset
+    reader = from_config(config=config, tool=None)
+    assert reader.__class__.__name__ == "SimTelEventSource"
+    assert reader.input_url == dataset
+
+
+def test_from_config_default():
+    old_default = EventSource.input_url.default_value
+    dataset = get_dataset_path("gamma_test_large.simtel.gz")
+    EventSource.input_url.default_value = dataset
+    config = Config()
+    reader = from_config(config=config, tool=None)
+    assert reader.__class__.__name__ == "SimTelEventSource"
+    assert reader.input_url == dataset
+    EventSource.input_url.default_value = old_default
+
+
+def test_from_config_invalid_type():
+    old_default = EventSource.input_url.default_value
+    dataset = get_dataset_path("gamma_test_large.simtel.gz")
+    EventSource.input_url.default_value = dataset
+    config = Config()
+    config['EventSource'] = Config()
+    config['EventSource']['input_url'] = 124
+    with pytest.raises(TraitError):
+        from_config(config=config, tool=None)
+
+
+def test_event_source_config():
+    dataset1 = get_dataset_path("gamma_test.simtel.gz")
+    dataset2 = get_dataset_path("gamma_test_large.simtel.gz")
+    config = Config()
+    config['EventSource'] = Config()
+    config['EventSource']['input_url'] = dataset1
+    reader = event_source(dataset2, config=config)
+    assert reader.__class__.__name__ == "SimTelEventSource"
+    assert reader.input_url == dataset2
+
+
+def test_event_source_input_url_config_override():
+    dataset1 = get_dataset_path("gamma_test.simtel.gz")
+    dataset2 = get_dataset_path("gamma_test_large.simtel.gz")
+    config = Config()
+    config['EventSource'] = Config()
+    config['EventSource']['input_url'] = dataset1
+    reader = event_source(input_url=dataset2, config=config)
+    assert reader.__class__.__name__ == "SimTelEventSource"
+    assert reader.input_url == dataset2
+
+
+def test_factory_max_events():
+    max_events = 10
+    dataset = get_dataset_path("gamma_test.simtel.gz")
+    reader = event_source(input_url=dataset, max_events=max_events)
+    assert reader.max_events == max_events
+
+
+def test_factory_max_events_from_config():
+    dataset = get_dataset_path("gamma_test.simtel.gz")
+    max_events = 10
+    config = Config()
+    config['EventSource'] = Config()
+    config['EventSource']['input_url'] = dataset
+    config['EventSource']['max_events'] = max_events
+    reader = from_config(config=config, tool=None)
+    assert reader.max_events == max_events
+
+
+def test_factory_allowed_tels():
+    dataset = get_dataset_path("gamma_test.simtel.gz")
+    reader = event_source(input_url=dataset)
+    assert len(reader.allowed_tels) == 0
+    reader = event_source(input_url=dataset, allowed_tels={1, 3})
+    assert len(reader.allowed_tels) == 2
+
+
+def test_factory_allowed_tels_from_config():
+    dataset = get_dataset_path("gamma_test.simtel.gz")
+    config = Config()
+    config['EventSource'] = Config()
+    config['EventSource']['input_url'] = dataset
+    config['EventSource']['allowed_tels'] = {1, 3}
+    reader = from_config(config=config, tool=None)
+    assert len(reader.allowed_tels) == 2
