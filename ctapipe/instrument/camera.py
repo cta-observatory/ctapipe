0410c13ed1409d868352ffb1c2fd4376d17975cc
watsonjj <jason.jw@live.co.uk>
2019-03-15 23:10:07 +0100

    Correctly remove empty bins in row col calculation

--- ctapipe/instrument/camera.py
+++ ctapipe/instrument/camera.py
@@ -686,19 +686,21 @@ def _rectangular_pixel_row_column(pix_x, pix_y):
 
     """
     # Estimate the maximum number of rows and columns of pixels
-    dist = _get_min_pixel_seperation(pix_x, pix_y) / 2
-    max_nrow = int(np.ceil((pix_y.max() - pix_y.min()) / dist))
-    max_ncol = int(np.ceil((pix_x.max() - pix_x.min()) / dist))
+    dist = _get_min_pixel_seperation(pix_x, pix_y)
+    max_nrow = int(np.ceil((pix_y.max() - pix_y.min()) / dist)) + 1
+    max_ncol = int(np.ceil((pix_x.max() - pix_x.min()) / dist)) + 1
 
     # Bin the pixel positions on a 2D grid
     hist_x = np.histogram2d(pix_x, pix_y, weights=pix_x, bins=[max_ncol, max_nrow])[0]
     hist_y = np.histogram2d(pix_x, pix_y, weights=pix_y, bins=[max_ncol, max_nrow])[0]
+    hist_xc = np.histogram2d(pix_x, pix_y, bins=[max_ncol, max_nrow])[0]
+    hist_yc = np.histogram2d(pix_x, pix_y, bins=[max_ncol, max_nrow])[0]
     # Find row and col with a complete number of pixels along them
     full_col = np.bincount(hist_x.nonzero()[0]).argmax()
     full_row = np.bincount(hist_y.nonzero()[0]).argmax()
     # Obtain coordinates of the pixels along this row and column
-    full_x = hist_x[:, full_col][hist_x[:, full_col].nonzero()]
-    full_y = hist_y[full_row, :][hist_y[full_row, :].nonzero()]
+    full_x = hist_x[:, full_col][hist_xc[:, full_col].nonzero()]
+    full_y = hist_y[full_row, :][hist_yc[full_row, :].nonzero()]
 
     # Define pixel bin edges based on full row and column of pixels
     mid_dist = _get_min_pixel_seperation(full_x, full_y) / 2
