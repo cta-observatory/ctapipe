5403cbe9a7eae8b8eb253cf530de16c292763e2d
watsonjj <jason.jw@live.co.uk>
2019-03-21 20:28:45 +0100

    Remove dependency on example_event (from ctapipe_extra)

--- ctapipe/image/tests/test_charge_extraction.py
+++ ctapipe/image/tests/test_charge_extraction.py
@@ -1,6 +1,8 @@
+import pytest
 import numpy as np
+from scipy.stats import norm
 from numpy.testing import assert_almost_equal
-
+from ctapipe.instrument import CameraGeometry
 from ctapipe.image.charge_extractors import (
     ChargeExtractor,
     FullIntegrator,
@@ -12,95 +14,71 @@ from ctapipe.image.charge_extractors import (
 )
 
 
-def test_full_integration(example_event):
-    telid = list(example_event.r0.tel)[0]
-    data = example_event.r0.tel[telid].waveform
-    nsamples = data.shape[2]
-    ped = example_event.mc.tel[telid].pedestal
-    data_ped = data - np.atleast_3d(ped / nsamples)
-    data_ped = np.array([data_ped[0], data_ped[0]])  # Test LG functionality
+@pytest.fixture(scope='module')
+def camera_waveforms():
+    camera = CameraGeometry.from_name("CHEC")
 
+    n_pixels = camera.n_pixels
+    n_samples = 96
+    pulse_sigma = 6
+    r = np.random.RandomState(1)
+    r.uniform(0, 10, 5)
+
+    x = np.arange(n_samples)
+    y = norm.pdf(x, r.uniform(n_samples // 2 - 10, n_samples // 2 + 10, n_pixels)[:, None], pulse_sigma)
+    y *= r.uniform(100, 1000, n_pixels)[:, None]
+
+    # 2 Channels
+    y = np.stack([y, y * r.uniform(0, 0.5, n_pixels)[:, None]])
+
+    return y, camera
+
+
+def test_full_integration(camera_waveforms):
+    waveforms, camera = camera_waveforms
     integrator = FullIntegrator()
-    integration, peakpos, window = integrator.extract_charge(data_ped)
+    integration, peakpos, window = integrator.extract_charge(waveforms)
 
 
-def test_simple_integration(example_event):
-    telid = list(example_event.r0.tel)[0]
-
-    data = example_event.r0.tel[telid].waveform
-    nsamples = data.shape[2]
-    ped = example_event.mc.tel[telid].pedestal
-    data_ped = data - np.atleast_3d(ped / nsamples)
-    data_ped = np.array([data_ped[0], data_ped[0]])  # Test LG functionality
-
+def test_simple_integration(camera_waveforms):
+    waveforms, camera = camera_waveforms
     integrator = SimpleIntegrator()
-    integration, peakpos, window = integrator.extract_charge(data_ped)
+    integration, peakpos, window = integrator.extract_charge(waveforms)
 
 
-def test_global_peak_integration(example_event):
-    telid = list(example_event.r0.tel)[0]
-    data = example_event.r0.tel[telid].waveform
-    nsamples = data.shape[2]
-    ped = example_event.mc.tel[telid].pedestal
-    data_ped = data - np.atleast_3d(ped / nsamples)
-    data_ped = np.array([data_ped[0], data_ped[0]])  # Test LG functionality
-
+def test_global_peak_integration(camera_waveforms):
+    waveforms, camera = camera_waveforms
     integrator = GlobalPeakIntegrator()
-    integration, peakpos, window = integrator.extract_charge(data_ped)
+    integration, peakpos, window = integrator.extract_charge(waveforms)
 
 
-def test_local_peak_integration(example_event):
-    telid = list(example_event.r0.tel)[0]
-    data = example_event.r0.tel[telid].waveform
-    nsamples = data.shape[2]
-    ped = example_event.mc.tel[telid].pedestal
-    data_ped = data - np.atleast_3d(ped / nsamples)
-    data_ped = np.array([data_ped[0], data_ped[0]])  # Test LG functionality
-
+def test_local_peak_integration(camera_waveforms):
+    waveforms, camera = camera_waveforms
     integrator = LocalPeakIntegrator()
-    integration, peakpos, window = integrator.extract_charge(data_ped)
+    integration, peakpos, window = integrator.extract_charge(waveforms)
 
 
-def test_nb_peak_integration(example_event):
-    telid = list(example_event.r0.tel)[0]
-    data = example_event.r0.tel[telid].waveform
-    nsamples = data.shape[2]
-    ped = example_event.mc.tel[telid].pedestal
-    data_ped = data - np.atleast_3d(ped / nsamples)
-    data_ped = np.array([data_ped[0], data_ped[0]])  # Test LG functionality
-    geom = example_event.inst.subarray.tel[telid].camera
-    nei = geom.neighbor_matrix_where
-
+def test_nb_peak_integration(camera_waveforms):
+    waveforms, camera = camera_waveforms
+    nei = camera.neighbor_matrix_where
     integrator = NeighbourPeakIntegrator()
     integrator.neighbours = nei
-    integration, peakpos, window = integrator.extract_charge(data_ped)
+    integration, peakpos, window = integrator.extract_charge(waveforms)
 
 
-def test_averagewf_peak_integration(example_event):
-    telid = list(example_event.r0.tel)[0]
-    data = example_event.r0.tel[telid].waveform
-    nsamples = data.shape[2]
-    ped = example_event.mc.tel[telid].pedestal
-    data_ped = data - np.atleast_3d(ped / nsamples)
-    data_ped = np.array([data_ped[0], data_ped[0]])  # Test LG functionality
-
+def test_averagewf_peak_integration(camera_waveforms):
+    waveforms, camera = camera_waveforms
     integrator = AverageWfPeakIntegrator()
-    integration, peakpos, window = integrator.extract_charge(data_ped)
+    integration, peakpos, window = integrator.extract_charge(waveforms)
 
-    assert_almost_equal(integration[0][0], 73, 0)
-    assert_almost_equal(integration[1][0], 73, 0)
+    assert_almost_equal(integration[0][0], 51.647, 3)
+    assert_almost_equal(integration[1][0], 1.194, 3)
 
 
-def test_charge_extractor_factory(example_event):
+def test_charge_extractor_factory(camera_waveforms):
+    waveforms, camera = camera_waveforms
     extractor = ChargeExtractor.from_name('LocalPeakIntegrator')
-
-    telid = list(example_event.r0.tel)[0]
-    data = example_event.r0.tel[telid].waveform
-    nsamples = data.shape[2]
-    ped = example_event.mc.tel[telid].pedestal
-    data_ped = data - np.atleast_3d(ped / nsamples)
-
-    extractor.extract_charge(data_ped)
+    extractor.extract_charge(waveforms)
 
 
 def test_charge_extractor_factory_args():
