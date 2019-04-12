47e53f755a631f5a83e130f540c899176b286af2
watsonjj <jason.jw@live.co.uk>
2019-04-12 12:21:52 +0200

    Update calibrator

--- ctapipe/calib/camera/calibrator.py
+++ ctapipe/calib/camera/calibrator.py
@@ -6,7 +6,6 @@ This calibrator will apply the calibrations found in r1.py, dl0.py and dl1.py.
 
 from ctapipe.core import Component
 from ctapipe.calib.camera import (
-    CameraR1Calibrator,
     CameraDL0Reducer,
     CameraDL1Calibrator,
 )
@@ -25,9 +24,7 @@ class CameraCalibrator(Component):
 
     """
     def __init__(self, config=None, parent=None,
-                 r1_product=None,
                  extractor_name='NeighborPeakWindowSum',
-                 eventsource=None,
                  **kwargs):
         """
         Parameters
@@ -40,14 +37,8 @@ class CameraCalibrator(Component):
             Tool executable that is calling this component.
             Passes the correct logger to the component.
             Set to None if no Tool to pass.
-        r1_product : str
-            The R1 calibrator to use. Manually overrides the Factory.
         extractor_name : str
             The name of the ImageExtractor to use.
-        eventsource : ctapipe.io.eventsource.EventSource
-            EventSource that is being used to read the events. The EventSource
-            contains information (such as metadata or inst) which indicates
-            the appropriate R1Calibrator to use.
         kwargs
         """
         super().__init__(config=config, parent=parent, **kwargs)
@@ -56,17 +47,6 @@ class CameraCalibrator(Component):
             extractor_name,
             parent=self,
         )
-
-        if r1_product:
-            self.r1 = CameraR1Calibrator.from_name(
-                r1_product,
-                parent=self,
-            )
-        else:
-            self.r1 = CameraR1Calibrator.from_eventsource(
-                eventsource,
-                parent=self,
-            )
 
         self.dl0 = CameraDL0Reducer(parent=parent)
         self.dl1 = CameraDL1Calibrator(
@@ -85,6 +65,5 @@ class CameraCalibrator(Component):
         event : container
             A `ctapipe` event container
         """
-        self.r1.calibrate(event)
         self.dl0.reduce(event)
         self.dl1.calibrate(event)
