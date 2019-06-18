e6c397faff946b7c78888d167969e791975b373f
Thomas Gasparetto <thomas.gasparetto@ts.infn.it>
2019-06-18 18:32:08 +0200

    Added tests to HillasIntersection.

--- ctapipe/reco/tests/test_hillas_intersection.py
+++ ctapipe/reco/tests/test_hillas_intersection.py
@@ -3,8 +3,15 @@ import astropy.units as u
 from numpy.testing import assert_allclose
 import numpy as np
 from astropy.coordinates import SkyCoord
-from ctapipe.coordinates import NominalFrame, AltAz
+from ctapipe.coordinates import NominalFrame, AltAz, CameraFrame
 from ctapipe.io.containers import HillasParametersContainer
+
+from ctapipe.io import event_source
+
+from ctapipe.utils import get_dataset_path
+
+from ctapipe.image.cleaning import tailcuts_clean
+from ctapipe.image.hillas import hillas_parameters, HillasParameterizationError
 
 
 def test_intersect():
@@ -55,13 +62,14 @@ def test_intersection_xmax_reco():
     hill_inter = HillasIntersection()
 
     horizon_frame = AltAz()
-    zen_pointing = 0 * u.deg
-    array_direction = SkyCoord(alt=90*u.deg,
+    zen_pointing = 10 * u.deg
+
+    array_direction = SkyCoord(alt=90*u.deg - zen_pointing,
                                az=0 * u.deg,
                                frame=horizon_frame)
     nom_frame = NominalFrame(origin=array_direction)
 
-    source_sky_pos_reco = SkyCoord(alt=90 * u.deg,
+    source_sky_pos_reco = SkyCoord(alt=90 * u.deg - zen_pointing,
                                    az=0 * u.deg,
                                    frame=horizon_frame)
 
@@ -71,7 +79,7 @@ def test_intersection_xmax_reco():
     # LST focal length
     focal_length = 28 * u.m
 
-    hillas_parameters = {
+    hillas_dict = {
         1: HillasParametersContainer(x=-(delta/focal_length)*u.rad,
                                      y=((0 * u.m)/focal_length) * u.rad,
                                      intensity=1),
@@ -85,7 +93,7 @@ def test_intersection_xmax_reco():
         source_y=nom_pos_reco.delta_alt,
         core_x=0 * u.m,
         core_y=0 * u.m,
-        hillas_parameters=hillas_parameters,
+        hillas_parameters=hillas_dict,
         tel_x={1: (150 * u.m), 2: (0 * u.m)},
         tel_y={1: (0 * u.m), 2: (150 * u.m)},
         zen=zen_pointing
@@ -156,25 +164,117 @@ def test_intersection_nominal_reconstruction():
     """
     hill_inter = HillasIntersection()
 
-    delta = 0.04 * u.rad
+    delta = 1.0 * u.m
+    horizon_frame = AltAz()
     altitude = 70 * u.deg
     azimuth = 10 * u.deg
 
-    hillas_dict = {
-        1: HillasParametersContainer(x=0 * u.rad + azimuth,
-                                     y=delta + altitude,
-                                     intensity=100,
-                                     psi=-90 * u.deg),
-        2: HillasParametersContainer(x=0.7*delta + azimuth,
-                                     y=-0.7*delta + altitude,
-                                     intensity=100,
-                                     psi=-45 * u.deg),
-        3: HillasParametersContainer(x=delta + azimuth,
-                                     y=0 * u.rad + altitude,
-                                     intensity=100,
-                                     psi=0 * u.deg)
-    }
+    array_direction = SkyCoord(alt=altitude,
+                               az=azimuth,
+                               frame=horizon_frame)
+
+    nominal_frame = NominalFrame(origin=array_direction)
+
+    focal_length = 28 * u.m
+
+    camera_frame = CameraFrame(focal_length=focal_length,
+                               telescope_pointing=array_direction)
+
+    cog_coords_camera_1 = SkyCoord(x=delta, y=0 * u.m, frame=camera_frame)
+    cog_coords_camera_2 = SkyCoord(x=delta / 0.7, y=delta / 0.7, frame=camera_frame)
+    cog_coords_camera_3 = SkyCoord(x=0 * u.m, y=delta, frame=camera_frame)
+
+    cog_coords_nom_1 = cog_coords_camera_1.transform_to(nominal_frame)
+    cog_coords_nom_2 = cog_coords_camera_2.transform_to(nominal_frame)
+    cog_coords_nom_3 = cog_coords_camera_3.transform_to(nominal_frame)
+
+    #  x-axis is along the altitude and y-axis is along the azimuth
+    hillas_1 = HillasParametersContainer(x=cog_coords_nom_1.delta_alt,
+                                         y=cog_coords_nom_1.delta_az,
+                                         intensity=100,
+                                         psi=0 * u.deg)
+
+    hillas_2 = HillasParametersContainer(x=cog_coords_nom_2.delta_alt,
+                                         y=cog_coords_nom_2.delta_az,
+                                         intensity=100,
+                                         psi=45 * u.deg)
+
+    hillas_3 = HillasParametersContainer(x=cog_coords_nom_3.delta_alt,
+                                         y=cog_coords_nom_3.delta_az,
+                                         intensity=100,
+                                         psi=90 * u.deg)
+
+    hillas_dict = {1: hillas_1, 2: hillas_2, 3: hillas_3}
+
     reco_nominal = hill_inter.reconstruct_nominal(hillas_parameters=hillas_dict)
 
-    np.testing.assert_allclose(u.Quantity(reco_nominal[0], u.rad).to_value(u.deg), azimuth.to_value(u.deg), atol=1e-8)
-    np.testing.assert_allclose(u.Quantity(reco_nominal[1], u.rad).to_value(u.deg), altitude.to_value(u.deg), atol=1e-8)
+    nominal_pos = SkyCoord(
+        delta_az=u.Quantity(reco_nominal[0], u.rad),
+        delta_alt=u.Quantity(reco_nominal[1], u.rad),
+        frame=nominal_frame
+    )
+
+    np.testing.assert_allclose(nominal_pos.altaz.az.to_value(u.deg), azimuth.to_value(u.deg), atol=1e-8)
+    np.testing.assert_allclose(nominal_pos.altaz.alt.to_value(u.deg), altitude.to_value(u.deg), atol=1e-8)
+
+
+def test_reconstruction():
+    """
+    a test of the complete fit procedure on one event including:
+    • tailcut cleaning
+    • hillas parametrisation
+    • direction fit
+    • position fit
+
+    in the end, proper units in the output are asserted """
+
+    filename = get_dataset_path("gamma_test_large.simtel.gz")
+
+    fit = HillasIntersection()
+
+    source = event_source(filename, max_events=10)
+
+    horizon_frame = AltAz()
+
+    for event in source:
+        array_pointing = SkyCoord(
+            az=event.mc.az,
+            alt=event.mc.alt,
+            frame=horizon_frame
+        )
+
+        hillas_dict = {}
+        telescope_pointings = {}
+
+        for tel_id in event.dl0.tels_with_data:
+
+            geom = event.inst.subarray.tel[tel_id].camera
+
+            telescope_pointings[tel_id] = SkyCoord(alt=event.mc.tel[tel_id].altitude_raw * u.rad,
+                                                   az=event.mc.tel[tel_id].azimuth_raw * u.rad,
+                                                   frame=horizon_frame)
+            pmt_signal = event.r0.tel[tel_id].waveform[0].sum(axis=1)
+
+            mask = tailcuts_clean(geom, pmt_signal,
+                                  picture_thresh=10., boundary_thresh=5.)
+            pmt_signal[mask == 0] = 0
+
+            try:
+                moments = hillas_parameters(geom, pmt_signal)
+                hillas_dict[tel_id] = moments
+            except HillasParameterizationError as e:
+                print(e)
+                continue
+
+        if len(hillas_dict) < 2:
+            continue
+
+        # divergent mode put to on even though the file has parallel pointing.
+        fit_result = fit.predict(hillas_dict, event.inst, array_pointing, telescope_pointings)
+
+        print(fit_result)
+        print(event.mc.core_x, event.mc.core_y)
+        fit_result.alt.to(u.deg)
+        fit_result.az.to(u.deg)
+        fit_result.core_x.to(u.m)
+        assert fit_result.is_valid
