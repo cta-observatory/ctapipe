371c6315c7df3c615b13ab44aedf48e4da30d407
Moritz Hütten <mhuetten@mpp.mpg.de>
2019-07-16 12:26:23 +0100

    test added

---
+++ ctapipe/utils/tests/test_astro.py
@@ -0,0 +1,12 @@
+from ..astro import get_bright_stars
+from astropy.coordinates import SkyCoord
+from astropy import units as u
+
+def test_get_bright_stars():
+
+    pointing = SkyCoord(ra=83.275 * u.deg, dec=21.791 * u.deg, frame='icrs')
+
+    table = get_bright_stars(pointing, radius=5. * u.deg, magnitude_cut=3.5)
+
+    assert len(table) == 1
+    assert table[0]['Name'] == '123Zet Tau'
