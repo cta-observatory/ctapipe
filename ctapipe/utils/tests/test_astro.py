9df553eff780ddc70e2fa4cc33d8d7e526e5d5e7
Moritz Hütten <mhuetten@mpp.mpg.de>
2019-07-16 14:54:04 +0100

    smaller radius in test

--- ctapipe/utils/tests/test_astro.py
+++ ctapipe/utils/tests/test_astro.py
@@ -6,7 +6,7 @@ def test_get_bright_stars():
 
     pointing = SkyCoord(ra=83.275 * u.deg, dec=21.791 * u.deg, frame='icrs')
 
-    table = get_bright_stars(pointing, radius=5. * u.deg, magnitude_cut=3.5)
+    table = get_bright_stars(pointing, radius=2. * u.deg, magnitude_cut=3.5)
 
     assert len(table) == 1
     assert table[0]['Name'] == '123Zet Tau'
