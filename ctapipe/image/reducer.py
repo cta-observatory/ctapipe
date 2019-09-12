7c8fddf67c25ef851069ce172713a7fcd259d4a1
Jonas Hackfeld <Jonas.Hackfeld@rub.de>
2019-09-12 17:34:39 +0200

    Added tailcuts data volume reducer
    - added a data volume reducer using modified tailcuts and dilate
    - added test for tailcuts data volume reducer

--- ctapipe/image/reducer.py
+++ ctapipe/image/reducer.py
@@ -4,11 +4,14 @@ Algorithms for the data volume reduction.
 
 from abc import abstractmethod
 
+import numpy as np
 from ctapipe.core import Component
+from ctapipe.image.cleaning import tailcuts_clean, dilate
 
 __all__ = [
     'DataVolumeReducer',
     'NullDataVolumeReducer',
+    'TailCutsDataVolumeReducer',
 ]
 
 
@@ -44,3 +47,90 @@ class NullDataVolumeReducer(DataVolumeReducer):
 
     def __call__(self, waveforms):
         return waveforms
+
+
+class TailCutsDataVolumeReducer(DataVolumeReducer):
+
+    def __call__(
+        self,
+        geom,
+        waveforms,
+        end_dilates=1,
+        picture_thresh=7,
+        boundary_thresh=5,
+        iteration_thresh=5,
+        keep_isolated_pixels=True,
+        min_number_picture_neighbors=0,
+    ):
+        """
+        Reduce the image in 3 Steps:
+
+        1) Select pixels with tailcuts_clean.
+        2) Add iteratively all pixels with Signal S >= iteration_thresh
+           with ctapipe module dilate until no new pixels were added.
+        3) Adding new pixels with dilate to get more conservative.
+
+        Parameters
+        ----------
+        geom: `ctapipe.instrument.CameraGeometry`
+            Camera geometry information
+        waveforms: ndarray
+            Waveforms stored in a numpy array of shape
+            (n_pix, n_samples).
+        picture_thresh: float or array
+            threshold for tailcuts_clean above which all pixels are retained
+        boundary_thresh: float or array.
+            threshold for tailcuts_clean above which pixels are retained if
+            they have a neighbor already above the picture_thresh.
+        keep_isolated_pixels: bool
+            For tailcuts_clean: If True, pixels above the picture threshold
+            will be included always, if not they are only included if a
+            neighbor is in the picture or boundary.
+        min_number_picture_neighbors: int
+            For tailcuts_clean: A picture pixel survives cleaning only if it
+            has at least this number of picture neighbors. This has no effect
+            in case keep_isolated_pixels is True
+        iteration_thresh: float
+            Threshold for the iteration step 2), above which pixels are
+            selected.
+        end_dilates: int
+            Number of how many times to dilate at the end in Step 3).
+
+        Returns
+        -------
+        reduced_waveforms : ndarray
+            Reduced waveforms stored in a numpy array of shape
+            (n_pix, n_samples).
+
+        """
+
+        reduced_waveforms_mask = np.empty([waveforms.shape[0], 0], dtype=bool)
+
+        for i in range(waveforms.shape[1]):
+            image = waveforms[:, [i]]
+            # 1) Step: TailcutCleaning at first
+            mask = tailcuts_clean(
+                geom=geom,
+                image=image,
+                picture_thresh=picture_thresh,
+                boundary_thresh=boundary_thresh,
+                keep_isolated_pixels=keep_isolated_pixels,
+                min_number_picture_neighbors=min_number_picture_neighbors
+            )
+            pixels_above_iteration_thresh = image >= iteration_thresh
+            mask_for_loop = np.array([])
+            # 2) Step: Add iteratively all pixels with Signal
+            #          S > iteration_thresh with ctapipe module
+            #          'dilate' until no new pixels were added.
+            while not np.array_equal(mask, mask_for_loop):
+                mask_for_loop = mask
+                mask = dilate(geom, mask) & pixels_above_iteration_thresh
+
+            # 3) Step: Adding Pixels with 'dilate' to get more conservative.
+            for p in range(end_dilates):
+                mask = dilate(geom, mask)
+
+            reduced_waveforms_mask = np.column_stack((reduced_waveforms_mask,
+                                                      mask))
+
+        return np.ma.masked_array(waveforms, mask=reduced_waveforms_mask)
--- ctapipe/image/tests/test_reducer.py
+++ ctapipe/image/tests/test_reducer.py
@@ -1,6 +1,8 @@
 import numpy as np
 from numpy.testing import assert_array_equal
 from ctapipe.image.reducer import NullDataVolumeReducer
+from ctapipe.image.reducer import TailCutsDataVolumeReducer
+from ctapipe.instrument import CameraGeometry
 
 
 def test_null_data_volume_reducer():
@@ -8,3 +10,38 @@ def test_null_data_volume_reducer():
     reducer = NullDataVolumeReducer()
     reduced_waveforms = reducer(waveforms)
     assert_array_equal(waveforms, reduced_waveforms)
+
+
+def test_tailcuts_data_volume_reducer():
+    # create set of waveforms and expected masks
+    geom = CameraGeometry.from_name("LSTCam")
+    image = np.zeros_like(geom.pix_id, dtype=np.float)
+    mask = np.zeros_like(geom.pix_id, dtype=np.bool)
+    expected_masks = np.empty([geom.pix_id.shape[0], 0], dtype=bool)
+    created_waveforms = np.empty([geom.pix_id.shape[0], 0], dtype=float)
+    # created set of pixels are connected in one line, not in a blob
+    image[9] = 10.0
+    # Should be selected as core-pixel from Step 1) tailcuts_clean
+    image[[10, 8, 6, 5]] = 4.0
+    mask[[10, 9, 8, 6, 5]] = True
+    # 10 and 8 as boundary-pixel from Step 1) tailcuts_clean
+    # 6 and 5 as iteration-pixel in Step 2)
+    mask[geom.neighbor_matrix_sparse.dot(mask)] = True
+    # pixels from dilate at the end in Step 3)
+
+    for i in range(42):  # more dim
+        expected_masks = np.column_stack((expected_masks, mask))
+        created_waveforms = np.column_stack((created_waveforms, image))
+
+    expected_return = np.ma.masked_array(created_waveforms,
+                                         mask=expected_masks)
+    reducer = TailCutsDataVolumeReducer()
+    reduced_waveforms = reducer(
+        geom=geom,
+        waveforms=created_waveforms,
+        picture_thresh=7,
+        boundary_thresh=3,
+        iteration_thresh=3,
+        end_dilates=1
+    )
+    assert_array_equal(expected_return, reduced_waveforms)
