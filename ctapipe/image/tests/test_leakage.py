4d530d0da496920136a5fb01e8d5e9981598c4b7
Karl Kosack <karl.kosack@cea.fr>
2019-09-30 15:36:12 +0200

    fix forgotten rename in test

--- ctapipe/image/tests/test_leakage.py
+++ ctapipe/image/tests/test_leakage.py
@@ -18,4 +18,4 @@ def test_leakage():
     assert l.intensity_1pix == ratio1
     assert l.intensity_2pix == ratio2
     assert l.percent_1pix == ratio1
-    assert l.leakage2_percent == ratio2
+    assert l.percent_2pix == ratio2
