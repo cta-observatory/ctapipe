062108adb962092c6642f8fce4ab5c7456ddf42f
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2019-10-17 20:23:50 +0200

    Yet more missing deps for the docs

--- setup.py
+++ setup.py
@@ -48,6 +48,7 @@ tests_require = [
 ]
 docs_require = [
     'sphinx_rtd_theme', 'sphinx_automodapi', 'sphinx', 'nbsphinx', 'numpydoc',
+    'jupyter_client',
 ]
 
 package.version.update_release_version()
