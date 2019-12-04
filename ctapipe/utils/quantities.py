646331b0c42699100566375c371bd1356cf31745
Dominik Neise <neised@phys.ethz.ch>
2019-12-04 15:37:32 +0100

    remove lying docs

--- ctapipe/utils/quantities.py
+++ ctapipe/utils/quantities.py
@@ -5,7 +5,6 @@ def all_to_value(*args, unit):
     '''converts all args to value if convertible to the same unit.
 
     - does not copy the data
-    - the unit returned, will be the one of the 1st arg
     - makes sure all args are convertible to the same unit
     - return the values of all args and the unit
     - Raise a meaningful error in case the args are not of a convertible unit.
