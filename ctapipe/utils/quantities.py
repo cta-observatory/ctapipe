7314f7c3be8ac8d1741aeb2cc39a4e2e5cf92d6e
Dominik Neise <neised@phys.ethz.ch>
2019-12-04 15:39:46 +0100

    further fix docs

--- ctapipe/utils/quantities.py
+++ ctapipe/utils/quantities.py
@@ -2,12 +2,11 @@
 from astropy.units import Quantity
 
 def all_to_value(*args, unit):
-    '''converts all args to value if convertible to the same unit.
+    '''converts all `args` to value after converting to `unit`.
 
     - does not copy the data
     - makes sure all args are convertible to the same unit
-    - return the values of all args and the unit
-    - Raise a meaningful error in case the args are not of a convertible unit.
+    - raises a meaningful error in case the args are not of a convertible unit.
 
     Returns: *args_without_unit
     '''
