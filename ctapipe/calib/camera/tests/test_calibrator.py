40f1a4dcfadcf228f435d094f5b38a4e9292c29f
Jason Watson <jason.jw@live.co.uk>
2020-03-13 10:41:58 +0100

    Improve tests for integration_correction

--- ctapipe/calib/camera/tests/test_calibrator.py
+++ ctapipe/calib/camera/tests/test_calibrator.py
@@ -15,6 +15,31 @@ from ctapipe.instrument import CameraGeometry
 from ctapipe.io.containers import DataContainer
 
 
+@pytest.fixture('module')
+def reference_pulse():
+    reference_pulse_step = 0.09
+    n_reference_pulse_samples = 1280
+    reference_pulse_shape = np.array([
+        norm.pdf(np.arange(n_reference_pulse_samples), 600, 100) * 1.7,
+        norm.pdf(np.arange(n_reference_pulse_samples), 700, 100) * 1.7,
+    ])
+    return reference_pulse_shape, reference_pulse_step
+
+
+@pytest.fixture('module')
+def sampled_reference_pulse(reference_pulse):
+    reference_pulse_shape, reference_pulse_step = reference_pulse
+    n_channels, n_reference_pulse_samples = reference_pulse_shape.shape
+    pulse_max_sample = n_reference_pulse_samples * reference_pulse_step
+    sampled_step = 2
+    pulse_shape_x = np.arange(0, pulse_max_sample, reference_pulse_step)
+    sampled_edges = np.arange(0, pulse_max_sample, sampled_step)
+    sampled_pulse = np.array([np.histogram(
+        pulse_shape_x, sampled_edges, weights=reference_pulse_shape[ichan], density=True
+    )[0] for ichan in range(n_channels)])
+    return sampled_pulse, sampled_step
+
+
 def test_camera_calibrator(example_event):
     telid = list(example_event.r0.tel)[0]
     calibrator = CameraCalibrator(subarray=example_event.inst.subarray)
@@ -50,17 +75,46 @@ def test_config():
     assert calibrator.image_extractor.window_width.tel[None] == window_width
 
 
-def test_integration_correction(example_event):
-    telid = list(example_event.r0.tel)[0]
+def test_integration_correction(reference_pulse, sampled_reference_pulse):
+    reference_pulse_shape, reference_pulse_step = reference_pulse
+    sampled_pulse, sampled_step = sampled_reference_pulse
+    sampled_pulse_fc = sampled_pulse[0]  # Test first channel
+    full_integral = np.sum(sampled_pulse[0] * sampled_step)
 
-    width = 7
-    shift = 3
-    shape = example_event.mc.tel[telid].reference_pulse_shape
-    n_chan = shape.shape[0]
-    step = example_event.mc.tel[telid].meta["refstep"]
-    time_slice = example_event.mc.tel[telid].time_slice
-    correction = integration_correction(n_chan, shape, step, time_slice, width, shift)
-    assert correction is not None
+    for window_start in range(0, sampled_pulse_fc.size):
+        for window_end in range(window_start+1, sampled_pulse_fc.size):
+            window_width = window_end - window_start
+            window_shift = sampled_pulse_fc.argmax() - window_start
+            correction = integration_correction(
+                len(reference_pulse_shape), reference_pulse_shape,
+                reference_pulse_step, sampled_step,
+                window_width, window_shift
+            )[0]
+            window_integral = np.sum(
+                sampled_pulse_fc[window_start:window_end] * sampled_step
+            )
+            np.testing.assert_allclose(full_integral, window_integral * correction)
+
+
+def test_integration_correction_outofbounds(reference_pulse, sampled_reference_pulse):
+    reference_pulse_shape, reference_pulse_step = reference_pulse
+    sampled_pulse, sampled_step = sampled_reference_pulse
+    sampled_pulse_fc = sampled_pulse[0]  # Test first channel
+    full_integral = np.sum(sampled_pulse[0] * sampled_step)
+
+    for window_start in range(0, sampled_pulse_fc.size):
+        for window_end in range(sampled_pulse_fc.size-1, sampled_pulse_fc.size*2):
+            window_width = window_end - window_start
+            window_shift = sampled_pulse_fc.argmax() - window_start
+            correction = integration_correction(
+                len(reference_pulse_shape), reference_pulse_shape,
+                reference_pulse_step, sampled_step,
+                window_width, window_shift
+            )[0]
+            window_integral = np.sum(
+                sampled_pulse_fc[window_start:window_end] * sampled_step
+            )
+            np.testing.assert_allclose(full_integral, window_integral * correction)
 
 
 def test_integration_correction_no_ref_pulse(example_event):
