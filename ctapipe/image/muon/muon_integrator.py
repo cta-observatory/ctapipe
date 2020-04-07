f053f29abfb0558204987395d6bfd3e487456936
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2020-04-07 10:48:53 +0200

    Start refactoring muon_line_integrate

--- ctapipe/image/muon/muon_integrator.py
+++ ctapipe/image/muon/muon_integrator.py
@@ -12,14 +12,219 @@ from scipy.ndimage.filters import correlate1d
 from iminuit import Minuit
 from astropy import units as u
 from astropy.constants import alpha
-from ...containers import MuonIntensityParameter
 from scipy.stats import norm
 
-import logging
+from ...containers import MuonIntensityParameter
+
+
+# ratio of the areas of the unit circle and a square of side lengths 2
+CIRCLE_SQUARE_AREA_RATIO = np.pi / 4
+
 
 __all__ = ['MuonLineIntegrate']
 
-logger = logging.getLogger(__name__)
+
+def chord_length(radius, rho, phi):
+    """
+    Function for integrating the length of a chord across a circle
+
+    Parameters
+    ----------
+    radius: float
+        radius of circle
+    rho: float
+        fractional distance of impact point from circle center
+    phi: ndarray in radians or angle Quantity
+        rotation angles to calculate length
+
+    Returns
+    -------
+    ndarray: chord length
+    """
+    scalar = np.isscalar(phi)
+    phi = u.Quantity(phi, ndmin=1, copy=False)
+
+    chord = 1 - (rho**2 * np.sin(phi)**2)
+
+    if rho <= 1.0:
+        # muon has hit the mirror
+        chord = radius * (np.sqrt(chord) + rho * np.cos(phi))
+    else:
+        # muon did not hit the mirror
+        chord = 2 * radius * np.sqrt(chord)
+
+    chord[np.isnan(chord)] = 0
+    chord[chord < 0] = 0
+
+    if scalar:
+        return chord[0]
+    return chord
+
+
+def intersect_circle(mirror_radius, r, angle, hole_radius=0 * u.m):
+    """Perform line integration along a given axis in the mirror frame
+    given an impact point on the mirror
+
+    Parameters
+    ----------
+    angle: ndarray
+        Angle along which to integrate mirror
+
+    Returns
+    --------
+    float: length from impact point to mirror edge
+
+    """
+    mirror_length = chord_length(
+        mirror_radius,
+        (r / mirror_radius).to_value(u.dimensionless_unscaled),
+        angle
+    )
+
+    hole_length = 0 * mirror_length  # .unit
+    if hole_radius.value > 0:
+        hole_length = chord_length(
+            hole_radius,
+            (r / hole_radius).to_value(u.dimensionless_unscaled),
+            angle
+        )
+
+    return mirror_length - hole_length
+
+
+def pixels_on_ring(radius, pixel_diameter):
+    '''Calculate number of pixels of diameter ``pixel_diameter`` on the circumference
+    of a circle with radius ``radius``
+    '''
+    circumference = 2 * np.pi * radius
+    n_pixels = u.Quantity(circumference / pixel_diameter)
+    return int(n_pixels.to_value(u.dimensionless_unscaled))
+
+
+def create_profile(mirror_radius, impact_parameter, radius, phi, pixel_width, oversampling=3):
+    """
+    Perform intersection over all angles and return length
+
+    Parameters
+    ----------
+    impact_parameter: float
+        Impact distance from mirror center
+    ang: ndarray
+        Angles over which to integrate
+    phi: float
+        Rotation angle of muon image
+
+    Returns
+    -------
+    ndarray
+        Chord length for each angle
+    """
+
+    phi = u.Quantity(phi, u.rad, copy=False)
+
+    circumference = 2 * np.pi * radius
+    pixels_on_circle = int(u.Quantity(
+        circumference / pixel_width, copy=False
+    ).to_value(u.dimensionless_unscaled))
+
+    ang = phi + u.Quantity(np.linspace(-np.pi, np.pi, pixels_on_circle * oversampling), u.rad, copy=False)
+
+    length = intersect_circle(mirror_radius, impact_parameter, ang)
+    length = correlate1d(length, np.ones(oversampling), mode='wrap', axis=0)
+    length /= oversampling
+
+    return ang, length
+
+
+def image_prediction(
+    mirror_radius,
+    impact_parameter,
+    phi,
+    center_x,
+    center_y,
+    radius,
+    ring_width,
+    pixel_x,
+    pixel_y,
+    pixel_width,
+    oversampling=3,
+    min_lambda=300 * u.nm,
+    max_lambda=600 * u.nm
+):
+    """Function for producing the expected image for a given set of trial
+    muon parameters
+
+    Parameters
+    ----------
+    impact_parameter: float
+        Impact distance of muon
+    center_x: float
+        Muon ring center in field of view
+    center_y: float
+        Muon ring center in field of view
+    radius: float
+        Radius of muon ring
+    ring_width: float
+        Gaussian width of muon ring
+    pixel_x: ndarray
+        Pixel x coordinate
+    pixel_y: ndarray
+        Pixel y coordinate
+
+    Returns
+    -------
+    ndarray:
+        Predicted signal
+
+    """
+
+    # First produce angular position of each pixel w.r.t muon center
+    ang = np.arctan2(pixel_y - center_y, pixel_x - center_x)
+    # Add muon rotation angle
+    ang += phi
+
+    # Produce smoothed muon profile
+    ang_prof, profile = create_profile(
+        mirror_radius, impact_parameter, radius, phi, pixel_width, oversampling=oversampling,
+    )
+
+    # Produce gaussian weight for each pixel given ring width
+    radial_dist = np.sqrt((pixel_x - center_x)**2 + (pixel_y - center_y)**2)
+    # The weight is the integral of the ring's radial gaussian profile inside the
+    # ring's width
+    delta = pixel_width / 2
+    cdfs = norm.cdf([radial_dist + delta, radial_dist - delta], radius, ring_width)
+    gauss = cdfs[0] - cdfs[1]
+
+    # interpolate profile to find prediction for each pixel
+    pred = u.Quantity(np.interp(ang, ang_prof, profile), u.m, copy=False)
+
+    # Multiply by integrated emissivity between 300 and 600 nm, and rest of factors to
+    # get total number of photons per pixel
+    # ^ would be per radian, but no need to put it here, would anyway cancel out below
+
+    pred *= alpha * (min_lambda**-1 - max_lambda**-1)
+    pred *= (pixel_width / radius).to_value(u.dimensionless_unscaled)
+    # multiply by angle (in radians) subtended by pixel width as seen from ring center
+
+    pred *= np.sin(2 * np.deg2rad(radius))
+
+    # multiply by gaussian weight, to account for "fraction of muon ring" which falls
+    # within the pixel
+    pred *= gauss
+
+    # Now it would be the total light in an area S delimited by: two radii of the
+    # ring, tangent to the sides of the pixel in question, and two circles concentric
+    # with the ring, also tangent to the sides of the pixel.
+    # A rough correction, assuming pixel is round, is introduced here:
+    # [pi*(pixel_width/2)**2]/ S. Actually, for the large rings (relative to pixel
+    # size) we are concerned with, a good enough approximation is the ratio between a
+    # circle's area and that of the square whose side is equal to the circle's
+    # diameter. In any case, since in the end we do a data-MC comparison of the muon
+    # ring analysis outputs, it is not critical that this value is exact.
+    pred *= CIRCLE_SQUARE_AREA_RATIO
+
+    return pred.to_value(u.dimensionless_unscaled)
 
 
 class MuonLineIntegrate:
@@ -58,8 +263,15 @@ class MuonLineIntegrate:
         multiplied by the fine structure constant (1/137)
     """
 
-    def __init__(self, mirror_radius, hole_radius, pixel_width=0.2,
-                 oversample_bins=3, sct_flag=False, secondary_radius=1.):
+    def __init__(
+        self,
+        mirror_radius,
+        hole_radius,
+        pixel_width=0.2,
+        oversample_bins=3,
+        sct_flag=False,
+        secondary_radius=1.
+    ):
 
         self.mirror_radius = mirror_radius
         self.hole_radius = hole_radius
@@ -73,216 +285,9 @@ class MuonLineIntegrate:
         self.prediction = 0
         self.minlambda = 300.e-9 * u.m
         self.maxlambda = 600.e-9 * u.m
-        self.photemit = alpha * (self.minlambda**-1 -
-                                 self.maxlambda**-1)  # 12165.45
         self.unit = u.deg
 
-
-    @staticmethod
-    def chord_length(radius, rho, phi):
-        """
-        Function for integrating the length of a chord across a circle
-
-        Parameters
-        ----------
-        radius: float
-            radius of circle
-        rho: float
-            fractional distance of impact point from array centre
-        phi: ndarray
-            rotation angles to calculate length
-
-        Returns
-        -------
-        ndarray: chord length
-        """
-        chord = 1 - (rho * rho * np.sin(phi) * np.sin(phi))
-        if rho <= 1.0:
-            chord = radius * (np.sqrt(chord) + rho * np.cos(phi))
-        elif rho > 1.0:
-            chord = 2. * radius * np.sqrt(chord)
-
-        chord[np.isnan(chord)] = 0
-        chord[chord < 0] = 0
-
-        return chord
-
-    def intersect_circle(self, r, angle):
-        """Perform line integration along a given axis in the mirror frame
-        given an impact point on the mirrot
-
-        Parameters
-        ----------
-        impact_x: float
-            Impact position on mirror (tilted telescope system)
-        impact_y: float
-            Impact position on mirror (tilted telescope system)
-        angle: float
-            Angle along which to integrate mirror
-
-        Returns
-        --------
-        float: length from impact point to mirror edge
-
-        """
-        mirror_length = self.chord_length(
-            self.mirror_radius, r / self.mirror_radius.value, angle
-        )
-
-        hole_length = 0 * mirror_length  # .unit
-        if self.hole_radius > 0:
-            hole_length = self.chord_length(
-                self.hole_radius, r / self.hole_radius.value, angle
-            )
-
-        if self.sct_flag:
-            self.sct_flag = False  # Do not treat differently for now...work in progress
-
-        if self.sct_flag:
-            secondary_length = self.chord_length(
-                self.secondary_radius, r / self.secondary_radius, angle
-            )
-
-            # Should be areas not lengths here?
-            factor = mirror_length - (secondary_length)
-            factor /= (mirror_length - hole_length)
-
-        if not self.sct_flag:
-            return mirror_length - hole_length
-        else:
-            return (mirror_length - hole_length + secondary_length)
-
-    def plot_pos(self, impact_parameter, radius, phi):
-        """
-        Perform intersection over all angles and return length
-
-        Parameters
-        ----------
-        impact_parameter: float
-            Impact distance from mirror centre
-        ang: ndarray
-            Angles over which to integrate
-        phi: float
-            Rotation angle of muon image
-
-        Returns
-        -------
-        ndarray
-            Chord length for each angle
-        """
-
-        bins = int((2 * np.pi * radius) / self.pixel_width.value) * self.oversample_bins
-        ang = np.linspace(-np.pi + phi, np.pi + phi, bins)
-        l = self.intersect_circle(impact_parameter, ang)
-        l = correlate1d(l, np.ones(self.oversample_bins), mode='wrap', axis=0)
-        l /= self.oversample_bins
-
-        return ang, l
-
-    def pos_to_angle(self, centre_x, centre_y, pixel_x, pixel_y):
-        """
-        Convert pixel positions from x,y coordinates to rotation angle
-
-        Parameters
-        ----------
-        centre_x: float
-            Reconstructed image centre
-        centre_y: float
-            Reconstructed image centre
-        pixel_x: ndarray
-            Pixel x position
-        pixel_y: ndarray
-            Pixel y position
-
-        Returns
-        -------
-         ndarray:
-            Pixel rotation angle
-
-        """
-        del_x = pixel_x - centre_x
-        del_y = pixel_y - centre_y
-
-        ang = np.arctan2(del_x, del_y)
-
-        return ang
-
-    def image_prediction(self, impact_parameter, phi, centre_x,
-                         centre_y, radius, ring_width, pixel_x, pixel_y, ):
-        """Function for producing the expected image for a given set of trial
-        muon parameters
-
-        Parameters
-        ----------
-        impact_parameter: float
-            Impact distance of muon
-        centre_x: float
-            Muon ring centre in field of view
-        centre_y: float
-            Muon ring centre in field of view
-        radius: float
-            Radius of muon ring
-        ring_width: float
-            Gaussian width of muon ring
-        pixel_x: ndarray
-            Pixel x coordinate
-        pixel_y: ndarray
-            Pixel y coordinate
-
-        Returns
-        -------
-        ndarray:
-            Predicted signal
-
-        """
-
-        # First produce angular position of each pixel w.r.t muon centre
-        ang = self.pos_to_angle(centre_x, centre_y, pixel_x, pixel_y)
-        # Add muon rotation angle
-        ang += phi
-        # Produce smoothed muon profile
-
-        ang_prof, profile = self.plot_pos(impact_parameter, radius, phi)
-        # Produce gaussian weight for each pixel given ring width
-        radial_dist = np.sqrt((pixel_x - centre_x)**2 + (pixel_y - centre_y)**2)
-        # The weight is the integral of the ring's radial gaussian profile inside the
-        # ring's width
-        gauss = norm.cdf(radial_dist+0.5*self.pixel_width.value, radius, ring_width) - \
-                norm.cdf(radial_dist-0.5*self.pixel_width.value, radius, ring_width)
-
-        # interpolate profile to find prediction for each pixel
-        pred = np.interp(ang, ang_prof, profile) * u.m
-
-        # Multiply by integrated emissivity between 300 and 600 nm, and rest of factors to
-        # get total number of photons per pixel
-        photval = self.photemit
-        # ^ would be per radian, but no need to put it here, would anyway cancel out below
-
-        pred *= 0.5 * photval
-        pred *= (self.pixel_width.value / radius)
-        # multiply by angle (in radians) subtended by pixel width as seen from ring center
-
-        pred *= np.sin(2 * (radius*u.deg).to_value(u.rad))
-
-        # multiply by gaussian weight, to account for "fraction of muon ring" which falls
-        # within the pixel
-        pred *= gauss
-
-        # Now it would be the total light in an area S delimited by: two radii of the
-        # ring, tangent to the sides of the pixel in question, and two circles concentric
-        # with the ring, also tangent to the sides of the pixel.
-        # A rough correction, assuming pixel is round, is introduced here:
-        # [pi*(pixel_width/2)**2]/ S. Actually, for the large rings (relative to pixel
-        # size) we are concerned with, a good enough approximation is the ratio between a
-        # circle's area and that of the square whose side is equal to the circle's
-        # diameter. In any case, since in the end we do a data-MC comparison of the muon
-        # ring analysis outputs, it is not critical that this value is exact.
-        pred *= 0.7854
-
-        return pred
-
-
-    def likelihood(self, impact_parameter, phi, centre_x, centre_y,
+    def likelihood(self, impact_parameter, phi, center_x, center_y,
                    radius, ring_width, optical_efficiency_muon):
         """
         Likelihood function to be called by minimiser
@@ -290,11 +295,11 @@ class MuonLineIntegrate:
         Parameters
         ----------
         impact_parameter: float
-            Impact distance from telescope centre
-        centre_x: float
-            Centre of muon ring in FoV
-        centre_y: float
-            Centre of muon ring in FoV
+            Impact distance from telescope center
+        center_x: float
+            center of muon ring in FoV
+        center_y: float
+            center of muon ring in FoV
         radius: float
             Radius of muon ring
         ring_width: float
@@ -306,8 +311,8 @@ class MuonLineIntegrate:
         -------
         float: Likelihood that model matches data
         """
-        # centre_x *= self.unit
-        # centre_y *= self.unit
+        # center_x *= self.unit
+        # center_y *= self.unit
         # radius *= self.unit
         # ring_width *= self.unit
         # impact_parameter *= u.m
@@ -317,8 +322,8 @@ class MuonLineIntegrate:
         self.prediction = self.image_prediction(
             impact_parameter,
             phi,
-            centre_x,
-            centre_y,
+            center_x,
+            center_y,
             radius,
             ring_width,
             self.pixel_x.value,
@@ -373,15 +378,15 @@ class MuonLineIntegrate:
 
         return likelihood_value
 
-    def fit_muon(self, centre_x, centre_y, radius, pixel_x, pixel_y, image):
+    def fit_muon(self, center_x, center_y, radius, pixel_x, pixel_y, image):
         """
 
         Parameters
         ----------
-        centre_x: float
-            Centre of muon ring in the field of view from circle fitting
-        centre_y: float
-            Centre of muon ring in the field of view from circle fitting
+        center_x: float
+            center of muon ring in the field of view from circle fitting
+        center_y: float
+            center of muon ring in the field of view from circle fitting
         radius: float
             Radius of muon ring from circle fitting
         pixel_x: ndarray
@@ -403,8 +408,8 @@ class MuonLineIntegrate:
         self.unit = pixel_x.unit
 
         radius.to(u.deg)
-        centre_x.to(u.deg)
-        centre_y.to(u.deg)
+        center_x.to(u.deg)
+        center_y.to(u.deg)
 
         # Return interesting stuff
         fitoutput = MuonIntensityParameter()
@@ -415,8 +420,8 @@ class MuonLineIntegrate:
         init_params['impact_parameter'] = 4.
         init_params['phi'] = 0.
         init_params['radius'] = radius.value
-        init_params['centre_x'] = centre_x.value
-        init_params['centre_y'] = centre_y.value
+        init_params['center_x'] = center_x.value
+        init_params['center_y'] = center_y.value
         init_params['ring_width'] = 0.1
         init_params['optical_efficiency_muon'] = 0.1
         init_errs['error_impact_parameter'] = 2.
@@ -426,13 +431,11 @@ class MuonLineIntegrate:
         init_errs['error_optical_efficiency_muon'] = 0.05
         init_constrain['limit_phi'] = (-np.pi, np.pi)
         init_constrain['fix_radius'] = True
-        init_constrain['fix_centre_x'] = True
-        init_constrain['fix_centre_y'] = True
+        init_constrain['fix_center_x'] = True
+        init_constrain['fix_center_y'] = True
         init_constrain['limit_ring_width'] = (0., 1.)
-        #init_constrain['limit_optical_efficiency_muon'] = (0., 1.)
+        # init_constrain['limit_optical_efficiency_muon'] = (0., 1.)
         # ^Unneeded constraint - and misleading in case of changes leading to >1 values!
-
-        logger.debug("radius = %3.3f pre migrad", radius)
 
         # Create Minuit object with first guesses at parameters
         # strip away the units as Minuit doesnt like them
--- ctapipe/image/muon/tests/test_muon_integrator.py
+++ ctapipe/image/muon/tests/test_muon_integrator.py
@@ -1,15 +1,21 @@
-import numpy as np
 import astropy.units as u
-
-from ctapipe.image.muon import muon_integrator
 
 
 def test_chord_length():
+    from ctapipe.image.muon.muon_integrator import chord_length
+
     radius = 12 * u.m
-    rho = 0.1
-    phi = np.asarray([0, 10, 20, 30]) * u.degree
-    chord_length = muon_integrator.MuonLineIntegrate.chord_length(radius, rho, phi)
-    assert(chord_length is not np.nan)
+    rho = 0.0
+    phi = 0 * u.deg
+
+    length = chord_length(radius, rho, phi)
+    assert length == radius
+
+    rho = 1
+    phi = 90 * u.deg
+    length = chord_length(radius, rho, phi)
+    assert u.isclose(length, 0 * u.m, atol=1e-15 * u.m)
+
 
 if __name__ == '__main__':
     test_chord_length()
