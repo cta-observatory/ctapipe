65079fd73828491610fcd6266c1725d74bcb291e
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2020-04-08 13:54:43 +0200

    Add kundu fit to __all__

--- ctapipe/image/muon/__init__.py
+++ ctapipe/image/muon/__init__.py
@@ -1,4 +1,4 @@
-from .fitting import *
+from .fitting import kundu_chaudhuri_circle_fit
 from .features import *
 from .muon_ring_finder import *
 from .intensity_fitter import MuonIntensityFitter
@@ -6,4 +6,5 @@ from .intensity_fitter import MuonIntensityFitter
 
 __all__ = [
     'MuonIntensityFitter',
+    'kundu_chaudhuri_circle_fit',
 ]
