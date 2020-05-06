86ec7b899fc80bf7c475edb48b85e2f2e82c8e31
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2020-05-06 18:50:40 +0200

    Hdf5 column order (#1308)
    * Avoid copies for astropy units in hdftablewriter
    
    * Make sure columns are written in order they were given
    
    * Add continue for duplicated columns

--- ctapipe/io/hdf5tableio.py
+++ ctapipe/io/hdf5tableio.py
@@ -144,6 +144,7 @@ class HDF5TableWriter(TableWriter):
         meta = {}  # any extra meta-data generated here (like units, etc)
 
         # create pytables schema description for the given container
+        pos = 0
         for container in containers:
             for col_name, value in container.items(add_prefix=self.add_prefix):
 
@@ -152,6 +153,10 @@ class HDF5TableWriter(TableWriter):
 
                 if self._is_column_excluded(table_name, col_name):
                     self.log.debug("excluded column: %s/%s", table_name, col_name)
+                    continue
+
+                if col_name in Schema.columns:
+                    self.log.warning(f'Found duplicated column {col_name}, skipping')
                     continue
 
                 # apply any user-defined transforms first
@@ -188,18 +193,27 @@ class HDF5TableWriter(TableWriter):
                     typename = value.dtype.name
                     coltype = PYTABLES_TYPE_MAP[typename]
                     shape = value.shape
-                    Schema.columns[col_name] = coltype(shape=shape)
+                    Schema.columns[col_name] = coltype(shape=shape, pos=pos)
 
-                if isinstance(value, Time):
+                elif isinstance(value, Time):
                     # TODO: really should use MET, but need a func for that
-                    Schema.columns[col_name] = tables.Float64Col()
+                    Schema.columns[col_name] = tables.Float64Col(pos=pos)
                     self.add_column_transform(table_name, col_name, tr_time_to_float)
 
                 elif type(value).__name__ in PYTABLES_TYPE_MAP:
                     typename = type(value).__name__
                     coltype = PYTABLES_TYPE_MAP[typename]
-                    Schema.columns[col_name] = coltype()
+                    Schema.columns[col_name] = coltype(pos=pos)
 
+                else:
+                    self.log.warning(
+                        f'Column {col_name} of'
+                        f' container {container.__class__.__name__}'
+                        'not writable, skipping'
+                    )
+                    continue
+
+                pos += 1
                 self.log.debug(
                     "Table %s: added col: %s type: %s shape: %s",
                     table_name,
@@ -440,7 +454,7 @@ class HDF5TableReader(TableReader):
 
 
 def tr_convert_and_strip_unit(quantity, unit):
-    return quantity.to(unit).value
+    return quantity.to_value(unit)
 
 
 def tr_list_to_mask(thelist, length):
@@ -455,4 +469,4 @@ def tr_time_to_float(thetime):
 
 
 def tr_add_unit(value, unitname):
-    return Quantity(value, unitname)
+    return Quantity(value, unitname, copy=False)
