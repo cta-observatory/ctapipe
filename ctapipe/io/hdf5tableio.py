8ad39bfdda17ddfb7c5da0a7b094757c7e45dd3c
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2020-05-07 15:50:23 +0200

    Fix writing of default image parameters container (#1311)

--- ctapipe/io/hdf5tableio.py
+++ ctapipe/io/hdf5tableio.py
@@ -211,7 +211,7 @@ class HDF5TableWriter(TableWriter):
                     self.log.warning(
                         f'Column {col_name} of'
                         f' container {container.__class__.__name__}'
-                        'not writable, skipping'
+                        ' not writable, skipping'
                     )
                     continue
 
@@ -275,9 +275,15 @@ class HDF5TableWriter(TableWriter):
             )
             for colname, value in selected_fields:
 
-                value = self._apply_col_transform(table_name, colname, value)
-
-                row[colname] = value
+                try:
+                    value = self._apply_col_transform(table_name, colname, value)
+                    row[colname] = value
+                except Exception:
+                    self.log.error(
+                        f'Error writing col "{colname}" of'
+                        f' container "{container.__class__.__name__}"'
+                    )
+                    raise
         row.append()
 
     def write(self, table_name, containers):
