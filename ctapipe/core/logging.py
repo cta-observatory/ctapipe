9c7abbb666e1c64274966fa3883333c8e621fbfc
Noah Biederbeck <nbiederbeck@tutanota.com>
2020-05-11 09:50:06 +0200

    break out method into function

--- ctapipe/core/logging.py
+++ ctapipe/core/logging.py
@@ -63,62 +63,28 @@ class FancyFormatter(logging.Formatter):
     def format(self, record):
         rec = record.__dict__.copy()
 
-        rec["levelname"] = self.apply_colors(record.levelname)
+        rec["levelname"] = apply_colors(record.levelname)
         rec["message"] = record.getMessage()
 
         return self._fmt % rec
 
-    def apply_colors(self, levelname):
-        black, red, green, yellow, blue, magenta, cyan, white = range(8)
-        reset_seq = "\033[0m"
-        color_seq = "\033[1;%dm"
-        colors = {
-            'INFO': green,
-            'DEBUG': blue,
-            'WARNING': yellow,
-            'CRITICAL': magenta,
-            'ERROR': red
-        }
 
-        if levelname in colors:
-            levelname_color = (
-                color_seq % (30 + colors[levelname])
-                + levelname + reset_seq
-            )
-        return levelname_color
+def apply_colors(levelname):
+    """Use ANSI escape sequences to add colors the levelname of log entries."""
+    _black, red, green, yellow, blue, magenta, _cyan, _white = range(8)
+    reset_seq = "\033[0m"
+    color_seq = "\033[1;%dm"
+    colors = {
+        'INFO': green,
+        'DEBUG': blue,
+        'WARNING': yellow,
+        'CRITICAL': magenta,
+        'ERROR': red
+    }
 
-
-class ColoredFormatter(logging.Formatter):
-    """
-    Custom logging.Formatter that adds colors in addition to the original
-    Application logger functionality from LevelFormatter (in application.py)
-    """
-    highlevel_limit = logging.WARN
-    highlevel_format = " %(levelname)s |"
-
-    def format(self, record):
-        black, red, green, yellow, blue, magenta, cyan, white = range(8)
-        reset_seq = "\033[0m"
-        color_seq = "\033[1;%dm"
-        colors = {
-            'INFO': green,
-            'DEBUG': blue,
-            'WARNING': yellow,
-            'CRITICAL': magenta,
-            'ERROR': red
-        }
-
-        levelname = record.levelname
-        if levelname in colors:
-            levelname_color = (
-                color_seq % (30 + colors[levelname])
-                + levelname + reset_seq
-            )
-            record.levelname = levelname_color
-
-        if record.levelno >= self.highlevel_limit:
-            record.highlevel = self.highlevel_format % record.__dict__
-        else:
-            record.highlevel = ""
-
-        return super().format(record)
+    if levelname in colors:
+        levelname_color = (
+            color_seq % (30 + colors[levelname])
+            + levelname + reset_seq
+        )
+    return levelname_color
