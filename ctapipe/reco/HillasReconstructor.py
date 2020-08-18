c1752ae0ce3dfaee7eeab52c5244041587b7dbe3
Michele Peresano <michele.peresano@.cea.fr>
2020-08-18 15:40:44 +0200

    Generalize offset coordinate and divergent pointing

--- ctapipe/reco/HillasReconstructor.py
+++ ctapipe/reco/HillasReconstructor.py
@@ -15,6 +15,7 @@ from itertools import combinations
 
 from ctapipe.coordinates import (
     CameraFrame,
+    TelescopeFrame,
     GroundFrame,
     TiltedGroundFrame,
     project_to_ground,
@@ -224,13 +225,15 @@ class HillasReconstructor(Reconstructor):
                 az=telescopes_pointings[tel_id].az,
                 frame=horizon_frame,
             )
+            
+            focal_length = subarray.tel[tel_id].optics.equivalent_focal_length
+            unit = moments.x.unit
+            
+            # we just need any point on the main shower axis a bit away from the cog
+            p2_x = moments.x + 0.1 * unit * np.cos(moments.psi)
+            p2_y = moments.y + 0.1 * unit * np.sin(moments.psi)
 
             if moments.x.unit == u.Unit("m"):  # Image parameters are in CameraFrame
-
-                # we just need any point on the main shower axis a bit away from the cog
-                p2_x = moments.x + 0.1 * u.m * np.cos(moments.psi)
-                p2_y = moments.y + 0.1 * u.m * np.sin(moments.psi)
-                focal_length = subarray.tel[tel_id].optics.equivalent_focal_length
 
                 camera_frame = CameraFrame(
                     focal_length=focal_length, telescope_pointing=pointing
@@ -239,33 +242,9 @@ class HillasReconstructor(Reconstructor):
                 cog_coord = SkyCoord(x=moments.x, y=moments.y, frame=camera_frame,)
                 p2_coord = SkyCoord(x=p2_x, y=p2_y, frame=camera_frame)
 
-                # ==============
-                # DIVERGENT MODE
-
-                # re-project from sky to a "fake"-parallel-pointing telescope
-                # then recalculate the psi angle
-
-                # WARNING: this part will need to be reproduced accordingly in the TelescopeFrame case!
-
-                if self.divergent_mode:
-                    camera_frame_parallel = CameraFrame(
-                        focal_length=focal_length, telescope_pointing=array_pointing
-                    )
-                    cog_sky_to_parallel = cog_coord.transform_to(camera_frame_parallel)
-                    p2_sky_to_parallel = p2_coord.transform_to(camera_frame_parallel)
-                    angle_psi_corr = np.arctan2(
-                        cog_sky_to_parallel.y - p2_sky_to_parallel.y,
-                        cog_sky_to_parallel.x - p2_sky_to_parallel.x,
-                    )
-                    self.corrected_angle_dict[tel_id] = angle_psi_corr
-
-                # ============
-
             else:  # Image parameters are already in TelescopeFrame
-
-                # we just need any point on the main shower axis a bit away from the cog
-                p2_x = moments.x + 0.1 * u.deg * np.cos(moments.psi)
-                p2_y = moments.y + 0.1 * u.deg * np.sin(moments.psi)
+            
+                
 
                 telescope_frame = TelescopeFrame(telescope_pointing=pointing)
 
@@ -273,6 +252,25 @@ class HillasReconstructor(Reconstructor):
                     fov_lon=moments.x, fov_lat=moments.y, frame=telescope_frame,
                 )
                 p2_coord = SkyCoord(fov_lon=p2_x, fov_lat=p2_y, frame=telescope_frame)
+                
+            # DIVERGENT MODE
+            # re-project from sky to a "fake"-parallel-pointing telescope
+            # then recalculate the psi angle
+            # In this case there is a posterior transformation to a parallel 
+            # so the origin of the coordinates shouldn't matter
+            # WARNING: possibly missing unit-test!
+            if self.divergent_mode:
+                
+                camera_frame_parallel = CameraFrame(
+                    focal_length=focal_length, telescope_pointing=array_pointing
+                )
+                cog_sky_to_parallel = cog_coord.transform_to(camera_frame_parallel)
+                p2_sky_to_parallel = p2_coord.transform_to(camera_frame_parallel)
+                angle_psi_corr = np.arctan2(
+                    cog_sky_to_parallel.y - p2_sky_to_parallel.y,
+                    cog_sky_to_parallel.x - p2_sky_to_parallel.x,
+                )
+                self.corrected_angle_dict[tel_id] = angle_psi_corr
 
             cog_coord = cog_coord.transform_to(horizon_frame)
             p2_coord = p2_coord.transform_to(horizon_frame)
