b927abb47d50be397d01227ff0d6122ae393f2a9
Karl Kosack <karl.kosack@cea.fr>
2020-09-16 15:53:48 +0200

    add function to get astropy table from ctapipe h5 file

---
+++ ctapipe/io/astropy_helpers.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import tables
+from astropy.table import Table
+from astropy.units import Unit
+
+__all__ = ["h5_table_to_astropy"]
+
+
+def h5_table_to_astropy(h5file, path) -> Table:
+    """Get a table from a ctapipe-format HDF5 table as an `astropy.table.Table`
+    object, retaining units. This uses the same unit storage convention as
+    defined by the `HDF5TableWriter`, namely that the units are in attributes
+    named by `<column name>_UNIT` that are parsible by `astropy.units`.
+
+    Parameters
+    ----------
+    h5file: Union[str, tables.file.File]
+       input filename or PyTables file handle
+    path: str
+       path to table in the file
+
+    """
+
+    should_close_file = False
+    if not isinstance(h5file, tables.file.File) and isinstance(h5file, str):
+        h5file = tables.open_file(h5file)
+        should_close_file = True
+    else:
+        raise ValueError(
+            f"expected a string or PyTables filehandle for argument 'h5file', got {h5file}"
+        )
+
+    table: tables.table.Table = h5file.get_node(path)
+
+    column_units = {}  # mapping of colname to unit
+    for attr in table.attrs._f_list():
+        if attr.endswith("_UNIT"):
+            colname = attr[:-5]
+            column_units[colname] = table.attrs[attr]
+
+    astropy_table = Table(table[:])
+
+    for column, unit in column_units.items():
+        astropy_table[column].unit = Unit(unit)
+
+    if should_close_file:
+        h5file.close()
+
+    return astropy_table
