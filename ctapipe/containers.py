6b64c3be416066066a5cbfcb56d236ea5089bdbc
andres-baquero <obaquero@ucm.es>
2020-10-06 13:34:14 +0000

    Implementation of Hillas Parameters uncertainties

--- ctapipe/containers.py
+++ ctapipe/containers.py
@@ -112,9 +112,9 @@ class HillasParametersContainer(Container):
     phi = Field(nan * u.deg, "polar coordinate of centroid", unit=u.deg)
 
     length = Field(nan * u.m, "standard deviation along the major-axis", unit=u.m)
-    length = Field(nan * u.m, "standard deviation along the major-axis", unit=u.m)
+    length_uncertainty = Field(nan * u.m, "uncertainty of legth", unit=u.m)
     width = Field(nan * u.m, "standard spread along the minor-axis", unit=u.m)
-    length = Field(nan * u.m, "standard deviation along the major-axis", unit=u.m)
+    width_uncertainty = Field(nan * u.m, "uncertainty of width", unit=u.m)
     psi = Field(nan * u.deg, "rotation angle of ellipse", unit=u.deg)
 
     skewness = Field(nan, "measure of the asymmetry")
