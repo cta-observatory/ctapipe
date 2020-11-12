958227ad5d3bc64d7549c731b33c69e15eac51f9
Karl Kosack <karl.kosack@cea.fr>
2020-11-12 18:00:24 +0100

    also process images in example

--- examples/load_one_event.py
+++ examples/load_one_event.py
@@ -5,6 +5,7 @@ purposes
 import sys
 
 from ctapipe.calib import CameraCalibrator
+from ctapipe.image import ImageProcessor
 from ctapipe.io import event_source
 from ctapipe.utils import get_dataset_path
 
@@ -17,7 +18,12 @@ if __name__ == "__main__":
 
     with event_source(filename, max_events=1) as source:
         calib = CameraCalibrator(subarray=source.subarray)
+        process_images = ImageProcessor(
+            subarray=source.subarray, is_simulation=source.is_simulation
+        )
+
         for event in source:
             calib(event)
+            process_images(event)
 
     print(event)
