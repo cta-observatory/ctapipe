187a11ede12bbabd4560b067f1e3452959b469af
Karl Kosack <karl.kosack@cea.fr>
2021-01-29 17:17:50 +0100

    also update data model version number
    and add a history field for future use.

--- ctapipe/io/dl1eventsource.py
+++ ctapipe/io/dl1eventsource.py
@@ -28,7 +28,7 @@ from ctapipe.utils import IndexFinder
 logger = logging.getLogger(__name__)
 
 
-COMPATIBLE_DL1_VERSIONS = ["v1.0.0", "v1.0.1", "v1.0.2"]
+COMPATIBLE_DL1_VERSIONS = ["v1.0.0", "v1.0.1", "v1.0.2", "v1.0.3"]
 
 
 class DL1EventSource(EventSource):
