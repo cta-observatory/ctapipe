1668825b55f7693da73d95ce7e7a299b2a262679
Karl Kosack <kosack@users.noreply.github.com>
2021-01-29 19:05:00 +0100

    to support eventio 1.5 update dtype of true_image (#1585)
    * to support eventio 1.5 update dtype of true_image
    
    * also update data model version number
    
    and add a history field for future use.
    
    * fix also the default type in SimTelEventSource
    
    * do not write lots of 0s out when no photoelectron true_image is present
    
    * remove superfluous comment
    
    * small fix to display_integrator for missing true_image

--- ctapipe/io/simteleventsource.py
+++ ctapipe/io/simteleventsource.py
@@ -398,7 +398,7 @@ class SimTelEventSource(EventSource):
                 true_image = (
                     array_event.get("photoelectrons", {})
                     .get(tel_id - 1, {})
-                    .get("photoelectrons", np.zeros(n_pixels, dtype="float32"))
+                    .get("photoelectrons", None)
                 )
 
                 data.simulation.tel[tel_id] = SimulatedCameraContainer(
