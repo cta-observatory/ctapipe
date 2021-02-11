bf2d2e473371c72aaa39efebb2b478735c83e425
Julian Sitarek <jsitarek@uni.lodz.pl>
2021-02-11 11:44:37 +0100

    follow up of PR 1568
    removed a monkeypatch that is unnecessary after PR 1451

--- ctapipe/image/tests/test_sliding_window_correction.py
+++ ctapipe/image/tests/test_sliding_window_correction.py
@@ -12,58 +12,47 @@ from ctapipe.image.extractor import SlidingWindowMaxSum, ImageExtractor
 from ctapipe.image.toymodel import WaveformModel
 from ctapipe.instrument import SubarrayDescription, TelescopeDescription
 
-from ctapipe.utils import datasets
 
-
-def test_sw_pulse_lst(monkeypatch):
+def test_sw_pulse_lst():
     """
     Test function of sliding window extractor for LST camera pulse shape with
 the correction for the integration window completeness
     """
 
-    with monkeypatch.context() as monkey_patch:
-        monkey_patch.setattr(
-            datasets,
-            "DEFAULT_URL",
-            "http://cccta-dataserver.in2p3.fr/data/ctapipe-extra/v0.3.2/",
-        )
+    # prepare array with 1 LST
+    subarray = SubarrayDescription(
+        "LST1",
+        tel_positions={1: np.zeros(3) * u.m},
+        tel_descriptions={
+            1: TelescopeDescription.from_name(optics_name="LST", camera_name="LSTCam")
+        },
+    )
 
-        # prepare array with 1 LST
-        subarray = SubarrayDescription(
-            "LST1",
-            tel_positions={1: np.zeros(3) * u.m},
-            tel_descriptions={
-                1: TelescopeDescription.from_name(
-                    optics_name="LST", camera_name="LSTCam"
-                )
-            },
-        )
+    telid = list(subarray.tel.keys())[0]
 
-        telid = list(subarray.tel.keys())[0]
+    n_pixels = subarray.tel[telid].camera.geometry.n_pixels
+    n_samples = 40
+    readout = subarray.tel[telid].camera.readout
 
-        n_pixels = subarray.tel[telid].camera.geometry.n_pixels
-        n_samples = 40
-        readout = subarray.tel[telid].camera.readout
+    random = np.random.RandomState(1)
+    min_charge = 100
+    max_charge = 1000
+    charge_true = random.uniform(min_charge, max_charge, n_pixels)
+    time_true = random.uniform(
+        n_samples // 2 - 1, n_samples // 2 + 1, n_pixels
+    ) / readout.sampling_rate.to_value(u.GHz)
 
-        random = np.random.RandomState(1)
-        min_charge = 100
-        max_charge = 1000
-        charge_true = random.uniform(min_charge, max_charge, n_pixels)
-        time_true = random.uniform(
-            n_samples // 2 - 1, n_samples // 2 + 1, n_pixels
-        ) / readout.sampling_rate.to_value(u.GHz)
+    waveform_model = WaveformModel.from_camera_readout(readout)
+    waveform = waveform_model.get_waveform(charge_true, time_true, n_samples)
+    selected_gain_channel = np.zeros(charge_true.size, dtype=np.int)
 
-        waveform_model = WaveformModel.from_camera_readout(readout)
-        waveform = waveform_model.get_waveform(charge_true, time_true, n_samples)
-        selected_gain_channel = np.zeros(charge_true.size, dtype=np.int)
+    # define extractor
+    config = Config({"SlidingWindowMaxSum": {"window_width": 8}})
+    extractor = SlidingWindowMaxSum(subarray=subarray)
+    extractor = ImageExtractor.from_name(
+        "SlidingWindowMaxSum", subarray=subarray, config=config
+    )
 
-        # define extractor
-        config = Config({"SlidingWindowMaxSum": {"window_width": 8}})
-        extractor = SlidingWindowMaxSum(subarray=subarray)
-        extractor = ImageExtractor.from_name(
-            "SlidingWindowMaxSum", subarray=subarray, config=config
-        )
-
-        charge, _ = extractor(waveform, telid, selected_gain_channel)
-        print(charge / charge_true)
-        assert_allclose(charge, charge_true, rtol=0.02)
+    charge, _ = extractor(waveform, telid, selected_gain_channel)
+    print(charge / charge_true)
+    assert_allclose(charge, charge_true, rtol=-0.02)
