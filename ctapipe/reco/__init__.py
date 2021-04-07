1ccb56dba932b7446d0adcd0114c85fc91e7c036
Michele Peresano <michele.peresano@cea.fr>
2021-04-07 17:07:24 +0200

    add class to init

--- ctapipe/reco/__init__.py
+++ ctapipe/reco/__init__.py
@@ -1,6 +1,10 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from .HillasReconstructor import HillasReconstructor, Reconstructor
 from .ImPACT import ImPACTReconstructor
+from .shower_processor import ShowerProcessor
 
 
-__all__ = ["HillasReconstructor", "Reconstructor", "ImPACTReconstructor"]
+__all__ = ["ShowerProcessor",
+           "HillasReconstructor",
+           "Reconstructor",
+           "ImPACTReconstructor"]
