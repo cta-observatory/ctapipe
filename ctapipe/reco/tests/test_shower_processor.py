5cd82d8b0c17c9ccb2426e027cc9e1e34c6db799
Michele Peresano <michele.peresano@cea.fr>
2021-04-08 12:49:41 +0200

    Expand test to all possible usage cases

--- ctapipe/reco/tests/test_shower_processor.py
+++ ctapipe/reco/tests/test_shower_processor.py
@@ -1,7 +1,10 @@
 """
 Tests for ShowerProcessor functionalities.
 """
+import pytest
 from numpy import isfinite
+
+from traitlets.config.loader import Config
 
 from ctapipe.calib import CameraCalibrator
 from ctapipe.image import ImageProcessor
@@ -12,35 +15,78 @@ def test_shower_processor_geometry(example_event, example_subarray):
     """Ensure we get shower geometry when we input an event with parametrized images."""
 
     calibrate = CameraCalibrator(subarray=example_subarray)
+
     process_images = ImageProcessor(
         subarray=example_subarray,
         is_simulation=True,
         image_cleaner_type="MARSImageCleaner",
     )
+
     process_shower = ShowerProcessor(
         subarray=example_subarray,
         is_simulation=True,
         reconstruct_energy=False,
-        reconstruct_classification=False)
+        classify=False
+    )
 
     calibrate(example_event)
     process_images(example_event)
 
-    # config = traitlets.config({
-    #
-    #     "ShowerQualityQuery" : "quality_criteria": [
-    # ["enough_pixels", "lambda im: np.count_nonzero(im) > 3"]
-    # ]
-    #
-    # })
+    process_shower(example_event)
+    print(process_shower.check_shower.to_table())
+
+    DL2a = example_event.dl2.shower["HillasReconstructor"]
+    print(DL2a)
+    assert isfinite(DL2a.alt)
+    assert isfinite(DL2a.az)
+    assert isfinite(DL2a.core_x)
+    assert isfinite(DL2a.core_x)
+    assert isfinite(DL2a.core_y)
+    assert DL2a.is_valid
+    assert isfinite(DL2a.average_intensity)
+
+    # Increase some quality cuts and check that we get defaults
+    config = Config()
+    config.ShowerQualityQuery.quality_criteria = [("> 500 phes", "lambda hillas: hillas.intensity > 500")]
+
+    process_shower = ShowerProcessor(
+        config=config,
+        subarray=example_subarray,
+        is_simulation=True,
+        reconstruct_energy=False,
+        classify=False
+    )
 
     process_shower(example_event)
+    print(process_shower.check_shower.to_table())
 
-    for dl2 in example_event.dl2.shower.values():
-        assert isfinite(event.dl2.shower['HillasReconstructor'].alt)
-        assert isfinite(event.dl2.shower['HillasReconstructor'].az)
-        assert isfinite(event.dl2.shower['HillasReconstructor'].core_x)
-        assert isfinite(event.dl2.shower['HillasReconstructor'].core_x)
-        assert isfinite(event.dl2.shower['HillasReconstructor'].core_y)
-        assert event.dl2.shower['HillasReconstructor'].is_valid
-        assert isfinite(event.dl2.shower['HillasReconstructor'].average_intensity)
+    DL2a = example_event.dl2.shower["HillasReconstructor"]
+    assert not isfinite(DL2a.alt)
+    assert not isfinite(DL2a.az)
+    assert not isfinite(DL2a.core_x)
+    assert not isfinite(DL2a.core_x)
+    assert not isfinite(DL2a.core_y)
+    assert not DL2a.is_valid
+    assert not isfinite(DL2a.average_intensity)
+
+    # Now check that if energy reconstruction is enabled we get a TODO error
+    process_shower = ShowerProcessor(
+        config=config,
+        subarray=example_subarray,
+        is_simulation=True,
+        reconstruct_energy=True,
+        classify=False
+    )
+    with pytest.raises(NotImplementedError) as error_info:
+        process_shower(example_event)
+
+    # also for classification
+    process_shower = ShowerProcessor(
+        config=config,
+        subarray=example_subarray,
+        is_simulation=True,
+        reconstruct_energy=False,
+        classify=True
+    )
+    with pytest.raises(NotImplementedError) as error_info:
+        process_shower(example_event)
