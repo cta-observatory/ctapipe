40c123280860296dc4200e826739b2eda860a8d0
Michele Peresano <michele.peresano@cea.fr>
2021-05-07 10:07:33 +0200

    Fix imports

--- ctapipe/reco/__init__.py
+++ ctapipe/reco/__init__.py
@@ -1,10 +1,12 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-from .HillasReconstructor import HillasReconstructor, Reconstructor
-from .ImPACT import ImPACTReconstructor
+from .hillas_reconstructor import HillasReconstructor, Reconstructor
+from .impact import ImPACTReconstructor
 from .shower_processor import ShowerProcessor
+from .hillas_intersection import HillasIntersection
 
 
 __all__ = ["ShowerProcessor",
            "HillasReconstructor",
            "Reconstructor",
-           "ImPACTReconstructor"]
+           "ImPACTReconstructor",
+           "HillasIntersection"]
