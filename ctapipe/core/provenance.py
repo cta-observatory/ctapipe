181801b8d95bb1d6d619c3772eef7ad51dedccd7
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2021-05-10 14:47:53 +0200

    Store package versions in provenance (#1685)

--- ctapipe/core/provenance.py
+++ ctapipe/core/provenance.py
@@ -15,14 +15,17 @@ from contextlib import contextmanager
 from importlib import import_module
 from os.path import abspath
 
+import pkg_resources
+
 import psutil
 from astropy.time import Time
 from pkg_resources import get_distribution
 
 import ctapipe
-from .support import Singleton
 from collections import UserList
 from pathlib import Path
+
+from .support import Singleton
 
 log = logging.getLogger(__name__)
 
@@ -282,6 +285,13 @@ class _ActivityProvenance:
         return self._prov
 
 
+def _get_python_packages():
+    return [
+        {"name": p.project_name, "version": p.version, "path": p.module_path}
+        for p in sorted(pkg_resources.working_set, key=lambda p: p.project_name)
+    ]
+
+
 def _get_system_provenance():
     """return JSON string containing provenance for all things that are
     fixed during the runtime"""
@@ -312,6 +322,7 @@ def _get_system_provenance():
             version=platform.python_version_tuple(),
             compiler=platform.python_compiler(),
             implementation=platform.python_implementation(),
+            packages=_get_python_packages(),
         ),
         environment=_get_env_vars(),
         arguments=sys.argv,
