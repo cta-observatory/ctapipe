a514d768a72ef6ba38759366f177f57fe5001f4b
Michele Peresano <michele.peresano@cea.fr>
2021-05-20 16:41:52 +0200

    Import reconstructor from correct submodule

--- ctapipe/reco/__init__.py
+++ ctapipe/reco/__init__.py
@@ -1,5 +1,6 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-from .hillas_reconstructor import HillasReconstructor, Reconstructor
+from .hillas_reconstructor import HillasReconstructor
+from .reco_algorithms import Reconstructor
 from .shower_processor import ShowerProcessor
 from .hillas_intersection import HillasIntersection
 from .impact import ImPACTReconstructor
