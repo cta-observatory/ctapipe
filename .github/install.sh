6d0051e1320e9bb2e30ea09ae6bd67441afc7bc3
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2021-06-02 13:56:31 +0200

    Parallel tests (#1740)
    * Add and use parametrized fixture for camera geometries, fixes #1641
    
    * Fix tmp_path and fixture usage in tool and hdf5 writer tests
    
    * Run tests in parallel in CI
    
    * Fix unused imports
    
    * Fix a race condition in download_file_cached
    
    * Use n=auto for parallel tests in CI
    
    * Use mamba in CI

--- .github/install.sh
+++ .github/install.sh
@@ -8,10 +8,13 @@ if [[ "$INSTALL_METHOD" == "conda" ]]; then
   conda update -q conda  # get latest conda version
   # Useful for debugging any issues with conda
   conda info -a
+  conda install -c conda-forge mamba
 
   sed -i -e "s/- python=.*/- python=$PYTHON_VERSION/g" environment.yml
-  conda env create -n ci --file environment.yml
+  mamba env create -n ci --file environment.yml
   conda activate ci
+  echo 'source $CONDA/etc/profile.d/conda.sh' >> ~/.bash_profile
+  echo 'conda activate ci' >> ~/.bash_profile
 else
   echo "Using pip"
   pip install -U pip setuptools wheel
--- ctapipe/instrument/camera/tests/test_description.py
+++ ctapipe/instrument/camera/tests/test_description.py
@@ -1,14 +1,6 @@
 from ctapipe.instrument import CameraDescription
 
 
-def test_known_camera_names(camera_geometries):
+def test_known_camera_names(camera_geometry):
     """ Check that we can get a list of known camera names """
-    cams = CameraDescription.get_known_camera_names()
-    assert len(cams) > 4
-    assert "FlashCam" in cams
-    assert "NectarCam" in cams
-
-    # TODO: Requires camreadout files to be generated
-    # for cam in cams:
-    #     camera = CameraDescription.from_name(cam)
-    #     camera.info()
+    assert camera_geometry.camera_name in CameraDescription.get_known_camera_names()
