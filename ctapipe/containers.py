4d99404198bfdc25478b924d8243012669d12fd5
Michele Peresano <michele.peresano@cea.fr>
2021-06-03 12:05:47 +0200

    add core position in the tilted frame to ReconstructedGeometryContainer

--- ctapipe/containers.py
+++ ctapipe/containers.py
@@ -512,10 +512,27 @@ class ReconstructedGeometryContainer(Container):
         nan * u.m, "reconstructed x coordinate of the core position", unit=u.m
     )
     core_y = Field(
-        nan * u.m, "reconstructed y coordinate of the core position", unit=u.m
+        nan * u.m,
+        "reconstructed y coordinate of the core position",
+        unit=u.m
     )
     core_uncert = Field(
-        nan * u.m, "uncertainty of the reconstructed core position", unit=u.m
+        nan * u.m,
+        "uncertainty of the reconstructed core position in the ground frame",
+        unit=u.m
+    )
+    core_tilted_x = Field(
+        nan * u.m, "reconstructed x coordinate of the core position", unit=u.m
+    )
+    core_tilted_y = Field(
+        nan * u.m,
+        "reconstructed y coordinate of the core position",
+        unit=u.m
+    )
+    core_uncert_tilted = Field(
+        nan * u.m,
+        "uncertainty of the reconstructed core position in the tilted frame",
+        unit=u.m
     )
     h_max = Field(nan * u.m, "reconstructed height of the shower maximum", unit=u.m)
     h_max_uncert = Field(nan * u.m, "uncertainty of h_max", unit=u.m)
