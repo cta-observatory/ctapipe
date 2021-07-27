496f33bb0c4dac23dcbd15e1fac8a49c49b904a1
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2021-07-27 13:41:35 +0200

    Add model io, simplify tests

--- ctapipe/ml/tests/test_sklearn.py
+++ ctapipe/ml/tests/test_sklearn.py
@@ -23,6 +23,25 @@ def test_supported_classifiers():
     assert SUPPORTED_CLASSIFIERS["RandomForestClassifier"] is RandomForestClassifier
 
 
+@pytest.fixture()
+def example_table():
+    from sklearn.datasets import make_regression
+    from sklearn.datasets import make_blobs
+
+    X, y = make_regression(n_samples=100, n_features=5, n_informative=3, random_state=0)
+    t = Table({f"X{i}": col for i, col in enumerate(X.T)})
+    t["energy"] = y
+    t["X0"][10] = np.nan
+    t["X1"][30] = np.nan
+
+    X, y = make_blobs(n_samples=100, n_features=3, centers=2, random_state=0)
+    for i, col in enumerate(X.T, start=5):
+        t[f"X{i}"] = col
+    t["particle"] = y
+
+    return t
+
+
 def test_model_init():
     from ctapipe.ml.sklearn import Classifier
     from sklearn.ensemble import RandomForestClassifier
@@ -56,23 +75,15 @@ def test_model_init():
 
 
 @pytest.mark.parametrize("model_cls", ["LinearRegression", "RandomForestRegressor"])
-def test_regressor(model_cls):
-    from sklearn.datasets import make_regression
+def test_regressor(model_cls, example_table):
     from ctapipe.ml.sklearn import Regressor
 
-    X, y = make_regression(n_samples=100, n_features=5, n_informative=3, random_state=0)
-
-    t = Table({f"X{i}": col for i, col in enumerate(X.T)})
-    t["target"] = y
-    t["X0"][10] = np.nan
-    t["X1"][30] = np.nan
-
     regressor = Regressor(
-        model_cls=model_cls, target="target", features=[f"X{i}" for i in range(5)]
+        model_cls=model_cls, target="energy", features=[f"X{i}" for i in range(8)]
     )
 
-    regressor.fit(t)
-    prediction = regressor.predict(t)
+    regressor.fit(example_table)
+    prediction = regressor.predict(example_table)
     assert prediction.shape == (100,)
     assert np.isnan(prediction[10])
     assert np.isnan(prediction[30])
@@ -81,34 +92,48 @@ def test_regressor(model_cls):
 @pytest.mark.parametrize(
     "model_cls", ["KNeighborsClassifier", "RandomForestClassifier"]
 )
-def test_classifier(model_cls):
-    from sklearn.datasets import make_blobs
+def test_classifier(model_cls, example_table):
     from ctapipe.ml.sklearn import Classifier
 
-    X, y = make_blobs(n_samples=100, n_features=3, centers=2, random_state=0)
-
-    t = Table({f"X{i}": col for i, col in enumerate(X.T)})
-    t["target"] = y
-    t["X0"][10] = np.nan
-    t["X1"][30] = np.nan
-
     classifier = Classifier(
-        model_cls=model_cls,
-        target="target",
-        features=[f"X{i}" for i in range(X.shape[1])],
+        model_cls=model_cls, target="particle", features=[f"X{i}" for i in range(8)]
     )
 
-    classifier.fit(t)
-    prediction = classifier.predict(t)
+    classifier.fit(example_table)
+    prediction = classifier.predict(example_table)
     assert prediction.shape == (100,)
     assert_array_equal(np.unique(prediction), [-1, 0, 1])
     assert prediction[10] == -1
     assert prediction[30] == -1
 
-    score = classifier.predict_score(t)
+    score = classifier.predict_score(example_table)
     assert score.shape == (100,)
     assert np.isnan(score[10])
     assert np.isnan(score[30])
 
     valid = np.isfinite(score)
     assert_array_equal((score[valid] > 0.5).astype(int), prediction[valid])
+
+
+def test_io(example_table, tmp_path):
+    from ctapipe.ml.sklearn import Classifier, Regressor
+
+    classifier = Classifier(
+        model_cls="RandomForestClassifier",
+        model_config=dict(n_estimators=5, max_depth=3),
+        target="particle",
+        features=[f"X{i}" for i in range(8)],
+    )
+
+    classifier.fit(example_table)
+    path = tmp_path / "classifier.pkl"
+
+    classifier.write(path)
+    loaded = Classifier.load(path)
+    assert loaded.features == classifier.features
+    assert_array_equal(
+        loaded.model.feature_importances_, classifier.model.feature_importances_
+    )
+
+    with pytest.raises(TypeError):
+        Regressor.load(path)
