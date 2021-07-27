8d606527f7ff0b385fda2555bfc2ff5cfca6128b
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2021-07-27 10:34:59 +0200

    Add preprocessing functions from aict-tools

---
+++ ctapipe/ml/tests/test_preprocessing.py
@@ -0,0 +1,64 @@
+from astropy.table import Table
+import astropy.units as u
+import numpy as np
+from numpy.testing import assert_array_equal
+
+
+def test_table_to_float32():
+    from ctapipe.ml.preprocessing import table_to_float
+
+    t = Table({"a": [1.0, 1e50, np.inf, -np.inf, np.nan], "b": [1, 2, 3, 4, 5]})
+
+    fmax = np.finfo(np.float32).max
+    fmin = np.finfo(np.float32).min
+    expected = np.array(
+        [[1.0, fmax, fmax, fmin, np.nan], [1.0, 2.0, 3.0, 4.0, 5.0]], dtype=np.float32
+    ).T
+
+    array = table_to_float(t)
+    assert array.dtype == np.float32
+    assert_array_equal(array, expected)
+
+
+def test_table_to_float32_units():
+    from ctapipe.ml.preprocessing import table_to_float
+
+    t = Table(
+        {"a": [1.0, 1e50, np.inf, -np.inf, np.nan] * u.m, "b": [1, 2, 3, 4, 5] * u.deg}
+    )
+
+    fmax = np.finfo(np.float32).max
+    fmin = np.finfo(np.float32).min
+    expected = np.array(
+        [[1.0, fmax, fmax, fmin, np.nan], [1.0, 2.0, 3.0, 4.0, 5.0]], dtype=np.float32
+    ).T
+
+    array = table_to_float(t)
+    assert array.dtype == np.float32
+    assert_array_equal(array, expected)
+
+
+def test_table_to_float64():
+    from ctapipe.ml.preprocessing import table_to_float
+
+    t = Table({"a": [1.0, 1e50, np.inf, -np.inf, np.nan], "b": [1, 2, 3, 4, 5]})
+
+    fmax = np.finfo(np.float64).max
+    fmin = np.finfo(np.float64).min
+    expected = np.array(
+        [[1.0, 1e50, fmax, fmin, np.nan], [1.0, 2.0, 3.0, 4.0, 5.0]], dtype=np.float64
+    ).T
+
+    array = table_to_float(t, dtype=np.float64)
+    assert_array_equal(array, expected)
+    assert array.dtype == np.float64
+    assert_array_equal(array, expected)
+
+
+def test_check_valid_rows():
+    from ctapipe.ml.preprocessing import check_valid_rows
+
+    t = Table({"a": [1.0, 2, np.inf, np.nan, np.nan], "b": [1, np.inf, 3, 4, 5]})
+
+    valid = check_valid_rows(t, warn=False)
+    assert_array_equal(valid, [True, True, True, False, False])
