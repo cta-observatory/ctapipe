8cdc03a501c84af6229b242d60518dcb278ed74e
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2021-07-27 10:34:59 +0200

    Add preprocessing functions from aict-tools

---
+++ ctapipe/ml/preprocessing.py
@@ -0,0 +1,38 @@
+"""
+Utility functions for preprocessing data before handing them to machine
+learning algorithms like sklearn models.
+
+These functions are mainly taken from https://github.com/fact-project/aict-tools
+and adapted to work with astropy Tables instead of pandas dataframes
+"""
+import numpy as np
+from astropy.table import Table
+from numpy.lib.recfunctions import structured_to_unstructured
+import logging
+
+log = logging.getLogger(__name__)
+
+
+def table_to_float(table: Table, dtype=np.float32) -> np.ndarray:
+    """Convert a table to a float32 array, replacing inf/-inf with float min/max"""
+    X = structured_to_unstructured(table.as_array(), dtype=dtype)
+    np.nan_to_num(X, nan=np.nan, copy=False)
+    return X
+
+
+def check_valid_rows(table: Table, warn=True, log=log) -> np.ndarray:
+    """Check for nans, returning a mask of the rows not containing any nans"""
+
+    nans = np.array([np.isnan(col) for col in table.columns.values()]).T
+    valid = ~nans.any(axis=1)
+
+    if warn:
+        nan_counts = np.count_nonzero(nans, axis=0)
+        if (nan_counts > 0).any():
+            nan_counts_str = ", ".join(
+                f"{k}: {v}" for k, v in zip(table.colnames, nan_counts) if v > 0
+            )
+            log.warning("Data contains not-predictable events.")
+            log.warning(f"Number of nan-values in columns: {nan_counts_str}")
+
+    return valid
