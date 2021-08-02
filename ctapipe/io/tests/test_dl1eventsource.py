42ce511540fbfacfbae03c19fdce752d6de87a31
Lukas Nickel <lukas.nickel@tu-dortmund.de>
2021-08-02 17:29:44 +0200

    Remove unused import

--- ctapipe/io/tests/test_dl1eventsource.py
+++ ctapipe/io/tests/test_dl1eventsource.py
@@ -1,6 +1,5 @@
 import astropy.units as u
 import numpy as np
-import pytest
 from ctapipe.io import DataLevel, EventSource
 from ctapipe.io.dl1eventsource import DL1EventSource
 from ctapipe.utils import get_dataset_path
