1ce535e4752d347a4070cf3a4083b578ca027ed8
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2022-01-13 11:03:23 +0100

    Fix typo

--- ctapipe/io/datawriter.py
+++ ctapipe/io/datawriter.py
@@ -655,7 +655,7 @@ class DataWriter(Component):
             if self.write_images:
                 if dl1_camera.image is None:
                     raise ValueError(
-                        "DataWrite.write_images is True but event does not contain image"
+                        "DataWriter.write_images is True but event does not contain image"
                     )
                 # note that we always write the image, even if the image quality
                 # criteria are not met (those are only to determine if the parameters
