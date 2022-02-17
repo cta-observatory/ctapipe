5255692c510a50f4409b0d080da2b1bed545cdd0
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2022-02-17 13:23:32 +0100

    Fix matplotlib version to 3.2 to allow docs build

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
         "eventio>=1.5.0,<2.0.0a0",
         "iminuit~=1.3",
         "joblib",
-        "matplotlib~=3.0",
+        "matplotlib~=3.2.0",
         "numba>=0.43",
         "numpy~=1.16",
         "pandas>=0.24.0",
