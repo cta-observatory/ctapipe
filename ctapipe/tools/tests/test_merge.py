a7d288559cc4a5258cfe4379d4f2ce0ba7fc45cb
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2022-03-30 13:29:55 +0200

    Implement merging dl2 subarray data, fixes #1861

--- ctapipe/tools/tests/test_merge.py
+++ ctapipe/tools/tests/test_merge.py
@@ -4,6 +4,10 @@ import shutil
 
 from ctapipe.core import run_tool
 from pathlib import Path
+from ctapipe.io.astropy_helpers import read_table
+from astropy.table import vstack
+from astropy.utils.diff import report_diff_values
+from io import StringIO
 
 from ctapipe.tools.process import ProcessorTool
 
@@ -38,7 +42,7 @@ def run_stage1(input_path, cwd, output_path=None):
 
 
 def test_simple(tmp_path, dl1_file, dl1_proton_file):
-    from ctapipe.tools.dl1_merge import MergeTool
+    from ctapipe.tools.merge import MergeTool
 
     output = tmp_path / "merged_simple.dl1.h5"
     ret = run_tool(
@@ -51,7 +55,7 @@ def test_simple(tmp_path, dl1_file, dl1_proton_file):
 
 
 def test_pattern(tmp_path: Path, dl1_file, dl1_proton_file):
-    from ctapipe.tools.dl1_merge import MergeTool
+    from ctapipe.tools.merge import MergeTool
 
     # touch a random file to test that the pattern does not use it
     open(dl1_file.parent / "foo.h5", "w").close()
@@ -78,7 +82,7 @@ def test_pattern(tmp_path: Path, dl1_file, dl1_proton_file):
 
 
 def test_skip_images(tmp_path, dl1_file, dl1_proton_file):
-    from ctapipe.tools.dl1_merge import MergeTool
+    from ctapipe.tools.merge import MergeTool
 
     # create a second file so we can test the patterns
     output = tmp_path / "merged_no_images.dl1.h5"
@@ -102,7 +106,7 @@ def test_skip_images(tmp_path, dl1_file, dl1_proton_file):
 
 
 def test_allowed_tels(tmp_path, dl1_file, dl1_proton_file):
-    from ctapipe.tools.dl1_merge import MergeTool
+    from ctapipe.tools.merge import MergeTool
     from ctapipe.instrument import SubarrayDescription
 
     # create file to test 'allowed-tels' option
@@ -123,3 +127,40 @@ def test_allowed_tels(tmp_path, dl1_file, dl1_proton_file):
 
     s = SubarrayDescription.from_hdf(output)
     assert s.tel.keys() == {1, 2}
+
+    tel_keys = {"tel_001", "tel_002"}
+    with tables.open_file(output) as f:
+        assert set(f.root.dl1.event.telescope.parameters._v_children).issubset(tel_keys)
+        assert set(f.root.dl1.event.telescope.images._v_children).issubset(tel_keys)
+        assert set(f.root.monitoring.telescope.pointing._v_children).issubset(tel_keys)
+
+
+def test_dl2(tmp_path, dl2_shower_geometry_file, dl2_proton_geometry_file):
+    from ctapipe.tools.merge import MergeTool
+
+    output = tmp_path / "merged.dl2.h5"
+    ret = run_tool(
+        MergeTool(),
+        argv=[
+            f"--output={output}",
+            str(dl2_shower_geometry_file),
+            str(dl2_proton_geometry_file),
+        ],
+    )
+    assert ret == 0, f"Running merge for dl2 files failed with exit code {ret}"
+
+    table1 = read_table(
+        dl2_shower_geometry_file, "/dl2/event/subarray/geometry/HillasReconstructor"
+    )
+    table2 = read_table(
+        dl2_proton_geometry_file, "/dl2/event/subarray/geometry/HillasReconstructor"
+    )
+    table_merged = read_table(
+        output, "/dl2/event/subarray/geometry/HillasReconstructor"
+    )
+
+    diff = StringIO()
+    identical = report_diff_values(vstack([table1, table2]), table_merged, fileobj=diff)
+    assert (
+        identical
+    ), f"Merged table not equal to individual tables. Diff:\n {diff.getvalue()}"
