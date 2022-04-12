700ff854671d212f93149d1bab5515ac9f7f8fc9
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2022-04-12 12:00:31 +0200

    Train one model per telescope type

--- ctapipe/ml/tools/train_energy_regressor.py
+++ ctapipe/ml/tools/train_energy_regressor.py
@@ -1,3 +1,5 @@
+import pickle
+
 from ctapipe.core.tool import Tool
 from ctapipe.core.traits import Path
 from ctapipe.io import TableLoader
@@ -19,36 +21,43 @@ class TrainEnergyRegressor(Tool):
         ('o', 'output'): 'TrainEnergyRegressor.output_path',
     }
 
+    classes = [
+        TableLoader,
+        Regressor,
+    ]
+
     def setup(self):
         ''''''
         self.loader = TableLoader(
-            parent=self, 
+            parent=self,
             load_dl1_images=False,
             load_dl1_parameters=True,
             load_dl2_geometry=True,
             load_simulated=True,
         )
-        self.model = Regressor(
-            parent=self,
-            target="true_energy",
-            features=[
-                "hillas_intensity",
-                "hillas_width",
-                "hillas_length",
-                "morphology_num_pixels",
-                "concentration_cog",
-                "concentration_core",
-            ]
-
-        )
 
     def start(self):
-        table = self.loader.read_telescope_events()
-        self.model.fit(table)
+        self.models = {}
+        for tel_type in self.loader.subarray.telescope_types:
+            self.log.info("Training model for type %s", tel_type)
+            self.models[tel_type] = Regressor(
+                parent=self,
+                target="true_energy",
+                features=[
+                    "hillas_intensity",
+                    "hillas_width",
+                    "hillas_length",
+                    "morphology_num_pixels",
+                    "concentration_cog",
+                    "concentration_core",
+                ]
+            )
+            table = self.loader.read_telescope_events([tel_type])
+            self.models[tel_type].fit(table)
 
     def finish(self):
-        self.model.write(self.output_path)
-
+        with open(self.output_path, 'wb') as f:
+            pickle.dump(self.models, f)
 
 
 
