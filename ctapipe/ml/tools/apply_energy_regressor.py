c75669286a9352a7f32ef2c45fed72f7aad48fd0
Noah Biederbeck <noah.biederbeck@tu-dortmund.de>
2022-04-14 17:26:47 +0200

    use the component

--- ctapipe/ml/tools/apply_energy_regressor.py
+++ ctapipe/ml/tools/apply_energy_regressor.py
@@ -1,6 +1,8 @@
+from astropy.table import Table
 from ctapipe.core.tool import Tool
-from ctapipe.core.traits import Path, Unicode
-from ctapipe.io import HDF5TableWriter, TableLoader
+from ctapipe.core.traits import Bool, Path, Unicode
+from ctapipe.io import TableLoader, write_table
+from ctapipe.ml.apply import EnergyRegressor
 
 from ..sklearn import Regressor
 
@@ -12,17 +14,14 @@ class ApplyEnergyRegressor(Tool):
         allow_none=False,
         directory_ok=False,
     ).tag(config=True)
-    model_path = Path(
-        default_value=None,
-        allow_none=False,
-        directory_ok=False,
-    ).tag(config=True)
-    prediction = Unicode(default_value="pred_energy")
+    prediction_key = Unicode(default_value="pred_energy")
+    overwrite = Bool(default_value=False).tag(config=True)
 
     aliases = {
         ("i", "input"): "TableLoader.input_url",
         ("o", "output"): "ApplyEnergyRegressor.output_path",
         ("m", "model"): "ApplyEnergyRegressor.model_path",
+        ("f", "force"): "ApplyEnergyRegressor.overwrite",
     }
 
     classes = [
@@ -39,14 +38,15 @@ class ApplyEnergyRegressor(Tool):
             load_dl2_geometry=True,
             load_simulated=True,
         )
-        self.model = Regressor.load(self.model_path)
+        self.estimator = EnergyRegressor(parent=self)
 
     def start(self):
         self.log.info("Applying model")
         table = self.loader.read_telescope_events()
-        prediction = self.model.predict(table)
-        table[self.prediction] = prediction
-        table.write(self.output_path)
+
+        prediction = Table({self.prediction_key: self.estimator.predict(table)})
+        prediction[self.prediction_key].description = "Estimated energy"
+        write_table(prediction, self.output_path, "/dl2/telescope/energy")
 
     def finish(self):
         self.loader.close()
