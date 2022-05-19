9fda9bb095154a89a663117ccb7022a7c6123dc5
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2022-05-19 12:11:55 +0200

    Add test that guessing the name works regardless of focal length choice

--- ctapipe/io/tests/test_simteleventsource.py
+++ ctapipe/io/tests/test_simteleventsource.py
@@ -317,8 +317,13 @@ def test_focal_length_choice():
     # this file does
     s = SimTelEventSource(prod5b_path, focal_length_choice='effective')
     assert u.isclose(s.subarray.tel[1].optics.equivalent_focal_length, 29.3 * u.m, atol=0.05 * u.m)
+    # check guessing of the name is not affected by focal length choice
+    assert str(s.subarray.tel[1]) == 'LST_LST_LSTCam'
+
     s = SimTelEventSource(prod5b_path, focal_length_choice='nominal')
     assert u.isclose(s.subarray.tel[1].optics.equivalent_focal_length, 28.0 * u.m, atol=0.05 * u.m)
+    # check guessing of the name is not affected by focal length choice
+    assert str(s.subarray.tel[1]) == 'LST_LST_LSTCam'
 
 
 
