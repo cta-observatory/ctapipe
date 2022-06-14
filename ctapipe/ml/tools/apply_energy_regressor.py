8494b31dce5b5e6b111ea0382b7a4bd8abf3c739
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2022-06-14 12:59:18 +0200

    Load model and write output properly

--- ctapipe/ml/tools/apply_energy_regressor.py
+++ ctapipe/ml/tools/apply_energy_regressor.py
@@ -1,27 +1,44 @@
 from astropy.table import Table
+import tables
 from ctapipe.core.tool import Tool
-from ctapipe.core.traits import Bool, Path, Unicode
+from ctapipe.core.traits import Bool, Path, flag
 from ctapipe.io import TableLoader, write_table
-from ctapipe.ml.apply import EnergyRegressor
+from tqdm.auto import tqdm
 
 from ..sklearn import Regressor
+from ..apply import EnergyRegressor
 
 
 class ApplyEnergyRegressor(Tool):
 
-    output_path = Path(
+    overwrite = Bool(default_value=False).tag(config=True)
+
+    input_url = Path(
         default_value=None,
         allow_none=False,
         directory_ok=False,
+        exists=True,
     ).tag(config=True)
-    prediction_key = Unicode(default_value="pred_energy")
-    overwrite = Bool(default_value=False).tag(config=True)
+    model_path = Path(
+        default_value=None, allow_none=False, exists=True, directory_ok=False
+    ).tag(config=True)
 
     aliases = {
-        ("i", "input"): "TableLoader.input_url",
-        ("o", "output"): "ApplyEnergyRegressor.output_path",
+        ("i", "input"): "ApplyEnergyRegressor.input_url",
         ("m", "model"): "ApplyEnergyRegressor.model_path",
-        ("f", "force"): "ApplyEnergyRegressor.overwrite",
+    }
+
+    flags = {
+        **flag(
+            "overwrite",
+            "ApplyEnergyRegressor.overwrite",
+            "Overwrite tables in output file if it exists",
+            "Don't overwrite tables in output file if it exists",
+        ),
+        "f": (
+            {"ApplyEnergyRegressor": {"overwrite": True}},
+            "Overwrite output file if it exists",
+        ),
     }
 
     classes = [
@@ -31,25 +48,46 @@ class ApplyEnergyRegressor(Tool):
 
     def setup(self):
         """"""
+        self.estimator = EnergyRegressor(
+            parent=self,
+            model=Regressor.load(self.model_path),
+        )
+        self.h5file = tables.open_file(self.input_url, mode="r+")
         self.loader = TableLoader(
             parent=self,
+            h5file=self.h5file,
             load_dl1_images=False,
             load_dl1_parameters=True,
             load_dl2_geometry=True,
             load_simulated=True,
         )
-        self.estimator = EnergyRegressor(parent=self)
 
     def start(self):
         self.log.info("Applying model")
-        table = self.loader.read_telescope_events()
 
-        prediction = Table({self.prediction_key: self.estimator.predict(table)})
-        prediction[self.prediction_key].description = "Estimated energy"
-        write_table(prediction, self.output_path, "/dl2/telescope/energy")
+        for tel_id in tqdm(self.loader.subarray.tel):
+            table = self.loader.read_telescope_events([tel_id])
+
+            prediction, valid = self.estimator.predict(table)
+            table = Table(
+                {
+                    "obs_id": table["obs_id"],
+                    "event_id": table["event_id"],
+                    "tel_id": table["tel_id"],
+                    "reconstructed_energy_energy": prediction,
+                    "reconstructed_energy_is_valid": valid,
+                }
+            )
+            write_table(
+                table,
+                self.loader.input_url,
+                f"/dl2/event/telescope/energy/{self.estimator.model.model_cls}/tel_{tel_id:03d}",
+                mode="a",
+                overwrite=self.overwrite,
+            )
 
     def finish(self):
-        self.loader.close()
+        self.h5file.close()
 
 
 def main():
