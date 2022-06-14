f5037abc6a1af73dd8fdb917f46709458753d177
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2022-06-14 12:59:18 +0200

    Load model and write output properly

--- ctapipe/ml/apply.py
+++ ctapipe/ml/apply.py
@@ -2,11 +2,17 @@ from abc import abstractmethod
 
 import numpy as np
 from astropy.table import Table
-from ctapipe.containers import ArrayEventContainer
+from ctapipe.containers import (
+    ArrayEventContainer,
+    ParticleClassificationContainer,
+    ReconstructedEnergyContainer,
+)
 from ctapipe.core import Component
-from ctapipe.core.traits import Path
 
-from .sklearn import Classifier, Regressor
+from traitlets import Instance
+from .sklearn import Classifier, Regressor, Model
+
+from typing import Tuple
 
 
 class Reconstructor(Component):
@@ -14,12 +20,7 @@ class Reconstructor(Component):
 
     # TODO: update model config (?)
     #       only settings that make sense, e.g. verbose, n_jobs
-
-    model_path = Path(
-        default_value=None,
-        allow_none=False,
-        directory_ok=False,
-    ).tag(config=True)
+    model = Instance(Model).tag(config=True)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -28,15 +29,15 @@ class Reconstructor(Component):
     def __call__(self, event: ArrayEventContainer) -> None:
         """Event-wise prediction for the EventSource-Loop.
 
-        Fill the event.dl2.<your-feature> container.
+        Fill the event.dl2.<your-feature>[name] container.
 
         Parameters
         ----------
         event: ArrayEventContainer
         """
 
-    def predict(self, table: Table) -> np.array:
-        """Tool"""
+    def predict(self, table: Table) -> Tuple[np.ndarray, np.ndarray]:
+        """Predict on a table of events"""
         return self.model.predict(table)
 
     def _collect_features(self, event: ArrayEventContainer, tel_id: int) -> Table:
@@ -67,50 +68,39 @@ class Reconstructor(Component):
 class RegressionReconstructor(Reconstructor):
     """Base class for sklearn regressors."""
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.model = Regressor.load(self.model_path)
+    model = Instance(Regressor).tag(config=True)
 
 
 class ClassificationReconstructor(Reconstructor):
     """Base class for sklearn regressors."""
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.model = Classifier.load(self.model_path)
+    model = Instance(Classifier).tag(config=True)
 
 
 class EnergyRegressor(RegressionReconstructor):
     """"""
 
-    # TODO: models per tel type
-    #       but this needs to be done in the Trainer first
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
     def __call__(self, event: ArrayEventContainer) -> None:
         for tel_id in event.trigger.tels_with_trigger:
             features = self._collect_features(event, tel_id)
-            prediction = self.model.predict(features)
-
-            event.dl2.tel[tel_id].energy.energy = prediction
-            event.dl2.tel[tel_id].energy.is_valid = True
-            event.dl2.tel[tel_id].energy.tel_ids = None  # mono-prediction
+            prediction, valid = self.model.predict(features)
+            container = ReconstructedEnergyContainer(
+                energy=prediction,
+                is_valid=valid,
+            )
+            event.dl2.tel[tel_id].energy[self.model.model_cls] = container
 
 
 class ParticleIdClassifier(ClassificationReconstructor):
     """"""
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
     def __call__(self, event: ArrayEventContainer) -> None:
         for tel_id in event.trigger.tels_with_trigger:
             features = self._collect_features(event, tel_id)
-            prediction = self.model.predict(features)
+            prediction, valid = self.model.predict(features)
 
-            event.dl2.tel[tel_id].classification.prediction = prediction
-            event.dl2.tel[tel_id].classification.is_valid = True
-            event.dl2.tel[tel_id].classification.goodness_of_fit = np.nan  # ?
-            event.dl2.tel[tel_id].classification.tel_ids = None  # mono-prediction
+            container = ParticleClassificationContainer(
+                prediction=prediction,
+                is_valid=valid,
+            )
+            event.dl2.tel[tel_id].classification[self.model.model_cls] = container
