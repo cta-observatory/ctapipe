f5684a0bf22d8b007c4faf2e4d2388f216ade2fd
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2022-06-15 16:17:05 +0200

    More realistic test config, use test file for training

--- ctapipe/ml/tools/tests/test_train.py
+++ ctapipe/ml/tools/tests/test_train.py
@@ -1,20 +1,22 @@
 from ctapipe.core import run_tool
-from ctapipe.ml import TrainEnergyRegressor
 from ctapipe.utils import resource_file
 
 
-def test_train_energy_regressor(tmp_path, dl2_shower_geometry_file):
+def test_train_energy_regressor(tmp_path):
+    from ctapipe.ml import TrainEnergyRegressor
+    from ctapipe.ml.sklearn import Regressor
+
     tool = TrainEnergyRegressor()
-    tool.load_config_file(resource_file("ml-config.yaml"))
+    config = resource_file("ml-config.yaml")
     out_file = tmp_path / "test_train_energy_regressor.pkl"
-    assert (
-        run_tool(
-            tool,
-            argv=[
-                f"--input={dl2_shower_geometry_file}",
-                f"--output={out_file}",
-                "--TrainEnergyRegressor.n_cross_validation=2",  # default of 5 does not work with sample size of 2
-            ],
-        )
-        == 0
+    ret = run_tool(
+        tool,
+        argv=[
+            f"--input=dataset://gamma_diffuse_dl2_train_small.dl2.h5",
+            f"--output={out_file}",
+            f"--config={config}",
+            "--log-level=INFO",
+        ],
     )
+    assert ret == 0
+    Regressor.load(out_file)
