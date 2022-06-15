d5447506037d8d4eb53db61e489b01076c801eb8
Noah Biederbeck <noah.biederbeck@tu-dortmund.de>
2022-06-15 11:12:39 +0200

    Add test for TrainEnergyRegressor.

---
+++ ctapipe/ml/tools/tests/test_train.py
@@ -0,0 +1,29 @@
+from ctapipe.core import run_tool
+from ctapipe.ml import TrainEnergyRegressor
+from ctapipe.utils import get_dataset_path
+
+try:
+    from importlib.resources import files
+except ImportError:
+    from importlib_resources import files
+
+
+def resource_file(filename):
+    return files("ctapipe").joinpath("resources", filename)
+
+
+def test_train_energy_regressor(tmp_path, dl2_shower_geometry_file):
+    tool = TrainEnergyRegressor()
+    tool.load_config_file(resource_file("ml-config.yaml"))
+    out_file = tmp_path / "test_train_energy_regressor.pkl"
+    assert (
+        run_tool(
+            tool,
+            argv=[
+                f"--input={dl2_shower_geometry_file}",
+                f"--output={out_file}",
+                "--TrainEnergyRegressor.n_cross_validation=2",  # default of 5 does not work with sample size of 2
+            ],
+        )
+        == 0
+    )
