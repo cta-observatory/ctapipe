d90a5f349e37ddc039223a39a504bb0486b23bd2
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2022-06-21 13:43:13 +0200

    Apply energy regressor in ctapipe-process

--- ctapipe/ml/tools/tests/test_train.py
+++ ctapipe/ml/tools/tests/test_train.py
@@ -1,22 +1,3 @@
-from ctapipe.core import run_tool
-from ctapipe.utils import resource_file
-
-
-def test_train_energy_regressor(tmp_path):
-    from ctapipe.ml import TrainEnergyRegressor
+def test_train_energy_regressor(energy_regressor_path):
     from ctapipe.ml.sklearn import Regressor
-
-    tool = TrainEnergyRegressor()
-    config = resource_file("ml-config.yaml")
-    out_file = tmp_path / "test_train_energy_regressor.pkl"
-    ret = run_tool(
-        tool,
-        argv=[
-            "--input=dataset://gamma_diffuse_dl2_train_small.dl2.h5",
-            f"--output={out_file}",
-            f"--config={config}",
-            "--log-level=INFO",
-        ],
-    )
-    assert ret == 0
-    Regressor.load(out_file)
+    Regressor.load(energy_regressor_path)
