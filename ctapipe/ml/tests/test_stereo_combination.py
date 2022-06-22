8d291850fb1a89fba4ffee77b9136ac4a296aa39
Lukas Nickel <lukas.nickel@tu-dortmund.de>
2022-06-22 15:51:09 +0200

    Implement call for array event container

--- ctapipe/ml/tests/test_stereo_combination.py
+++ ctapipe/ml/tests/test_stereo_combination.py
@@ -3,6 +3,14 @@ from astropy.table import Table
 import astropy.units as u
 import numpy as np
 from numpy.testing import assert_array_equal
+from ctapipe.containers import (
+    ReconstructedEnergyContainer,
+    ParticleClassificationContainer,
+    ArrayEventContainer,
+    ReconstructedContainer,
+)
+from ctapipe.io import EventSource
+from ctapipe.ml.stereo_combination import StereoMeanCombiner
 
 
 @pytest.fixture(scope="module")
@@ -24,8 +32,6 @@ def mono_table():
 
 
 def test_mean_prediction_table(mono_table):
-    from ctapipe.ml.stereo_combination import StereoMeanCombiner
-
     combine_no_weights = StereoMeanCombiner(mono_prediction_column="prediction")
     stereo_no_weights = combine_no_weights.predict(mono_table)
     assert stereo_no_weights.colnames == ["obs_id", "event_id", "mean_prediction"]
@@ -48,3 +54,38 @@ def test_mean_prediction_table(mono_table):
     assert_array_equal(
         stereo_weights["mean_prediction"], u.Quantity(np.array([3, 6, 1]), u.TeV)
     )
+
+
+def test_mean_prediction_single_event():
+    event = ArrayEventContainer()
+    # These are actually in the file
+    event.dl2.tel[25] = ReconstructedContainer(
+        energy={
+            "dummy": ReconstructedEnergyContainer(energy=10 * u.GeV, is_valid=True)
+        },
+        classification={
+            "dummy": ParticleClassificationContainer(prediction=1.0, is_valid=True)
+        },
+    )
+    event.dl2.tel[125] = ReconstructedContainer(
+        energy={
+            "dummy": ReconstructedEnergyContainer(energy=20 * u.GeV, is_valid=True)
+        },
+        classification={
+            "dummy": ParticleClassificationContainer(prediction=0.0, is_valid=True)
+        },
+    )
+    event.dl2.tel[130] = ReconstructedContainer(
+        energy={
+            "dummy": ReconstructedEnergyContainer(energy=0.03 * u.TeV, is_valid=True)
+        },
+        classification={
+            "dummy": ParticleClassificationContainer(prediction=0.8, is_valid=True)
+        },
+    )
+
+    combine = StereoMeanCombiner(algorithm="dummy", energy=True, classification=True)
+    combine(event)
+    assert event.dl2.stereo.energy["dummy"].energy == 20 * u.GeV
+    assert event.dl2.stereo.classification["dummy"].prediction == 0.6
+    assert False
