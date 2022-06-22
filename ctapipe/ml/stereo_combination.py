cab01e9780ea0b57b3ad0184465079aa851f97f5
Lukas Nickel <lukas.nickel@tu-dortmund.de>
2022-06-22 15:51:09 +0200

    Implement call for array event container

--- ctapipe/ml/stereo_combination.py
+++ ctapipe/ml/stereo_combination.py
@@ -20,9 +20,9 @@ def _calculate_ufunc_of_telescope_values(tel_data, n_array_events, indices, ufun
 
 class StereoCombiner(Component):
     # TODO: Add quality query (after #1888)
-    def __init__(self, mono_prediction_column, *args, **kwargs):
+    def __init__(self, algorithm, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.mono_prediction_column = mono_prediction_column
+        self.algorithm = algorithm
 
     @abstractmethod
     def __call__(self, event: ArrayEventContainer) -> None:
@@ -55,8 +55,8 @@ class StereoMeanCombiner(StereoCombiner):
     the reconstruction on telescope-level with an optional weighting.
     """
 
-    def __init__(self, mono_prediction_column, weight_column=None, *args, **kwargs):
-        super().__init__(mono_prediction_column=mono_prediction_column, *args, **kwargs)
+    def __init__(self, weight_column=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.weight_column = weight_column
 
     def _construct_weights(self, table):
@@ -68,7 +68,49 @@ class StereoMeanCombiner(StereoCombiner):
             return None
 
     def __call__(self, event: ArrayEventContainer) -> None:
-        pass
+        """
+        Calculate the mean energy / classification prediction for a single
+        array event.
+        TODO: This only uses the nominal telescope values, not their respective uncertainties!
+        (Which we dont have right now)
+        """
+        mono_energies = {"ids": [], "values": [], "weights": None}
+        mono_classifications = {"ids": [], "values": [], "weights": None}
+
+        for tel_id, dl2 in event.dl2.tel.items():
+            if self.algorithm in dl2.energy:
+                mono = dl2.energy[self.algorithm]
+                if mono.is_valid:
+                    mono_energies["values"].append(mono.energy.to_value(u.TeV))
+                    mono_energies["ids"].append(tel_id)
+            if self.algorithm in dl2.classification:
+                mono = dl2.classification[self.algorithm]
+                if mono.is_valid:
+                    mono_classifications["values"].append(mono.prediction)
+                    mono_classifications["ids"].append(tel_id)
+        if mono_energies["ids"]:
+            weighted_mean = np.average(
+                mono_energies["values"], weights=mono_energies["weights"]
+            )
+            stereo_energy = ReconstructedEnergyContainer(
+                energy=u.Quantity(weighted_mean, u.TeV, copy=False),
+                energy_uncert=u.Quantity(
+                    np.average((weighted_mean - mono_energies["values"]) ** 2),
+                    u.TeV,
+                    copy=False,
+                ),
+                tel_ids=mono_energies["ids"],
+            )
+            event.dl2.stereo.energy[self.algorithm] = stereo_energy
+        if mono_classifications["ids"]:
+            stereo_classification = ParticleClassificationContainer(
+                prediction=np.average(
+                    mono_classifications["values"],
+                    weights=mono_classifications["weights"],
+                ),
+                tel_ids=mono_classifications["ids"],
+            )
+            event.dl2.stereo.classification[self.algorithm] = stereo_classification
 
     def predict(self, mono_predictions: Table) -> Table:
         """
