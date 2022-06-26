3930ffdddb18f13e71311b661a189f594b128b89
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2022-06-26 17:42:01 +0200

    Apply StereoCombiners in ctapipe-process

--- ctapipe/ml/tools/tests/test_process.py
+++ ctapipe/ml/tools/tests/test_process.py
@@ -1,5 +1,6 @@
 from ctapipe.core import run_tool
 from ctapipe.io import read_table
+import json
 
 
 def test_process_apply_energy(tmp_path, energy_regressor_path):
@@ -7,16 +8,34 @@ def test_process_apply_energy(tmp_path, energy_regressor_path):
 
     output = tmp_path / "gamma_prod5.dl2_energy.h5"
 
+    config_path = tmp_path / "config.json"
+
+    config = {
+        "ProcessorTool": {
+            "stereo_combiner_configs": [
+                {
+                    "type": "StereoMeanCombiner",
+                    "combine_property": "energy",
+                    "algorithm": "ExtraTreesRegressor",
+                    "weights": "konrad",
+                }
+            ]
+        }
+    }
+
+    with config_path.open("w") as f:
+        json.dump(config, f)
+
     argv = [
         "--input=dataset://gamma_prod5.simtel.zst",
         f"--output={output}",
         "--write-images",
         "--write-stereo-shower",
         "--write-mono-shower",
-        f"--energy-regressor={energy_regressor_path}"
-
+        f"--energy-regressor={energy_regressor_path}",
+        f"--config={config_path}",
     ]
     assert run_tool(ProcessorTool(), argv=argv, cwd=tmp_path) == 0
 
-    print(read_table(output, '/dl2/event/telescope/energy/ExtraTreesRegressor/tel_004'))
-
+    print(read_table(output, "/dl2/event/telescope/energy/ExtraTreesRegressor/tel_004"))
+    print(read_table(output, "/dl2/event/subarray/energy/ExtraTreesRegressor"))
