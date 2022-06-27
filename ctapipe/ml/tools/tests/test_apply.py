cfc41cd1957a878a6a8c2f2330d5a398ed265341
Lukas Nickel <lukas.nickel@tu-dortmund.de>
2022-06-27 17:41:52 +0200

    Add apply tool for the particle id classifier
    Co-authored-by: Noah Biederbeck <noah.biederbeck@tu-dortmund.de>

--- ctapipe/ml/tools/tests/test_apply.py
+++ ctapipe/ml/tools/tests/test_apply.py
@@ -36,3 +36,40 @@ def test_apply_energy_regressor(energy_regressor_path, dl1_parameters_file, tmp_
 
     assert "ExtraTreesRegressor_energy_mono" in events.colnames
     assert "ExtraTreesRegressor_is_valid_mono" in events.colnames
+
+
+def test_apply_particle_classifier(
+    particle_classifier_path, dl1_parameters_file, tmp_path
+):
+    from ctapipe.ml.tools.apply_particle_classifier import ApplyParticleIdClassifier
+
+    input_path = tmp_path / dl1_parameters_file.name
+
+    # create copy to not mutate common test file
+    shutil.copy2(dl1_parameters_file, input_path)
+
+    ret = run_tool(
+        ApplyParticleIdClassifier(),
+        argv=[
+            f"--input={input_path}",
+            f"--model={particle_classifier_path}",
+            "--ApplyParticleIdClassifier.StereoMeanCombiner.weights=konrad",
+        ],
+    )
+    assert ret == 0
+
+    loader = TableLoader(input_path, load_dl2=True)
+    events = loader.read_subarray_events()
+    assert "ExtraTreesClassifier_prediction" in events.colnames
+    assert "ExtraTreesClassifier_tel_ids" in events.colnames
+    assert "ExtraTreesClassifier_is_valid" in events.colnames
+    assert "ExtraTreesClassifier_goodness_of_fit" in events.colnames
+
+    events = loader.read_telescope_events()
+    assert "ExtraTreesClassifier_prediction" in events.colnames
+    assert "ExtraTreesClassifier_tel_ids" in events.colnames
+    assert "ExtraTreesClassifier_is_valid" in events.colnames
+    assert "ExtraTreesClassifier_goodness_of_fit" in events.colnames
+
+    assert "ExtraTreesClassifier_prediction_mono" in events.colnames
+    assert "ExtraTreesClassifier_is_valid_mono" in events.colnames
