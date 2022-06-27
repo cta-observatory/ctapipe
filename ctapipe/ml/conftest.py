738e7faf5803d6420351e9215619ca8ac5cc7920
Lukas Nickel <lukas.nickel@tu-dortmund.de>
2022-06-27 16:11:27 +0200

    Add tool to train the ParticleIdClassifier
    Co-authored-by: Noah Biederbeck <noah.biederbeck@tu-dortmund.de>

--- ctapipe/ml/conftest.py
+++ ctapipe/ml/conftest.py
@@ -2,12 +2,13 @@ import pytest
 from ctapipe.utils.datasets import resource_file
 from ctapipe.core import run_tool
 
-@pytest.fixture(scope='session')
+
+@pytest.fixture(scope="session")
 def model_tmp_path(tmp_path_factory):
     return tmp_path_factory.mktemp("models")
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def energy_regressor_path(model_tmp_path):
     from ctapipe.ml.tools.train_energy_regressor import TrainEnergyRegressor
 
@@ -18,6 +19,27 @@ def energy_regressor_path(model_tmp_path):
         tool,
         argv=[
             "--input=dataset://gamma_diffuse_dl2_train_small.dl2.h5",
+            f"--output={out_file}",
+            f"--config={config}",
+            "--log-level=INFO",
+        ],
+    )
+    assert ret == 0
+    return out_file
+
+
+@pytest.fixture(scope="session")
+def particle_classifier_path(model_tmp_path):
+    from ctapipe.ml.tools.train_particle_classifier import TrainParticleIdClassifier
+
+    tool = TrainParticleIdClassifier()
+    config = resource_file("ml-config.yaml")
+    out_file = model_tmp_path / "particle_classifier.pkl"
+    ret = run_tool(
+        tool,
+        argv=[
+            "--input-background=dataset://proton_dl2_train_small.dl2.h5",
+            "--input-signal=dataset://gamma_diffuse_dl2_train_small.dl2.h5",
             f"--output={out_file}",
             f"--config={config}",
             "--log-level=INFO",
--- ctapipe/ml/tools/tests/test_train.py
+++ ctapipe/ml/tools/tests/test_train.py
@@ -1,3 +1,10 @@
 def test_train_energy_regressor(energy_regressor_path):
     from ctapipe.ml.sklearn import Regressor
+
     Regressor.load(energy_regressor_path)
+
+
+def test_train_particle_classifier(particle_classifier_path):
+    from ctapipe.ml.sklearn import Classifier
+
+    Classifier.load(particle_classifier_path)
