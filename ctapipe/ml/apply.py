26ab669f9c89760a90ab09d2ae5f49d8ffbf4a45
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2022-06-28 12:27:03 +0200

    Ml quality query (#1962)
    * Add qualityquery to train tool
    
    * Refactor training/application of ml model to include quality query
    
    * Add some docstrings
    
    * Fix case of no valid predicted mono events, fix test resource file
    
    * WIP: hardcoded path, but locally this works now.
    
    * Fix roc auc
    
    * Update dataset path
    
    * Fix typo
    
    * Remove workaround
    
    * Fix pyflakes complaints
    
    * Update comments in config file

--- ctapipe/ml/apply.py
+++ ctapipe/ml/apply.py
@@ -1,9 +1,14 @@
 from abc import abstractmethod
-from typing import Tuple
+from astropy.utils.decorators import lazyproperty
 
+import astropy.units as u
 import numpy as np
 from astropy.table import Table
 from traitlets import Instance
+from ctapipe.core.provenance import Provenance
+
+from ctapipe.core.qualityquery import QualityQuery
+import joblib
 
 from ..containers import (
     ArrayEventContainer,
@@ -26,35 +31,74 @@ __all__ = [
 class Reconstructor(Component):
     """Base class for sklearn reconstructors."""
 
-    # TODO: update model config (?)
-    #       only settings that make sense, e.g. verbose, n_jobs
+    target = None
     model_cls = Model
-    model = Instance(Model).tag(config=True)
+    model = Instance(model_cls, allow_none=True).tag(config=True)
 
     def __init__(self, subarray, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.subarray = subarray
-        self.instrument_table = self.subarray.to_table("joined")
+        self.qualityquery = QualityQuery(parent=self)
+        if self.model is None:
+            self.model = self.model_cls(parent=self, target=self.target)
+
+    def write(self, path):
+        Provenance().add_output_file(path, role="ml-model")
+        with open(path, "wb") as f:
+            joblib.dump(
+                (self.model, self.qualityquery.quality_criteria, self.subarray),
+                f,
+                compress=True,
+            )
 
     @classmethod
-    def read(cls, path, subarray, *args, **kwargs):
-        model = cls.model_cls.load(path)
-        return cls(subarray, *args, model=model, **kwargs)
+    def read(cls, path, check_cls=True, **kwargs):
+        with open(path, "rb") as f:
+            model, quality_criteria, subarray = joblib.load(f)
+
+        if check_cls is True and model.__class__ is not cls.model_cls:
+            raise TypeError(
+                f"File did not contain an instance of {cls.model_cls}, got {model.__class__}"
+            )
+
+        Provenance().add_input_file(path, role="ml-model")
+        instance = cls(subarray=subarray, model=model, **kwargs)
+        instance.qualityquery = QualityQuery(
+            quality_criteria=quality_criteria, parent=instance
+        )
+        return instance
+
+    @lazyproperty
+    def instrument_table(self):
+        return self.subarray.to_table("joined")
 
     @abstractmethod
     def __call__(self, event: ArrayEventContainer) -> None:
         """Event-wise prediction for the EventSource-Loop.
 
-        Fill the event.dl2.<your-feature>[name] container.
+        Fills the event.dl2.<your-feature>[name] container.
 
         Parameters
         ----------
         event: ArrayEventContainer
         """
 
-    def predict(self, key, table: Table) -> Tuple[np.ndarray, np.ndarray]:
-        """Predict on a table of events"""
-        return self.model.predict(key, table)
+    @abstractmethod
+    def predict(self, key, table: Table) -> Table:
+        """
+        Predict on a table of events
+
+        Parameters
+        ----------
+        table : `~astropy.table.Table`
+            Table of features
+
+        Returns
+        -------
+        table : `~astropy.table.Table`
+            Table with predictions, matches the corresponding
+            container definition
+        """
 
     def _collect_features(self, event: ArrayEventContainer, tel_id: int) -> Table:
         """Loop over all containers with features.
@@ -97,47 +141,112 @@ class RegressionReconstructor(Reconstructor):
     """Base class for sklearn regressors."""
 
     model_cls = Regressor
-    model = Instance(model_cls).tag(config=True)
+    model = Instance(model_cls, allow_none=True).tag(config=True)
 
 
 class ClassificationReconstructor(Reconstructor):
     """Base class for sklearn regressors."""
 
     model_cls = Classifier
-    model = Instance(model_cls).tag(config=True)
+    model = Instance(model_cls, allow_none=True).tag(config=True)
 
 
 class EnergyRegressor(RegressionReconstructor):
-    """"""
+    """
+    Predict dl2 energy for each telescope
+    """
+
+    target = "true_energy"
 
     def __call__(self, event: ArrayEventContainer) -> None:
+        """
+        Apply the quality query and model and fill the corresponding container
+        """
         for tel_id in event.trigger.tels_with_trigger:
             features = self._collect_features(event, tel_id)
-            prediction, valid = self.model.predict(
-                self.subarray.tel[tel_id],
-                features,
-            )
+            query = self.qualityquery.get_table_mask(features)
 
-            container = ReconstructedEnergyContainer(
-                energy=prediction[0],
-                is_valid=valid[0],
-            )
+            if query[0]:
+                prediction, valid = self.model.predict(
+                    self.subarray.tel[tel_id],
+                    features,
+                )
+                container = ReconstructedEnergyContainer(
+                    energy=prediction[0],
+                    is_valid=valid[0],
+                )
+            else:
+                container = ReconstructedEnergyContainer(
+                    energy=u.Quantity(np.nan, self.model.unit),
+                    is_valid=False,
+                )
             event.dl2.tel[tel_id].energy[self.model.model_cls] = container
+
+    def predict(self, key, table: Table) -> Table:
+        """Predict on a table of events"""
+        n_rows = len(table)
+        energy = u.Quantity(np.full(n_rows, np.nan), self.model.unit, copy=False)
+        is_valid = np.full(n_rows, False)
+
+        mask = self.qualityquery.get_table_mask(table)
+        energy[mask], is_valid[mask] = self.model.predict(key, table[mask])
+
+        result = Table(
+            {
+                f"{self.model.model_cls}_energy": energy,
+                f"{self.model.model_cls}_is_valid": is_valid,
+            }
+        )
+        return result
 
 
 class ParticleIdClassifier(ClassificationReconstructor):
-    """"""
+    """
+    Predict dl2 particle classification
+    """
+
+    target = "true_shower_primary_id"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # gammas have true_shower_primary_id = 0
+        self.model.positive_class = 0
 
     def __call__(self, event: ArrayEventContainer) -> None:
         for tel_id in event.trigger.tels_with_trigger:
             features = self._collect_features(event, tel_id)
-            prediction, valid = self.model.predict_score(
-                self.subarray.tel[tel_id],
-                features,
-            )
+            query = self.qualityquery.get_table_mask(features)
 
-            container = ParticleClassificationContainer(
-                prediction=prediction[0],
-                is_valid=valid[0],
-            )
+            if query[0]:
+                prediction, valid = self.model.predict_score(
+                    self.subarray.tel[tel_id],
+                    features,
+                )
+
+                container = ParticleClassificationContainer(
+                    prediction=prediction[0],
+                    is_valid=valid[0],
+                )
+            else:
+                container = ParticleClassificationContainer(
+                    prediction=np.nan, is_valid=False
+                )
+
             event.dl2.tel[tel_id].classification[self.model.model_cls] = container
+
+    def predict(self, key, table: Table) -> Table:
+        """Predict on a table of events"""
+        n_rows = len(table)
+        score = np.full(n_rows, np.nan)
+        is_valid = np.full(n_rows, False)
+
+        mask = self.qualityquery.get_table_mask(table)
+        score[mask], is_valid[mask] = self.model.predict_score(key, table[mask])
+
+        result = Table(
+            {
+                f"{self.model.model_cls}_prediction": score,
+                f"{self.model.model_cls}_is_valid": is_valid,
+            }
+        )
+        return result
