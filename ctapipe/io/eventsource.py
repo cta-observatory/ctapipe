bcf74c5711b1c258efd6f5a735c78ca9fe65c940
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2022-07-07 16:23:05 +0200

    Remove restriction on existing file in `EventSource.input_url`

--- ctapipe/io/eventsource.py
+++ ctapipe/io/eventsource.py
@@ -1,17 +1,18 @@
 """
 Handles reading of different event/waveform containing files
 """
+import warnings
 from abc import abstractmethod
-from traitlets.config.loader import LazyConfigValue
-from typing import Tuple, List, Generator
+from typing import Generator, List, Tuple
 
+from traitlets.config.loader import LazyConfigValue
+
+from ..containers import ArrayEventContainer
+from ..core import Provenance, ToolConfigurationError
+from ..core.component import Component, find_config_in_hierarchy, non_abstract_children
+from ..core.traits import CInt, Int, Path, Set, TraitError, Undefined
 from ..instrument import SubarrayDescription
 from .datalevels import DataLevel
-from ..containers import ArrayEventContainer
-from ..core import ToolConfigurationError, Provenance
-from ..core.component import Component, non_abstract_children, find_config_in_hierarchy
-from ..core.traits import Path, Int, CInt, Set, Undefined
-
 
 __all__ = ["EventSource"]
 
@@ -83,11 +84,7 @@ class EventSource(Component):
         generated events. If None, all available telescopes are used.
     """
 
-    input_url = Path(
-        directory_ok=False,
-        exists=True,
-        help="Path to the input file containing events.",
-    ).tag(config=True)
+    input_url = Path(help="Path to the input file containing events.").tag(config=True)
 
     max_events = Int(
         None,
@@ -298,8 +295,18 @@ class EventSource(Component):
         available_classes = non_abstract_children(cls)
 
         for subcls in available_classes:
-            if subcls.is_compatible(input_url):
-                return subcls
+            try:
+                if subcls.is_compatible(input_url):
+                    return subcls
+            except Exception as e:
+                warnings.warn(f"{subcls.__name__}.is_compatible raised exception: {e}")
+
+        # provide a more helpful error for non-existing input_url
+        if not input_url.exists():
+            raise TraitError(
+                f"input_url {input_url} is not an existing file "
+                " and no EventSource implementation claimed compatibility"
+            )
 
         raise ValueError(
             "Cannot find compatible EventSource for \n"
