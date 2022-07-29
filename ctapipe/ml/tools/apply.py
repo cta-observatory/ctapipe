8f7c228fca5bfb78dd45e2fd9559571fb04a421d
Noah Biederbeck <noah.biederbeck@tu-dortmund.de>
2022-07-29 12:52:40 +0200

    Use functions, reduce has_{regressor,classifier} checks.

--- ctapipe/ml/tools/apply.py
+++ ctapipe/ml/tools/apply.py
@@ -92,6 +92,10 @@ class Apply(Tool):
             load_instrument=True,
         )
 
+        self.apply_regressor = self._setup_regressor()
+        self.apply_classifier = self._setup_classifier()
+
+    def _setup_regressor(self):
         if self.energy_regressor_path is not None:
             self.regressor = EnergyRegressor.read(
                 self.energy_regressor_path,
@@ -104,10 +108,10 @@ class Apply(Tool):
                 algorithm=self.regressor.model.model_cls,
                 parent=self,
             )
-            self.apply_regressor = True
-        else:
-            self.apply_regressor = False
+            return True
+        return False
 
+    def _setup_classifier(self):
         if self.particle_classifier_path is not None:
             self.classifier = ParticleIdClassifier.read(
                 self.particle_classifier_path,
@@ -120,128 +124,137 @@ class Apply(Tool):
                 algorithm=self.classifier.model.model_cls,
                 parent=self,
             )
-            self.apply_classifier = True
-        else:
-            self.apply_classifier = False
-
-    @property
-    def _has_classifier(self):
-        return self.particle_classifier_path is not None
-
-    @property
-    def _has_regressor(self):
-        return self.energy_regressor_path is not None
+            return True
+        return False
 
     def start(self):
-        self.log.info("Applying model")
-        if self._has_regressor:
-            regressor_prefix = self.regressor.model.model_cls
-        if self._has_classifier:
-            classifier_prefix = self.classifier.model.model_cls
+        if self.apply_regressor:
+            self.log.info("Apply regressor.")
+            mono_predictions = self._apply_regressor()
+            self._combine_regressor(mono_predictions)
+
+        if self.apply_classifier:
+            self.log.info("Apply classifier.")
+            mono_predictions = self._apply_classifier()
+            self._combine_classifier(mono_predictions)
+
+    def _apply_regressor(self):
+        prefix = self.regressor.model.model_cls
 
         tables = []
+
         for tel_id, tel in tqdm(self.loader.subarray.tel.items()):
-            if self._has_regressor:
-                if tel not in self.regressor.model.models:
-                    self.log.warning(
-                        "No regressor model for telescope type %s, skipping tel %d",
-                        tel,
-                        tel_id,
-                    )
-                    self.apply_regressor = False
-                else:
-                    self.apply_regressor = True
-            if self._has_classifier:
-                if tel not in self.classifier.model.models:
-                    self.log.warning(
-                        "No classifier model for telescope type %s, skipping tel %d",
-                        tel,
-                        tel_id,
-                    )
-                    self.apply_classifier = False
-                else:
-                    self.apply_classifier = True
+            if tel not in self.regressor.model.models:
+                self.log.warning(
+                    "No regressor model for telescope type %s, skipping tel %d",
+                    tel,
+                    tel_id,
+                )
+                continue
 
             table = self.loader.read_telescope_events([tel_id])
-            if self.apply_regressor:
-                table.remove_columns(
-                    [c for c in table.colnames if c.startswith(regressor_prefix)]
-                )
-            if self.apply_classifier:
-                table.remove_columns(
-                    [c for c in table.colnames if c.startswith(classifier_prefix)]
-                )
-
             if len(table) == 0:
                 self.log.warning("No events for telescope %d", tel_id)
                 continue
 
-            if self.apply_regressor:
-                predictions = self.regressor.predict(tel, table)
-                table = hstack(
-                    [table, predictions],
-                    join_type="exact",
-                    metadata_conflicts="ignore",
-                )
-                write_table(
-                    table[["obs_id", "event_id", "tel_id"] + predictions.colnames],
-                    self.loader.input_url,
-                    f"/dl2/event/telescope/energy/{regressor_prefix}/tel_{tel_id:03d}",
-                    mode="a",
-                    overwrite=self.overwrite,
-                )
-            if self.apply_classifier:
-                predictions = self.classifier.predict(tel, table)
-                table = hstack(
-                    [table, predictions],
-                    join_type="exact",
-                    metadata_conflicts="ignore",
-                )
-                write_table(
-                    table[["obs_id", "event_id", "tel_id"] + predictions.colnames],
-                    self.loader.input_url,
-                    f"/dl2/event/telescope/classification/{classifier_prefix}/tel_{tel_id:03d}",
-                    mode="a",
-                    overwrite=self.overwrite,
-                )
+            table.remove_columns([c for c in table.colnames if c.startswith(prefix)])
+
+            predictions = self.regressor.predict(tel, table)
+            table = hstack(
+                [table, predictions],
+                join_type="exact",
+                metadata_conflicts="ignore",
+            )
+            write_table(
+                table[["obs_id", "event_id", "tel_id"] + predictions.colnames],
+                self.loader.input_url,
+                f"/dl2/event/telescope/energy/{prefix}/tel_{tel_id:03d}",
+                mode="a",
+                overwrite=self.overwrite,
+            )
             tables.append(table)
 
         if len(tables) == 0:
             raise ValueError("No predictions made for any telescope")
 
-        mono_predictions = vstack(tables)
-        if self.apply_regressor:
-            stereo_predictions = self.regressor_combine.predict(mono_predictions)
-            trafo = TelListToMaskTransform(self.loader.subarray)
-            for c in filter(
-                lambda c: c.name.endswith("tel_ids"),
-                stereo_predictions.columns.values(),
-            ):
-                stereo_predictions[c.name] = np.array([trafo(r) for r in c])
+        return vstack(tables)
 
+    def _combine_regressor(self, mono_predictions):
+        stereo_predictions = self.regressor_combine.predict(mono_predictions)
+
+        trafo = TelListToMaskTransform(self.loader.subarray)
+        for c in filter(
+            lambda c: c.name.endswith("tel_ids"),
+            stereo_predictions.columns.values(),
+        ):
+            stereo_predictions[c.name] = np.array([trafo(r) for r in c])
+
+        write_table(
+            stereo_predictions,
+            self.loader.input_url,
+            f"/dl2/event/subarray/energy/{self.regressor.model.model_cls}",
+            mode="a",
+            overwrite=self.overwrite,
+        )
+
+    def _apply_classifier(self):
+        prefix = self.classifier.model.model_cls
+
+        tables = []
+
+        for tel_id, tel in tqdm(self.loader.subarray.tel.items()):
+            if tel not in self.classifier.model.models:
+                self.log.warning(
+                    "No classifier model for telescope type %s, skipping tel %d",
+                    tel,
+                    tel_id,
+                )
+                continue
+
+            table = self.loader.read_telescope_events([tel_id])
+            if len(table) == 0:
+                self.log.warning("No events for telescope %d", tel_id)
+                continue
+
+            table.remove_columns([c for c in table.colnames if c.startswith(prefix)])
+
+            predictions = self.classifier.predict(tel, table)
+            table = hstack(
+                [table, predictions],
+                join_type="exact",
+                metadata_conflicts="ignore",
+            )
             write_table(
-                stereo_predictions,
+                table[["obs_id", "event_id", "tel_id"] + predictions.colnames],
                 self.loader.input_url,
-                f"/dl2/event/subarray/energy/{self.regressor.model.model_cls}",
+                f"/dl2/event/telescope/classification/{prefix}/tel_{tel_id:03d}",
                 mode="a",
                 overwrite=self.overwrite,
             )
-        if self.apply_classifier:
-            stereo_predictions = self.classifier_combine.predict(mono_predictions)
-            trafo = TelListToMaskTransform(self.loader.subarray)
-            for c in filter(
-                lambda c: c.name.endswith("tel_ids"),
-                stereo_predictions.columns.values(),
-            ):
-                stereo_predictions[c.name] = np.array([trafo(r) for r in c])
+            tables.append(table)
 
-            write_table(
-                stereo_predictions,
-                self.loader.input_url,
-                f"/dl2/event/subarray/classification/{self.classifier.model.model_cls}",
-                mode="a",
-                overwrite=self.overwrite,
-            )
+        if len(tables) == 0:
+            raise ValueError("No predictions made for any telescope")
+
+        return vstack(tables)
+
+    def _combine_classifier(self, mono_predictions):
+        stereo_predictions = self.classifier_combine.predict(mono_predictions)
+
+        trafo = TelListToMaskTransform(self.loader.subarray)
+        for c in filter(
+            lambda c: c.name.endswith("tel_ids"),
+            stereo_predictions.columns.values(),
+        ):
+            stereo_predictions[c.name] = np.array([trafo(r) for r in c])
+
+        write_table(
+            stereo_predictions,
+            self.loader.input_url,
+            f"/dl2/event/subarray/classification/{self.classifier.model.model_cls}",
+            mode="a",
+            overwrite=self.overwrite,
+        )
 
     def finish(self):
         self.h5file.close()
