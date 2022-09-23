27118ae327cd49fb1865397c633130bbee2e5a30
Felix Werner <felix.werner@mpi-hd.mpg.de>
2022-09-23 14:23:30 +0200

    Improve compatibility with traitlets >4.0.

--- ctapipe/image/extractor.py
+++ ctapipe/image/extractor.py
@@ -1364,12 +1364,12 @@ def deconvolve(waveforms, bls, up: int, pz: float):
 
 class FlashCamExtractor(ImageExtractor):
     upsampling = IntTelescopeParameter(
-        default_value=4, min=1, help="Define the upsampling factor for waveforms"
-    ).tag(config=True)
+        default_value=4, help="Define the upsampling factor for waveforms"
+    ).tag(config=True, min=1)
 
     window_width = IntTelescopeParameter(
-        default_value=4, min=1, help="Define the width of the integration window"
-    ).tag(config=True)
+        default_value=4, help="Define the width of the integration window"
+    ).tag(config=True, min=1)
 
     window_shift = IntTelescopeParameter(
         default_value=2,
