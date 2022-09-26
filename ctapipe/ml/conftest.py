542f8f4093617f2c484c65e52337e9888ce4b52b
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2022-09-26 14:41:52 +0200

    Use filelock for ml test fixtures

--- ctapipe/ml/conftest.py
+++ ctapipe/ml/conftest.py
@@ -1,6 +1,8 @@
 import pytest
-from ctapipe.utils.datasets import resource_file
+
 from ctapipe.core import run_tool
+from ctapipe.utils.datasets import resource_file
+from ctapipe.utils.filelock import FileLock
 
 
 @pytest.fixture(scope="session")
@@ -12,38 +14,48 @@ def model_tmp_path(tmp_path_factory):
 def energy_regressor_path(model_tmp_path):
     from ctapipe.ml.tools.train_energy_regressor import TrainEnergyRegressor
 
-    tool = TrainEnergyRegressor()
-    config = resource_file("ml_config.yaml")
     out_file = model_tmp_path / "energy.pkl"
-    ret = run_tool(
-        tool,
-        argv=[
-            "--input=dataset://gamma_diffuse_dl2_train_small.dl2.h5",
-            f"--output={out_file}",
-            f"--config={config}",
-            "--log-level=INFO",
-        ],
-    )
-    assert ret == 0
-    return out_file
+
+    with FileLock(out_file.with_suffix(out_file.suffix + ".lock")):
+        if out_file.is_file():
+            return out_file
+
+        tool = TrainEnergyRegressor()
+        config = resource_file("ml_config.yaml")
+        ret = run_tool(
+            tool,
+            argv=[
+                "--input=dataset://gamma_diffuse_dl2_train_small.dl2.h5",
+                f"--output={out_file}",
+                f"--config={config}",
+                "--log-level=INFO",
+            ],
+        )
+        assert ret == 0
+        return out_file
 
 
 @pytest.fixture(scope="session")
 def particle_classifier_path(model_tmp_path):
     from ctapipe.ml.tools.train_particle_classifier import TrainParticleIdClassifier
 
-    tool = TrainParticleIdClassifier()
-    config = resource_file("ml_config.yaml")
     out_file = model_tmp_path / "particle_classifier.pkl"
-    ret = run_tool(
-        tool,
-        argv=[
-            "--signal=dataset://gamma_diffuse_dl2_train_small.dl2.h5",
-            "--background=dataset://proton_dl2_train_small.dl2.h5",
-            f"--output={out_file}",
-            f"--config={config}",
-            "--log-level=INFO",
-        ],
-    )
-    assert ret == 0
-    return out_file
+    with FileLock(out_file.with_suffix(out_file.suffix + ".lock")):
+        if out_file.is_file():
+            return out_file
+
+        tool = TrainParticleIdClassifier()
+        config = resource_file("ml_config.yaml")
+
+        ret = run_tool(
+            tool,
+            argv=[
+                "--signal=dataset://gamma_diffuse_dl2_train_small.dl2.h5",
+                "--background=dataset://proton_dl2_train_small.dl2.h5",
+                f"--output={out_file}",
+                f"--config={config}",
+                "--log-level=INFO",
+            ],
+        )
+        assert ret == 0
+        return out_file
