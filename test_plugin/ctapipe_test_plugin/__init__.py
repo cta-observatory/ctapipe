fb5b291a5a6e099481b072ae1cdf2f63ea0a06a5
Maximilian Linhoff <maximilian.linhoff@tu-dortmund.de>
2022-10-21 16:48:20 +0200

    Register event sources, add test

--- test_plugin/ctapipe_test_plugin/__init__.py
+++ test_plugin/ctapipe_test_plugin/__init__.py
@@ -18,7 +18,7 @@ from ctapipe.io import DataLevel, EventSource
 from ctapipe.io.datawriter import ArrayEventContainer
 
 optics = OpticsDescription(
-    "dummy",
+    "plugin",
     size_type=SizeType.LST,
     n_mirrors=1,
     equivalent_focal_length=28 * u.m,
@@ -32,10 +32,10 @@ step = 0.1
 t = np.arange(0, 40, step)
 geometry = CameraGeometry.make_rectangular()
 camera = CameraDescription(
-    name="dummy",
+    name="plugin",
     geometry=geometry,
     readout=CameraReadout(
-        name="dummy",
+        name="plugin",
         sampling_rate=1 * u.GHz,
         reference_pulse_shape=np.exp(-0.5 * (t - 10) ** 2 / 4),
         reference_pulse_sample_width=step * u.ns,
@@ -47,21 +47,21 @@ camera = CameraDescription(
 
 
 telescope = TelescopeDescription(
-    "dummy",
+    "plugin",
     optics=optics,
     camera=camera,
 )
 
 
 subarray = SubarrayDescription(
-    name="Dummy",
+    name="plugin",
     tel_descriptions={1: telescope},
     tel_positions={1: [0, 0, 0] * u.m},
 )
 
 
 class PluginEventSource(EventSource):
-    """A minimal dummy event source"""
+    """A minimal plugin event source"""
 
     is_simulation = False
     datalevels = (DataLevel.DL1_IMAGES,)
@@ -71,7 +71,7 @@ class PluginEventSource(EventSource):
 
     @classmethod
     def is_compatible(cls, path):
-        return str(path).endswith(".dummy")
+        return str(path).endswith(".plugin")
 
     def _generator(self):
         for i in range(10):
