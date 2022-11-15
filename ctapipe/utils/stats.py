2bd7d0c6cbc3f138b98f0d8f8af130e475ab7f0c
Stefan Fröse <stefan.froese@udo.edu>
2022-11-15 18:25:53 +0100

    survival_function fix

--- ctapipe/utils/stats.py
+++ ctapipe/utils/stats.py
@@ -4,4 +4,4 @@ from scipy.special import erf
 
 def survival_function(x):
     # this 3x faster than scipy.stats.norm.sf
-    return (1.0 + erf(x / np.sqrt(2.0))) / 2.0
+    return 1 - (erf(x / np.sqrt(2.0))) / 2.0
