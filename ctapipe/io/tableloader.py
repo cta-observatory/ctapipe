cdadae1e39f139ad8bd4915deeffa2a8099ebaa6
Maximilian Linhoff <maximilian.linhoff@tu-dortmund.de>
2022-11-18 16:27:47 +0100

    Fall back to join if hstack is not possible

--- ctapipe/io/tableloader.py
+++ ctapipe/io/tableloader.py
@@ -2,6 +2,7 @@
 Class and related functions to read DL1 (a,b) and/or DL2 (a) data
 from an HDF5 file produced with ctapipe-process.
 """
+import warnings
 from collections import defaultdict
 from pathlib import Path
 from typing import Dict
@@ -35,6 +36,10 @@ DL2_TELESCOPE_GROUP = "/dl2/event/telescope"
 
 SUBARRAY_EVENT_KEYS = ["obs_id", "event_id"]
 TELESCOPE_EVENT_KEYS = ["obs_id", "event_id", "tel_id"]
+
+
+class IndexNotMatching(UserWarning):
+    """Warning that is raised if the order of two tables is not matching as expected"""
 
 
 class ChunkIterator:
@@ -113,7 +118,7 @@ def _join_telescope_events(table1, table2):
     return join_allow_empty(table1, table2, TELESCOPE_EVENT_KEYS, how)
 
 
-def _merge_table_same_index(table1, table2, index_keys):
+def _merge_table_same_index(table1, table2, index_keys, fallback_join_type="left"):
     """Merge two tables assuming their primary keys are identical"""
     if len(table1) != len(table2):
         raise ValueError("Tables must have identical length")
@@ -122,7 +127,10 @@ def _merge_table_same_index(table1, table2, index_keys):
         return table1
 
     if not np.all(table1[index_keys] == table2[index_keys]):
-        raise ValueError(f"Tables primary keys ({index_keys}) do not match")
+        warnings.warn(
+            "Table order does not match, falling back to join", IndexNotMatching
+        )
+        return join_allow_empty(table1, table2, index_keys, fallback_join_type)
 
     columns = [col for col in table2.columns if col not in index_keys]
     return hstack((table1, table2[columns]), join_type="exact")
@@ -348,10 +356,7 @@ class TableLoader(Component):
                             start=start,
                             stop=stop,
                         )
-                        try:
-                            table = _merge_subarray_tables(table, dl2)
-                        except ValueError:
-                            table = _join_subarray_events(table, dl2)
+                        table = _merge_subarray_tables(table, dl2)
 
         if self.load_observation_info:
             table = self._join_observation_info(table, start=start, stop=stop)
