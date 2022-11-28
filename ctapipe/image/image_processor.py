a0a9ee3f0049357a1acb77c708fae3bd7123a93d
Stefan Fröse <stefan.froese@udo.edu>
2022-11-28 09:44:33 +0100

    image processing in TelescopeFrame only

--- ctapipe/image/image_processor.py
+++ ctapipe/image/image_processor.py
@@ -1,23 +1,19 @@
 """
 High level image processing  (ImageProcessor Component)
 """
-from copy import deepcopy
-
 import numpy as np
 
 from ctapipe.coordinates import TelescopeFrame
 
 from ..containers import (
     ArrayEventContainer,
-    CameraHillasParametersContainer,
-    CameraTimingParametersContainer,
     ImageParametersContainer,
     IntensityStatisticsContainer,
     PeakTimeStatisticsContainer,
     TimingParametersContainer,
 )
 from ..core import QualityQuery, TelescopeComponent
-from ..core.traits import Bool, BoolTelescopeParameter, ComponentName, List
+from ..core.traits import BoolTelescopeParameter, ComponentName, List
 from ..instrument import SubarrayDescription
 from .cleaning import ImageCleaner
 from .concentration import concentration_parameters
@@ -40,13 +36,7 @@ DEFAULT_TRUE_IMAGE_PARAMETERS.intensity_statistics = IntensityStatisticsContaine
     kurtosis=np.float64(np.nan),
 )
 DEFAULT_TIMING_PARAMETERS = TimingParametersContainer()
-DEFAULT_TIMING_PARAMETERS_CAMFRAME = CameraTimingParametersContainer()
 DEFAULT_PEAKTIME_STATISTICS = PeakTimeStatisticsContainer()
-
-
-DEFAULT_IMAGE_PARAMETERS_CAMFRAME = deepcopy(DEFAULT_IMAGE_PARAMETERS)
-DEFAULT_IMAGE_PARAMETERS_CAMFRAME.hillas = CameraHillasParametersContainer()
-DEFAULT_IMAGE_PARAMETERS_CAMFRAME.timing = CameraTimingParametersContainer()
 
 
 class ImageQualityQuery(QualityQuery):
@@ -66,11 +56,6 @@ class ImageProcessor(TelescopeComponent):
 
     image_cleaner_type = ComponentName(
         ImageCleaner, default_value="TailcutsImageCleaner"
-    ).tag(config=True)
-
-    use_telescope_frame = Bool(
-        default_value=True,
-        help="Whether to calculate parameters in the telescope or camera frame",
     ).tag(config=True)
 
     apply_image_modifier = BoolTelescopeParameter(
@@ -105,16 +90,14 @@ class ImageProcessor(TelescopeComponent):
 
         self.check_image = ImageQualityQuery(parent=self)
 
-        self.default_image_container = DEFAULT_IMAGE_PARAMETERS_CAMFRAME
-        if self.use_telescope_frame:
-            self.default_image_container = DEFAULT_IMAGE_PARAMETERS
-            telescope_frame = TelescopeFrame()
-            self.telescope_frame_geometries = {
-                tel_id: self.subarray.tel[tel_id].camera.geometry.transform_to(
-                    telescope_frame
-                )
-                for tel_id in self.subarray.tel
-            }
+        self.default_image_container = DEFAULT_IMAGE_PARAMETERS
+        telescope_frame = TelescopeFrame()
+        self.telescope_frame_geometries = {
+            tel_id: self.subarray.tel[tel_id].camera.geometry.transform_to(
+                telescope_frame
+            )
+            for tel_id in self.subarray.tel
+        }
 
     def __call__(self, event: ArrayEventContainer):
         self._process_telescope_event(event)
@@ -182,10 +165,7 @@ class ImageProcessor(TelescopeComponent):
                     container_class=PeakTimeStatisticsContainer,
                 )
             else:
-                if self.use_telescope_frame:
-                    timing = DEFAULT_TIMING_PARAMETERS
-                else:
-                    timing = DEFAULT_TIMING_PARAMETERS_CAMFRAME
+                timing = DEFAULT_TIMING_PARAMETERS
 
                 peak_time_statistics = DEFAULT_PEAKTIME_STATISTICS
 
@@ -209,11 +189,8 @@ class ImageProcessor(TelescopeComponent):
         """
         for tel_id, dl1_camera in event.dl1.tel.items():
 
-            if self.use_telescope_frame:
-                # Use the transformed geometries
-                geometry = self.telescope_frame_geometries[tel_id]
-            else:
-                geometry = self.subarray.tel[tel_id].camera.geometry
+            # Use the transformed geometries
+            geometry = self.telescope_frame_geometries[tel_id]
 
             if self.apply_image_modifier.tel[tel_id]:
                 dl1_camera.image = self.modify(tel_id=tel_id, image=dl1_camera.image)
