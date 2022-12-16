51004a4f9541de5a920a638929e593e5e08b3c81
Rune Michael Dominik <rune.dominik@tu-dortmund.de>
2022-12-16 16:08:50 +0100

    Add asserts to processor test

--- ctapipe/image/muon/tests/test_processor.py
+++ ctapipe/image/muon/tests/test_processor.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from ctapipe.image import ImageProcessor
 from ctapipe.io import EventSource
 
@@ -8,6 +10,20 @@ def test_processor(dl1_muon_file):
     with EventSource(dl1_muon_file, focal_length_choice="EQUIVALENT") as source:
         image_processor = ImageProcessor(source.subarray)
         muon_processor = MuonProcessor(source.subarray)
+
+        efficiencies = []
         for event in source:
             image_processor(event)
             muon_processor(event)
+            for tel_id in event.dl1.tel:
+                efficiencies.append(
+                    event.muon.tel[tel_id].efficiency.optical_efficiency
+                )
+
+        assert len(efficiencies) > 0  # Assert there were events analyzed
+        assert np.any(
+            np.isfinite(efficiencies)
+        )  # Assert at least some were completely analyzed
+        assert np.all(
+            np.logical_or(np.isfinite(efficiencies), np.isnan(efficiencies))
+        )  # Assert all were at least provided defaults
