4dc95c8ba5f1559d4d4eaf47cb5bc08439a8ad61
Maximilian Linhoff <maximilian.linhoff@tu-dortmund.de>
2023-01-23 12:57:07 +0100

    Caplog tests seem to not work with pytest-xdist, remove for now

--- ctapipe/reco/tests/test_reconstructor.py
+++ ctapipe/reco/tests/test_reconstructor.py
@@ -1,28 +1,12 @@
 """
 Test for Reconstructor base class
 """
-import logging
 
 
-def test_plugin(subarray_prod5_paranal, caplog):
+def test_plugin(subarray_prod5_paranal):
     from ctapipe.reco import Reconstructor
 
     subarray = subarray_prod5_paranal
-    with caplog.at_level(logging.INFO, logger="ctapipe.core.plugins"):
-        reconstructor = Reconstructor.from_name("PluginReconstructor", subarray)
-
+    reconstructor = Reconstructor.from_name("PluginReconstructor", subarray)
     assert reconstructor.__module__ == "ctapipe_test_plugin"
     assert reconstructor.__class__.__name__ == "PluginReconstructor"
-
-    assert caplog.record_tuples == [
-        (
-            "ctapipe.core.plugins",
-            logging.INFO,
-            "Loading ctapipe_reco plugin: ctapipe_test_plugin:PluginReconstructor",
-        ),
-        (
-            "ctapipe.core.plugins",
-            logging.INFO,
-            "Entrypoint provides: <class 'ctapipe_test_plugin.PluginReconstructor'>",
-        ),
-    ]
