1cb4b7421eb34ad82d95841dd97ee5b74bc961c4
Noah Biederbeck <noah.biederbeck@tu-dortmund.de>
2023-01-24 14:30:22 +0100

    Make QualityQuery a TelescopeComponent

--- ctapipe/core/qualityquery.py
+++ ctapipe/core/qualityquery.py
@@ -6,8 +6,12 @@ __all__ = ["QualityQuery", "QualityCriteriaError"]
 
 import numpy as np  # for use in selection functions
 
-from .component import Component
 from .expression_engine import ExpressionEngine
+from .telescope_component import (
+    TelescopeComponent,
+    TelescopeParameter,
+    TelescopeParameterLookup,
+)
 from .traits import List, Tuple, Unicode
 
 
@@ -15,7 +19,7 @@ class QualityCriteriaError(TypeError):
     """Signal a problem with a user-defined selection criteria function"""
 
 
-class QualityQuery(Component):
+class QualityQuery(TelescopeComponent):
     """
     Manages a set of user-configurable (at runtime or in a config file) selection
     criteria that operate on the same type of input. Each time it is called, it
@@ -25,38 +29,39 @@ class QualityQuery(Component):
     """
 
     quality_criteria = List(
-        Tuple(Unicode(), Unicode()),
-        help=(
-            "list of tuples of ('<description', 'expression string') to accept "
-            "(select) a given data value.  E.g. ``[('mycut', 'x > 3'),]``. "
-            "You may use ``numpy`` as ``np`` and ``astropy.units`` as ``u``,"
-            " but no other modules."
-        ),
+        Tuple(Unicode(), TelescopeParameter(Unicode())),
+        help="List of tuples...",
     ).tag(config=True)
 
-    def __init__(self, config=None, parent=None, **kwargs):
-        super().__init__(config=config, parent=parent, **kwargs)
+    def __init__(self, subarray, config=None, parent=None, **kwargs):
+        super().__init__(subarray=subarray, config=config, parent=parent, **kwargs)
 
-        # add a selection to count all entries and make it the first one
-        self.criteria_names = [n for (n, _) in self.quality_criteria]
-        self.expressions = [e for (_, e) in self.quality_criteria]
+        self.engines = []
+        self.criteria_names = []
 
-        self.engine = ExpressionEngine(self.quality_criteria)
-        for _, expr in self.quality_criteria:
-            if "lambda" in expr:
-                raise ValueError(
-                    "As of ctapipe 0.16, do not give lambda expressions"
-                    " to QualityQuery. Directly give the expression."
-                    " E.g. instead of `lambda p: p.hillas.width.value > 0`"
-                    " use `parameters.hillas.width.value > 0`"
-                )
+        for name, criteria in self.quality_criteria:
+            for i, (command, arg, expr) in enumerate(criteria):
+                if "lambda" in expr:
+                    raise ValueError(
+                        "As of ctapipe 0.16, do not give lambda expressions"
+                        " to QualityQuery. Directly give the expression."
+                        " E.g. instead of `lambda p: p.hillas.width.value > 0`"
+                        " use `parameters.hillas.width.value > 0`"
+                    )
+                criteria[i] = (command, arg, ExpressionEngine(((name, expr),)))
+
+            self.criteria_names.append(name)
+
+            lookup = TelescopeParameterLookup(criteria)
+            lookup.attach_subarray(self.subarray)
+            self.engines.append((name, lookup))
 
         # arrays for recording overall statistics, add one for total count
-        n = len(self.quality_criteria) + 1
+        n = len(self.criteria_names) + 1
         self._counts = np.zeros(n, dtype=np.int64)
         self._cumulative_counts = np.zeros(n, dtype=np.int64)
 
-    def to_table(self, functions=False):
+    def to_table(self):
         """
         Return a tabular view of the latest quality summary
 
@@ -64,11 +69,6 @@ class QualityQuery(Component):
         - *criteria*: name of each criterion
         - *counts*: counts of each criterion independently
         - *cum_counts*: counts of cumulative application of each criterion in order
-
-        Parameters
-        ----------
-        functions: bool:
-            include the function string as a column
 
         Returns
         -------
@@ -81,8 +81,6 @@ class QualityQuery(Component):
             "counts": self._counts,
             "cumulative_counts": self._cumulative_counts,
         }
-        if functions:
-            cols["func"] = ["True"] + self.expressions
         return Table(cols)
 
     def _repr_html_(self):
@@ -93,7 +91,7 @@ class QualityQuery(Component):
         """Print a formatted string representation of the entire table."""
         return str(self.to_table())
 
-    def __call__(self, **kwargs) -> np.ndarray:
+    def __call__(self, tel_id, **kwargs) -> np.ndarray:
         """
         Test that value passes all cuts
 
@@ -108,10 +106,11 @@ class QualityQuery(Component):
             array of booleans with results of each selection criterion in order
         """
         # add 1 for total
-        result = np.ones(len(self.quality_criteria) + 1, dtype=bool)
+        result = np.ones(len(self.engines) + 1, dtype=bool)
 
-        for i, res in enumerate(self.engine(kwargs), start=1):
-            result[i] = res
+        for i, (_, lookup) in enumerate(self.engines, start=1):
+            expr = lookup[tel_id]
+            result[i] = list(expr(kwargs))[0]
 
         self._counts += result.astype(int)
         self._cumulative_counts += result.cumprod()
@@ -132,11 +131,13 @@ class QualityQuery(Component):
         mask : np.ndarray[bool]
             Boolean mask of valid entries.
         """
-        n_criteria = len(self.quality_criteria) + 1
+        n_criteria = len(self.criteria_names) + 1
         result = np.ones((n_criteria, len(table)), dtype=bool)
 
-        for i, res in enumerate(self.engine(table), start=1):
-            result[i] = res
+        for i, (_, engine) in enumerate(self.engines, start=1):
+            for tel_id in np.unique(table["tel_id"]):
+                tel_mask = table["tel_id"] == tel_id
+                result[i][tel_mask] = list(engine[tel_id](table[tel_mask]))[0]
 
         self._counts += np.count_nonzero(result, axis=1)
         self._cumulative_counts += np.count_nonzero(np.cumprod(result, axis=0), axis=1)
--- ctapipe/core/tests/test_qualityquery.py
+++ ctapipe/core/tests/test_qualityquery.py
@@ -8,33 +8,33 @@ from ctapipe.core.qualityquery import QualityQuery
 from ctapipe.core.traits import List
 
 
-def test_selector():
+def test_selector(subarray_prod5_paranal):
     """test the functionality of an example Selector subclass"""
 
     class ExampleQualityQuery(QualityQuery):
         """Available variables: x"""
 
         quality_criteria = List(
-            default_value=[
-                ("high_enough", "x > 3"),
-                ("a_value_not_too_high", "x < 100"),
-                ("smallish", "x < sqrt(100)"),
+            [
+                ("high_enough", [("type", "*", "x > 3")]),
+                ("a_value_not_too_high", [("type", "*", "x < 100")]),
+                ("smallish", [("type", "*", "x < np.sqrt(100)")]),
             ],
         ).tag(config=True)
 
-    query = ExampleQualityQuery()
+    query = ExampleQualityQuery(subarray=subarray_prod5_paranal)
 
-    criteria1 = query(x=0)  # pass smallish
+    criteria1 = query(tel_id=1, x=0)  # pass smallish
     assert len(criteria1) == 3
     assert (criteria1 == [False, True, True]).all()
 
-    criteria2 = query(x=20)  # pass high_enough + not_too_high
+    criteria2 = query(tel_id=1, x=20)  # pass high_enough + not_too_high
     assert (criteria2 == [True, True, False]).all()
 
-    criteria3 = query(x=200)  # pass high_enough, fail not_too_high
+    criteria3 = query(tel_id=1, x=200)  # pass high_enough, fail not_too_high
     assert (criteria3 == [True, False, False]).all()
 
-    criteria4 = query(x=8)  # pass all
+    criteria4 = query(tel_id=1, x=8)  # pass all
     assert np.all(criteria4)
 
     tab = query.to_table()
@@ -61,61 +61,75 @@ def test_selector():
     assert tab["cumulative_counts"][3] == 1
 
 
-def test_invalid_input():
+def test_invalid_input(subarray_prod5_paranal):
     class ExampleQualityQuery(QualityQuery):
         """Available variables: x"""
 
         quality_criteria = List(
-            default_value=[
-                ("high_enough", "x > 3"),
-                ("a_value_not_too_high", "x < 100"),
-                ("smallish", "x < np.sqrt(100)"),
+            [
+                ("high_enough", [("type", "*", "x > 3")]),
+                ("a_value_not_too_high", [("type", "*", "x < 100")]),
+                ("smallish", [("type", "*", "x < np.sqrt(100)")]),
             ],
         ).tag(config=True)
 
-    query = ExampleQualityQuery()
+    query = ExampleQualityQuery(subarray=subarray_prod5_paranal)
     with pytest.raises(ExpressionError):
-        query(y=5)
+        query(tel_id=1, y=5)
 
 
-def test_bad_selector():
+def test_bad_selector(subarray_prod5_paranal):
     """ensure failure if a selector function is not a function or can't be evaluated"""
 
     query = QualityQuery(
         quality_criteria=[
-            ("high_enough", "x > 3"),
-            ("not_good", "foo"),
-            ("smallish", "x < 10"),
-        ]
+            ("high_enough", [("type", "*", "x > 3")]),
+            ("not good", [("type", "*", "foo")]),
+            ("smallish", [("type", "*", "x < np.sqrt(100)")]),
+        ],
+        subarray=subarray_prod5_paranal,
     )
     with pytest.raises(ExpressionError):
-        query(x=5)
+        query(tel_id=1, x=5)
 
     # ensure we can't run arbitrary code.
     # try to construct something that is not in the
     # ALLOWED_GLOBALS list, but which is imported in selector.py
     # and see if it works in a function
     with pytest.raises(ExpressionError):
-        query = QualityQuery(quality_criteria=[("dangerous", "Component()")])
-        query(x=10)
+        query = QualityQuery(
+            quality_criteria=[("dangerous", [("type", "*", "Component()")])],
+            subarray=subarray_prod5_paranal,
+        )
+        query(tel_id=1, x=10)
 
     # test we only support expressions, not statements
     with pytest.raises(ExpressionError):
         query = QualityQuery(
-            quality_criteria=[("dangerous", "import numpy; np.array([])")]
+            quality_criteria=[
+                ("dangerous", [("type", "*", "import numpy; np.array([])")])
+            ],
+            subarray=subarray_prod5_paranal,
         )
 
 
-def test_table_mask():
+def test_table_mask_and_to_table(subarray_prod5_paranal):
     """Test getting a mask for a whole table"""
     query = QualityQuery(
         quality_criteria=[
-            ("foo", "(x**2 + y**2) < 1.0"),
-            ("bar", "x < 0.5"),
+            ("inside unit circle", [("type", "*", "(x**2 + y**2) < 1.0")]),
+            ("less than half", [("type", "*", "x < 0.5")]),
         ],
+        subarray=subarray_prod5_paranal,
     )
 
-    table = Table({"x": [1.0, 0.2, -0.5, 0.6, 0.7], "y": [0.0, 0.5, 1.0, 0.2, 0.1]})
+    table = Table(
+        {
+            "tel_id": [1, 2, 3, 4, 5],
+            "x": [1.0, 0.2, -0.5, 0.6, 0.7],
+            "y": [0.0, 0.5, 1.0, 0.2, 0.1],
+        }
+    )
 
     mask = query.get_table_mask(table)
     assert len(mask) == len(table)
@@ -126,32 +140,68 @@ def test_table_mask():
     np.testing.assert_equal(stats["cumulative_counts"], [5, 3, 1])
 
 
-def test_printing():
+def test_to_table_after_call(subarray_prod5_paranal):
+    """Test counting and conversion to table."""
+    query = QualityQuery(
+        quality_criteria=[
+            ("inside unit circle", [("type", "*", "(x**2 + y**2) < 1.0")]),
+            ("less than half", [("type", "*", "x < 0.5")]),
+        ],
+        subarray=subarray_prod5_paranal,
+    )
+    assert np.all(query(tel_id=1, x=0.3, y=0))
+    assert not np.all(query(tel_id=1, x=1, y=0))
+
+    assert np.all(query(tel_id=2, x=0.3, y=0))
+    assert not np.all(query(tel_id=2, x=1, y=0))
+
+    stats = query.to_table()
+    np.testing.assert_equal(stats["counts"], [4, 2, 2])
+    np.testing.assert_equal(stats["cumulative_counts"], [4, 2, 2])
+
+
+def test_printing(subarray_prod5_paranal):
     """Just check the query can be stringified correctly"""
     query = QualityQuery(
-        quality_criteria=[("check", "x>3")],
+        quality_criteria=[("check", [("type", "*", "x>3")])],
+        subarray=subarray_prod5_paranal,
     )
 
     assert isinstance(str(query), str)
 
 
-def test_setup():
+def test_setup(subarray_prod5_paranal):
     """Test that tuples can only have length 2"""
-    from traitlets import TraitError
+    QualityQuery(
+        quality_criteria=[
+            ("foo", [("type", "*", "expression")]),
+            ("bar", [("type", "*", "expression"), ("id", "1", "another-expresssion")]),
+            ("baz", "expression"),
+        ],
+        subarray=subarray_prod5_paranal,
+    )
 
-    # 2-tuple works
-    QualityQuery(quality_criteria=[("1", "2")])
 
-    # 3-tuple fails
-    with pytest.raises(TraitError):
-        QualityQuery(quality_criteria=[("1", "2", "3")])
-
-
-def test_with_lambda():
+def test_with_lambda(subarray_prod5_paranal):
     """Test that we raise an error when using lambda expresssions,
     which we don't support since v0.16.0"""
 
     with pytest.raises(ValueError, match="lambda"):
         QualityQuery(
-            quality_criteria=[("a", "lambda p: p > 2")],
+            quality_criteria=[("name", [("type", "*", "lambda p: p > 2")])],
+            subarray=subarray_prod5_paranal,
         )
+
+
+def test_telescope_component(subarray_prod5_paranal):
+    """Test that more specific patterns override more general."""
+    query = QualityQuery(
+        quality_criteria=[
+            ("foo", [("type", "*", "x > 0"), ("id", "2", "x < 0")]),
+        ],
+        subarray=subarray_prod5_paranal,
+    )
+
+    x = 1
+    assert query(x=x, tel_id=1)
+    assert not query(x=x, tel_id=2)
