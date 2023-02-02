5cbbd2974eab24bb11a17c115ec035a694038b3c
Rune Michael Dominik <rune.dominik@tu-dortmund.de>
2023-02-02 16:34:48 +0100

    Increase verbositiy of tests using test_plugin

--- ctapipe/io/tests/test_plugin.py
+++ ctapipe/io/tests/test_plugin.py
@@ -1,5 +1,14 @@
+import pytest
+import traitlets
+
+
 def test_plugin():
     """Test we can use the dummy event source provided by the test plugin"""
     from ctapipe.io import EventSource
 
-    assert EventSource("test.plugin").__class__.__name__ == "PluginEventSource"
+    try:
+        EventSource("test.plugin").__class__.__name__ == "PluginEventSource"
+    except traitlets.traitlets.TraitError:
+        pytest.fail(
+            "plugin event source not found, did you run pip install -e ./test_plugin"
+        )
