461e22197585acdfae235f089b84dd0405f5a745
Maximilian Linhoff <maximilian.linhoff@tu-dortmund.de>
2023-04-18 12:03:39 +0200

    Pass subarray to feature generator in sklearn reconstructors

--- ctapipe/reco/sklearn.py
+++ ctapipe/reco/sklearn.py
@@ -370,7 +370,7 @@ class EnergyRegressor(SKLearnRegressionReconstructor):
         """
         for tel_id in event.trigger.tels_with_trigger:
             table = collect_features(event, tel_id, self.instrument_table)
-            table = self.feature_generator(table)
+            table = self.feature_generator(table, subarray=self.subarray)
 
             # get_table_mask returns a table with a single row
             passes_quality_checks = self.quality_query.get_table_mask(table)[0]
@@ -397,7 +397,7 @@ class EnergyRegressor(SKLearnRegressionReconstructor):
 
     def predict_table(self, key, table: Table) -> Dict[ReconstructionProperty, Table]:
         """Predict on a table of events"""
-        table = self.feature_generator(table)
+        table = self.feature_generator(table, subarray=self.subarray)
 
         n_rows = len(table)
         energy = u.Quantity(np.full(n_rows, np.nan), self.unit, copy=False)
@@ -439,7 +439,7 @@ class ParticleClassifier(SKLearnClassificationReconstructor):
     def __call__(self, event: ArrayEventContainer) -> None:
         for tel_id in event.trigger.tels_with_trigger:
             table = collect_features(event, tel_id, self.instrument_table)
-            table = self.feature_generator(table)
+            table = self.feature_generator(table, subarray=self.subarray)
             passes_quality_checks = self.quality_query.get_table_mask(table)[0]
 
             if passes_quality_checks:
@@ -464,7 +464,7 @@ class ParticleClassifier(SKLearnClassificationReconstructor):
 
     def predict_table(self, key, table: Table) -> Dict[ReconstructionProperty, Table]:
         """Predict on a table of events"""
-        table = self.feature_generator(table)
+        table = self.feature_generator(table, subarray=self.subarray)
 
         n_rows = len(table)
         score = np.full(n_rows, np.nan)
@@ -681,7 +681,7 @@ class DispReconstructor(Reconstructor):
         """
         for tel_id in event.trigger.tels_with_trigger:
             table = collect_features(event, tel_id, self.instrument_table)
-            table = self.feature_generator(table)
+            table = self.feature_generator(table, subarray=self.subarray)
 
             passes_quality_checks = self.quality_query.get_table_mask(table)[0]
 
@@ -751,7 +751,7 @@ class DispReconstructor(Reconstructor):
         altaz_table : `~astropy.table.Table`
             Table with resulting predictions of horizontal coordinates
         """
-        table = self.feature_generator(table)
+        table = self.feature_generator(table, subarray=self.subarray)
 
         n_rows = len(table)
         disp = u.Quantity(np.full(n_rows, np.nan), self.unit, copy=False)
