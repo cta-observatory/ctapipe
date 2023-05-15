1283cdb8af6b9aebfd388ff46810b9af6a519cdb
nieves <clara.escanuela@mpi-hd.mpg.de>
2023-05-15 23:44:33 +0200

    added test when leading_edge_timing=False

--- ctapipe/image/tests/test_extractor.py
+++ ctapipe/image/tests/test_extractor.py
@@ -791,7 +791,14 @@ def test_FC_time(toymodel_mst_fc_time):
         true_time,
         mask,
     ) = toymodel_mst_fc_time
+
     extractor = FlashCamExtractor(subarray=subarray, leading_edge_timing=True)
+    broken_pixels = np.zeros(waveforms.shape[0], dtype=bool)
+    dl1 = extractor(waveforms, tel_id, selected_gain_channel, broken_pixels)
+    assert_allclose(dl1.peak_time[mask], true_time[mask], rtol=0.1)
+    assert dl1.is_valid == True
+
+    extractor = FlashCamExtractor(subarray=subarray, leading_edge_timing=False)
     broken_pixels = np.zeros(waveforms.shape[0], dtype=bool)
     dl1 = extractor(waveforms, tel_id, selected_gain_channel, broken_pixels)
     assert_allclose(dl1.peak_time[mask], true_time[mask], rtol=0.1)
