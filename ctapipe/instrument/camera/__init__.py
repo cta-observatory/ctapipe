23827a33eecdd31eb7554007675a1d811f336026
Anno Knierim <anno.knierim@tu-dortmund.de>
2023-07-10 11:40:56 +0200

    Fix isort failing in ctapipe/instrument/camera/__init__.py

--- ctapipe/instrument/camera/__init__.py
+++ ctapipe/instrument/camera/__init__.py
@@ -1,5 +1,5 @@
 from .description import CameraDescription  # noqa: F401
-from .geometry import CameraGeometry, UnknownPixelShapeWarning, PixelShape  # noqa: F401
+from .geometry import CameraGeometry, PixelShape, UnknownPixelShapeWarning  # noqa: F401
 from .readout import CameraReadout  # noqa: F401
 
 # __all__ = [
