dba13820c7f1402d8646bb65835b7da0f69a3b77
Maximilian Linhoff <maximilian.linhoff@tu-dortmund.de>
2023-07-17 11:30:46 +0200

    Fix duplicated reference warning for muon components

--- ctapipe/image/muon/__init__.py
+++ ctapipe/image/muon/__init__.py
@@ -5,17 +5,14 @@ from .features import (
     ring_containment,
 )
 from .fitting import kundu_chaudhuri_circle_fit
-from .intensity_fitter import MuonIntensityFitter
-from .processor import MuonProcessor
-from .ring_fitter import MuonRingFitter
+from .intensity_fitter import MuonIntensityFitter  # noqa: F401
+from .processor import MuonProcessor  # noqa: F401
+from .ring_fitter import MuonRingFitter  # noqa: F401
 
 __all__ = [
-    "MuonIntensityFitter",
-    "MuonRingFitter",
     "kundu_chaudhuri_circle_fit",
     "mean_squared_error",
     "intensity_ratio_inside_ring",
     "ring_completeness",
     "ring_containment",
-    "MuonProcessor",
 ]
