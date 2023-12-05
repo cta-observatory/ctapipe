e98bd0e9b9026139b95ef6f3aba2a09f1ede3dc5
Tomas Bylund <tomas.bylund@cea.fr>
2023-12-05 17:02:25 +0100

    Further refactoring to decouple things now that there are two tools

---
+++ ctapipe/irf/irfs.py
@@ -0,0 +1,157 @@
+"""components to generate irfs"""
+import astropy.units as u
+import numpy as np
+from pyirf.binning import create_bins_per_decade
+from pyirf.io import (
+    create_aeff2d_hdu,
+    create_energy_dispersion_hdu,
+    create_psf_table_hdu,
+)
+from pyirf.irf import effective_area_per_energy_and_fov, energy_dispersion, psf_table
+
+from ..core import Component
+from ..core.traits import Float, Integer
+from .binning import check_bins_in_range
+
+
+class PsfIrf(Component):
+    """
+    Collects information on generating energy and angular bins for
+    generating IRFs as per pyIRF requirements.
+    """
+
+    source_offset_min = Float(
+        help="Minimum value for Source offset for PSF IRF",
+        default_value=0,
+    ).tag(config=True)
+
+    source_offset_max = Float(
+        help="Maximum value for Source offset for PSF IRF",
+        default_value=1,
+    ).tag(config=True)
+
+    source_offset_n_edges = Integer(
+        help="Number of edges for Source offset for PSF IRF",
+        default_value=101,
+    ).tag(config=True)
+
+    def __init__(self, parent, energy_bins, valid_offset):
+
+        super().__init__(parent=parent)
+        self.energy_bins = energy_bins
+        self.valid_offset = valid_offset
+        self.source_offset_bins = (
+            np.linspace(
+                self.source_offset_min,
+                self.source_offset_max,
+                self.source_offset_n_edges,
+            )
+            * u.deg
+        )
+
+    def make_psf_table_hdu(self, signal_events, fov_offset_bins):
+        check_bins_in_range(fov_offset_bins, self.valid_offset)
+        psf = psf_table(
+            events=signal_events,
+            true_energy_bins=self.energy_bins,
+            fov_offset_bins=fov_offset_bins,
+            source_offset_bins=self.source_offset_bins,
+        )
+        return create_psf_table_hdu(
+            psf,
+            self.energy_bins,
+            self.source_offset_bins,
+            fov_offset_bins,
+        )
+
+
+class EnergyMigrationIrf(Component):
+    """Collects the functionality for generating Migration Matrix IRFs"""
+
+    energy_migration_min = Float(
+        help="Minimum value of Energy Migration matrix",
+        default_value=0.2,
+    ).tag(config=True)
+
+    energy_migration_max = Float(
+        help="Maximum value of Energy Migration matrix",
+        default_value=5,
+    ).tag(config=True)
+
+    energy_migration_n_bins = Integer(
+        help="Number of bins in log scale for Energy Migration matrix",
+        default_value=31,
+    ).tag(config=True)
+
+    def __init__(self, parent, energy_bins):
+        """
+        Creates bins per decade for true MC energy using pyirf function.
+        """
+        super().__init__(parent=parent)
+        self.energy_bins = energy_bins
+        self.migration_bins = np.geomspace(
+            self.energy_migration_min,
+            self.energy_migration_max,
+            self.energy_migration_n_bins,
+        )
+
+    def make_energy_dispersion_hdu(self, signal_events, fov_offset_bins):
+        edisp = energy_dispersion(
+            signal_events,
+            true_energy_bins=self.energy_bins,
+            fov_offset_bins=fov_offset_bins,
+            migration_bins=self.migration_bins,
+        )
+        return create_energy_dispersion_hdu(
+            edisp,
+            true_energy_bins=self.energy_bins,
+            migration_bins=self.migration_bins,
+            fov_offset_bins=fov_offset_bins,
+            extname="ENERGY_DISPERSION",
+        )
+
+
+class EffectiveAreaIrf(Component):
+    """Collects the functionality for generating Effective Area IRFs"""
+
+    true_energy_min = Float(
+        help="Minimum value for True Energy bins in TeV units",
+        default_value=0.005,
+    ).tag(config=True)
+
+    true_energy_max = Float(
+        help="Maximum value for True Energy bins in TeV units",
+        default_value=200,
+    ).tag(config=True)
+
+    true_energy_n_bins_per_decade = Float(
+        help="Number of edges per decade for True Energy bins",
+        default_value=10,
+    ).tag(config=True)
+
+    def __init__(self, parent, sim_info):
+        """
+        Creates bins per decade for true MC energy using pyirf function.
+        """
+        super().__init__(parent=parent)
+        self.true_energy_bins = create_bins_per_decade(
+            self.true_energy_min * u.TeV,
+            self.true_energy_max * u.TeV,
+            self.true_energy_n_bins_per_decade,
+        )
+        self.sim_info = sim_info
+
+    def make_effective_area_hdu(self, signal_events, fov_offset_bins):
+
+        effective_area = effective_area_per_energy_and_fov(
+            signal_events,
+            self.sim_info,
+            true_energy_bins=self.true_energy_bins,
+            fov_offset_bins=fov_offset_bins,
+        )
+        return create_aeff2d_hdu(
+            effective_area[..., np.newaxis],  # +1 dimension for FOV offset
+            self.true_energy_bins,
+            fov_offset_bins,
+            extname="EFFECTIVE AREA",
+        )
