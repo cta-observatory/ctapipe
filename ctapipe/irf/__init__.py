275f64beb22fcf4f77122b7535cfed1f1c6760da
Tomas Bylund <tomas.bylund@cea.fr>
2023-12-05 17:02:25 +0100

    Further refactoring to decouple things now that there are two tools

--- ctapipe/irf/__init__.py
+++ ctapipe/irf/__init__.py
@@ -1,19 +1,23 @@
 """Top level module for the irf functionality"""
-from .binning import FovOffsetBinning, OutputEnergyBinning, SourceOffsetBinning
+from .binning import FovOffsetBinning, OutputEnergyBinning, check_bins_in_range
 from .irf_classes import PYIRF_SPECTRA, Spectra
-from .optimise import GridOptimizer, OptimisationResult, OptimisationResultSaver
+from .irfs import EffectiveAreaIrf, EnergyMigrationIrf, PsfIrf
+from .optimise import GridOptimizer, OptimisationResult, OptimisationResultStore
 from .select import EventPreProcessor, EventsLoader, ThetaCutsCalculator
 
 __all__ = [
+    "EnergyMigrationIrf",
+    "PsfIrf",
+    "EffectiveAreaIrf",
     "OptimisationResult",
-    "OptimisationResultSaver",
+    "OptimisationResultStore",
     "GridOptimizer",
     "OutputEnergyBinning",
-    "SourceOffsetBinning",
     "FovOffsetBinning",
     "EventsLoader",
     "EventPreProcessor",
     "Spectra",
     "ThetaCutsCalculator",
     "PYIRF_SPECTRA",
+    "check_bins_in_range",
 ]
--- src/ctapipe/tools/make_irf.py
+++ src/ctapipe/tools/make_irf.py
@@ -2,36 +2,40 @@
 import operator
 
 import astropy.units as u
-import numpy as np
 from astropy.io import fits
 from astropy.table import vstack
 from pyirf.cuts import evaluate_binned_cut
-from pyirf.io import (
-    create_aeff2d_hdu,
-    create_energy_dispersion_hdu,
-    create_psf_table_hdu,
-    create_rad_max_hdu,
-)
-from pyirf.irf import effective_area_per_energy_and_fov, energy_dispersion, psf_table
+from pyirf.io import create_rad_max_hdu
 
 from ..core import Provenance, Tool, traits
 from ..core.traits import Bool, Float, Integer, Unicode
 from ..irf import (
     PYIRF_SPECTRA,
+    EffectiveAreaIrf,
+    EnergyMigrationIrf,
     EventPreProcessor,
     EventsLoader,
-    GridOptimizer,
-    OptimisationResultSaver,
+    FovOffsetBinning,
+    OptimisationResultStore,
     OutputEnergyBinning,
-    SourceOffsetBinning,
+    PsfIrf,
     Spectra,
     ThetaCutsCalculator,
+    check_bins_in_range,
 )
 
 
 class IrfTool(Tool):
     name = "ctapipe-make-irf"
     description = "Tool to create IRF files in GAD format"
+    do_background = Bool(
+        True,
+        help="Compute background rate IRF using supplied files",
+    ).tag(config=True)
+    do_benchmarks = Bool(
+        False,
+        help="Produce IRF related benchmarks",
+    ).tag(config=True)
 
     cuts_file = traits.Path(
         default_value=None, directory_ok=False, help="Path to optimised cuts input file"
@@ -46,7 +50,10 @@ class IrfTool(Tool):
         help="Name of the pyrif spectra used for the simulated gamma spectrum",
     ).tag(config=True)
     proton_file = traits.Path(
-        default_value=None, directory_ok=False, help="Proton input filename and path"
+        default_value=None,
+        allow_none=True,
+        directory_ok=False,
+        help="Proton input filename and path",
     ).tag(config=True)
     proton_sim_spectrum = traits.UseEnum(
         Spectra,
@@ -54,7 +61,10 @@ class IrfTool(Tool):
         help="Name of the pyrif spectra used for the simulated proton spectrum",
     ).tag(config=True)
     electron_file = traits.Path(
-        default_value=None, directory_ok=False, help="Electron input filename and path"
+        default_value=None,
+        allow_none=True,
+        directory_ok=False,
+        help="Electron input filename and path",
     ).tag(config=True)
     electron_sim_spectrum = traits.UseEnum(
         Spectra,
@@ -91,10 +101,12 @@ class IrfTool(Tool):
     ).tag(config=True)
 
     classes = [
-        GridOptimizer,
-        SourceOffsetBinning,
         OutputEnergyBinning,
+        FovOffsetBinning,
         EventPreProcessor,
+        PsfIrf,
+        EnergyMigrationIrf,
+        EffectiveAreaIrf,
     ]
 
     def calculate_selections(self):
@@ -146,30 +158,22 @@ class IrfTool(Tool):
             )
         )
 
-    def _check_bins_in_range(self, bins, range):
-        low = bins >= range.min
-        hig = bins <= range.max
-
-        if not all(low & hig):
-            raise ValueError(f"Valid range is {range.min} to {range.max}, got {bins}")
-
     def setup(self):
         self.theta = ThetaCutsCalculator(parent=self)
         self.e_bins = OutputEnergyBinning(parent=self)
-        self.bins = SourceOffsetBinning(parent=self)
+        self.bins = FovOffsetBinning(parent=self)
 
-        self.opt_result = OptimisationResultSaver().read(self.cuts_file)
-        # TODO: not very elegant to pass them this way, refactor later
+        self.opt_result = OptimisationResultStore().read(self.cuts_file)
         self.epp = EventPreProcessor(parent=self)
+        # TODO: not very elegant to pass them this way, refactor later
         self.epp.quality_criteria = self.opt_result.precuts.quality_criteria
         self.reco_energy_bins = self.e_bins.reco_energy_bins()
         self.true_energy_bins = self.e_bins.true_energy_bins()
-        self.energy_migration_bins = self.e_bins.energy_migration_bins()
-        self.source_offset_bins = self.bins.source_offset_bins()
+        self.fov_offset_bins = self.bins.fov_offset_bins()
 
-        self._check_bins_in_range(self.reco_energy_bins, self.opt_result.valid_energy)
-        self._check_bins_in_range(self.source_offset_bins, self.opt_result.valid_offset)
-        self.fov_offset_bins = self.opt_result.valid_offset.bins
+        check_bins_in_range(self.reco_energy_bins, self.opt_result.valid_energy)
+        check_bins_in_range(self.fov_offset_bins, self.opt_result.valid_offset)
+
         self.particles = [
             EventsLoader(
                 self.epp,
@@ -177,19 +181,85 @@ class IrfTool(Tool):
                 self.gamma_file,
                 PYIRF_SPECTRA[self.gamma_sim_spectrum],
             ),
-            EventsLoader(
-                self.epp,
-                "protons",
-                self.proton_file,
-                PYIRF_SPECTRA[self.proton_sim_spectrum],
-            ),
-            EventsLoader(
-                self.epp,
-                "electrons",
-                self.electron_file,
-                PYIRF_SPECTRA[self.electron_sim_spectrum],
-            ),
         ]
+        if self.do_background and self.proton_file:
+            self.particles.append(
+                EventsLoader(
+                    self.epp,
+                    "protons",
+                    self.proton_file,
+                    PYIRF_SPECTRA[self.proton_sim_spectrum],
+                )
+            )
+        if self.do_background and self.electron_file:
+            self.particles.append(
+                EventsLoader(
+                    self.epp,
+                    "electrons",
+                    self.electron_file,
+                    PYIRF_SPECTRA[self.electron_sim_spectrum],
+                )
+            )
+        if self.do_background and len(self.particles) == 1:
+            raise RuntimeError(
+                "At least one electron or proton file required when speficying `do_background`."
+            )
+
+        self.aeff = None
+
+        self.psf = PsfIrf(
+            parent=self,
+            energy_bins=self.true_energy_bins,
+            valid_offset=self.opt_result.valid_offset,
+        )
+        self.mig_matrix = EnergyMigrationIrf(
+            parent=self,
+            energy_bins=self.true_energy_bins,
+        )
+
+    def _stack_background(self, reduced_events):
+        bkgs = []
+        if self.proton_file:
+            bkgs.append("protons")
+        if self.electron_file:
+            bkgs.append("electrons")
+        if len(bkgs) == 2:
+            background = vstack(
+                [reduced_events["protons"], reduced_events["electrons"]]
+            )
+        else:
+            background = reduced_events[bkgs[0]]
+        return background
+
+    def _make_signal_irf_hdus(self, hdus):
+        hdus.append(
+            self.aeff.make_effective_area_hdu(
+                signal_events=self.signal_events[self.signal_events["selected"]],
+                fov_offset_bins=self.fov_offset_bins,
+            )
+        )
+        hdus.append(
+            self.mig_matrix.make_energy_dispersion_hdu(
+                signal_events=self.signal_events[self.signal_events["selected"]],
+                fov_offset_bins=self.fov_offset_bins,
+            )
+        )
+
+        hdus.append(
+            self.psf.make_psf_table_hdu(
+                signal_events=self.signal_events[self.signal_events["selected"]],
+                fov_offset_bins=self.fov_offset_bins,
+            )
+        )
+
+        hdus.append(
+            create_rad_max_hdu(
+                self.theta_cuts_opt["cut"].reshape(-1, 1),
+                self.true_energy_bins,
+                self.fov_offset_bins,
+            )
+        )
+        return hdus
 
     def start(self):
         # TODO: this event loading code seems to be largely repeated between all the tools,
@@ -203,83 +273,26 @@ class IrfTool(Tool):
             )
             reduced_events[sel.kind] = evs
             reduced_events[f"{sel.kind}_count"] = cnt
+            self.log.debug(
+                "Loaded %d %s events" % (reduced_events[f"{sel.kind}_count"], sel.kind)
+            )
             if sel.kind == "gammas":
-                self.sim_info = meta["sim_info"]
+                self.aeff = EffectiveAreaIrf(parent=self, sim_info=meta["sim_info"])
                 self.gamma_spectrum = meta["spectrum"]
 
-        self.log.debug(
-            "Loaded %d gammas, %d protons, %d electrons"
-            % (
-                reduced_events["gammas_count"],
-                reduced_events["protons_count"],
-                reduced_events["electrons_count"],
-            )
-        )
-
         self.signal_events = reduced_events["gammas"]
-        self.background_events = vstack(
-            [reduced_events["protons"], reduced_events["electrons"]]
-        )
+        if self.do_background:
+            self.background_events = self._stack_background(reduced_events)
 
         self.calculate_selections()
+
+        self.log.debug("True Energy bins: %s" % str(self.true_energy_bins.value))
+        self.log.debug("FoV offset bins: %s" % str(self.fov_offset_bins))
 
         hdus = [
             fits.PrimaryHDU(),
         ]
-        self.log.debug("True Energy bins: %s" % str(self.true_energy_bins.value))
-        self.log.debug("FoV offset bins: %s" % str(self.fov_offset_bins))
-        effective_area = effective_area_per_energy_and_fov(
-            self.signal_events[self.signal_events["selected"]],
-            self.sim_info,
-            true_energy_bins=self.true_energy_bins,
-            fov_offset_bins=self.fov_offset_bins,
-        )
-        hdus.append(
-            create_aeff2d_hdu(
-                effective_area[..., np.newaxis],  # +1 dimension for FOV offset
-                self.true_energy_bins,
-                self.fov_offset_bins,
-                extname="EFFECTIVE AREA",
-            )
-        )
-        edisp = energy_dispersion(
-            self.signal_events[self.signal_events["selected"]],
-            true_energy_bins=self.true_energy_bins,
-            fov_offset_bins=self.fov_offset_bins,
-            migration_bins=self.energy_migration_bins,
-        )
-        hdus.append(
-            create_energy_dispersion_hdu(
-                edisp,
-                true_energy_bins=self.true_energy_bins,
-                migration_bins=self.energy_migration_bins,
-                fov_offset_bins=self.fov_offset_bins,
-                extname="ENERGY_DISPERSION",
-            )
-        )
-
-        psf = psf_table(
-            self.signal_events[self.signal_events["selected_gh"]],
-            self.true_energy_bins,
-            fov_offset_bins=self.fov_offset_bins,
-            source_offset_bins=self.source_offset_bins,
-        )
-        hdus.append(
-            create_psf_table_hdu(
-                psf,
-                self.true_energy_bins,
-                self.source_offset_bins,
-                self.fov_offset_bins,
-            )
-        )
-
-        hdus.append(
-            create_rad_max_hdu(
-                self.theta_cuts_opt["cut"].reshape(-1, 1),
-                self.true_energy_bins,
-                self.fov_offset_bins,
-            )
-        )
+        hdus = self._make_signal_irf_hdus(hdus)
         self.hdus = hdus
 
     def finish(self):
