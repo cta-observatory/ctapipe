52b3017606a4447bd96a46dec5cc25fa3bd4ed20
Tomas Bylund <tomas.bylund@cea.fr>
2024-01-15 12:12:06 +0100

    Created a Background2D and Background3D IRF classes

--- ctapipe/irf/__init__.py
+++ ctapipe/irf/__init__.py
@@ -1,14 +1,22 @@
 """Top level module for the irf functionality"""
 from .binning import FovOffsetBinning, OutputEnergyBinning, check_bins_in_range
 from .irf_classes import PYIRF_SPECTRA, Spectra
-from .irfs import EffectiveAreaIrf, EnergyMigrationIrf, PsfIrf
+from .irfs import (
+    Background2dIrf,
+    Background3dIrf,
+    EffectiveAreaIrf,
+    EnergyMigrationIrf,
+    PsfIrf,
+)
 from .optimize import GridOptimizer, OptimizationResult, OptimizationResultStore
 from .select import EventPreProcessor, EventsLoader, ThetaCutsCalculator
 
 __all__ = [
+    "Background2dIrf",
+    "Background3dIrf",
+    "EffectiveAreaIrf",
     "EnergyMigrationIrf",
     "PsfIrf",
-    "EffectiveAreaIrf",
     "OptimizationResult",
     "OptimizationResultStore",
     "GridOptimizer",
