7072b100a9f6d89e4167a1ef713440a9f76dba12
Lukas Beiske <lukas.beiske@tu-dortmund.de>
2024-01-19 16:15:51 +0100

    Add BaseStatisticsContainer to __all__

--- src/ctapipe/containers.py
+++ src/ctapipe/containers.py
@@ -55,6 +55,7 @@ __all__ = [
     "TimingParametersContainer",
     "TriggerContainer",
     "WaveformCalibrationContainer",
+    "BaseStatisticsContainer",
     "StatisticsContainer",
     "IntensityStatisticsContainer",
     "PeakTimeStatisticsContainer",
