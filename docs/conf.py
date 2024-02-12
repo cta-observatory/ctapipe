8246d71c68a33b0eb786d7e1887a50d955bc4556
Maximilian Linhoff <maximilian.linhoff@tu-dortmund.de>
2024-02-12 14:47:26 +0100

    Fix intersphinx link for iminuit, their docs were moved

--- docs/conf.py
+++ docs/conf.py
@@ -393,6 +393,6 @@ intersphinx_mapping = {
     "pandas": ("https://pandas.pydata.org/pandas-docs/stable/", None),
     "matplotlib": ("https://matplotlib.org/stable", None),
     "cython": ("https://docs.cython.org/en/latest/", None),
-    "iminuit": ("https://iminuit.readthedocs.io/en/latest/", None),
+    "iminuit": ("https://scikit-hep.org/iminuit/", None),
     "traitlets": ("https://traitlets.readthedocs.io/en/stable/", None),
 }
