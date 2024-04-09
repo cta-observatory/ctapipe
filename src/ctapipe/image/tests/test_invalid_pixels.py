a85cc583d72c981cf3fe69d37eb436f0402b898a
Jonas Hackfeld <jonas.hackfeld@ruhr-uni-bochum.de>
2024-04-09 15:06:46 +0200

    Add test for 2 gains in InvaliPixelHandler

--- src/ctapipe/image/tests/test_invalid_pixels.py
+++ src/ctapipe/image/tests/test_invalid_pixels.py
@@ -11,11 +11,31 @@ def test_neighbor_average(prod5_gamma_simtel_path):
         geometry = subarray.tel[1].camera.geometry
 
     neighbor_average = NeighborAverage(subarray)
+    border = geometry.get_border_pixel_mask(1)
 
+    ### 1 GAIN ###
+    broken_pixels = np.zeros((1, geometry.n_pixels), dtype=bool)
+
+    # one border pixel, one non-border pixel
+    broken_pixels[:, np.nonzero(border)[0][0]] = True
+    broken_pixels[:, np.nonzero(~border)[0][0]] = True
+
+    image = np.ones((geometry.n_pixels))
+    image[broken_pixels[0]] = 9999
+    peak_time = np.full(geometry.n_pixels, 20.0)
+    peak_time[broken_pixels[0]] = -1
+
+    interpolated_image, interpolated_peaktime = neighbor_average(
+        tel_id=1, image=image, peak_time=peak_time, pixel_mask=broken_pixels
+    )
+
+    assert np.allclose(interpolated_image[broken_pixels[0]], 1.0)
+    assert np.allclose(interpolated_peaktime[broken_pixels[0]], 20.0)
+
+    ### 2 GAIN ###
     broken_pixels = np.zeros((2, geometry.n_pixels), dtype=bool)
 
     # one border pixel, one non-border pixel
-    border = geometry.get_border_pixel_mask(1)
     broken_pixels[:, np.nonzero(border)[0][0]] = True
     broken_pixels[:, np.nonzero(~border)[0][0]] = True
 
