26ff8a4aae421664f912871327335b3dd613b5e2
Tjark Miener <tjarkmiener@Tjarks-MacBook-Air.local>
2024-04-29 08:51:19 +0200

    added stats extractor parent component
    added PlainExtractor based on numpy and scipy functions

--- src/ctapipe/containers.py
+++ src/ctapipe/containers.py
@@ -414,9 +414,12 @@ class MorphologyContainer(Container):
 class StatisticsContainer(Container):
     """Store descriptive statistics"""
 
+    validity_start = Field(np.float32(nan), "start")
+    validity_stop = Field(np.float32(nan), "stop")
     max = Field(np.float32(nan), "value of pixel with maximum intensity")
     min = Field(np.float32(nan), "value of pixel with minimum intensity")
     mean = Field(np.float32(nan), "mean intensity")
+    median = Field(np.float32(nan), "median intensity")
     std = Field(np.float32(nan), "standard deviation of intensity")
     skewness = Field(nan, "skewness of intensity")
     kurtosis = Field(nan, "kurtosis of intensity")
