3232025ae4b528c88b34cc2816a3fb2b12f376bc
Maximilian Linhoff <maximilian.linhoff@tu-dortmund.de>
2024-05-13 11:44:28 +0200

    Docs update

--- src/ctapipe/tools/process.py
+++ src/ctapipe/tools/process.py
@@ -78,7 +78,7 @@ class ProcessorTool(Tool):
 
     event_viewer_name = ComponentName(
         EventViewer,
-        default_value="QTEventViewer",
+        default_value="QtEventViewer",
     ).tag(config=True)
 
     open_viewer = Bool(False, help="Open EventViewer").tag(config=True)
--- src/ctapipe/visualization/__init__.py
+++ src/ctapipe/visualization/__init__.py
@@ -3,12 +3,12 @@ Visualization: Methods for displaying data
 """
 from .mpl_array import ArrayDisplay
 from .mpl_camera import CameraDisplay
-from .qt_eventviewer import QTEventViewer
+from .qt_eventviewer import QtEventViewer
 from .viewer import EventViewer
 
 __all__ = [
     "CameraDisplay",
     "ArrayDisplay",
     "EventViewer",
-    "QTEventViewer",
+    "QtEventViewer",
 ]
