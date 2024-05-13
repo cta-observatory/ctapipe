62a7e06f80caf34f2d1f097b31440568f2674a01
Maximilian Linhoff <maximilian.linhoff@tu-dortmund.de>
2024-05-13 11:44:28 +0200

    Docs update

--- src/ctapipe/visualization/qt_eventviewer.py
+++ src/ctapipe/visualization/qt_eventviewer.py
@@ -4,12 +4,29 @@ from ..containers import ArrayEventContainer
 from .viewer import EventViewer
 
 
-class QTEventViewer(EventViewer):
+class QtEventViewer(EventViewer):
+    """
+    EventViewer implementation using QT.
+
+    Requires the ctapipe optional dependency ``pyside6``.
+    On Linux using wayland, make sure to have qt6 with wayland support,
+    e.g. when using conda-forge, also install ``qt6-wayland``.
+
+    Qt requires to have the GUI thread be the main thread, so it is started
+    as a subprocess and communication happens through a ``JoinableQueue``.
+
+    Actual GUI implementation is in ``_qt_viewer_impl`` to make this class
+    available always but make the qt dependency optional and error with a
+    nice message in case the optional dependencies are not installed.
+    """
+
     def __init__(self, subarray, **kwargs):
         try:
             from ._qt_viewer_impl import viewer_main
         except ModuleNotFoundError:
-            raise ModuleNotFoundError("PySide6 is needed for this EventViewer")
+            raise ModuleNotFoundError(
+                "PySide6 is needed for this EventViewer"
+            ) from None
 
         super().__init__(subarray=subarray, **kwargs)
 
