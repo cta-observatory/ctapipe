added a container for mean variance images and fixed docustring

src/ctapipe/containers.py
@@ -57,6 +57,7 @@ __all__ = [
     "TriggerContainer",
     "WaveformCalibrationContainer",
     "StatisticsContainer",
+    "VarianceStatisticsContainer",
     "ImageStatisticsContainer",
     "IntensityStatisticsContainer",
     "PeakTimeStatisticsContainer",
@@ -423,6 +424,13 @@ class StatisticsContainer(Container):
     std = Field(np.float32(nan), "Channel-wise and pixel-wise standard deviation")
     std_outliers = Field(np.float32(nan), "Channel-wise and pixel-wise standard deviation outliers")
 
+class VarianceStatisticsContainer(Container):
+     """Store descriptive statistics of a sequence of variance images"""
+
+     validity_start = Field(np.float32(nan), "start of the validity range")
+     validity_stop = Field(np.float32(nan), "stop of the validity range")
+     mean = Field(np.float32(nan), "Channel-wise and pixel-wise mean")
+
 class ImageStatisticsContainer(Container):
     """Store descriptive image statistics"""
 
@@ -432,7 +440,6 @@ class ImageStatisticsContainer(Container):
     std = Field(np.float32(nan), "standard deviation of intensity")
     skewness = Field(nan, "skewness of intensity")
     kurtosis = Field(nan, "kurtosis of intensity")
-
 
 class IntensityStatisticsContainer(ImageStatisticsContainer):
     default_prefix = "intensity"