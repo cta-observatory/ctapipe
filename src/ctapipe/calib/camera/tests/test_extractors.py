c4d6809798368718fde73277912a4bc084c54abc
Tjark Miener <tjarkmiener@Tjarks-MacBook-Air.local>
2024-05-31 17:42:35 +0200

    fix pylint
    Remove StarVarianceExtractor since is functionality is featured in the existing Extractors

--- src/ctapipe/calib/camera/tests/test_extractors.py
+++ src/ctapipe/calib/camera/tests/test_extractors.py
@@ -2,7 +2,6 @@
 Tests for StatisticsExtractor and related functions
 """
 
-import astropy.units as u
 from astropy.table import QTable
 import numpy as np
 
@@ -10,42 +9,51 @@ from ctapipe.calib.camera.extractor import PlainExtractor, SigmaClippingExtracto
 
 
 def test_extractors(example_subarray):
+    """test basic functionality of the StatisticsExtractors"""
+
     plain_extractor = PlainExtractor(subarray=example_subarray, sample_size=2500)
-    sigmaclipping_extractor = SigmaClippingExtractor(subarray=example_subarray, sample_size=2500)
+    sigmaclipping_extractor = SigmaClippingExtractor(
+        subarray=example_subarray, sample_size=2500
+    )
     times = np.linspace(60117.911, 60117.9258, num=5000)
     pedestal_dl1_data = np.random.normal(2.0, 5.0, size=(5000, 2, 1855))
     flatfield_dl1_data = np.random.normal(77.0, 10.0, size=(5000, 2, 1855))
 
-    pedestal_dl1_table = QTable([times, pedestal_dl1_data], names=('time', 'image'))
-    flatfield_dl1_table = QTable([times, flatfield_dl1_data], names=('time', 'image'))
-    
+    pedestal_dl1_table = QTable([times, pedestal_dl1_data], names=("time", "image"))
+    flatfield_dl1_table = QTable([times, flatfield_dl1_data], names=("time", "image"))
+
     plain_stats_list = plain_extractor(dl1_table=pedestal_dl1_table)
     sigmaclipping_stats_list = sigmaclipping_extractor(dl1_table=flatfield_dl1_table)
-    
-    assert np.any(np.abs(plain_stats_list[0].mean - 2.0) > 1.5) == False
-    assert np.any(np.abs(sigmaclipping_stats_list[0].mean - 77.0) > 1.5) == False
-                
-    assert np.any(np.abs(plain_stats_list[0].mean - 2.0) > 1.5) == False
-    assert np.any(np.abs(sigmaclipping_stats_list[0].mean - 77.0) > 1.5) == False
-    
-    assert np.any(np.abs(plain_stats_list[1].median - 2.0) > 1.5) == False
-    assert np.any(np.abs(sigmaclipping_stats_list[1].median - 77.0) > 1.5) == False
-    
-    assert np.any(np.abs(plain_stats_list[0].std - 5.0) > 1.5) == False
-    assert np.any(np.abs(sigmaclipping_stats_list[0].std - 10.0) > 1.5) == False
-    
+
+    assert np.any(np.abs(plain_stats_list[0].mean - 2.0) > 1.5) is False
+    assert np.any(np.abs(sigmaclipping_stats_list[0].mean - 77.0) > 1.5) is False
+
+    assert np.any(np.abs(plain_stats_list[0].mean - 2.0) > 1.5) is False
+    assert np.any(np.abs(sigmaclipping_stats_list[0].mean - 77.0) > 1.5) is False
+
+    assert np.any(np.abs(plain_stats_list[1].median - 2.0) > 1.5) is False
+    assert np.any(np.abs(sigmaclipping_stats_list[1].median - 77.0) > 1.5) is False
+
+    assert np.any(np.abs(plain_stats_list[0].std - 5.0) > 1.5) is False
+    assert np.any(np.abs(sigmaclipping_stats_list[0].std - 10.0) > 1.5) is False
+
+
 def test_check_outliers(example_subarray):
-    sigmaclipping_extractor = SigmaClippingExtractor(subarray=example_subarray, sample_size=2500)
+    """test detection ability of outliers"""
+
+    sigmaclipping_extractor = SigmaClippingExtractor(
+        subarray=example_subarray, sample_size=2500
+    )
     times = np.linspace(60117.911, 60117.9258, num=5000)
     flatfield_dl1_data = np.random.normal(77.0, 10.0, size=(5000, 2, 1855))
     # insert outliers
-    flatfield_dl1_data[:,0,120] = 120.0
-    flatfield_dl1_data[:,1,67] = 120.0
-    flatfield_dl1_table = QTable([times, flatfield_dl1_data], names=('time', 'image'))
+    flatfield_dl1_data[:, 0, 120] = 120.0
+    flatfield_dl1_data[:, 1, 67] = 120.0
+    flatfield_dl1_table = QTable([times, flatfield_dl1_data], names=("time", "image"))
     sigmaclipping_stats_list = sigmaclipping_extractor(dl1_table=flatfield_dl1_table)
-    
-    #check if outliers where detected correctly
-    assert sigmaclipping_stats_list[0].median_outliers[0][120] == True
-    assert sigmaclipping_stats_list[0].median_outliers[1][67] == True
-    assert sigmaclipping_stats_list[1].median_outliers[0][120] == True
-    assert sigmaclipping_stats_list[1].median_outliers[1][67] == True
+
+    # check if outliers where detected correctly
+    assert sigmaclipping_stats_list[0].median_outliers[0][120] is True
+    assert sigmaclipping_stats_list[0].median_outliers[1][67] is True
+    assert sigmaclipping_stats_list[1].median_outliers[0][120] is True
+    assert sigmaclipping_stats_list[1].median_outliers[1][67] is True
