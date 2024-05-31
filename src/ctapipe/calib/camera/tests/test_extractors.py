14a0910519bb2b12cd59f31f2cfbadc30681497c
Tjark Miener <tjarkmiener@Tjarks-MacBook-Air.local>
2024-05-31 18:15:53 +0200

    use pytest.fixture for Extractors

--- src/ctapipe/calib/camera/tests/test_extractors.py
+++ src/ctapipe/calib/camera/tests/test_extractors.py
@@ -7,14 +7,21 @@ import numpy as np
 
 from ctapipe.calib.camera.extractor import PlainExtractor, SigmaClippingExtractor
 
+@pytest.fixture
+    def test_plainextractor(example_subarray):
+        return PlainExtractor(
+            subarray=example_subarray, sample_size=2500
+        )
 
-def test_extractors(example_subarray):
+@pytest.fixture
+    def test_sigmaclippingextractor(example_subarray):
+        return SigmaClippingExtractor(
+            subarray=example_subarray, sample_size=2500
+        )
+
+def test_extractors(test_plainextractor, test_sigmaclippingextractor):
     """test basic functionality of the StatisticsExtractors"""
 
-    plain_extractor = PlainExtractor(subarray=example_subarray, sample_size=2500)
-    sigmaclipping_extractor = SigmaClippingExtractor(
-        subarray=example_subarray, sample_size=2500
-    )
     times = np.linspace(60117.911, 60117.9258, num=5000)
     pedestal_dl1_data = np.random.normal(2.0, 5.0, size=(5000, 2, 1855))
     flatfield_dl1_data = np.random.normal(77.0, 10.0, size=(5000, 2, 1855))
@@ -22,8 +29,10 @@ def test_extractors(example_subarray):
     pedestal_dl1_table = QTable([times, pedestal_dl1_data], names=("time", "image"))
     flatfield_dl1_table = QTable([times, flatfield_dl1_data], names=("time", "image"))
 
-    plain_stats_list = plain_extractor._extract(dl1_table=pedestal_dl1_table)
-    sigmaclipping_stats_list = sigmaclipping_extractor._extract(dl1_table=flatfield_dl1_table)
+    plain_stats_list = test_plainextractor._extract(dl1_table=pedestal_dl1_table)
+    sigmaclipping_stats_list = test_sigmaclippingextractor._extract(
+        dl1_table=flatfield_dl1_table
+    )
 
     assert np.any(np.abs(plain_stats_list[0].mean - 2.0) > 1.5) is False
     assert np.any(np.abs(sigmaclipping_stats_list[0].mean - 77.0) > 1.5) is False
@@ -38,19 +47,18 @@ def test_extractors(example_subarray):
     assert np.any(np.abs(sigmaclipping_stats_list[0].std - 10.0) > 1.5) is False
 
 
-def test_check_outliers(example_subarray):
+def test_check_outliers(test_sigmaclippingextractor):
     """test detection ability of outliers"""
 
-    sigmaclipping_extractor = SigmaClippingExtractor(
-        subarray=example_subarray, sample_size=2500
-    )
     times = np.linspace(60117.911, 60117.9258, num=5000)
     flatfield_dl1_data = np.random.normal(77.0, 10.0, size=(5000, 2, 1855))
     # insert outliers
     flatfield_dl1_data[:, 0, 120] = 120.0
     flatfield_dl1_data[:, 1, 67] = 120.0
     flatfield_dl1_table = QTable([times, flatfield_dl1_data], names=("time", "image"))
-    sigmaclipping_stats_list = sigmaclipping_extractor._extract(dl1_table=flatfield_dl1_table)
+    sigmaclipping_stats_list = sigmaclipping_extractor._extract(
+        dl1_table=flatfield_dl1_table
+    )
 
     # check if outliers where detected correctly
     assert sigmaclipping_stats_list[0].median_outliers[0][120] is True
