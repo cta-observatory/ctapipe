7c3dda74730e219065a4461f355a134339bd21f5
Lukas Beiske <lukas.beiske@tu-dortmund.de>
2024-06-14 16:13:59 +0200

    Do not expose base classes

--- src/ctapipe/irf/__init__.py
+++ src/ctapipe/irf/__init__.py
@@ -1,32 +1,22 @@
 """Top level module for the irf functionality"""
+
 from .benchmarks import (
     AngularResolution2dMaker,
-    AngularResolutionMakerBase,
     EnergyBiasResolution2dMaker,
-    EnergyBiasResolutionMakerBase,
     Sensitivity2dMaker,
-    SensitivityMakerBase,
 )
 from .binning import (
-    FoVOffsetBinsBase,
-    RecoEnergyBinsBase,
     ResultValidRange,
-    TrueEnergyBinsBase,
     check_bins_in_range,
     make_bins_per_decade,
 )
 from .irfs import (
     BackgroundRate2dMaker,
-    BackgroundRateMakerBase,
     EffectiveArea2dMaker,
-    EffectiveAreaMakerBase,
     EnergyMigration2dMaker,
-    EnergyMigrationMakerBase,
     Psf3dMaker,
-    PsfMakerBase,
 )
 from .optimize import (
-    CutOptimizerBase,
     GhPercentileCutCalculator,
     OptimizationResult,
     OptimizationResultStore,
@@ -38,19 +28,9 @@ from .select import EventPreProcessor, EventsLoader
 from .spectra import SPECTRA, Spectra
 
 __all__ = [
-    "AngularResolutionMakerBase",
     "AngularResolution2dMaker",
-    "EnergyBiasResolutionMakerBase",
     "EnergyBiasResolution2dMaker",
-    "SensitivityMakerBase",
     "Sensitivity2dMaker",
-    "TrueEnergyBinsBase",
-    "RecoEnergyBinsBase",
-    "FoVOffsetBinsBase",
-    "PsfMakerBase",
-    "BackgroundRateMakerBase",
-    "EnergyMigrationMakerBase",
-    "EffectiveAreaMakerBase",
     "Psf3dMaker",
     "BackgroundRate2dMaker",
     "EnergyMigration2dMaker",
@@ -58,7 +38,6 @@ __all__ = [
     "ResultValidRange",
     "OptimizationResult",
     "OptimizationResultStore",
-    "CutOptimizerBase",
     "PointSourceSensitivityOptimizer",
     "PercentileCuts",
     "EventsLoader",
