628e81feead99e0fdb3a5bd674e36d1be2f2e672
Mykhailo Dalchenko <mykhailo.dalchenko@cern.ch>
2024-06-19 17:07:52 +0200

    Add test for SystemExit handling

--- src/ctapipe/core/tests/test_run_tool.py
+++ src/ctapipe/core/tests/test_run_tool.py
@@ -1,4 +1,5 @@
 from subprocess import CalledProcessError
+import sys
 
 import pytest
 
@@ -15,5 +16,16 @@ def test_run_tool_raises_exit_code():
 
     ret = run_tool(ErrorTool(), ["--non-existing-alias"], raises=False)
     assert ret == 2
+
+    class SysExitTool(Tool):
+        def setup(self):
+            pass
+
+        def start(self):
+            sys.exit(4)
+
+    ret = run_tool(SysExitTool(), raises=False)
+    assert ret == 4
+
     with pytest.raises(CalledProcessError):
         run_tool(ErrorTool(), ["--non-existing-alias"], raises=True)
