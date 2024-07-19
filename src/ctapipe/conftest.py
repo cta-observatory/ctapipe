2def749dc0fbfc55e26ee45ce24aaad2ef8910a3
Lukas Beiske <lukas.beiske@tu-dortmund.de>
2024-07-19 18:09:03 +0200

    Move pytest fixtures to conftest.py

--- src/ctapipe/conftest.py
+++ src/ctapipe/conftest.py
@@ -1,6 +1,7 @@
 """
 common pytest fixtures for tests in ctapipe
 """
+
 import shutil
 from copy import deepcopy
 
@@ -684,3 +685,79 @@ def dl1_mon_pointing_file(dl1_file, dl1_tmp_path):
         f.remove_node("/configuration/telescope/pointing", recursive=True)
 
     return path
+
+
+@pytest.fixture(scope="session")
+def gamma_diffuse_full_reco_file(
+    gamma_train_clf,
+    particle_classifier_path,
+    model_tmp_path,
+):
+    """
+    Energy reconstruction and geometric origin reconstruction have already been done.
+    """
+    from ctapipe.tools.apply_models import ApplyModels
+
+    output_path = model_tmp_path / "gamma_diffuse_full_reco.dl2.h5"
+    run_tool(
+        ApplyModels(),
+        argv=[
+            f"--input={gamma_train_clf}",
+            f"--output={output_path}",
+            f"--reconstructor={particle_classifier_path}",
+            "--no-dl1-parameters",
+            "--StereoMeanCombiner.weights=konrad",
+        ],
+        raises=True,
+    )
+    return output_path
+
+
+@pytest.fixture(scope="session")
+def proton_full_reco_file(
+    proton_train_clf,
+    particle_classifier_path,
+    model_tmp_path,
+):
+    """
+    Energy reconstruction and geometric origin reconstruction have already been done.
+    """
+    from ctapipe.tools.apply_models import ApplyModels
+
+    output_path = model_tmp_path / "proton_full_reco.dl2.h5"
+    run_tool(
+        ApplyModels(),
+        argv=[
+            f"--input={proton_train_clf}",
+            f"--output={output_path}",
+            f"--reconstructor={particle_classifier_path}",
+            "--no-dl1-parameters",
+            "--StereoMeanCombiner.weights=konrad",
+        ],
+        raises=True,
+    )
+    return output_path
+
+
+@pytest.fixture(scope="session")
+def irf_events_loader_test_config():
+    from traitlets.config import Config
+
+    return Config(
+        {
+            "EventPreProcessor": {
+                "energy_reconstructor": "ExtraTreesRegressor",
+                "geometry_reconstructor": "HillasReconstructor",
+                "gammaness_classifier": "ExtraTreesClassifier",
+                "quality_criteria": [
+                    (
+                        "multiplicity 4",
+                        "np.count_nonzero(tels_with_trigger,axis=1) >= 4",
+                    ),
+                    ("valid classifier", "ExtraTreesClassifier_is_valid"),
+                    ("valid geom reco", "HillasReconstructor_is_valid"),
+                    ("valid energy reco", "ExtraTreesRegressor_is_valid"),
+                ],
+            }
+        }
+    )
