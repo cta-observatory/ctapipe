b729f8294cd558a5072bf443f4da4ac63fb5a178
Christoph Toennis <christoph.toennis1988@gmail.com>
2024-08-20 15:31:20 +0200

    Copying over code for interpolators and pointing calculators

---
+++ src/ctapipe/io/interpolation.py
@@ -0,0 +1,345 @@
+from abc import ABCMeta, abstractmethod
+from typing import Any
+
+import astropy.units as u
+import numpy as np
+import tables
+from astropy.time import Time
+from scipy.interpolate import interp1d
+
+from ctapipe.core import Component, traits
+
+from .astropy_helpers import read_table
+
+
+class StepFunction:
+
+    """
+    Step function Interpolator for the gain and pedestals
+    Interpolates data so that for each time the value from the closest previous
+    point given.
+
+    Parameters
+    ----------
+    values : None | np.array
+        Numpy array of the data that is to be interpolated.
+        The first dimension needs to be an index over time
+    times : None | np.array
+        Time values over which data are to be interpolated
+        need to be sorted and have same length as first dimension of values
+    """
+
+    def __init__(
+        self,
+        times,
+        values,
+        bounds_error=True,
+        fill_value="extrapolate",
+        assume_sorted=True,
+        copy=False,
+    ):
+        self.values = values
+        self.times = times
+        self.bounds_error = bounds_error
+        self.fill_value = fill_value
+
+    def __call__(self, point):
+        if point < self.times[0]:
+            if self.bounds_error:
+                raise ValueError("below the interpolation range")
+
+            if self.fill_value == "extrapolate":
+                return self.values[0]
+
+            else:
+                a = np.empty(self.values[0].shape)
+                a[:] = np.nan
+                return a
+
+        else:
+            i = np.searchsorted(self.times, point, side="left")
+            return self.values[i - 1]
+
+
+class Interpolator(Component, metaclass=ABCMeta):
+    """
+    Interpolator parent class.
+
+    Parameters
+    ----------
+    h5file : None | tables.File
+        A open hdf5 file with read access.
+    """
+
+    bounds_error = traits.Bool(
+        default_value=True,
+        help="If true, raises an exception when trying to extrapolate out of the given table",
+    ).tag(config=True)
+
+    extrapolate = traits.Bool(
+        help="If bounds_error is False, this flag will specify whether values outside"
+        "the available values are filled with nan (False) or extrapolated (True).",
+        default_value=False,
+    ).tag(config=True)
+
+    telescope_data_group = None
+    required_columns = set()
+    expected_units = {}
+
+    def __init__(self, h5file=None, **kwargs):
+        super().__init__(**kwargs)
+
+        if h5file is not None and not isinstance(h5file, tables.File):
+            raise TypeError("h5file must be a tables.File")
+        self.h5file = h5file
+
+        self.interp_options: dict[str, Any] = dict(assume_sorted=True, copy=False)
+        if self.bounds_error:
+            self.interp_options["bounds_error"] = True
+        elif self.extrapolate:
+            self.interp_options["bounds_error"] = False
+            self.interp_options["fill_value"] = "extrapolate"
+        else:
+            self.interp_options["bounds_error"] = False
+            self.interp_options["fill_value"] = np.nan
+
+        self._interpolators = {}
+
+    @abstractmethod
+    def add_table(self, tel_id, input_table):
+        """
+        Add a table to this interpolator
+        This method reads input tables and creates instances of the needed interpolators
+        to be added to _interpolators. The first index of _interpolators needs to be
+        tel_id, the second needs to be the name of the parameter that is to be interpolated
+
+        Parameters
+        ----------
+        tel_id : int
+            Telescope id
+        input_table : astropy.table.Table
+            Table of pointing values, expected columns
+            are always ``time`` as ``Time`` column and
+            other columns for the data that is to be interpolated
+        """
+
+        pass
+
+    def _check_tables(self, input_table):
+        missing = self.required_columns - set(input_table.colnames)
+        if len(missing) > 0:
+            raise ValueError(f"Table is missing required column(s): {missing}")
+        for col in self.expected_units:
+            unit = input_table[col].unit
+            if unit is None:
+                if self.expected_units[col] is not None:
+                    raise ValueError(
+                        f"{col} must have units compatible with '{self.expected_units[col].name}'"
+                    )
+            elif not self.expected_units[col].is_equivalent(unit):
+                if self.expected_units[col] is None:
+                    raise ValueError(f"{col} must have units compatible with 'None'")
+                else:
+                    raise ValueError(
+                        f"{col} must have units compatible with '{self.expected_units[col].name}'"
+                    )
+
+    def _check_interpolators(self, tel_id):
+        if tel_id not in self._interpolators:
+            if self.h5file is not None:
+                self._read_parameter_table(tel_id)  # might need to be removed
+            else:
+                raise KeyError(f"No table available for tel_id {tel_id}")
+
+    def _read_parameter_table(self, tel_id):
+        input_table = read_table(
+            self.h5file,
+            f"{self.telescope_data_group}/tel_{tel_id:03d}",
+        )
+        self.add_table(tel_id, input_table)
+
+
+class PointingInterpolator(Interpolator):
+    """
+    Interpolator for pointing and pointing correction data
+    """
+
+    telescope_data_group = "/dl0/monitoring/telescope/pointing"
+    required_columns = frozenset(["time", "azimuth", "altitude"])
+    expected_units = {"azimuth": u.rad, "altitude": u.rad}
+
+    def __call__(self, tel_id, time):
+        """
+        Interpolate alt/az for given time and tel_id.
+
+        Parameters
+        ----------
+        tel_id : int
+            telescope id
+        time : astropy.time.Time
+            time for which to interpolate the pointing
+
+        Returns
+        -------
+        altitude : astropy.units.Quantity[deg]
+            interpolated altitude angle
+        azimuth : astropy.units.Quantity[deg]
+            interpolated azimuth angle
+        """
+
+        self._check_interpolators(tel_id)
+
+        mjd = time.tai.mjd
+        az = u.Quantity(self._interpolators[tel_id]["az"](mjd), u.rad, copy=False)
+        alt = u.Quantity(self._interpolators[tel_id]["alt"](mjd), u.rad, copy=False)
+        return alt, az
+
+    def add_table(self, tel_id, input_table):
+        """
+        Add a table to this interpolator
+
+        Parameters
+        ----------
+        tel_id : int
+            Telescope id
+        input_table : astropy.table.Table
+            Table of pointing values, expected columns
+            are ``time`` as ``Time`` column, ``azimuth`` and ``altitude``
+            as quantity columns for pointing and pointing correction data.
+        """
+
+        self._check_tables(input_table)
+
+        if not isinstance(input_table["time"], Time):
+            raise TypeError("'time' column of pointing table must be astropy.time.Time")
+
+        input_table = input_table.copy()
+        input_table.sort("time")
+
+        az = input_table["azimuth"].quantity.to_value(u.rad)
+        # prepare azimuth for interpolation by "unwrapping": i.e. turning
+        # [359, 1] into [359, 361]. This assumes that if we get values like
+        # [359, 1] the telescope moved 2 degrees through 0, not 358 degrees
+        # the other way around. This should be true for all telescopes given
+        # the sampling speed of pointing values and their maximum movement speed.
+        # No telescope can turn more than 180° in 2 seconds.
+        az = np.unwrap(az)
+        alt = input_table["altitude"].quantity.to_value(u.rad)
+        mjd = input_table["time"].tai.mjd
+        self._interpolators[tel_id] = {}
+        self._interpolators[tel_id]["az"] = interp1d(mjd, az, **self.interp_options)
+        self._interpolators[tel_id]["alt"] = interp1d(mjd, alt, **self.interp_options)
+
+
+class FlatFieldInterpolator(Interpolator):
+    """
+    Interpolator for flatfield data
+    """
+
+    telescope_data_group = "dl1/calibration/gain"  # TBD
+    required_columns = frozenset(["time", "gain"])  # TBD
+    expected_units = {"gain": u.one}
+
+    def __call__(self, tel_id, time):
+        """
+        Interpolate flatfield data for a given time and tel_id.
+
+        Parameters
+        ----------
+        tel_id : int
+            telescope id
+        time : astropy.time.Time
+            time for which to interpolate the calibration data
+
+        Returns
+        -------
+        ffield : array [float]
+            interpolated flatfield data
+        """
+
+        self._check_interpolators(tel_id)
+
+        ffield = self._interpolators[tel_id]["gain"](time)
+        return ffield
+
+    def add_table(self, tel_id, input_table):
+        """
+        Add a table to this interpolator
+
+        Parameters
+        ----------
+        tel_id : int
+            Telescope id
+        input_table : astropy.table.Table
+            Table of pointing values, expected columns
+            are ``time`` as ``Time`` column and "gain"
+            for the flatfield data
+        """
+
+        self._check_tables(input_table)
+
+        input_table = input_table.copy()
+        input_table.sort("time")
+        time = input_table["time"]
+        gain = input_table["gain"]
+        self._interpolators[tel_id] = {}
+        self._interpolators[tel_id]["gain"] = StepFunction(
+            time, gain, **self.interp_options
+        )
+
+
+class PedestalInterpolator(Interpolator):
+    """
+    Interpolator for Pedestal data
+    """
+
+    telescope_data_group = "dl1/calibration/pedestal"  # TBD
+    required_columns = frozenset(["time", "pedestal"])  # TBD
+    expected_units = {"pedestal": u.one}
+
+    def __call__(self, tel_id, time):
+        """
+        Interpolate pedestal or gain for a given time and tel_id.
+
+        Parameters
+        ----------
+        tel_id : int
+            telescope id
+        time : astropy.time.Time
+            time for which to interpolate the calibration data
+
+        Returns
+        -------
+        pedestal : array [float]
+            interpolated pedestal values
+        """
+
+        self._check_interpolators(tel_id)
+
+        pedestal = self._interpolators[tel_id]["pedestal"](time)
+        return pedestal
+
+    def add_table(self, tel_id, input_table):
+        """
+        Add a table to this interpolator
+
+        Parameters
+        ----------
+        tel_id : int
+            Telescope id
+        input_table : astropy.table.Table
+            Table of pointing values, expected columns
+            are ``time`` as ``Time`` column and "pedestal"
+            for the pedestal data
+        """
+
+        self._check_tables(input_table)
+
+        input_table = input_table.copy()
+        input_table.sort("time")
+        time = input_table["time"]
+        pedestal = input_table["pedestal"]
+        self._interpolators[tel_id] = {}
+        self._interpolators[tel_id]["pedestal"] = StepFunction(
+            time, pedestal, **self.interp_options
+        )
---
+++ src/ctapipe/io/tests/test_interpolator.py
@@ -0,0 +1,179 @@
+import astropy.units as u
+import numpy as np
+import pytest
+import tables
+from astropy.table import Table
+from astropy.time import Time
+
+from ctapipe.io.interpolation import (
+    FlatFieldInterpolator,
+    PedestalInterpolator,
+    PointingInterpolator,
+)
+
+t0 = Time("2022-01-01T00:00:00")
+
+
+def test_azimuth_switchover():
+    """Test pointing interpolation"""
+
+    table = Table(
+        {
+            "time": t0 + [0, 1, 2] * u.s,
+            "azimuth": [359, 1, 3] * u.deg,
+            "altitude": [60, 61, 62] * u.deg,
+        },
+    )
+
+    interpolator = PointingInterpolator()
+    interpolator.add_table(1, table)
+
+    alt, az = interpolator(tel_id=1, time=t0 + 0.5 * u.s)
+    assert u.isclose(az, 360 * u.deg)
+    assert u.isclose(alt, 60.5 * u.deg)
+
+
+def test_invalid_input():
+    """Test invalid pointing tables raise nice errors"""
+
+    wrong_time = Table(
+        {
+            "time": [1, 2, 3] * u.s,
+            "azimuth": [1, 2, 3] * u.deg,
+            "altitude": [1, 2, 3] * u.deg,
+        }
+    )
+
+    interpolator = PointingInterpolator()
+    with pytest.raises(TypeError, match="astropy.time.Time"):
+        interpolator.add_table(1, wrong_time)
+
+    wrong_unit = Table(
+        {
+            "time": Time(1.7e9 + np.arange(3), format="unix"),
+            "azimuth": [1, 2, 3] * u.m,
+            "altitude": [1, 2, 3] * u.deg,
+        }
+    )
+    with pytest.raises(ValueError, match="compatible with 'rad'"):
+        interpolator.add_table(1, wrong_unit)
+
+    wrong_unit = Table(
+        {
+            "time": Time(1.7e9 + np.arange(3), format="unix"),
+            "azimuth": [1, 2, 3] * u.deg,
+            "altitude": [1, 2, 3],
+        }
+    )
+    with pytest.raises(ValueError, match="compatible with 'rad'"):
+        interpolator.add_table(1, wrong_unit)
+
+
+def test_hdf5(tmp_path):
+    """Test writing interpolated data to file"""
+    from ctapipe.io import write_table
+
+    table = Table(
+        {
+            "time": t0 + np.arange(0.0, 10.1, 2.0) * u.s,
+            "azimuth": np.linspace(0.0, 10.0, 6) * u.deg,
+            "altitude": np.linspace(70.0, 60.0, 6) * u.deg,
+        },
+    )
+
+    path = tmp_path / "pointing.h5"
+    write_table(table, path, "/dl0/monitoring/telescope/pointing/tel_001")
+    with tables.open_file(path) as h5file:
+        interpolator = PointingInterpolator(h5file)
+        alt, az = interpolator(tel_id=1, time=t0 + 1 * u.s)
+        assert u.isclose(alt, 69 * u.deg)
+        assert u.isclose(az, 1 * u.deg)
+
+
+def test_bounds():
+    """Test invalid pointing tables raise nice errors"""
+
+    table_pointing = Table(
+        {
+            "time": t0 + np.arange(0.0, 10.1, 2.0) * u.s,
+            "azimuth": np.linspace(0.0, 10.0, 6) * u.deg,
+            "altitude": np.linspace(70.0, 60.0, 6) * u.deg,
+        },
+    )
+
+    table_pedestal = Table(
+        {
+            "time": np.arange(0.0, 10.1, 2.0),
+            "pedestal": np.reshape(np.random.normal(4.0, 1.0, 1850 * 6), (6, 1850))
+            * u.Unit(),
+        },
+    )
+
+    table_flatfield = Table(
+        {
+            "time": np.arange(0.0, 10.1, 2.0),
+            "gain": np.reshape(np.random.normal(1.0, 1.0, 1850 * 6), (6, 1850))
+            * u.Unit(),
+        },
+    )
+
+    interpolator_pointing = PointingInterpolator()
+    interpolator_pedestal = PedestalInterpolator()
+    interpolator_flatfield = FlatFieldInterpolator()
+    interpolator_pointing.add_table(1, table_pointing)
+    interpolator_pedestal.add_table(1, table_pedestal)
+    interpolator_flatfield.add_table(1, table_flatfield)
+
+    error_message = "below the interpolation range"
+
+    with pytest.raises(ValueError, match=error_message):
+        interpolator_pointing(tel_id=1, time=t0 - 0.1 * u.s)
+
+    with pytest.raises(ValueError, match=error_message):
+        interpolator_pedestal(tel_id=1, time=-0.1)
+
+    with pytest.raises(ValueError, match=error_message):
+        interpolator_flatfield(tel_id=1, time=-0.1)
+
+    with pytest.raises(ValueError, match="above the interpolation range"):
+        interpolator_pointing(tel_id=1, time=t0 + 10.2 * u.s)
+
+    alt, az = interpolator_pointing(tel_id=1, time=t0 + 1 * u.s)
+    assert u.isclose(alt, 69 * u.deg)
+    assert u.isclose(az, 1 * u.deg)
+
+    pedestal = interpolator_pedestal(tel_id=1, time=1.0)
+    assert all(pedestal == table_pedestal["pedestal"][0])
+    flatfield = interpolator_flatfield(tel_id=1, time=1.0)
+    assert all(flatfield == table_flatfield["gain"][0])
+    with pytest.raises(KeyError):
+        interpolator_pointing(tel_id=2, time=t0 + 1 * u.s)
+    with pytest.raises(KeyError):
+        interpolator_pedestal(tel_id=2, time=1.0)
+    with pytest.raises(KeyError):
+        interpolator_flatfield(tel_id=2, time=1.0)
+
+    interpolator_pointing = PointingInterpolator(bounds_error=False)
+    interpolator_pedestal = PedestalInterpolator(bounds_error=False)
+    interpolator_flatfield = FlatFieldInterpolator(bounds_error=False)
+    interpolator_pointing.add_table(1, table_pointing)
+    interpolator_pedestal.add_table(1, table_pedestal)
+    interpolator_flatfield.add_table(1, table_flatfield)
+
+    for dt in (-0.1, 10.1) * u.s:
+        alt, az = interpolator_pointing(tel_id=1, time=t0 + dt)
+        assert np.isnan(alt.value)
+        assert np.isnan(az.value)
+
+    assert all(np.isnan(interpolator_pedestal(tel_id=1, time=-0.1)))
+    assert all(np.isnan(interpolator_flatfield(tel_id=1, time=-0.1)))
+
+    interpolator_pointing = PointingInterpolator(bounds_error=False, extrapolate=True)
+    interpolator_pointing.add_table(1, table_pointing)
+    alt, az = interpolator_pointing(tel_id=1, time=t0 - 1 * u.s)
+    assert u.isclose(alt, 71 * u.deg)
+    assert u.isclose(az, -1 * u.deg)
+
+    alt, az = interpolator_pointing(tel_id=1, time=t0 + 11 * u.s)
+    assert u.isclose(alt, 59 * u.deg)
+    assert u.isclose(az, 11 * u.deg)
