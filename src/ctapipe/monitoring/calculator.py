9540f21415d18bf9ef5c67ab2925a5228997af0e
TjarkMiener <tjark.miener4692@googlemail.com>
2024-08-24 11:19:38 +0200

    removed the helper function to get the start and end slices
    Since agregation is chunk has always the same n_events, we can simplify the retrieving of the start and end slices. Therefore we do not need a helper function anymore

--- src/ctapipe/monitoring/calculator.py
+++ src/ctapipe/monitoring/calculator.py
@@ -159,18 +159,19 @@ class StatisticsCalculator(CalibrationCalculator):
     chunk_shift = Int(
         default_value=None,
         allow_none=True,
-        help="Number of samples to shift the extraction chunk for the calculation of the statistical values",
+        help="Number of samples to shift the aggregation chunk for the calculation of the statistical values",
     ).tag(config=True)
 
-    two_pass = Bool(default_value=False, help="overwrite output file if it exists").tag(
-        config=True
-    )
+    second_pass = Bool(
+        default_value=False, help="overwrite output file if it exists"
+    ).tag(config=True)
 
     faulty_pixels_threshold = Float(
-        default_value=0.1,
+        default_value=10.0,
         allow_none=True,
         help=(
-            "Percentage of faulty pixels over the camera to conduct second pass with refined shift of the chunk"
+            "Threshold in percentage of faulty pixels over the camera "
+            "to conduct second pass with a refined shift of the chunk."
         ),
     ).tag(config=True)
 
@@ -197,31 +198,33 @@ class StatisticsCalculator(CalibrationCalculator):
         )
 
         # Check if the chunk_shift is set for two pass mode
-        if self.two_pass and self.chunk_shift is None:
-            raise ValueError("chunk_shift must be set for two pass mode")
+        if self.second_pass and self.chunk_shift is None:
+            raise ValueError(
+                "chunk_shift must be set if second pass over the data is selected"
+            )
 
         # Get the aggregator
         aggregator = self.stats_aggregator[self.stats_aggregator_type.tel[tel_id]]
         # Pass through the whole provided dl1 data
-        if self.two_pass:
-            self.aggregated_stats = aggregator(
+        if self.second_pass:
+            aggregated_stats = aggregator(
                 table=dl1_table[tel_id], col_name=col_name, chunk_shift=None
             )
         else:
-            self.aggregated_stats = aggregator(
+            aggregated_stats = aggregator(
                 table=dl1_table[tel_id], col_name=col_name, chunk_shift=self.chunk_shift
             )
         # Detect faulty pixels with mutiple instances of OutlierDetector
-        outlier_mask = np.zeros_like(self.aggregated_stats[0]["mean"], dtype=bool)
+        outlier_mask = np.zeros_like(aggregated_stats[0]["mean"], dtype=bool)
         for aggregated_val, outlier_detector in self.outlier_detectors.items():
             outlier_mask = np.logical_or(
                 outlier_mask,
-                outlier_detector(self.aggregated_stats[aggregated_val]),
+                outlier_detector(aggregated_stats[aggregated_val]),
             )
         # Add the outlier mask to the aggregated statistics
-        self.aggregated_stats["outlier_mask"] = outlier_mask
+        aggregated_stats["outlier_mask"] = outlier_mask
 
-        if self.two_pass:
+        if self.second_pass:
             # Check if the camera has two gain channels
             if outlier_mask.shape[1] == 2:
                 # Combine the outlier mask of both gain channels
@@ -232,24 +235,33 @@ class StatisticsCalculator(CalibrationCalculator):
             # Calculate the fraction of faulty pixels over the camera
             faulty_pixels_percentage = (
                 np.count_nonzero(outlier_mask, axis=-1) / np.shape(outlier_mask)[-1]
-            )
+            ) * 100.0
 
             # Check for faulty chunks if the threshold is exceeded
             faulty_chunks = faulty_pixels_percentage > self.faulty_pixels_threshold
             if np.any(faulty_chunks):
+                chunk_size = aggregated_stats["n_events"][0]
                 faulty_chunks_indices = np.where(faulty_chunks)[0]
                 for index in faulty_chunks_indices:
                     # Log information of the faulty chunks
                     self.log.warning(
-                        f"Faulty chunks ({int(faulty_pixels_percentage[index]*100.0)}% of the camera unavailable) detected in the first pass: time_start={self.aggregated_stats['time_start'][index]}; time_end={self.aggregated_stats['time_end'][index]}"
+                        f"Faulty chunk ({int(faulty_pixels_percentage[index]*100.0)}% of the camera unavailable) detected in the first pass: time_start={aggregated_stats['time_start'][index]}; time_end={aggregated_stats['time_end'][index]}"
                     )
-
+                    # Calculate the start of the slice based
+                    slice_start = (
+                        chunk_size * index
+                        if index - 1 in faulty_chunks_indices
+                        else chunk_size * (index - 1)
+                    )
+                    # Set the start of the slice to the first element of the dl1 table if out of bound
+                    # and add one ``chunk_shift``.
+                    slice_start = max(0, slice_start) + self.chunk_shift
+                    # Set the end of the slice to the last element of the dl1 table if out of bound
+                    # and subtract one ``chunk_shift``.
+                    slice_end = min(
+                        len(dl1_table[tel_id]) - 1, chunk_size * (index + 2)
+                    ) - (self.chunk_shift - 1)
                     # Slice the dl1 table according to the previously caluclated start and end.
-                    slice_start, slice_end = self._get_slice_range(
-                        chunk_index=index,
-                        faulty_previous_chunk=(index-1 in faulty_chunks_indices),
-                        dl1_table_length=len(dl1_table[tel_id]) - 1,
-                    )
                     dl1_table_sliced = dl1_table[tel_id][slice_start:slice_end]
 
                     # Run the stats aggregator on the sliced dl1 table with a chunk_shift
@@ -261,19 +273,28 @@ class StatisticsCalculator(CalibrationCalculator):
                     )
 
                     # Detect faulty pixels with mutiple instances of OutlierDetector of the second pass
-                    outlier_mask = np.zeros_like(aggregated_stats_secondpass[0]["mean"], dtype=bool)
-                    for aggregated_val, outlier_detector in self.outlier_detectors.items():
-                        outlier_mask = np.logical_or(
-                            outlier_mask,
-                            outlier_detector(aggregated_stats_secondpass[aggregated_val]),
+                    outlier_mask_secondpass = np.zeros_like(
+                        aggregated_stats_secondpass[0]["mean"], dtype=bool
+                    )
+                    for (
+                        aggregated_val,
+                        outlier_detector,
+                    ) in self.outlier_detectors.items():
+                        outlier_mask_secondpass = np.logical_or(
+                            outlier_mask_secondpass,
+                            outlier_detector(
+                                aggregated_stats_secondpass[aggregated_val]
+                            ),
                         )
                     # Add the outlier mask to the aggregated statistics
-                    aggregated_stats_secondpass["outlier_mask"] = outlier_mask
+                    aggregated_stats_secondpass["outlier_mask"] = outlier_mask_secondpass
 
                     # Stack the aggregated statistics of the second pass to the first pass
-                    self.aggregated_stats = vstack([self.aggregated_stats, aggregated_stats_secondpass])
+                    aggregated_stats = vstack(
+                        [aggregated_stats, aggregated_stats_secondpass]
+                    )
                     # Sort the aggregated statistics based on the starting time
-                    self.aggregated_stats.sort(["time_start"])
+                    aggregated_stats.sort(["time_start"])
             else:
                 self.log.info(
                     "No faulty chunks detected in the first pass. The second pass with a finer chunk shift is not executed."
@@ -281,55 +302,8 @@ class StatisticsCalculator(CalibrationCalculator):
 
         # Write the aggregated statistics and their outlier mask to the output file
         write_table(
-            self.aggregated_stats,
+            aggregated_stats,
             self.output_path,
             f"{self.group[self.calibration_type]}/tel_{tel_id:03d}",
             overwrite=self.overwrite,
         )
-
-    def _get_slice_range(
-        self,
-        chunk_index,
-        faulty_previous_chunk,
-        dl1_table_length,
-    ) -> (int, int):
-        """
-        Calculate the start and end indices for slicing the DL1 table to be used for the second pass.
-
-        Parameters
-        ----------
-        chunk_index : int
-            The index of the current faulty chunk being processed.
-        faulty_previous_chunk : bool
-            A flag indicating if the previous chunk was faulty.
-        dl1_table_length : int
-            The total length of the DL1 table.
-
-        Returns
-        -------
-        tuple
-            A tuple containing the start and end indices for slicing the DL1 table.
-        """
-
-        # Set the start of the slice to the first element of the dl1 table
-        slice_start = 0
-        if chunk_index > 0:
-            # Get the start of the previous chunk
-            if faulty_previous_chunk:
-                slice_start = np.sum(self.aggregated_stats["n_events"][:chunk_index])
-            else:
-                slice_start = np.sum(
-                    self.aggregated_stats["n_events"][: chunk_index - 1]
-                )
-
-        # Set the end of the slice to the last element of the dl1 table
-        slice_end = dl1_table_length
-        if chunk_index < len(self.aggregated_stats) - 1:
-            # Get the stop of the next chunk
-            slice_end = np.sum(self.aggregated_stats["n_events"][: chunk_index + 2])
-
-        # Shift the start and end of the slice by the chunk_shift
-        slice_start += self.chunk_shift
-        slice_end -= self.chunk_shift - 1
-
-        return int(slice_start), int(slice_end)
