32894fd81dd152f862b30ce324cf4d7ee8c21e8f
Christoph Toennis <christoph.toennis1988@gmail.com>
2024-09-30 14:45:27 +0200

    I separated the star tracking out to the StarTracer class

--- src/ctapipe/image/psf_model.py
+++ src/ctapipe/image/psf_model.py
@@ -8,7 +8,6 @@ from abc import abstractmethod
 
 import numpy as np
 from scipy.stats import laplace, laplace_asymmetric
-from traitlets import List
 
 
 class PSFModel:
@@ -40,6 +39,10 @@ class PSFModel:
         pass
 
     @abstractmethod
+    def update_location(self, *args):
+        pass
+
+    @abstractmethod
     def update_model_parameters(self, *args):
         pass
 
@@ -49,18 +52,23 @@ class ComaModel(PSFModel):
     PSF model, describing pure coma aberrations PSF effect
     """
 
-    asymmetry_params = List(
-        default_value=[0.49244797, 9.23573115, 0.15216096],
-        help="Parameters describing the dependency of the asymmetry of the psf on the distance to the center of the camera",
-    ).tag(config=True)
-    radial_scale_params = List(
-        default_value=[0.01409259, 0.02947208, 0.06000271, -0.02969355],
-        help="Parameters describing the dependency of the radial scale on the distance to the center of the camera",
-    ).tag(config=True)
-    az_scale_params = List(
-        default_value=[0.24271557, 7.5511501, 0.02037972],
-        help="Parameters describing the dependency of the azimuthal scale scale on the distance to the center of the camera",
-    ).tag(config=True)
+    def __init__(
+        self,
+        asymmetry_params=[0.49244797, 9.23573115, 0.15216096],
+        radial_scale_params=[0.01409259, 0.02947208, 0.06000271, -0.02969355],
+        az_scale_params=[0.24271557, 7.5511501, 0.02037972],
+    ):
+        """
+        PSF model, describing pure coma aberrations PSF effect
+
+        param list asymmetry_params    Parameters describing the dependency of the asymmetry of the psf on the distance to the center of the camera
+        param list radial_scale_params Parameters describing the dependency of the radial scale on the distance to the center of the camera
+        param list radial_scale_params Parameters describing the dependency of the azimuthal scale scale on the distance to the center of the camera
+        """
+
+        self.asymmetry_params = asymmetry_params
+        self.radial_scale_params = radial_scale_params
+        self.az_scale_params = az_scale_params
 
     def k_func(self, x):
         return (
@@ -87,7 +95,21 @@ class ComaModel(PSFModel):
             f, *self.azimuthal_pdf_params
         )
 
-    def update_model_parameters(self, r, f):
+    def update_model_parameters(self, model_params):
+        if not (
+            model_params["asymmetry_params"] == 3
+            and model_params["radial_scale_params"] == 4
+            and model_params["az_scale_params"] == 3
+        ):
+            raise ValueError(
+                "asymmetry_params and az_scale_params needs to have length 3 and radial_scale_params length 4"
+            )
+
+        self.asymmetry_params = model_params["asymmetry_params"]
+        self.radial_scale_params = model_params["radial_scale_params"]
+        self.az_scale_params = model_params["az_scale_params"]
+
+    def update_location(self, r, f):
         k = self.k_func(r)
         sr = self.sr_func(r)
         sf = self.sf_func(r)
