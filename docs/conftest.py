5bd9c81bfe6eeeb4928e870e321c694a1b675227
Maximilian Linhoff <maximilian.linhoff@tu-dortmund.de>
2024-11-11 11:49:07 +0100

    Ignore examples and skip conditionaly doctests in case of missing modules

--- docs/conftest.py
+++ docs/conftest.py
@@ -2,4 +2,6 @@ collect_ignore = [
     "conf.py",
     "_build",
     "auto_examples",
+    "api-reference/instrument/camerageometry_example.py",
+    "api-reference/coordinates/plot_camera_frames.py",
 ]
