ccd427bab9366d9560f4b49c2daa34384608e2d3
Georgios.Voutsinas <Georgios>
2025-07-17 15:06:25 +0200

    trying to fix the minimal dependencies issue

--- src/ctapipe/io/tests/test_preprocessing.py
+++ src/ctapipe/io/tests/test_preprocessing.py
@@ -2,8 +2,6 @@ import astropy.units as u
 import numpy as np
 import pytest
 from astropy.table import Column, QTable, Table
-from pyirf.simulations import SimulatedEventsInfo
-from pyirf.spectral import PowerLaw
 from traitlets.config import Config
 
 
@@ -120,52 +118,6 @@ def test_normalise_column_names(dummy_table):
             output_table_schema=output_table_schema,
         )
         _ = epp.normalise_column_names(dummy_table)
-
-
-def test_event_loader(gamma_diffuse_full_reco_file, irf_event_loader_test_config):
-    from ctapipe.io.dl2_tables_preprocessing import DL2EventLoader
-    from ctapipe.irf import Spectra
-
-    loader = DL2EventLoader(
-        config=irf_event_loader_test_config,
-        file=gamma_diffuse_full_reco_file,
-        target_spectrum=Spectra.CRAB_HEGRA,
-    )
-    events, count, meta = loader.load_preselected_events(
-        chunk_size=10000,
-        obs_time=u.Quantity(50, u.h),
-    )
-
-    columns = [
-        "obs_id",
-        "event_id",
-        "true_energy",
-        "true_az",
-        "true_alt",
-        "reco_energy",
-        "reco_az",
-        "reco_alt",
-        "reco_fov_lat",
-        "reco_fov_lon",
-        "gh_score",
-        "pointing_az",
-        "pointing_alt",
-        "theta",
-        "true_source_fov_offset",
-        "reco_source_fov_offset",
-        "weight",
-    ]
-
-    assert sorted(columns) == sorted(events.colnames)
-    assert isinstance(count, int)
-    assert isinstance(meta["sim_info"], SimulatedEventsInfo)
-    assert isinstance(meta["spectrum"], PowerLaw)
-
-    events = loader.make_event_weights(
-        events, meta["spectrum"], "gammas", (0 * u.deg, 1 * u.deg)
-    )
-
-    assert "weight" in events.colnames
 
 
 def test_preprocessor_tel_table_with_custom_reconstructor(tmp_path, test_config):
