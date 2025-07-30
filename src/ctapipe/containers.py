e06135e65b519e37a9742829b399e78c6f3d007f
TjarkMiener <tjark.miener4692@googlemail.com>
2025-07-30 17:35:37 +0200

    fix docs

--- src/ctapipe/containers.py
+++ src/ctapipe/containers.py
@@ -14,6 +14,7 @@ from .core import Container, Field, Map
 
 __all__ = [
     "ArrayEventContainer",
+    "CameraCalibrationCoefficientsContainer",
     "ConcentrationContainer",
     "DL0CameraContainer",
     "DL0Container",
