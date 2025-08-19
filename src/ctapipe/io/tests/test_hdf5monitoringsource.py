b5fb579f388079966b27ba738692c769c67feab8
TjarkMiener <tjark.miener4692@googlemail.com>
2025-08-19 16:58:01 +0200

    fix tests
    Remove interpolation test because dl1_mon_pointing_file is not an EventSource anymore; add a dedicated test in the HDF5EventSource

--- src/ctapipe/io/tests/test_hdf5monitoringsource.py
+++ src/ctapipe/io/tests/test_hdf5monitoringsource.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pytest
+import astropy.units as u
 
 from ctapipe.core import ToolConfigurationError
 from ctapipe.io import (
@@ -34,13 +35,13 @@ def test_passing_subarray(dl1_file, calibpipe_camcalib_same_chunks):
 
 
 def test_get_monitoring_types(
-    dl1_file,
+    gamma_diffuse_full_reco_file,
     dl1_mon_pointing_file,
     calibpipe_camcalib_different_chunks,
     dl1_merged_monitoring_file,
 ):
     # Test with a file that has no monitoring types
-    assert tuple([]) == get_hdf5_monitoring_types(dl1_file)
+    assert tuple([]) == get_hdf5_monitoring_types(gamma_diffuse_full_reco_file)
     # Test with a file that has pointing-related monitoring types
     assert tuple([MonitoringTypes.TELESCOPE_POINTING]) == get_hdf5_monitoring_types(
         dl1_mon_pointing_file
@@ -59,7 +60,7 @@ def test_get_monitoring_types(
     ) == get_hdf5_monitoring_types(dl1_merged_monitoring_file)
 
 
-def test_filling(dl1_file, dl1_merged_monitoring_file):
+def test_camcalib_filling(gamma_diffuse_full_reco_file, dl1_merged_monitoring_file):
     from ctapipe.io import read_table
 
     # Read the camera monitoring data with the coefficients
@@ -69,50 +70,88 @@ def test_filling(dl1_file, dl1_merged_monitoring_file):
     )
     allowed_tels = {1}
     with HDF5EventSource(
-        input_url=dl1_file, allowed_tels=allowed_tels, max_events=1
+        input_url=gamma_diffuse_full_reco_file, allowed_tels=allowed_tels, max_events=1
     ) as source:
         monitoring_source = HDF5MonitoringSource(
             subarray=source.subarray,
             input_url=dl1_merged_monitoring_file,
-            overwrite_telescope_pointings=True,
+            overwrite_telescope_pointings=False,
         )
         assert monitoring_source.is_simulation
         assert monitoring_source.pixel_statistics
         assert monitoring_source.camera_coefficients
-        assert monitoring_source.telescope_pointings
+        assert not monitoring_source.telescope_pointings
         # Check that the camcalib_coefficients match the event calibration data
         for e in source:
             # Fill the monitoring container for the event
             monitoring_source.fill_monitoring_container(e)
             # Check that the values match
             np.testing.assert_array_equal(
-                e.monitoring.tel[1].camera.coefficients.factor,
+                e.monitoring.tel[1].camera.coefficients["factor"],
                 camcalib_coefficients["factor"][0],
-                err_msg="Factors do not match after reading the monitoring file through the SimTelEventSource",
+                err_msg="Factors do not match after reading the monitoring file through the HDF5MonitoringSource",
             )
             np.testing.assert_array_equal(
-                e.monitoring.tel[1].camera.coefficients.pedestal_offset,
+                e.monitoring.tel[1].camera.coefficients["pedestal_offset"],
                 camcalib_coefficients["pedestal_offset"][0],
-                err_msg="Pedestal offsets do not match after reading the monitoring file through the SimTelEventSource",
+                err_msg="Pedestal offsets do not match after reading the monitoring file through the HDF5MonitoringSource",
             )
             np.testing.assert_array_equal(
-                e.monitoring.tel[1].camera.coefficients.time_shift,
+                e.monitoring.tel[1].camera.coefficients["time_shift"],
                 camcalib_coefficients["time_shift"][0],
-                err_msg="Time shifts do not match after reading the monitoring file through the SimTelEventSource",
+                err_msg="Time shifts do not match after reading the monitoring file through the HDF5MonitoringSource",
             )
             np.testing.assert_array_equal(
-                e.monitoring.tel[1].camera.coefficients.outlier_mask,
+                e.monitoring.tel[1].camera.coefficients["outlier_mask"],
                 camcalib_coefficients["outlier_mask"][0],
-                err_msg="Outlier masks do not match after reading the monitoring file through the SimTelEventSource",
+                err_msg="Outlier masks do not match after reading the monitoring file through the HDF5MonitoringSource",
             )
         # Close the monitoring source
         monitoring_source.close()
 
 
-def test_exceptions(dl1_file, calibpipe_camcalib_same_chunks):
+def test_tel_pointing_filling(gamma_diffuse_full_reco_file, dl1_mon_pointing_file):
+    from ctapipe.io import read_table
+    tel_id = 1
+    # Read the camera monitoring data with the coefficients
+    pointing_time = read_table(
+        dl1_mon_pointing_file,
+        f"/dl1/monitoring/telescope/calibration/pointing/tel_{tel_id:03d}",
+    )["time"][0] + 1 * u.s
+    allowed_tels = {tel_id}
+    with HDF5EventSource(
+        input_url=gamma_diffuse_full_reco_file, allowed_tels=allowed_tels, max_events=1
+    ) as source:
+        monitoring_source = HDF5MonitoringSource(
+            subarray=source.subarray,
+            input_url=dl1_mon_pointing_file,
+            overwrite_telescope_pointings=True,
+        )
+        assert monitoring_source.is_simulation
+        assert not monitoring_source.pixel_statistics
+        assert not monitoring_source.camera_coefficients
+        assert monitoring_source.telescope_pointings
+        for e in source:
+            # Test exception of interpolating outside the valid range
+            with pytest.raises(ValueError, match="A value"):
+                monitoring_source.fill_monitoring_container(e)
+            # Set the trigger time to the pointing time
+            e.trigger.time = pointing_time
+            # Save the old pointing values
+            old_az = e.monitoring.tel[tel_id].pointing.azimuth
+            old_alt = e.monitoring.tel[tel_id].pointing.altitude
+            # Fill the monitoring container for the event and overwrite the pointing
+            monitoring_source.fill_monitoring_container(e)
+            # Check that the values do not match
+            assert e.monitoring.tel[tel_id].pointing.azimuth != old_az
+            assert e.monitoring.tel[tel_id].pointing.altitude != old_alt
+        # Close the monitoring source
+        monitoring_source.close()
+
+def test_exceptions(gamma_diffuse_full_reco_file, calibpipe_camcalib_same_chunks):
     # Pass a subarray with more telescopes than available in the monitoring file.
     # This should raise a ToolConfigurationError.
-    with HDF5EventSource(input_url=dl1_file) as source:
+    with HDF5EventSource(input_url=gamma_diffuse_full_reco_file) as source:
         with pytest.raises(
             ToolConfigurationError, match="HDF5MonitoringSource: Requested telescopes"
         ):
