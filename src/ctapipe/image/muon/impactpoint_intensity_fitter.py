47ddd19d4c71b254a477e33fa5604083d7e1ac53
burmist-git <burm.leonid@gmail.com>
2025-08-20 18:05:29 +0200

    add impactpoint_intensity_fitter.py

---
+++ src/ctapipe/image/muon/impactpoint_intensity_fitter.py
@@ -0,0 +1,165 @@
+"""
+Class description to be added.
+
+"""
+
+import numpy as np
+
+from ...containers import MuonEfficiencyContainer
+from ...coordinates import TelescopeFrame
+from ...core import TelescopeComponent
+from ...core.traits import FloatTelescopeParameter, IntTelescopeParameter
+from ...exceptions import OptionalDependencyMissing
+
+try:
+    from iminuit import Minuit
+except ModuleNotFoundError:
+    Minuit = None
+
+__all__ = [
+    "MuonImpactpointIntensityFitter",
+]
+
+
+def chord_length_loss_function(radius, rho, phi):
+    """
+    Function for integrating the length of a chord across a circle (effective chord length).
+
+    A circular mirror is used for signal, and a circular camera is used for shadowing.
+
+    Parameters
+    ----------
+    radius: float or ndarray
+        radius of circle
+    rho: float or ndarray
+        fractional distance of impact point from circle center
+    phi: float or ndarray in radians
+        rotation angles to calculate length
+
+    Returns
+    -------
+    float or ndarray:
+        effective chord length
+
+    References
+    ----------
+    See :cite:p:`vacanti19941`.
+    Equation 6: for effective chord length calculations inside/outside the ring.
+    Equation 7: for filtering out non-physical solutions.
+
+
+    """
+    discriminant_norm = 1 - (rho**2 * np.sin(phi) ** 2)
+    valid = discriminant_norm >= 0
+
+    if not valid:
+        return 0
+
+    if rho <= 1.0:
+        # muon has hit the mirror
+        effective_chord_length = radius * (
+            np.sqrt(discriminant_norm) + rho * np.cos(phi)
+        )
+    else:
+        # muon did not hit the mirror
+        # Filtering out non-physical solutions for phi
+        if np.abs(phi) < np.arcsin(1.0 / rho):
+            effective_chord_length = 2 * radius * np.sqrt(discriminant_norm)
+        else:
+            return 0
+
+    return effective_chord_length
+
+
+class MuonImpactpointIntensityFitter(TelescopeComponent):
+    """
+    Fit muon ring images with a theoretical model to estimate optical efficiency.
+
+    Function for producing the expected image for a given set of trial
+    muon parameters without using astropy units but expecting the input to
+    be in the correct ones.
+
+    The image prediction function is currently modeled after :cite:p:`chalmecalvet2013`.
+
+    For more information, also see :cite:p:`muon-review`.
+    """
+
+    spe_width = FloatTelescopeParameter(
+        help="Width of a single photo electron distribution", default_value=0.5
+    ).tag(config=True)
+
+    min_lambda_m = FloatTelescopeParameter(
+        help="Minimum wavelength for Cherenkov light in m", default_value=300e-9
+    ).tag(config=True)
+
+    max_lambda_m = FloatTelescopeParameter(
+        help="Minimum wavelength for Cherenkov light in m", default_value=600e-9
+    ).tag(config=True)
+
+    hole_radius_m = FloatTelescopeParameter(
+        help="The radius of the hole in the center of the primary mirror dish in meters."
+        "The hole is not circular in shape; however, it can be well approximated as a circle with the same area."
+        "It is defined with the flat-to-flat distance (LST: 1.51 m, MST: 1.2 m, SST: 0.78 m)."
+        "We approximate the hexagonal hole with a circle that has the same surface area.",
+        default_value=[
+            ("type", "LST_*", 0.74),
+            ("type", "MST_*", 0.59),
+            ("type", "SST_1M_*", 0.38),
+        ],
+    ).tag(config=True)
+
+    oversampling = IntTelescopeParameter(
+        help="Oversampling for the line integration", default_value=3
+    ).tag(config=True)
+
+    def __init__(self, subarray, **kwargs):
+        if Minuit is None:
+            raise OptionalDependencyMissing("iminuit") from None
+
+        super().__init__(subarray=subarray, **kwargs)
+        self._geometries_tel_frame = {
+            tel_id: tel.camera.geometry.transform_to(TelescopeFrame())
+            for tel_id, tel in subarray.tel.items()
+        }
+
+    def __call__(self, tel_id, center_x, center_y, radius, image, pedestal, mask=None):
+        """
+
+        Parameters
+        ----------
+        tel_id: int
+            the telescope id
+        center_x: Angle quantity
+            Initial guess for muon ring center in telescope frame
+        center_y: Angle quantity
+            Initial guess for muon ring center in telescope frame
+        radius: Angle quantity
+            Initial guess for muon ring radius in telescope frame
+        image: ndarray
+            Amplitude of image pixels
+        pedestal: ndarray
+            Pedestal standard deviation in each pixel
+        mask: ndarray
+            mask marking the pixels to be used in the likelihood fit
+
+        Returns
+        -------
+        MuonEfficiencyContainer
+        """
+        telescope = self.subarray.tel[tel_id]
+        if telescope.optics.n_mirrors != 1:
+            raise NotImplementedError(
+                "Currently only single mirror telescopes"
+                f" are supported in {self.__class__.__name__}"
+            )
+
+        return MuonEfficiencyContainer(
+            impact=None,
+            impact_x=None,
+            impact_y=None,
+            width=None,
+            optical_efficiency=None,
+            is_valid=False,
+            parameters_at_limit=True,
+            likelihood_value=None,
+        )
