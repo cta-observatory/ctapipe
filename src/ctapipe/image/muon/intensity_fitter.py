Add the PixelShape. For image_prediction, we fixed the normalization factor in this way.

src/ctapipe/image/muon/intensity_fitter.py
@@ -17,6 +17,7 @@ from ...core import TelescopeComponent
 from ...core.env import CTAPIPE_DISABLE_NUMBA_CACHE
 from ...core.traits import FloatTelescopeParameter, IntTelescopeParameter
 from ...exceptions import OptionalDependencyMissing
+from ...instrument.camera.geometry import PixelShape
 from ..pixel_likelihood import neg_log_likelihood_approx
 
 try:
@@ -30,6 +31,9 @@ __all__ = [
 
 # ratio of the areas of the unit circle and a square of side lengths 2
 CIRCLE_SQUARE_AREA_RATIO = np.pi / 4
+
+# ratio of the areas of the unit circle and a hexagon of flat-to-flat lengths 2
+CIRCLE_HEXAGON_AREA_RATIO = np.pi / 2 / np.sqrt(3)
 
 # Sqrt of 2, as it is needed multiple times
 SQRT2 = np.sqrt(2)
@@ -257,6 +261,7 @@ def image_prediction_no_units(
     oversampling=3,
     min_lambda_m=300e-9,
     max_lambda_m=600e-9,
+    pix_type=PixelShape.HEXAGON,
 ):
     """
     Unit-less version of `image_prediction`.
@@ -317,7 +322,12 @@ def image_prediction_no_units(
     # diameter. In any case, since in the end we do a data-MC comparison of the muon
     # ring analysis outputs, it is not critical that this value is exact.
 
-    pred *= CIRCLE_SQUARE_AREA_RATIO
+    if pix_type == PixelShape.HEXAGON:
+        pred *= CIRCLE_HEXAGON_AREA_RATIO
+    elif pix_type == PixelShape.SQUARE:
+        pred *= CIRCLE_SQUARE_AREA_RATIO
+    elif pix_type == PixelShape.CIRCLE:
+        pass
 
     return pred
 
@@ -333,6 +343,7 @@ def build_negative_log_likelihood(
     spe_width,
     pedestal,
     hole_radius=0 * u.m,
+    pix_type=PixelShape.HEXAGON,
 ):
     """Create an efficient negative log_likelihood function that does
     not rely on astropy units internally by defining needed values as closures
@@ -414,6 +425,7 @@ def build_negative_log_likelihood(
             oversampling=oversampling,
             min_lambda_m=min_lambda,
             max_lambda_m=max_lambda,
+            pix_type=pix_type,
         )
 
         # scale prediction by optical efficiency of the telescope
@@ -535,9 +547,9 @@ class MuonIntensityFitter(TelescopeComponent):
             spe_width=self.spe_width.tel[tel_id],
             pedestal=pedestal,
             hole_radius=self.hole_radius_m.tel[tel_id] * u.m,
+            pix_type=telescope.camera.geometry.pix_type,
         )
         negative_log_likelihood.errordef = Minuit.LIKELIHOOD
-
         initial_guess = create_initial_guess(center_x, center_y, radius, telescope)
 
         # Create Minuit object with first guesses at parameters