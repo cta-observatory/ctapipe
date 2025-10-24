acef830eae1dd09573041a37955ce55e19e1a5b6
Jonas Hackfeld <jonas.hackfeld@ruhr-uni-bochum.de>
2025-10-24 17:05:00 +0200

    parametrize over all cleaners in unit-test

--- src/ctapipe/image/tests/test_image_processor.py
+++ src/ctapipe/image/tests/test_image_processor.py
@@ -6,6 +6,7 @@ from copy import deepcopy
 
 import astropy.units as u
 import numpy as np
+import pytest
 from numpy import isfinite
 
 from ctapipe.calib import CameraCalibrator
@@ -13,38 +14,41 @@ from ctapipe.containers import (
     CameraHillasParametersContainer,
     CameraTimingParametersContainer,
 )
-from ctapipe.image import ImageProcessor
-from ctapipe.image.cleaning import MARSImageCleaner, NSBImageCleaner
+from ctapipe.image import ImageCleaner, ImageProcessor
 
 
-def test_image_processor(example_event, example_subarray):
+@pytest.mark.parametrize("cleaner", ImageCleaner.non_abstract_subclasses().values())
+def test_image_processor(cleaner, example_event, example_subarray):
     """ensure we get parameters out when we input an event with images"""
 
     calibrate = CameraCalibrator(subarray=example_subarray)
     process_images = ImageProcessor(
-        subarray=example_subarray, image_cleaner_type="NSBImageCleaner"
+        subarray=example_subarray, image_cleaner_type=cleaner.__name__
     )
 
-    assert isinstance(process_images.clean, NSBImageCleaner)
+    assert isinstance(process_images.clean, cleaner)
 
     calibrate(example_event)
     process_images(example_event)
 
     for dl1tel in example_event.dl1.tel.values():
-        assert isfinite(dl1tel.image_mask.sum())
-        assert isfinite(dl1tel.parameters.hillas.length.value)
-        dl1tel.parameters.hillas.length.to("deg")
-        assert isfinite(dl1tel.parameters.timing.slope.value)
-        assert isfinite(dl1tel.parameters.leakage.pixels_width_1)
-        assert isfinite(dl1tel.parameters.concentration.cog)
-        assert isfinite(dl1tel.parameters.morphology.n_pixels)
-        assert isfinite(dl1tel.parameters.intensity_statistics.max)
-        assert isfinite(dl1tel.parameters.peak_time_statistics.max)
+        n_survived_pixels = dl1tel.image_mask.sum()
+        assert isfinite(n_survived_pixels)
+        if n_survived_pixels > 0:
+            assert isfinite(dl1tel.parameters.hillas.length.value)
+            dl1tel.parameters.hillas.length.to("deg")
+            assert isfinite(dl1tel.parameters.timing.slope.value)
+            assert isfinite(dl1tel.parameters.leakage.pixels_width_1)
+            assert isfinite(dl1tel.parameters.concentration.cog)
+            assert isfinite(dl1tel.parameters.morphology.n_pixels)
+            assert isfinite(dl1tel.parameters.intensity_statistics.max)
+            assert isfinite(dl1tel.parameters.peak_time_statistics.max)
 
     process_images.check_image.to_table()
 
 
-def test_image_processor_camera_frame(example_event, example_subarray):
+@pytest.mark.parametrize("cleaner", ImageCleaner.non_abstract_subclasses().values())
+def test_image_processor_camera_frame(cleaner, example_event, example_subarray):
     """ensure we get parameters in the camera frame if explicitly specified"""
     event = deepcopy(example_event)
 
@@ -52,24 +56,26 @@ def test_image_processor_camera_frame(example_event, example_subarray):
     process_images = ImageProcessor(
         subarray=example_subarray,
         use_telescope_frame=False,
-        image_cleaner_type="MARSImageCleaner",
+        image_cleaner_type=cleaner.__name__,
     )
 
-    assert isinstance(process_images.clean, MARSImageCleaner)
+    assert isinstance(process_images.clean, cleaner)
 
     calibrate(event)
     process_images(event)
 
     for dl1tel in event.dl1.tel.values():
-        assert isfinite(dl1tel.image_mask.sum())
-        assert isfinite(dl1tel.parameters.hillas.length.value)
-        dl1tel.parameters.hillas.length.to("meter")
-        assert isfinite(dl1tel.parameters.timing.slope.value)
-        assert isfinite(dl1tel.parameters.leakage.pixels_width_1)
-        assert isfinite(dl1tel.parameters.concentration.cog)
-        assert isfinite(dl1tel.parameters.morphology.n_pixels)
-        assert isfinite(dl1tel.parameters.intensity_statistics.max)
-        assert isfinite(dl1tel.parameters.peak_time_statistics.max)
+        n_survived_pixels = dl1tel.image_mask.sum()
+        assert isfinite(n_survived_pixels)
+        if n_survived_pixels > 0:
+            assert isfinite(dl1tel.parameters.hillas.length.value)
+            dl1tel.parameters.hillas.length.to("meter")
+            assert isfinite(dl1tel.parameters.timing.slope.value)
+            assert isfinite(dl1tel.parameters.leakage.pixels_width_1)
+            assert isfinite(dl1tel.parameters.concentration.cog)
+            assert isfinite(dl1tel.parameters.morphology.n_pixels)
+            assert isfinite(dl1tel.parameters.intensity_statistics.max)
+            assert isfinite(dl1tel.parameters.peak_time_statistics.max)
 
     process_images.check_image.to_table()
 
