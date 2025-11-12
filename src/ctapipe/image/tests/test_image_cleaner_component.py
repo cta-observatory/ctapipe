ca609d34fe979376e18292fc8f7a18427c517cd1
TjarkMiener <tjark.miener4692@googlemail.com>
2025-11-12 13:30:17 +0100

    fix cleaner test with new container field

--- src/ctapipe/image/tests/test_image_cleaner_component.py
+++ src/ctapipe/image/tests/test_image_cleaner_component.py
@@ -43,7 +43,7 @@ def test_image_cleaner(method, prod5_mst_nectarcam, reference_location):
     )
 
     monitoring = CameraMonitoringContainer()
-    monitoring.pixel_statistics.sky_pedestal_image.std = np.ones(
+    monitoring.pixel_statistics.pedestal_image.std = np.ones(
         prod5_mst_nectarcam.camera.geometry.n_pixels
     )
 
