01ed1ac28c512a695d9540153f280dca6a19700d
Maximilian Linhoff <maximilian.linhoff@cta-observatory.org>
2026-01-26 15:47:54 +0100

    Add mock for decorators of optional dependencies

--- src/ctapipe/exceptions.py
+++ src/ctapipe/exceptions.py
@@ -1,3 +1,12 @@
+__all__ = [
+    "CTAPipeException",
+    "TooFewEvents",
+    "OptionalDependencyMissing",
+    "InputMissing",
+    "MockOptionalDecorator",
+]
+
+
 class CTAPipeException(Exception):
     pass
 
@@ -17,3 +26,46 @@ class OptionalDependencyMissing(ModuleNotFoundError):
 
 class InputMissing(ValueError):
     """Raised in case an input was not specified."""
+
+
+class MockOptionalDecorator:
+    """A decorator that can be used in-place of an imported decorator.
+
+    Will throw the corresponding OptionalDependencyMissing exception when
+    the decorated function is called.
+
+    Examples
+    --------
+    You might want to use this class for optional dependencies that provide
+    decorators. With decorators, it is hard to defer import of the dependency
+    to runtime. See this example of how one might make numba with njit optional:
+
+        from unittest import MagicMock
+        from ctapipe.exceptions import MockOptionalDecorator
+        try:
+            import numba
+        except ModuleNotFoundError:
+            numba = MagicMock()
+            numba.njit = MockOptionalDecorator("numba")
+
+        @numba.njit(cache=True)
+        def example(x):
+            return 5 * x
+    """
+
+    def __init__(self, module):
+        self.module = module
+
+    def __call__(self, *args, **kwargs):
+        def _raise(*args, **kwargs):
+            raise OptionalDependencyMissing(self.module)
+
+        # decorator called as @decorator without arguments
+        if len(args) == 1 and len(kwargs) == 0 and callable(args[0]):
+            return _raise
+
+        # decorator called as @decorator(*args, **kwargs)
+        def wrapper(func):
+            return _raise
+
+        return wrapper
