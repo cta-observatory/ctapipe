482da1cf414d957486d6186849f818fa726149af
mykhailo.dalchenko <mykhailo.dalchenko@cta-consortium.org>
2026-08-05 13:53:55 +0200

    Revert "Change default values to None"
    This reverts commit 958345231ae80ce45df0b9c24110d11816f3a27f.

--- src/ctapipe/io/tests/test_simteleventsource.py
+++ src/ctapipe/io/tests/test_simteleventsource.py
@@ -180,8 +180,8 @@ def test_pointing():
         for e in reader:
             assert np.isclose(e.monitoring.pointing.array_altitude.to_value(u.deg), 70)
             assert np.isclose(e.monitoring.pointing.array_azimuth.to_value(u.deg), 0)
-            assert e.monitoring.pointing.array_ra is None
-            assert e.monitoring.pointing.array_dec is None
+            assert np.isnan(e.monitoring.pointing.array_ra)
+            assert np.isnan(e.monitoring.pointing.array_dec)
 
             # normal run, all telescopes point to the array direction
             for tel_id in e.monitoring.tel.keys():
