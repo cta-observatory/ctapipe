cf20499f5bdfbfdc9d951936e426be4a79e03314
Mykhailo Dalchenko <mexanick@users.noreply.github.com>
2026-08-11 14:04:38 +0200

    Apply suggestions from code review
    Co-authored-by: Maximilian Linhoff <maximilian.linhoff@tu-dortmund.de>

--- src/ctapipe/containers.py
+++ src/ctapipe/containers.py
@@ -1279,10 +1279,10 @@ class TelescopePointingCorrectionContainer(Container):
     delta_azimuth = Field(nan * u.rad, "Residual Azimuth offset correction", unit=u.rad)
 
     # Optional uncertainty tracking
-    uncertainty_altitude = Field(
+    altitude_uncertainty = Field(
         nan * u.rad, "Uncertainty on Altitude offset correction", unit=u.rad
     )
-    uncertainty_azimuth = Field(
+    azimuth_uncertainty = Field(
         nan * u.rad, "Uncertainty on Azimuth offset correction", unit=u.rad
     )
 
