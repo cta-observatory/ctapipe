10c28c202857ef799c3a1cf80c6a9555ce808771
Maximilian Linhoff <maximilian.linhoff@cta-observatory.org>
2026-09-11 09:56:11 +0200

    Fill broken pixels from pixel_status in HDF5EventSource

--- src/ctapipe/io/tests/test_roundtrips.py
+++ src/ctapipe/io/tests/test_roundtrips.py
@@ -1,8 +1,10 @@
 """Test assuring combining several steps keeps data consistent"""
 
+import numpy as np
+
 
 def test_r1_simtel_broken_pixels(prod5_gamma_simtel_path, tmp_path):
-    """Test that broken pixel and timeshift information is preserved for simtel r1"""
+    """Test that broken pixel information is preserved for simtel r1"""
     from ctapipe.io import DataWriter, EventSource
 
     # write r1 waveforms for simtel file
@@ -16,9 +18,26 @@ def test_r1_simtel_broken_pixels(prod5_gamma_simtel_path, tmp_path):
     with EventSource(r1_path) as source:
         n_checked = 0
         for event in source:
-            for mon in event.monitoring.tel.values():
-                assert mon.camera.coefficients.time_shift is not None
-                assert mon.camera.coefficients.outlier_mask is not None
+            for tel_id, mon in event.monitoring.tel.items():
+                # not filled at the moment
+                # assert mon.camera.coefficients.time_shift is not None
+                readout = source.subarray.tel[tel_id].camera.readout
+
+                # filled from pixel status
+                outlier_mask = mon.camera.coefficients.outlier_mask
+                assert outlier_mask is not None
+                assert outlier_mask.shape == (readout.n_channels, readout.n_pixels)
+
+                # flashcam has 6 disabled pixels, only one gain
+                if readout.name == "FlashCam":
+                    assert np.count_nonzero(outlier_mask[0]) == 6
+                # we expect no other cameras to have disabled pixels here
+                else:
+                    pixel_index = np.arange(readout.n_pixels)
+                    selected_gain_channel = event.r1.tel[tel_id].selected_gain_channel
+                    active_mask = outlier_mask[selected_gain_channel, pixel_index]
+                    assert np.count_nonzero(active_mask) == 0
+
                 n_checked += 1
 
         assert n_checked > 0
